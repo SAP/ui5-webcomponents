2f5c4efacb4cac3d8ee5fdbd557dae9da16b0931
Vladislav Tasev <vladislav.tasev@sap.com>
2019-02-14 13:00:09 +0200

    slight refactoring

--- packages/base/src/sap/ui/webcomponents/base/util/setupSystem.js
+++ packages/base/src/sap/ui/webcomponents/base/util/setupSystem.js
@@ -1,8 +1,8 @@
 import Device from "@ui5/webcomponents-core/dist/sap/ui/Device";
 
 const setupSystem = (node) => {
-	const sysTypes = Object.entries(Device.system.SYSTEMTYPE).map(item => item[1]);
-	
+	const sysTypes = Object.entries(Device.system.SYSTEMTYPE).map(([_key, value]) => value);
+
 	node.classList.remove(...sysTypes);
 	sysTypes.forEach(sysType => {
 		if (Device.system[sysType]) {
