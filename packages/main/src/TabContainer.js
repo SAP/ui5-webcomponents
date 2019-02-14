a6f5c2b1f8e88c5c75433cecb54072b27d7b56d7
ilhan orhan <ilhan.myumyun@sap.com>
2019-02-14 10:07:50 +0200

    fix(ui5-tabcontainer): import popover dependency (#46)
    As the ui5-tabcontainer uses the ui5-popover, ui5-button, ui5-icon, ui5-list and ui5-li-custom
    internally for its overflow menu, the ui5-popover should be imported.

--- packages/main/src/TabContainer.js
+++ packages/main/src/TabContainer.js
@@ -8,9 +8,14 @@ import TabContainerTemplateContext from "./TabContainerTemplateContext";
 import TabBase from "./TabBase";
 import Tab from "./Tab";
 import TabSeparator from "./TabSeparator";
+import Popover from "./Popover";
+import List from "./List";
+import CustomListItem from "./CustomListItem";
+import Icon from "./Icon";
+import Button from "./Button";
+
 import TabDesignMode from "./types/TabDesignMode";
 import IconColor from "./types/IconColor";
-
 import BackgroundDesign from "./types/BackgroundDesign";
 import TabContainerHeaderMode from "./types/TabContainerHeaderMode";
 import ListItemType from "./types/ListItemType";
@@ -745,6 +750,18 @@ class TabContainer extends WebComponent {
 
 	static get calculateTemplateContext() {
 		return TabContainerTemplateContext.calculate;
+	}
+
+	static async define(...params) {
+		await Promise.all([
+			Icon.define(),
+			Button.define(),
+			CustomListItem.define(),
+			List.define(),
+			Popover.define(),
+		]);
+
+		super.define(...params);
 	}
 }
 
