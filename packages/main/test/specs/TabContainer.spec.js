25b55859d425575ec22cc3cff8306da9775c9678
ilhan orhan <ilhan.myumyun@sap.com>
2019-02-25 14:10:15 +0200

    refactor(ui5-tabcontainer): item selection and itemSelect event (#83)
    The following changes are applied:
    
    * remove the selectedKey property
    * remove the key property
    * introduce selectedIndex to substitute selectedKey and key pair for selection purposes
    * change itemSelect event param to provide the selected item, instead of
      the tab container internal key to make app development easier.
      Furthermore, in React, the key is reserved for the purpose of VDOM optimisations.
    * start firing itemSelect when item is selected from the overflow menu as well.
    * add simple interaction test
    
    BREAKING CHANGE: The TabContainer "selected-key" and Tab "key" properties are removed. Use TabContainer "selectedIndex" property (selected-index attribute) to set and get the selected tab.

--- packages/main/test/specs/TabContainer.spec.js
+++ packages/main/test/specs/TabContainer.spec.js
@@ -1,1 +1,21 @@
 const assert = require('assert');
+
+describe("TabContainer general interaction", () => {
+	browser.url("http://localhost:8080/test-resources/sap/ui/webcomponents/main/pages/TabContainer.html");
+
+	it("tests itemSelect event", () => {
+		const item = browser.findElementDeep("#tabContainer1 >>> .sapMITBItem:nth-child(3)");
+		const field = browser.$("#field");
+		const field2 = browser.$("#field2");
+		const field3 = browser.$("#field3");
+
+		const SELECTED_TAB_KEY = "item2";
+		const SELECTED_TAB_TEXT = "Tab 2";
+
+		item.click();
+
+		assert.strictEqual(field.getProperty("value"), "1", "itemSelect event should be fired once");
+		assert.strictEqual(field2.getProperty("value"), SELECTED_TAB_KEY, "Item data-key is retrieved correctly");
+		assert.strictEqual(field3.getProperty("value"), SELECTED_TAB_TEXT, "Item text is retrieved correctly.");
+	});
+});
