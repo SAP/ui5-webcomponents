b84a49952e8e9091dfdd10ce78ff7a08d1485d20
Vladislav Tasev <vladislav.tasev@sap.com>
2019-02-26 15:01:26 +0200

    Add some comments

--- packages/base/src/sap/ui/webcomponents/base/support/Edge.js
+++ packages/base/src/sap/ui/webcomponents/base/support/Edge.js
@@ -1,9 +1,3 @@
+// Edge only needs the Web Components polyfill and the pseudo mutation observer fix
 import "../thirdparty/webcomponents-polyfill";
 import "../compatibility/patchNodeValue";
-
-// runtime polyfills for ES5 environments
-// import "@ui5/webcomponents-core/dist/sap/ui/thirdparty/es6-promise";
-// import "@ui5/webcomponents-core/dist/sap/ui/thirdparty/es6-other-polyfills";
-// import "regenerator-runtime/runtime";
-
-// import "./Еdge";
--- packages/base/src/sap/ui/webcomponents/base/support/IE11.js
+++ packages/base/src/sap/ui/webcomponents/base/support/IE11.js
@@ -1,6 +1,7 @@
-// runtime polyfills for ES5 environments
+// IE11 only - runtime polyfills for ES5 environments
 import "@ui5/webcomponents-core/dist/sap/ui/thirdparty/es6-promise";
 import "@ui5/webcomponents-core/dist/sap/ui/thirdparty/es6-other-polyfills";
 import "regenerator-runtime/runtime";
 
-// import "./Еdge";
+// Plus all polyfills neede for edge are also needed for IE11
+import "./Edge";
