de176a7f893696ff4e452b0c987d8692d3411f98
Martin R. Hristov <martin.r.hristov@sap.com>
2019-02-26 19:46:20 +0200

    refactor(configuration): remove localeData as dependency
    - CalendarUtils switched to es6 module
    - CalendarUtils#getLocaleBasedCalendarType added
    - Non calendar bundle size reduced ~ 6kb (angular webpack build)
    - Full bundle reduced by ~2b (compared with current master)
    - Add LocaleDataConfigurationProvider that injects LocaleData to the configuration

---
+++ packages/base/src/sap/ui/webcomponents/base/LocaleDataConfigurationProvider.js
@@ -0,0 +1,4 @@
+import LocaleData from "@ui5/webcomponents-core/dist/sap/ui/core/LocaleData";
+import { injectCalendarTypeProvider } from "./Configuration";
+
+injectCalendarTypeProvider(LocaleData);
