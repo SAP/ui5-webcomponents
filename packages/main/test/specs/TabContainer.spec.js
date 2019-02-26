a89de1a4ca92167cb5ac0e808da800d84b2238c1
Adrian Bobev <adrian.bobev@gmail.com>
2019-02-26 16:20:07 +0200

    fix(ui5-tabcontainer): left arrow correctly scrolls to left in textOnly (#97)
    Fixes: https://github.com/SAP/ui5-webcomponents/issues/94

--- packages/main/test/specs/TabContainer.spec.js
+++ packages/main/test/specs/TabContainer.spec.js
@@ -18,4 +18,49 @@ describe("TabContainer general interaction", () => {
 		assert.strictEqual(field2.getProperty("value"), SELECTED_TAB_KEY, "Item data-key is retrieved correctly");
 		assert.strictEqual(field3.getProperty("value"), SELECTED_TAB_TEXT, "Item text is retrieved correctly.");
 	});
+
+	it("scroll works on iconsOnly TabContainer", () => {
+		browser.setWindowSize(250, 1080);
+
+		const arrowLeft = browser.findElementDeep("#tabContainerIconOnly >>> .sapMITBArrowScrollLeft");
+		const arrowRight = browser.findElementDeep("#tabContainerIconOnly >>> .sapMITBArrowScrollRight");
+
+		assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be initially hidden");
+		assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be initially shown");
+
+		arrowRight.click();
+		browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
+
+		assert.ok(arrowLeft.isDisplayed(), "'Left Arrow' should be shown after 'Right Arrow' click");
+		assert.ok(!arrowRight.isDisplayed(), "'Right Arrow' should be hidden after 'Right Arrow' click");
+
+		arrowLeft.click();
+		browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
+
+		assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be hidden after 'Left Arrow' click");
+		assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be shown after 'Left Arrow' click");
+	});
+
+	it("scroll works on textOnly TabContainer", () => {
+		browser.setWindowSize(250, 1080);
+		browser.findElementDeep("#tabContainerTextOnly").scrollIntoView();
+
+		const arrowLeft = browser.findElementDeep("#tabContainerTextOnly >>> .sapMITBArrowScrollLeftTextOnly");
+		const arrowRight = browser.findElementDeep("#tabContainerTextOnly >>> .sapMITBArrowScrollRightTextOnly");
+
+		assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be initially hidden");
+		assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be initially shown");
+
+		arrowRight.click();
+		browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
+
+		assert.ok(arrowLeft.isDisplayed(), "'Left Arrow' should be shown after 'Right Arrow' click");
+		assert.ok(!arrowRight.isDisplayed(), "'Right Arrow' should be hidden after 'Right Arrow' click");
+
+		arrowLeft.click();
+		browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
+
+		assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be hidden after 'Left Arrow' click");
+		assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be shown after 'Left Arrow' click");
+	});
 });
