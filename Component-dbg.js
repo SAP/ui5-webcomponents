a3510e3dc27a21b64f25901002fe2b886d435908
Petar Skelin <petar.skelin@sap.com>
2019-03-06 15:29:36 +0200

    Revert "Automatic update by Travis CI #499, commit 3928e939f0baee1912276d47a372f9c13382d3e2"
    This reverts commit 233d641eeb0d289595c7656177f8d34d1118e201.

--- Component-dbg.js
+++ Component-dbg.js
@@ -3,40 +3,10 @@ sap.ui.define([
 	"sap/ui/core/IconPool",
 	"playground/model/models",
 	"sap/base/util/UriParameters",
-	"sap/ui/core/routing/Router",
 	"sap/ui/core/ComponentSupport", // => include in custom bundle!
 	"sap/m/routing/Router" // => include in custom bundle!
-], function (UIComponent, IconPool, models, UriParameters, Router) {
+], function (UIComponent, IconPool, models, UriParameters) {
 	"use strict";
-	var popstateHandler;
-	function urlToRoute (url) {
-		// part after playground
-		var endPath = url.split("/playground/")[1];
-		// strip trailing slash
-		return endPath.replace(/\/$/, "");
-	};
-
-	function routeToURL (route) {
-		if (route) {
-			return getRealBaseURI() + "playground/" + route + "/";
-		} else {
-			return getRealBaseURI() + "playground/";
-		}
-	}
-
-	function getRealBaseURI() {
-		var baseURI = document.baseURI;
-		// var basePath = new URL(document.baseURI).pathname
-		// strip any url parameters
-		baseURI = baseURI.split("?")[0];
-		var isComponentPage = /components\/\w+\//.test(baseURI);
-		if (isComponentPage) {
-			return baseURI.split("playground/components")[0]
-		} else {
-			// ends with "/playground/" or has unknown path after it
-			return baseURI.split("playground/")[0]
-		}
-	}
 
 	return UIComponent.extend("playground.Component", {
 
@@ -50,7 +20,6 @@ sap.ui.define([
 		 * @override
 		 */
 		init: function () {
-			this.patchRouter();
 
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
@@ -89,56 +58,21 @@ sap.ui.define([
 
 		},
 
-		patchRouter: function () {
-			// add additional initialization logic
-			Router.prototype.initialize = function () {
-				this._bIsInitialized = true;
-				//initializeOrig.apply(this);
-				// stop the hash change listener
-				this.stop();
-				// do initial routing (if there is a path) - updates the UI according to the config
-				this.parse(urlToRoute(location.pathname));
-				// attach listener for route changes via the browser back/forward buttons
-				popstateHandler = (function (event) {
-					console.log("popstate:", document.location.pathname);
-					// trigger the UI update logic for the new path
-					this.parse(urlToRoute(document.location.pathname));
-				}).bind(this);
-				window.addEventListener('popstate', popstateHandler);
-				return this;
-			}
-
-			var navToOrig = sap.ui.core.routing.Router.prototype.navTo;
-			// called by apps to change the route
-			sap.ui.core.routing.Router.prototype.navTo = function (sName, oParameters) {
-				// calculate the path based on the parameters
-				var path = this.getURL(sName, oParameters);
-				console.log("navTo:", path);
-				// update the URL entry
-				window.history.pushState({}, undefined, routeToURL(path));
-				// trigger the UI update logic for the new path
-				this.parse(path);
-			}
-		},
-
 		selectView: function(sComponent) {
 			if (sComponent) {
 				this.getModel().getProperty("/navigation/1/items").forEach(function(oComponent) {
 					if (oComponent.key === sComponent) {
-						// set title
-						document.title = "" + sComponent + " - UI5 Web Components";
 						this.getModel().setProperty("/showNavigationLinks", true);
 						var sFrameUrlParameters = this.getModel().getProperty("/selectedView").slice(
 							this.getModel().getProperty("/selectedView").indexOf("?") + 1,
 							this.getModel().getProperty("/selectedView").length);
 						this.getModel().setProperty("/selectedKey", sComponent);
-						this.getModel().setProperty("/selectedView", getRealBaseURI() + oComponent.url + (sFrameUrlParameters ? "?" + sFrameUrlParameters : ""));
+						this.getModel().setProperty("/selectedView", oComponent.url + (sFrameUrlParameters ? "?" + sFrameUrlParameters : ""));
 					}
 				}.bind(this));
 			} else {
-				document.title = "Home - UI5 Web Components";
 				this.getModel().setProperty("/showNavigationLinks", false);
-				this.getModel().setProperty("/selectedView", getRealBaseURI() + "www/home/");
+				this.getModel().setProperty("/selectedView", "www/home/index.html");
 			}
 		},
 
