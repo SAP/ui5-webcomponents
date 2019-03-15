07b33e0cd417efec61e231191cc754ca7bebb839
Peter Skelin <petar.skelin@sap.com>
2019-03-15 09:50:08 +0200

    improvement: externalise webcomponents polyfill (#185)
    The polyfill is now loaded via the official webcomponents-loader, instead of imports
    This allows users of web components to provide their own version and copy of the polyfill
    This also decreases the bundle size for the playground for modern browsers.

--- packages/base/src/sap/ui/webcomponents/base/Bootstrap.js
+++ packages/base/src/sap/ui/webcomponents/base/Bootstrap.js
@@ -18,7 +18,17 @@ const Bootstrap = {
 			whenDOMReady().then(() => {
 				insertIconFontFace();
 				DOMEventHandler.start();
-				resolve();
+
+				if (window.WebComponents && window.WebComponents.waitFor) {
+					// the polyfill loader is present
+					window.WebComponents.waitFor(() => {
+						// the polyfills are loaded, safe to execute code depending on their APIs
+						resolve();
+					});
+				} else {
+					// polyfill loader missing, modern browsers only
+					resolve();
+				}
 			});
 		});
 
--- packages/base/src/sap/ui/webcomponents/base/browsersupport/Edge.js
+++ packages/base/src/sap/ui/webcomponents/base/browsersupport/Edge.js
@@ -1,9 +1,6 @@
 // URLSearchParams
 import "url-search-params-polyfill";
 
-// Web Components polyfills
-import "../thirdparty/shadydom.min";
-import "../thirdparty/custom-elements.min";
 
 // "pseudo mutation observer" fix for nodeValue
 import "../compatibility/patchNodeValue";
