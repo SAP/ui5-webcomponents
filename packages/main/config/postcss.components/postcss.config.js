41bb5976b7d293f4862db769bbd2c5d2cb4a2d8e
Peter Skelin <petar.skelin@sap.com>
2019-03-28 05:36:33 +0200

    improvement: optimize css (#266)
    - combine :root selectors in the parameters bundle
    - remove duplicate properties in the parameters bundle
    - minify css of both bundle and component outputs

--- packages/main/config/postcss.components/postcss.config.js
+++ packages/main/config/postcss.components/postcss.config.js
@@ -1,9 +1,11 @@
 const postcssNesting = require('postcss-nesting');
 const postcssAddFallback = require('../../lib/postcss-add-fallback/index.js');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
     postcssNesting(),
     postcssAddFallback({importFrom: "./dist/themes-next/sap_fiori_3/parameters-bundle.css"}),
+    cssnano(),
   ]
 };
