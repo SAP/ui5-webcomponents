fe89ef65f1169612b948a072902d9f84b78ac5c8
Petar Skelin <petar.skelin@sap.com>
2019-04-23 15:00:52 +0300

    remove props provider from abstract class

--- packages/main/src/ListItem.js
+++ packages/main/src/ListItem.js
@@ -10,9 +10,6 @@ import "./Button.js";
 // Styles
 import styles from "./themes/ListItem.css.js";
 
-// all themes should work via the convenience import (inlined now, switch to json when elements can be imported individyally)
-import "./ThemePropertiesProvider.js";
-
 /**
  * @public
  */
