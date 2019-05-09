91c29bd89b87a2f829d991038e786fc9f5c00339
Petar Skelin <petar.skelin@sap.com>
2019-05-09 09:57:23 +0300

    chore: add derived colors as es6 modules in dist

--- packages/main/rollup.config.js
+++ packages/main/rollup.config.js
@@ -11,7 +11,7 @@ import postcss from 'rollup-plugin-postcss';
 import postcssImport from 'postcss-import';
 import postcssNesting from 'postcss-nesting';
 import csso from 'postcss-csso';
-
+import commonjs from 'rollup-plugin-commonjs';
 
 const DIST = path.normalize("dist");
 const DIST_PLAYGROUND = path.normalize("dist/resources/sap/ui/webcomponents/main");
@@ -132,6 +132,25 @@ const getES5Config = () => {
 let config = [];
 
 config = config.concat(getES6Config());
+
+
+const getDerivedColorsConfig = (theme) => {
+	return {
+		input: `src/themes/${theme}/derived-colors.js`,
+		output: {
+			dir: `dist/themes/${theme}/`,
+			format: "esm",
+			sourcemap: true
+		},
+		plugins: [commonjs()],
+	}
+}
+
+config = config.concat([
+	getDerivedColorsConfig("sap_belize"),
+	getDerivedColorsConfig("sap_belize_hcb"),
+	getDerivedColorsConfig("sap_fiori_3"),
+]);
 
 if (process.env.ES5_BUILD) {
 	config = config.concat(getES5Config());
