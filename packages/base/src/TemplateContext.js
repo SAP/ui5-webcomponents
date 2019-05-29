f7c4e7384b2a4b5c6bb0397085ae145d59293ec1
Martin Hristov <martin.r.hristov@gmail.com>
2019-05-29 13:03:58 +0300

    use built in directive for styles and classes transformation

--- packages/base/src/TemplateContext.js
+++ packages/base/src/TemplateContext.js
@@ -1,45 +1,3 @@
-const _convertClasses = (classes, customStyleClasses) => {
-	for (const i in classes) { // eslint-disable-line
-		const customStyleClassesToAdd = i === "main" ? customStyleClasses : undefined;
-		classes[i] = _convertSingleClass(classes[i], customStyleClassesToAdd);
-	}
-
-	return classes;
-};
-
-const _convertSingleClass = (oClass, customStyleClasses) => {
-	// Push all custom classes first, if any
-	if (Array.isArray(customStyleClasses)) {
-		customStyleClasses.forEach(sClassName => {
-			oClass[sClassName] = true;
-		});
-	}
-
-	return Object.keys(oClass).filter(className => {
-		return oClass[className];
-	}).join(" ");
-};
-
-const _convertStyles = function _convertStyles(styles) {
-	if (!styles) {
-		return "";
-	}
-
-	for (const i in styles) { // eslint-disable-line
-		const stylesNs = styles[i];
-
-		const result = [];
-		Object.keys(stylesNs).forEach(key => {
-			if (stylesNs[key]) {
-				result.push(`${key}: ${stylesNs[key]}`);
-			}
-		});
-		styles[i] = result.length ? result.join("; ") : "";
-	}
-
-	return styles || "";
-};
-
 class TemplateContext {
 	// calculate control template context
 	static calculate(control) {
@@ -48,9 +6,6 @@ class TemplateContext {
 		if (!templateContext.hasOwnProperty("ariaHidden")) { // eslint-disable-line
 			templateContext.ariaHidden = control._state.hidden ? "true" : undefined;
 		}
-
-		templateContext.classes = _convertClasses(templateContext.classes, control._customClasses);
-		templateContext.styles = _convertStyles(templateContext.styles);
 
 		return templateContext;
 	}
