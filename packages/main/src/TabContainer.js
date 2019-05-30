bd9716ecb52c8825997ab9706054d41a51ef6863
Vladislav Tasev <vladislav.tasev@sap.com>
2019-05-30 14:59:19 +0300

    refactor: remove template context for tab container

--- packages/main/src/TabContainer.js
+++ packages/main/src/TabContainer.js
@@ -4,7 +4,6 @@ import ResizeHandler from "@ui5/webcomponents-base/src/delegate/ResizeHandler.js
 import ScrollEnablement from "@ui5/webcomponents-base/src/delegate/ScrollEnablement.js";
 import ItemNavigation from "@ui5/webcomponents-base/src/delegate/ItemNavigation.js";
 import { isSpace, isEnter } from "@ui5/webcomponents-base/src/events/PseudoEvents.js";
-import TabContainerTemplateContext from "./TabContainerTemplateContext.js";
 import TabContainerRenderer from "./build/compiled/TabContainerRenderer.lit.js";
 import Button from "./Button.js";
 import CustomListItem from "./CustomListItem.js";
@@ -12,6 +11,7 @@ import Icon from "./Icon.js";
 import List from "./List.js";
 import Popover from "./Popover.js";
 import TabBase from "./TabBase.js";
+import IconColor from "./types/IconColor.js";
 
 // Styles
 import tabContainerCss from "./themes/TabContainer.css.js";
@@ -233,6 +233,40 @@ class TabContainer extends UI5Element {
 		if (!hasSelected) {
 			this.items[0].selected = true;
 		}
+
+		this.calculateRenderItems();
+	}
+
+	calculateRenderItems() {
+		this.renderItems = this.items.map((item, index) => {
+			const isSeparator = item.isSeparator();
+
+			if (isSeparator) {
+				return { isSeparator, _tabIndex: item._tabIndex, _id: item._id };
+			}
+
+			return {
+				item,
+				isMixedModeTab: !item.icon && this.mixedMode,
+				isTextOnlyTab: !item.icon && !this.mixedMode,
+				isIconTab: item.icon,
+				position: index + 1,
+				disabled: item.disabled || undefined,
+				selected: item.selected || false,
+				hidden: !item.selected,
+				ariaLabelledBy: calculateAriaLabelledBy(item),
+				contentItemClasses: calculateContentItemClasses(item),
+				headerItemClasses: calculateHeaderItemClasses(item, this.mixedMode),
+				headerItemContentClasses: calculateHeaderItemContentClasses(item),
+				headerItemIconClasses: calculateHeaderItemIconClasses(item),
+				headerItemSemanticIconClasses: calculateHeaderItemSemanticIconClasses(item),
+				headerItemTextClasses: calculateHeaderItemTextClasses(item),
+				headerItemAdditionalTextClasses: calculateHeaderItemAdditionalTextClasses(item),
+				overflowItemClasses: calculateOverflowItemClasses(item),
+				overflowItemContentClasses: calculateOverflowItemContentClasses(item),
+				overflowItemState: calculateOverflowItemState(item),
+			};
+		}, this);
 	}
 
 	onAfterRendering() {
@@ -359,8 +393,47 @@ class TabContainer extends UI5Element {
 		return this.shadowRoot.querySelector(`#${this._id}-overflowMenu`);
 	}
 
-	static get calculateTemplateContext() {
-		return TabContainerTemplateContext.calculate;
+	get classes() {
+		return {
+			main: {
+				"ui5-tab-container": true,
+			},
+			header: {
+				"ui5-tc__header": true,
+				"ui5-tc__header--scrollable": this._scrollable,
+			},
+			headerScrollContainer: {
+				"ui-tc__headerScrollContainer": true,
+			},
+			headerList: {
+				"ui5-tc__headerList": true,
+			},
+			separator: {
+				"ui5-tc__separator": true,
+			},
+			headerBackArrow: {
+				"ui5-tc__headerArrow": true,
+				"ui5-tc__headerArrowLeft": true,
+				"ui5-tc__headerArrow--visible": this._scrollableBack,
+			},
+			headerForwardArrow: {
+				"ui5-tc__headerArrow": true,
+				"ui5-tc__headerArrowRight": true,
+				"ui5-tc__headerArrow--visible": this._scrollableForward,
+			},
+			overflowButton: {
+				"ui-tc__overflowButton": true,
+				"ui-tc__overflowButton--visible": this._scrollable,
+			},
+			content: {
+				"ui5-tc__content": true,
+				"ui5-tc__content--collapsed": this.collapsed,
+			},
+		};
+	}
+
+	get mixedMode() {
+		return this.items.some(item => item.icon) && this.items.some(item => item.text);
 	}
 
 	static async define(...params) {
@@ -386,6 +459,120 @@ const findIndex = (arr, predicate) => {
 	}
 
 	return -1;
+};
+
+/* CSS classes calculation helpers */
+
+const calculateAriaLabelledBy = item => {
+	const labels = [];
+
+	if (item.text) {
+		labels.push(`${item._id}-text`);
+	}
+
+	if (item.additionalText) {
+		labels.push(`${item._id}-additionalText`);
+	}
+
+	if (item.icon) {
+		labels.push(`${item._id}-icon`);
+	}
+
+	return labels.join(" ");
+};
+
+const calculateHeaderItemClasses = (item, mixedMode) => {
+	const classes = ["ui5-tc__headerItem"];
+
+	if (item.selected) {
+		classes.push("ui5-tc__headerItem--selected");
+	}
+
+	if (item.disabled) {
+		classes.push("ui5-tc__headerItem--disabled");
+	}
+
+	if (!item.icon && !mixedMode) {
+		classes.push("ui5-tc__headerItem--textOnly");
+	}
+
+	if (item.icon) {
+		classes.push("ui5-tc__headerItem--withIcon");
+	}
+
+	if (!item.icon && mixedMode) {
+		classes.push("ui5-tc__headerItem--mixedMode");
+	}
+
+	if (item.iconColor !== IconColor.Default) {
+		classes.push(`ui5-tc__headerItem--${item.iconColor.toLowerCase()}`);
+	}
+
+	return classes.join(" ");
+};
+
+const calculateHeaderItemContentClasses = item => {
+	const classes = ["ui5-tc__headerItemContent"];
+
+	return classes.join(" ");
+};
+
+const calculateHeaderItemIconClasses = item => {
+	const classes = ["ui5-tc-headerItemIcon"];
+
+	return classes.join(" ");
+};
+
+const calculateHeaderItemSemanticIconClasses = item => {
+	const classes = ["ui5-tc-headerItemSemanticIcon"];
+
+	if (item.iconColor !== IconColor.Default) {
+		classes.push(`ui5-tc-headerItemSemanticIcon--${item.iconColor.toLowerCase()}`);
+	}
+
+	return classes.join(" ");
+};
+
+const calculateHeaderItemTextClasses = item => {
+	const classes = ["ui5-tc__headerItemText"];
+
+	return classes.join(" ");
+};
+
+const calculateHeaderItemAdditionalTextClasses = item => {
+	const classes = ["ui5-tc__headerItemAdditionalText"];
+
+	return classes.join(" ");
+};
+
+const calculateOverflowItemClasses = item => {
+	const classes = ["ui5-tc__overflowItem"];
+
+	if (item.iconColor !== IconColor.Default) {
+		classes.push(`ui5-tc__overflowItem--${item.iconColor.toLowerCase()}`);
+	}
+
+	if (item.disabled) {
+		classes.push("ui5-tc__overflowItem--disabled");
+	}
+
+	return classes.join(" ");
+};
+
+const calculateOverflowItemContentClasses = item => {
+	const classes = ["ui5-tc__overflowItemContent"];
+
+	return classes.join(" ");
+};
+
+const calculateOverflowItemState = item => {
+	return item.disabled ? "Inactive" : "Active";
+};
+
+const calculateContentItemClasses = item => {
+	const classes = ["ui5-tc__contentItem"];
+
+	return classes.join(" ");
 };
 
 Bootstrap.boot().then(_ => {
