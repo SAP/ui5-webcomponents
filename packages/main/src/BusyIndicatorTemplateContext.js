bc735e5bf8137d8b1e5b057c18f317a8cb0986ff
Filip Siderov <filip.siderov@sap.com>
2019-05-30 13:31:56 +0300

    fix eslint

--- packages/main/src/BusyIndicatorTemplateContext.js
+++ packages/main/src/BusyIndicatorTemplateContext.js
@@ -6,7 +6,7 @@ class BusyIndicatorTemplateContext {
 				main: {
 					"ui5-busyindicator-wrapper": true,
 					[`ui5-busyindicator-${state.size.toLowerCase()}`]: true,
-				}
+				},
 			},
 		};
 	}
