ff59a9811776a64ccd1a2e0e1882db6ffb84849c
Martin <martin.r.hristov@gmail.com>
2019-06-10 13:29:58 +0300

    feat(ui5-busyindicator): introduce active property and simplify usage (#519)
    - busy indicator is invisible by default
    - it can slot any content
    - active property sets its visibility

--- packages/main/src/themes/base/component-derived-colors.js
+++ packages/main/src/themes/base/component-derived-colors.js
@@ -31,6 +31,7 @@ const derivationsFactory = ({ darken, lighten, contrast, fade, saturate, desatur
 		"--sapUiButtonRejectActiveBackgroundLighten5": () => lighten("--sapUiButtonRejectActiveBackground", 5),
 		"--sapUiButtonAcceptActiveBackgroundLighten5": () => lighten("--sapUiButtonAcceptActiveBackground", 5),
 		"--sapUiButtonBackgroundDarken10": () => darken("--sapUiButtonBackground", 10),
+		"--sapBackgroundColorFade72": () => fade("--sapBackgroundColor", 72),
 		"--sapUiAccent1Lighten50": () => lighten("--sapUiAccent1", 50),
 		"--sapUiAccent2Lighten40": () => lighten("--sapUiAccent2", 40),
 		"--sapUiAccent3Lighten46": () => lighten("--sapUiAccent3", 46),
