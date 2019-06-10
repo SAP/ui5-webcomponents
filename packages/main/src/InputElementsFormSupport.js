ad85d6665d7a64f1f7b8259ad05d2b2d48892f53
Vladislav Tasev <vladislav.tasev@sap.com>
2019-06-10 12:19:05 +0300

    fix eslint

--- packages/main/src/InputElementsFormSupport.js
+++ packages/main/src/InputElementsFormSupport.js
@@ -1,5 +1,5 @@
-import FormSupport from "./util/FormSupport.js";
 import { registerFeature } from "@ui5/webcomponents-base/src/FeaturesRegistry.js";
+import FormSupport from "./util/FormSupport.js";
 
 // Add form support to the global features registry so that Web Components can find and use it
 registerFeature("FormSupport", FormSupport);
