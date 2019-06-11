d0740893d0ea2e0caded354dfaa4d54c797d06bd
Vladislav Tasev <vladislav.tasev@sap.com>
2019-06-11 14:45:41 +0300

    chore: fix import maps

--- packages/base/src/renderer/LitRenderer.js
+++ packages/base/src/renderer/LitRenderer.js
@@ -8,8 +8,8 @@ const litRender = (templateResult, domNode, styles, { eventContext } = {}) => {
 };
 
 export { html, svg } from "lit-html";
-export { repeat } from "lit-html/directives/repeat";
-export { classMap } from "lit-html/directives/class-map";
-export { styleMap } from "lit-html/directives/style-map";
+export { repeat } from "lit-html/directives/repeat.js";
+export { classMap } from "lit-html/directives/class-map.js";
+export { styleMap } from "lit-html/directives/style-map.js";
 
 export default litRender;
