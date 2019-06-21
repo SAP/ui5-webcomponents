5d048cf5244d59e5fd5555f5e9cba43d16dd1d8b
Elena Stoyanova <elena.stoyanova@sap.com>
2019-06-21 15:19:23 +0300

    chore: add common ACC invisible text styles

--- packages/main/config/postcss.components/postcss.config.js
+++ packages/main/config/postcss.components/postcss.config.js
@@ -1,18 +1,20 @@
+const postcssImport = require('postcss-import');
 const postcssAddFallback = require('../../lib/postcss-add-fallback/index.js');
 const postcssCSStoESM = require('../../lib/postcss-css-to-esm/index.js');
-const cssnano = require('cssnano');
+const cssnano = require("cssnano");
 
 module.exports = {
 	plugins: [
-		postcssAddFallback({importFrom: "./dist/css/themes/sap_fiori_3/parameters-bundle.css"}),
+		postcssImport(),
+		postcssAddFallback({ importFrom: "./dist/css/themes/sap_fiori_3/parameters-bundle.css" }),
 		cssnano({
 			preset: [
-				'default', {
+				"default", {
 					mergeLonghand: false, // https://github.com/cssnano/cssnano/issues/675
 					mergeRules: false, // https://github.com/cssnano/cssnano/issues/730
 				},
-			]
+			],
 		}),
 		postcssCSStoESM(),
-	]
+	],
 };
