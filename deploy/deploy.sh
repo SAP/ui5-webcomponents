1abdec75d7193d8dc8e4f9e1155078bf796b818e
Filip Siderov <filip.siderov@sap.com>
2019-07-10 10:31:30 +0300

    fix: fix deletion of old files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -51,7 +51,7 @@ if [ "$TRAVIS_BRANCH" == "$TRAVIS_LATEST_RELEASE_WEBSITE_BRANCH" ]; then
 
   # Remove all folders and files from gh-pages, but folder master
   cd gh-pages
-  rm -v !("master")|| exit 0
+  rm -rf ./!(master)|| exit 0
   cd ..
 
   # Run the build again so rollup can generate the correct public path urls
