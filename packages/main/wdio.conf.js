6a960da2d010c1d841420ea789aa8b3d848701d4
Vladislav Tasev <vladislav.tasev@sap.com>
2019-07-11 11:26:34 +0300

    fix config usage

--- packages/main/wdio.conf.js
+++ packages/main/wdio.conf.js
@@ -277,7 +277,7 @@ exports.config = {
         if (waitFor.includes(commandName)) {
             browser.executeAsync(function (done) {
                 // run all the tests in no conflict mode
-                window["sap-ui-webcomponents-main-bundle"].Configuration.setNoConflict(true);
+                window["sap-ui-webcomponents-main-bundle"].configuration.setNoConflict(true);
                 RenderScheduler.whenFinished().then(done);
             });
         }
