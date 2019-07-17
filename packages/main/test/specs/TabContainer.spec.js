ac25b52b0112fd7870bbd060b8ecf9d4165eb0a1
Martin Hristov <martin.r.hristov@gmail.com>
2019-07-17 17:41:28 +0300

    adopt tests

--- packages/main/test/specs/TabContainer.spec.js
+++ packages/main/test/specs/TabContainer.spec.js
@@ -3,59 +3,59 @@ const assert = require('assert');
 describe("TabContainer general interaction", () => {
 	browser.url("http://localhost:8080/test-resources/sap/ui/webcomponents/main/pages/TabContainer.html");
 
-	// it("tests itemSelect event", () => {
-	// 	const item = browser.findElementDeep("#tabContainer1 >>> .ui5-tc__headerItem:nth-child(3)");
-	// 	const result = browser.$("#result");
+	it("tests itemSelect event", () => {
+		const item = browser.findElementDeep("#tabContainer1 >>> .ui5-tc__headerItem:nth-child(3)");
+		const result = browser.$("#result");
 
-	// 	const SELECTED_TAB_TEXT = "Laptops";
+		const SELECTED_TAB_TEXT = "Laptops";
 
-	// 	item.click();
+		item.click();
 
-	// 	assert.strictEqual(result.getText(), SELECTED_TAB_TEXT, "Item text is retrieved correctly.");
-	// });
+		assert.strictEqual(result.getText(), SELECTED_TAB_TEXT, "Item text is retrieved correctly.");
+	});
 
-	// it("scroll works on iconsOnly TabContainer", () => {
-	// 	browser.setWindowSize(310, 1080);
+	it("scroll works on iconsOnly TabContainer", () => {
+		browser.setWindowSize(310, 1080);
 
-	// 	const arrowLeft = browser.findElementDeep("#tabContainerIconOnly >>> .ui5-tc__headerArrowLeft");
-	// 	const arrowRight = browser.findElementDeep("#tabContainerIconOnly >>> .ui5-tc__headerArrowRight");
+		const arrowLeft = browser.findElementDeep("#tabContainerIconOnly >>> .ui5-tc__headerArrowLeft");
+		const arrowRight = browser.findElementDeep("#tabContainerIconOnly >>> .ui5-tc__headerArrowRight");
 
-	// 	assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be initially hidden");
-	// 	assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be initially shown");
+		assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be initially hidden");
+		assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be initially shown");
 
-	// 	arrowRight.click();
-	// 	browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
+		arrowRight.click();
+		browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
 
-	// 	assert.ok(arrowLeft.isDisplayed(), "'Left Arrow' should be shown after 'Right Arrow' click");
-	// 	assert.ok(!arrowRight.isDisplayed(), "'Right Arrow' should be hidden after 'Right Arrow' click");
+		assert.ok(arrowLeft.isDisplayed(), "'Left Arrow' should be shown after 'Right Arrow' click");
+		assert.ok(!arrowRight.isDisplayed(), "'Right Arrow' should be hidden after 'Right Arrow' click");
 
-	// 	arrowLeft.click();
-	// 	browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
+		arrowLeft.click();
+		browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
 
-	// 	assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be hidden after 'Left Arrow' click");
-	// 	assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be shown after 'Left Arrow' click");
-	// });
+		assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be hidden after 'Left Arrow' click");
+		assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be shown after 'Left Arrow' click");
+	});
 
-	// it("scroll works on textOnly TabContainer", () => {
-	// 	browser.setWindowSize(310, 1080);
-	// 	browser.findElementDeep("#tabContainerTextOnly").scrollIntoView();
+	it("scroll works on textOnly TabContainer", () => {
+		browser.setWindowSize(310, 1080);
+		browser.findElementDeep("#tabContainerTextOnly").scrollIntoView();
 
-	// 	const arrowLeft = browser.findElementDeep("#tabContainerTextOnly >>> .ui5-tc__headerArrowLeft");
-	// 	const arrowRight = browser.findElementDeep("#tabContainerTextOnly >>> .ui5-tc__headerArrowRight");
+		const arrowLeft = browser.findElementDeep("#tabContainerTextOnly >>> .ui5-tc__headerArrowLeft");
+		const arrowRight = browser.findElementDeep("#tabContainerTextOnly >>> .ui5-tc__headerArrowRight");
 
-	// 	assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be initially hidden");
-	// 	assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be initially shown");
+		assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be initially hidden");
+		assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be initially shown");
 
-	// 	arrowRight.click();
-	// 	browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
+		arrowRight.click();
+		browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
 
-	// 	assert.ok(arrowLeft.isDisplayed(), "'Left Arrow' should be shown after 'Right Arrow' click");
-	// 	assert.ok(!arrowRight.isDisplayed(), "'Right Arrow' should be hidden after 'Right Arrow' click");
+		assert.ok(arrowLeft.isDisplayed(), "'Left Arrow' should be shown after 'Right Arrow' click");
+		assert.ok(!arrowRight.isDisplayed(), "'Right Arrow' should be hidden after 'Right Arrow' click");
 
-	// 	arrowLeft.click();
-	// 	browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
+		arrowLeft.click();
+		browser.pause(300); // TODO: wait for animation finish. Remove when solved on framework level
 
-	// 	assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be hidden after 'Left Arrow' click");
-	// 	assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be shown after 'Left Arrow' click");
-	// });
+		assert.ok(!arrowLeft.isDisplayed(), "'Left Arrow' should be hidden after 'Left Arrow' click");
+		assert.ok(arrowRight.isDisplayed(), "'Right Arrow' should be shown after 'Left Arrow' click");
+	});
 });
