7548ed03adeb59673075a937f403bc34aea66855
Vladislav Tasev <vladitasev@users.noreply.github.com>
2019-07-22 13:35:37 +0300

    refactor: fix icon imports and typo in function name (#663)

--- packages/base/src/icons/sort-ascending.js
+++ packages/base/src/icons/sort-ascending.js
@@ -1,4 +1,4 @@
-import { registerIcon } from "../SVGIconRegistry";
+import { registerIcon } from "../SVGIconRegistry.js";
 
 const name = "sap-icon://sort-ascending";
 const d = "M82 288h351l15 32H65zm-48 96h446l15 32H17zm366-160H113l16-32h255zm-47-96H161l17-32h158z";
--- packages/base/src/icons/text-align-right.js
+++ packages/base/src/icons/text-align-right.js
@@ -1,4 +1,4 @@
-import { registerIcon } from "../SVGIconRegistry";
+import { registerIcon } from "../SVGIconRegistry.js";
 
 const name = "sap-icon://text-align-right";
 const d = "M448 64v32H80q-16 0-16-16t16-16h368zM256 176q0-16 16-16h176v32H272q-16 0-16-16zM64 272q0-16 16-16h368v32H80q-16 0-16-16zm0 192q0-16 16-16h368v32H80q-16 0-16-16zm192-96q0-16 16-16h176v32H272q-16 0-16-16z";
