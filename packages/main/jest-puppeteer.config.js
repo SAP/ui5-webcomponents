a16c3736c592c8a8d702daaba299ac94a1a9931b
Filip Siderov <filip.siderov@sap.com>
2019-07-29 15:04:43 +0300

    add jest tests

--- packages/main/jest-puppeteer.config.js
+++ packages/main/jest-puppeteer.config.js
@@ -6,7 +6,7 @@ module.exports = {
 		// headless: false
 	},
 	server: {
-		command: 'cd ../playground/app && jekyll serve',
+		command: 'cd ../playground/ && serve',
 		port: 8080,
 		launchTimeout: 10000,
 		debug: true,
