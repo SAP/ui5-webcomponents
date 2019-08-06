52764a41dfb64efae0da7faeaa5f51f0cfab6fc3
Petar Skelin <petar.skelin@sap.com>
2019-08-06 11:11:55 +0300

    svg template simplification

--- packages/main/src/IconTemplate.lit.js
+++ packages/main/src/IconTemplate.lit.js
@@ -1,32 +1,24 @@
 import ifDefined from "@ui5/webcomponents-base/dist/renderer/ifDefined.js";
-import { html, svg } from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+import { svg } from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
 
 const mainBlock = context => {
-	return html`<svg
-		class="ui5-icon-root"	
-		dir="${ifDefined(context.dir)}"	
-		viewBox="0 0 512 512"	
-		role="img"	
-		preserveAspectRatio="xMidYMid meet"	
+	return svg`<svg
+		class="ui5-icon-root"
+		dir="${ifDefined(context.dir)}"
+		viewBox="0 0 512 512"
+		role="img"
+		preserveAspectRatio="xMidYMid meet"
 		xmlns="http://www.w3.org/2000/svg"
-	>    
-		${svgBlock(context)};
-	</svg>`;
-};
-
-const svgBlock = context => {
-	return svg`
-		${context.title ? svgBlockTitle(context) : undefined}
+	>
+		${context.title
+		? svg`<title>${context.title}</title>`
+		: undefined}
 		<g role="presentation" aria-hidden="true">
-			<path transform="translate(0, 512) scale(1, -1)" 
-				d=${ifDefined(context.d)} 
+			<path transform="translate(0, 512) scale(1, -1)"
+				d=${ifDefined(context.d)}
 			/>
 		</g>
-	`;
-};
-
-const svgBlockTitle = context => {
-	return svg`<title>${ifDefined(context.title)}</title>`;
+	</svg>`;
 };
 
 export default mainBlock;
