de0d445cb0794d9f9bc2676d8529d6ad52260029
Martin Hristov <martin.r.hristov@gmail.com>
2019-08-22 14:48:51 +0300

    add shadow root seletor too be waited untill rendering

--- packages/main/wdio.conf.js
+++ packages/main/wdio.conf.js
@@ -275,7 +275,7 @@ exports.config = {
      * @param {Object} error error object if any
      */
     afterCommand: function (commandName, args, result, error) {
-        const waitFor = ["$", "$$", "click", "elementClick", "keys", "sendKeys", "findElement", "elementClear", "elementSendKeys", "setValue", "addValue", "getHTML", "getProperty", "setAttribute"];
+        const waitFor = ["$", "$$", "shadow$", "click", "elementClick", "keys", "sendKeys", "findElement", "elementClear", "elementSendKeys", "setValue", "addValue", "getHTML", "getProperty", "setAttribute"];
         if (waitFor.includes(commandName)) {
             browser.executeAsync(function (done) {
                 // run all the tests in no conflict mode
