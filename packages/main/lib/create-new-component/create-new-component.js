04575d6e3ecfe7b0d4d2834d41a0439c0396cdae
Tsanislav Gatev <tsanislav.gatev@sap.com>
2019-08-26 14:05:48 +0300

    feat: implement create control script

---
+++ packages/main/lib/create-new-component/create-new-component.js
@@ -0,0 +1,65 @@
+const jsFileContentTemplate = sComponentName => {
+	return `import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
+import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+import ${sComponentName}Template from "./generated/templates/${sComponentName}Template.lit.js";
+
+// Styles
+import ${sComponentName}Css from "./generated/themes/${sComponentName}.css.js";
+
+
+const metadata = {
+	tag: "ui5-${sComponentName.toLowerCase()}",
+	properties: {
+		//
+	},
+	slots: {
+		//
+	},
+	events: {
+		//
+	},
+};
+
+class ${sComponentName} extends UI5Element {
+	static get metadata() {
+		return metadata;
+	}
+
+	static get render() {
+		return litRender;
+	}
+
+	static get styles() {
+		return ${sComponentName}Css;
+	}
+
+	static get template() {
+		return ${sComponentName}Template;
+	}
+
+	static async define(...params) {
+		super.define(...params);
+	}
+}
+
+${sComponentName}.define();
+
+export default ${sComponentName};
+`;
+};
+
+const fs = require("fs");
+
+
+const aArgs = process.argv.slice(2);
+const sComponentName = aArgs[0];
+const oPaths = {
+	"js": `./src/${sComponentName}.js`,
+	"css": `./src/themes/${sComponentName}.css`,
+	"hbs": `./src/${sComponentName}.hbs`,
+};
+const sJsFileContentTemplate = jsFileContentTemplate(sComponentName);
+
+fs.writeFileSync(oPaths.js, sJsFileContentTemplate, { flag: "wx+" });
+fs.writeFileSync(oPaths.css, "", { flag: "wx+" });
+fs.writeFileSync(oPaths.hbs, "<div>Hello World</div>", { flag: "wx+" });
