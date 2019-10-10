8261c67174a6cbb5afdcb64f866184a73b669749
Filip Siderov <filip.siderov@sap.com>
2019-10-10 11:23:01 +0300

    fix(ui5-multi-combobox): close popover & empty value on selection

--- packages/main/src/MultiComboBox.js
+++ packages/main/src/MultiComboBox.js
@@ -1,7 +1,7 @@
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
 import ValueState from "@ui5/webcomponents-base/dist/types/ValueState.js";
-import { isShow, isDown, isBackSpace } from "@ui5/webcomponents-base/dist/events/PseudoEvents.js";
+import { isShow, isDown, isBackSpace, isEnter } from "@ui5/webcomponents-base/dist/events/PseudoEvents.js";
 import "./icons/slim-arrow-down.js";
 import { getRTL } from "@ui5/webcomponents-base/dist/config/RTL.js";
 import { isIE } from "@ui5/webcomponents-core/dist/sap/ui/Device.js";
@@ -402,6 +402,12 @@ class MultiComboBox extends UI5Element {
 		});
 
 		this.fireEvent("selectionChange", { items: this._getSelectedItems() });
+
+		if (isEnter(event) || !event.detail.selectionComponentPressed) {
+			this._getPopover().close();
+			this.value = "";
+			this.fireEvent("input");
+		}
 	}
 
 	_getPopover(isMorePopover) {
