1b3e40d17e64a8c297a7ba8c9df779f6962b140c
Filip Siderov <filip.siderov@sap.com>
2019-10-16 14:57:05 +0300

    fix(ui5-multi-combobox): close popover & empty value on selection (#832)

--- packages/main/test/specs/MultiComboBox.spec.js
+++ packages/main/test/specs/MultiComboBox.spec.js
@@ -9,10 +9,10 @@ describe("MultiComboBox general interaction", () => {
 			const popover = browser.$("#multi1").shadow$(".ui5-multi-combobox-all-items-popover");
 
 			icon.click();
-			assert.ok(popover.isDisplayedInViewport(), "Popover should be displayed in the viewport");
+			assert.ok(popover.getProperty("opened"), "Popover should be displayed in the viewport");
 
 			icon.click();
-			assert.ok(!popover.isDisplayedInViewport(), "Popover should close");
+			assert.ok(!popover.getProperty("opened"), "Popover should close");
 		});
 	});
 
@@ -30,7 +30,7 @@ describe("MultiComboBox general interaction", () => {
 			
 			icon.click();
 
-			assert.ok(popover.isDisplayedInViewport(), "Popover should be displayed in the viewport");
+			assert.ok(popover.getProperty("opened"), "Popover should be displayed in the viewport");
 			assert.equal(firstItem.getAttribute("selected"), null, "First item should not be selected");
 
 			firstItemCheckbox.click();
@@ -59,7 +59,7 @@ describe("MultiComboBox general interaction", () => {
 
 			const list = browser.$("#mcb").shadow$(".ui5-multi-combobox-all-items-list");
 
-			assert.ok(popover.isDisplayedInViewport(), "Popover should be displayed in the viewport");
+			assert.ok(popover.getProperty("opened"), "Popover should be displayed in the viewport");
 
 
 			assert.strictEqual(list.getProperty("items").length, 3, "3 items should be shown");
@@ -97,13 +97,47 @@ describe("MultiComboBox general interaction", () => {
 			}, 2500, "expect value state to be different after 2.5 seconds");
 		});
 
-		// it("tests if n more is applied and corresponding popover", () => {
-		// 	$("#more-mcb").scrollIntoView();
+		it("When item is clicked, the popover should be closed and the value in the input should be removed", () => {
+			const input = browser.$("#another-mcb").shadow$("#ui5-multi-combobox-input");
+			const popover = browser.$("#another-mcb").shadow$(".ui5-multi-combobox-all-items-popover");
+			const firstItem = browser.$("#another-mcb").shadow$(".ui5-multi-combobox-all-items-list > ui5-li");
 
-		// 	const nMoreText = browser.$("#more-mcb").shadow$("ui5-tokenizer").shadow$(".ui5-tokenizer-more-text");
+			input.click();
+			input.keys("c");
 
-		// 	assert.ok(nMoreText.getText(), "1 More", "token 1 should be visible");
-		// });
+			assert.strictEqual(popover.getProperty("opened"), true, "The popover should be opened");
+			assert.strictEqual(input.getValue(), "c", "Value is c (as typed)");
+
+			firstItem.click();
+
+			assert.strictEqual(popover.getProperty("opened"), false, "When the content is clicked, the popover should close");
+			assert.strictEqual(input.getValue(), "", "When the content is clicked, the value should be removed");
+		});
+
+		it("When item's checkbox is clicked, the popover should not be closed and the value in the input should be kept", () => {
+			const input = browser.$("#another-mcb").shadow$("#ui5-multi-combobox-input");
+			const popover = browser.$("#another-mcb").shadow$(".ui5-multi-combobox-all-items-popover");
+			const firstItemCheckbox = browser.$("#another-mcb").shadow$(".ui5-multi-combobox-all-items-list > ui5-li").shadow$("ui5-checkbox");
+
+			input.click();
+			input.keys("c");
+
+			assert.strictEqual(popover.getProperty("opened"), true, "The popover should be opened");
+			assert.strictEqual(input.getValue(), "c", "Value is c (as typed)");
+
+			firstItemCheckbox.click();
+
+			assert.strictEqual(popover.getProperty("opened"), true, "When the content is clicked, the popover should close");
+			assert.strictEqual(input.getValue(), "c", "When the content is clicked, the value should be removed");
+		});
+
+		it("tests if n more is applied and corresponding popover", () => {
+			$("#more-mcb").scrollIntoView();
+
+			const nMoreText = browser.$("#more-mcb").shadow$("ui5-tokenizer").shadow$(".ui5-tokenizer-more-text");
+
+			assert.ok(nMoreText.getText(), "1 More", "token 1 should be visible");
+		});
 	});
 
 	describe("keyboard handling", () => {
