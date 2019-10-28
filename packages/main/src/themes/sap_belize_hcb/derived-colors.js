603a9ecc0e5560cb1308c8b45be3310e39b160e4
Vladislav Tasev <vladislav.tasev@sap.com>
2019-10-28 17:06:52 +0200

    add theme-base package

--- packages/main/src/themes/sap_belize_hcb/derived-colors.js
+++ packages/main/src/themes/sap_belize_hcb/derived-colors.js
@@ -1,4 +1,4 @@
-const themeDerivationsFactory = require("./global-derived-colors");
-const componentDerivationsFactory = require("../base/component-derived-colors");
+const themeDerivationsFactory = require("@ui5/webcomponents-theme-base/src/themes/sap_belize_hcb/global-derived-colors.js");
+const componentDerivationsFactory = require("../base/component-derived-colors.js");
 
 module.exports = [themeDerivationsFactory, componentDerivationsFactory];
---
+++ packages/playground/_site/webapp/model/models.js
@@ -0,0 +1,107 @@
+sap.ui.define([
+	"jquery.sap.global",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], function (jQuery, JSONModel, Device) {
+	"use strict";
+
+	function getRealBaseURI() {
+		var baseURI = document.baseURI;
+		// var basePath = new URL(document.baseURI).pathname
+		// strip any url parameters
+		baseURI = baseURI.split("?")[0];
+		var isComponentPage = /\/components\/\w+\//.test(baseURI);
+		if (isComponentPage) {
+			return baseURI.split("playground/components")[0]
+		} else {
+			// ends with "/playground/" or has unknown path after it
+			return baseURI.split("playground/")[0]
+		}
+	}
+
+	return {
+
+		createUIModel: function () {
+			var oSideNavigationKeys = {
+				home: "home",
+				apiReference: "api-reference",
+				samples: "samples",
+				api: "api"
+			};
+
+			var model = new JSONModel();
+			var data = {
+				ui5Image: getRealBaseURI() + "images/ui5.png",
+				showSettingsMenu: false,
+				showNavigationLinks: false,
+				selectedView: "",
+				selectedHTMLContent: "",
+				selectedKey: "",
+				sideNavigationKeys: oSideNavigationKeys,
+				navigation: [{
+					key: oSideNavigationKeys.home,
+					section: oSideNavigationKeys.home,
+					title: "Getting Started"
+				}, {
+					title: "Components",
+					key: oSideNavigationKeys.samples,
+					items: []
+				}],
+				fixedNavigation: [{
+					title: "Source Code",
+					icon: "sap-icon://SAP-icons-TNT/code1",
+					url: "https://github.com/SAP/ui5-webcomponents",
+					target: "_blank"
+				}, {
+					title: "Read more...",
+					icon: "sap-icon://sys-help",
+					url: "https://sap.github.io/ui5-webcomponents",
+					target: "_blank"
+				}]
+			};
+			model.setData(data);
+			return model;
+		},
+
+		createDeviceModel: function () {
+			var oModel = new JSONModel(Device);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+
+		createAPIModel: function () {
+			var oModel = new JSONModel();
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+
+		createComponentsModel: function () {
+			var oModel = new JSONModel();
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+
+		discoverSamples: function (oModel) {
+			var newComponents = [
+				// Add new components here
+			];
+			return jQuery.ajax({
+				url: getRealBaseURI() + "/resources/playground.json"
+			}).then(function (jsonData) {
+				var parsedData = jsonData.map(function (value, index) {
+					return {
+						key: /(.*)\.sample\.html$/.exec(value)[1],
+						title: /(.*)\.sample\.html$/.exec(value)[1],
+						url: "test-resources/api/" + value,
+						new: newComponents.indexOf(/(.*)\.sample\.html$/.exec(value)[1]) > -1
+					};
+				}).sort(function (a, b) {
+					return a.title.localeCompare(b.title);
+				});
+				oModel.setProperty("/navigation/1/items", parsedData);
+			});
+		}
+
+	};
+
+});
