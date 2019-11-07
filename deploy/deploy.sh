a6ebe8934f20230d1023452d2ec2ab19f7384247
Filip Siderov <filip.siderov@sap.com>
2019-11-07 10:48:32 +0200

    Start deploying playground on master branch

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -85,7 +85,7 @@ if [ "$TRAVIS_BRANCH" == "$TRAVIS_LATEST_RELEASE_WEBSITE_BRANCH" ]; then
 
   # Run the build again so rollup can generate the correct public path urls
   cd $TRAVIS_BUILD_DIR
-  DEPLOY_PUBLIC_PATH=https://sap.github.io/ui5-webcomponents/master/assets/js/ui5-webcomponents/ yarn build:playground
+  DEPLOY_PUBLIC_PATH=https://sap.github.io/ui5-webcomponents/master/assets/js/ui5-webcomponents/ yarn build:playground --config _config.yml,_config_master.yml
 
   # Move master build folder to gh-pages folder
   cp -Rf $TRAVIS_BUILD_DIR/packages/playground/dist/* gh-pages/master
