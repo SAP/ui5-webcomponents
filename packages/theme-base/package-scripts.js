64108998887fe49eeb803a76db7dd394ae53b21a
Vladislav Tasev <vladislav.tasev@sap.com>
2019-11-13 14:14:20 +0200

    make theme-base separate style tag again

--- packages/theme-base/package-scripts.js
+++ packages/theme-base/package-scripts.js
@@ -4,11 +4,15 @@ module.exports = {
         build: {
             default: "nps clean build.styles",
             styles: {
-				default: "nps build.styles.belize build.styles.belize_hcb build.styles.fiori_3 build.styles.fiori_3_dark",
-				belize: "lessc src/themes/sap_belize/parameters-bundle.less dist/themes/sap_belize/parameters-bundle.css",
-				belize_hcb: "lessc src/themes/sap_belize_hcb/parameters-bundle.less dist/themes/sap_belize_hcb/parameters-bundle.css",
-				fiori_3: "lessc src/themes/sap_fiori_3/parameters-bundle.less dist/themes/sap_fiori_3/parameters-bundle.css",
-				fiori_3_dark: "lessc src/themes/sap_fiori_3_dark/parameters-bundle.less dist/themes/sap_fiori_3_dark/parameters-bundle.css"
+				default: "nps build.styles.less build.styles.bundles",
+				less: {
+					default: "nps build.styles.less.belize build.styles.less.belize_hcb build.styles.less.fiori_3 build.styles.less.fiori_3_dark",
+					belize: "lessc src/themes/sap_belize/parameters-bundle.less dist/themes/sap_belize/parameters-bundle.css",
+					belize_hcb: "lessc src/themes/sap_belize_hcb/parameters-bundle.less dist/themes/sap_belize_hcb/parameters-bundle.css",
+					fiori_3: "lessc src/themes/sap_fiori_3/parameters-bundle.less dist/themes/sap_fiori_3/parameters-bundle.css",
+					fiori_3_dark: "lessc src/themes/sap_fiori_3_dark/parameters-bundle.less dist/themes/sap_fiori_3_dark/parameters-bundle.css"
+				},
+				bundles: "postcss dist/**/parameters-bundle.css --config config/postcss.bundles --base dist/ --dir dist/assets/",
 			},
         },
 		start: 'nps watch',
