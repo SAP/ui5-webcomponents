be3e1a7431f6b73b582f57b3b3f07a37a81ce5aa
Vladislav Tasev <vladislav.tasev@sap.com>
2019-11-13 10:39:25 +0200

    add simple build

--- packages/theme-base/package-scripts.js
+++ packages/theme-base/package-scripts.js
@@ -4,11 +4,14 @@ module.exports = {
         build: {
             default: "nps clean build.styles",
             styles: {
-                default: "nps build.styles.bundles",
-                bundles: "lessc src/**/parameters-bundle.less dist/",
-            },
+				default: "nps build.styles.belize build.styles.belize_hcb build.styles.fiori_3 build.styles.fiori_3_dark",
+				belize: "lessc src/themes/sap_belize/parameters-bundle.less dist/themes/sap_belize/parameters-bundle.css",
+				belize_hcb: "lessc src/themes/sap_belize_hcb/parameters-bundle.less dist/themes/sap_belize_hcb/parameters-bundle.css",
+				fiori_3: "lessc src/themes/sap_fiori_3/parameters-bundle.less dist/themes/sap_fiori_3/parameters-bundle.css",
+				fiori_3_dark: "lessc src/themes/sap_fiori_3_dark/parameters-bundle.less dist/themes/sap_fiori_3_dark/parameters-bundle.css"
+			},
         },
 		start: 'nps watch',
-        watch: 'nps "build.styles.bundles -w"',
+        watch: 'nps "build.styles -w"',
     },
 };
