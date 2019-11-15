78495409bea1267e80c57c00b92566a2b06e3aa0
Vladislav Tasev <vladislav.tasev@sap.com>
2019-11-15 10:13:27 +0200

    add theme-base watch

--- packages/theme-base/package-scripts.js
+++ packages/theme-base/package-scripts.js
@@ -1,21 +1,23 @@
 module.exports = {
     scripts: {
         clean: "rimraf dist",
-        build: {
-            default: "nps clean build.styles",
-            styles: {
-				default: "nps build.styles.less build.styles.bundles",
-				less: {
-					default: "nps build.styles.less.belize build.styles.less.belize_hcb build.styles.less.fiori_3 build.styles.less.fiori_3_dark",
-					belize: "lessc src/themes/sap_belize/parameters-bundle.less dist/themes/sap_belize/parameters-bundle.css",
-					belize_hcb: "lessc src/themes/sap_belize_hcb/parameters-bundle.less dist/themes/sap_belize_hcb/parameters-bundle.css",
-					fiori_3: "lessc src/themes/sap_fiori_3/parameters-bundle.less dist/themes/sap_fiori_3/parameters-bundle.css",
-					fiori_3_dark: "lessc src/themes/sap_fiori_3_dark/parameters-bundle.less dist/themes/sap_fiori_3_dark/parameters-bundle.css"
-				},
-				bundles: "postcss dist/**/parameters-bundle.css --config config/postcss.bundles --base dist/ --dir dist/css/",
+		build: "nps clean compile",
+		start: "nps build watch",
+		compile: {
+			default: "nps compile.less compile.postcss",
+			less: {
+				default: "nps compile.less.belize compile.less.belize_hcb compile.less.fiori_3 compile.less.fiori_3_dark",
+				belize: "lessc src/themes/sap_belize/parameters-bundle.less dist/themes/sap_belize/parameters-bundle.css",
+				belize_hcb: "lessc src/themes/sap_belize_hcb/parameters-bundle.less dist/themes/sap_belize_hcb/parameters-bundle.css",
+				fiori_3: "lessc src/themes/sap_fiori_3/parameters-bundle.less dist/themes/sap_fiori_3/parameters-bundle.css",
+				fiori_3_dark: "lessc src/themes/sap_fiori_3_dark/parameters-bundle.less dist/themes/sap_fiori_3_dark/parameters-bundle.css"
 			},
-        },
-		start: 'nps watch',
-        watch: 'nps "build.styles.bundles -w"', // TODO: watch mode for the less files
+			postcss: "postcss dist/**/parameters-bundle.css --config config/postcss.bundles --base dist/ --dir dist/css/",
+		},
+        watch: {
+        	default: 'concurrently "nps watch.less" "nps watch.postcss"',
+			less: 'chokidar "src/themes/**/*.less" -c "nps compile.less"',
+			postcss: 'nps "compile.postcss -w"'
+		},
     },
 };
