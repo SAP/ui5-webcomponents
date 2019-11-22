c60043325b333b283347c33d0facbe0b68d3320d
ilhan <ilhan.myumyun@sap.com>
2019-11-22 15:42:18 +0200

    add wdio test

---
+++ packages/main/test/specs/RTL.spec.js
@@ -0,0 +1,11 @@
+const assert = require("chai").assert;
+
+
+describe("RTL", () => {
+	browser.url("http://localhost:8080/test-resources/pages/Button.html?sap-ui-language=he");
+
+	it("language forces RTL", () => {
+		const buttonRoot = browser.$("#button1").shadow$(".ui5-button-root");
+		assert.strictEqual(buttonRoot.getProperty("rtl"), "true", "dir is correctly set");
+	});
+});
