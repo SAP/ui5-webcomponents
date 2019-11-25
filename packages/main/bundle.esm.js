9194cc5867aca21c4cb11f163b830a8a9e475c2e
Naydenov <nayden.naydenov@sap.com>
2019-11-25 14:21:16 +0200

    Resolve merge conflicts

--- packages/main/bundle.esm.js
+++ packages/main/bundle.esm.js
@@ -33,14 +33,6 @@ import Panel from "./dist/Panel.js";
 import RadioButton from "./dist/RadioButton.js";
 import Select from "./dist/Select.js";
 import Option from "./dist/Option.js";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import ProductSwitch from "./dist/ProductSwitch.js";
-import ProductSwitchItem from "./dist/ProductSwitchItem.js";
->>>>>>> feat(ui5-productswitch): ui5-button internal usage
-=======
->>>>>>> feat(ui5-product-switch): stand alone component
 import Switch from "./dist/Switch.js";
 import MessageStrip from "./dist/MessageStrip.js";
 import MultiComboBox from "./dist/MultiComboBox.js";
