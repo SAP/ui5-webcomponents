46ba150ea661635480be497bed3d7776bd0ca001
Vladislav Tasev <vladislav.tasev@sap.com>
2019-12-02 11:44:39 +0200

    theme-base clean readded

--- packages/theme-base/package-scripts.js
+++ packages/theme-base/package-scripts.js
@@ -2,12 +2,12 @@ module.exports = {
 	scripts: {
 		clean: "rimraf dist",
 		build: {
-			default: "nps build.less build.postcss",
+			default: "nps clean build.less build.postcss",
 			less: {
-				default: "nps build.less.fiori_3 build.less.belize build.less.belize_hcb build.less.fiori_3_dark",
-				fiori_3: "lessc src/themes/sap_fiori_3/parameters-bundle.less dist/themes/sap_fiori_3/parameters-bundle.css",
+				default: "nps build.less.belize build.less.belize_hcb build.less.fiori_3 build.less.fiori_3_dark",
 				belize: "lessc src/themes/sap_belize/parameters-bundle.less dist/themes/sap_belize/parameters-bundle.css",
 				belize_hcb: "lessc src/themes/sap_belize_hcb/parameters-bundle.less dist/themes/sap_belize_hcb/parameters-bundle.css",
+				fiori_3: "lessc src/themes/sap_fiori_3/parameters-bundle.less dist/themes/sap_fiori_3/parameters-bundle.css",
 				fiori_3_dark: "lessc src/themes/sap_fiori_3_dark/parameters-bundle.less dist/themes/sap_fiori_3_dark/parameters-bundle.css",
 			},
 			postcss: "postcss dist/**/parameters-bundle.css --config config/postcss.themes --base dist/ --dir dist/css/",
