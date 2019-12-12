d110dcedc8b63cbc8817d0bae8b00db04a9fb1c3
Vladislav Tasev <vladislav.tasev@sap.com>
2019-12-12 16:34:33 +0200

    chore: remove live reload

--- packages/tools/components-package/rollup.js
+++ packages/tools/components-package/rollup.js
@@ -7,7 +7,6 @@ const url = require("rollup-plugin-url");
 const { terser } = require("rollup-plugin-terser");
 const notify = require('rollup-plugin-notify');
 const filesize = require('rollup-plugin-filesize');
-const livereload = require('rollup-plugin-livereload');
 
 const getConfig = (options) => {
 
@@ -78,17 +77,6 @@ const getConfig = (options) => {
 
 		if (process.env.DEV) {
 			plugins.push(notify());
-		}
-
-		const es6DevMain = process.env.DEV && !transpile && options.name === "@ui5/webcomponents";
-		if (es6DevMain) {
-			plugins.push(livereload({
-				watch: [
-					"dist/resources/bundle.esm.js",
-					"dist/**/*.html",
-					"dist/**/*.json",
-				]
-			}));
 		}
 
 		return plugins;
