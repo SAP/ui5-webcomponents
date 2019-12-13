f42fe48ebdd1b224a4e0f4bd3f2d3dfa4b07ebe4
Vladislav Tasev <vladislav.tasev@sap.com>
2019-12-13 11:18:15 +0200

    refactor: simplify package configuration

--- packages/fiori/config/postcss.components/postcss.config.js
+++ packages/fiori/config/postcss.components/postcss.config.js
@@ -1,3 +1,1 @@
-const config = require("@ui5/webcomponents-tools/components-package/postcss.components.js");
-
-module.exports = config;
+module.exports = require("@ui5/webcomponents-tools/components-package/postcss.components.js");
