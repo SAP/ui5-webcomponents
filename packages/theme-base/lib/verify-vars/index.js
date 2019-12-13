94a7ddaa988f313c5a2d8fce0ec48f55dc2aeee2
Vladislav Tasev <vladislav.tasev@sap.com>
2019-12-13 11:39:09 +0200

    chore: theme-base verify task also find variables missing between themes

--- packages/theme-base/lib/verify-vars/index.js
+++ packages/theme-base/lib/verify-vars/index.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
-const definitionFiles = [
-	`base/component-derived-colors`,
+const allThemesDefinitionFiles = [
 	`sap_belize/base-parameters`,
 	`sap_belize_hcb/base-parameters`,
 	`sap_fiori_3/base-parameters`,
-	`sap_fiori_3_dark/base-parameters`
+	`sap_fiori_3_dark/base-parameters`,
+];
+
+const allDefinitionFiles = [
+	`base/component-derived-colors`,
+	...allThemesDefinitionFiles
 ];
 
 const extractVarsFromDefinitionFile = str => {
@@ -25,7 +29,8 @@ const extractVarsFromVarsFile = str => {
 	}).filter(item => !!item);
 };
 
-definitionFiles.forEach(name => {
+// Check for missing variables in the *-vars.less files
+allDefinitionFiles.forEach(name => {
 	const defsFilePath = path.join(`src/themes`, `${name}.less`);
 	const varsFilePath = path.join(`src/themes`, `${name}-vars.less`);
 
@@ -35,7 +40,7 @@ definitionFiles.forEach(name => {
 	// Missing definitions
 	const missing = defsFileVars.filter(v => !varsFileVars.includes(v));
 	if (missing.length) {
-		console.log(`\nMissing variables in ${varsFilePath}`);
+		console.log(`\nMissing variables ${varsFilePath}`);
 		console.log(missing.join(`\n`));
 	}
 
@@ -47,3 +52,27 @@ definitionFiles.forEach(name => {
 	}
 
 });
+
+// Check for discrepancies between themes
+const varArrays = allThemesDefinitionFiles.map(name => {
+	const defsFilePath = path.join(`src/themes`, `${name}.less`);
+	return extractVarsFromDefinitionFile(fs.readFileSync(defsFilePath).toString());
+});
+
+const missingVars = [];
+for (let i = 0; i < varArrays.length; i++) {
+	for (let j = 0; j < varArrays.length; j++) {
+		if (i !== j) {
+			const missing = varArrays[i].filter(v => !varArrays[j].includes(v));
+			missing.forEach(v => {
+				if (!missingVars.includes(v)) {
+					missingVars.push(v);
+				}
+			})
+		}
+	}
+}
+if (missingVars.length) {
+	console.log(`\nVars missing in one or more themes:`);
+	console.log(missingVars.join(`\n`));
+}
