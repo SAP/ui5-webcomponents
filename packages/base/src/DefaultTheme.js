08e92283a6f6bc0f37e7f86f4b6a7edd8d3f3764
Vladislav Tasev <vladislav.tasev@sap.com>
2019-12-16 14:27:48 +0200

    move all references to theme-base to base

---
+++ packages/base/src/DefaultTheme.js
@@ -0,0 +1,5 @@
+import fiori3 from "@ui5/webcomponents-theme-base/dist/generated/themes/sap_fiori_3/parameters-bundle.css.js";
+
+import { registerThemeProperties } from "./asset-registries/Themes.js";
+
+registerThemeProperties("@ui5/webcomponents-theme-base", "sap_fiori_3", fiori3);
