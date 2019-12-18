ddf6f9692bc139ece3b14c56005a3bf157b94d93
Filip Siderov <filip.siderov@sap.com>
2019-12-18 14:36:44 +0200

    fix build

--- packages/tools/components-package/nps.js
+++ packages/tools/components-package/nps.js
@@ -59,7 +59,7 @@ const getScripts = (options) => {
 		test: {
 			default: {
 				// --success first - report the exit code of the test run (first command to finish), as serve is always terminated and has a non-0 exit code
-				default: 'concurrently "nps serve" "nps test.wdio.run" --kill-others --success first',
+				default: 'concurrently "nps serve" "nps test.default.run" --kill-others --success first',
 				run: "cross-env WDIO_LOG_LEVEL=error FORCE_COLOR=0 wdio config/wdio.conf.js",
 			},
 		},
