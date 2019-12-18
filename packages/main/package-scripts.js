a6158321d0bd7768b0dd809d5aa4df39bac496e7
Filip Siderov <filip.siderov@sap.com>
2019-12-18 15:46:49 +0200

    refactor(framework): remove dependency to jest & pupeteer (#1078)

--- packages/main/package-scripts.js
+++ packages/main/package-scripts.js
@@ -2,7 +2,6 @@ const getScripts = require("@ui5/webcomponents-tools/components-package/nps.js")
 
 const options = {
 	port: 8080,
-	hasJest: true,
 };
 
 const scripts = getScripts(options);
--- packages/main/test/qunit/run-all-local.js
+++
@@ -1,7 +0,0 @@
-QUnit.testSuites(tests.map(function (test) {
-	var location = window.location.href;
-	var startFileIndex = location.lastIndexOf("/");
-	var path = location.slice(0, startFileIndex + 1);
-
-	return path + "runner.html?testName=" + test;
-}));
