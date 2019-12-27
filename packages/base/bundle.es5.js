9e8d98453760dc816df6f8755cd5a54c326d3bf3
Vladislav Tasev <vladislav.tasev@sap.com>
2019-12-27 12:17:54 +0200

    first tests

--- packages/base/bundle.es5.js
+++ packages/base/bundle.es5.js
@@ -3,20 +3,27 @@ import "./dist/features/browsersupport/IE11.js";
 
 import "./bundle.esm.js";
 
-import { getAnimationMode } from "@ui5/webcomponents-base/dist/config/AnimationMode.js";
-import { getTheme, setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
-import { setNoConflict } from "@ui5/webcomponents-base/dist/config/NoConflict.js";
-import { getCompactSize, setCompactSize } from "@ui5/webcomponents-base/dist/config/CompactSize.js";
-import { getRTL } from "@ui5/webcomponents-base/dist/config/RTL.js";
-import { getRegisteredNames as getIconNames } from  "@ui5/webcomponents-base/dist/SVGIconRegistry.js"
+import { getAnimationMode } from "./dist/config/AnimationMode.js";
+import { getLanguage } from "./dist/config/Language.js";
+import { getCalendarType } from "./dist/config/CalendarType.js";
+import { getTheme, setTheme } from "./dist/config/Theme.js";
+import { getNoConflict, setNoConflict } from "./dist/config/NoConflict.js";
+import { getCompactSize, setCompactSize } from "./dist/config/CompactSize.js";
+import { getRTL } from "./dist/config/RTL.js";
+import { getFirstDayOfWeek } from "./dist/config/FormatSettings.js";
+import { getRegisteredNames as getIconNames } from  "./dist/SVGIconRegistry.js"
 const configuration = {
 	getAnimationMode,
+	getLanguage,
 	getTheme,
 	setTheme,
+	getNoConflict,
 	setNoConflict,
 	getCompactSize,
 	setCompactSize,
+	getCalendarType,
 	getRTL,
+	getFirstDayOfWeek,
 };
 export {
 	configuration,
--- packages/base/bundle.esm.js
+++ packages/base/bundle.esm.js
@@ -1,3 +1,8 @@
+import "./dist/features/calendar/Buddhist.js";
+import "./dist/features/calendar/Islamic.js";
+import "./dist/features/calendar/Japanese.js";
+import "./dist/features/calendar/Persian.js";
+
 // ESM bundle targets Edge + browsers with native support
 import "./dist/features/browsersupport/Edge.js";
 
@@ -11,20 +16,27 @@ window.isIE = isIE; // attached to the window object for testing purposes
 
 // Note: keep in sync with rollup.config value for IIFE
 import { getAnimationMode } from "./dist/config/AnimationMode.js";
+import { getLanguage } from "./dist/config/Language.js";
+import { getCalendarType } from "./dist/config/CalendarType.js";
 import { getTheme, setTheme } from "./dist/config/Theme.js";
-import { setNoConflict } from "./dist/config/NoConflict.js";
+import { getNoConflict, setNoConflict } from "./dist/config/NoConflict.js";
 import { getCompactSize, setCompactSize } from "./dist/config/CompactSize.js";
 import { getRTL } from "./dist/config/RTL.js";
+import { getFirstDayOfWeek } from "./dist/config/FormatSettings.js";
 import { getRegisteredNames as getIconNames } from  "./dist/SVGIconRegistry.js"
 window["sap-ui-webcomponents-bundle"] = {
 	configuration : {
 		getAnimationMode,
+		getLanguage,
 		getTheme,
 		setTheme,
+		getNoConflict,
 		setNoConflict,
 		getCompactSize,
 		setCompactSize,
+		getCalendarType,
 		getRTL,
+		getFirstDayOfWeek,
 	},
 	getIconNames,
 };
