03b301784104fb99d2c0ea8dffd58c4f5c1d2a34
ilhan <ilhan.myumyun@sap.com>
2020-01-07 06:57:42 +0200

    update

--- .github/actions/pre-release/index.js
+++ .github/actions/pre-release/index.js
@@ -1,29 +1,27 @@
 const fs = require("fs");
 const { promisify } = require("util");
+const glob = require("glob-promise");
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 const child_process = require("child_process");
-const glob = require("glob-promise");
-const gitRev = child_process.execSync("git rev-parse HEAD").toString();
 const execSync = child_process.execSync;
+const gitRev = execSync("git rev-parse HEAD").toString();
 
 const PACKAGES = {};
 const NPM_ORG = "@ui5/webcomponents";
 
 const run = async () => {
 	const FILES = await glob("**/packages/**/package.json", { 
-		"ignore": ["**/node_modules/**/*.*", "**/dist/**/package.json", "**/playground/**/package.json"]
+		"ignore": ["**/node_modules/**/*.*", "**/dist/**/*.*", "**/playground/**/*.*"]
 	});
-	
+
 	// Step 1: process package.json files
 	const pkgs = await Promise.all(FILES.map(processPackageJSON));
-	console.log(FILES)
-	console.log(pkgs)
 
 	// Step 2: update package.json files and publish each package to npm
 	await Promise.all(pkgs.map(async pkg => {
-			await updatePackageJSON(pkg);
-			publishPackage(pkg);
+		await updatePackageJSON(pkg);
+		publishPackage(pkg);
 	}));
 };
 
@@ -55,16 +53,16 @@ const updatePackageJSON = async pkg => {
 		fileContent.devDependencies[dep] = PACKAGES[dep].version;
 	});
 
-	await writeFileAsync(file, JSON.stringify(fileContent, null, "  "));
+	return writeFileAsync(file, JSON.stringify(fileContent, null, "  "));
 };
 
 const getDependencies = (dependencies) => {
 	return Object.keys(dependencies).filter(dep => dep.startsWith(NPM_ORG));
 };
 
-const publishPackage = async pkg => {
+const publishPackage = pkg => {
 	console.info(`Publish ${pkg.name}: ${pkg.version} ...`);
-	execSync(`yarn publish ${pkg.folder} --tag=next --registry=http://localhost:4873/`);
+	execSync(`yarn publish ${pkg.folder} --tag=next`);
 };
 
 run().catch(error => {
