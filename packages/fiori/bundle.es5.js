2db62badf60a89d885ed8b5229bac5e0c9c82362
Martin <martin.r.hristov@gmail.com>
2020-01-17 13:33:59 +0200

    refactor: propagate compact size when ui5-content-density-compact class is set (#1136)
    - add a global classes `ui5-content-density-compact`, `sapUiSizeCompact`(private) which redefine all **COSY** variables with **COMPACT** values
    - adopt all components
    - remove compact setting from Configuration
    - update documentation for compact / cosy setting
    - remove content density related modules
    
    BREAKING CHANGE: set/get for compact size is removed, use CSS class ui5-content-density-compact as a replacement.

--- packages/fiori/bundle.es5.js
+++ packages/fiori/bundle.es5.js
@@ -6,7 +6,6 @@ import "./bundle.esm.js";
 import { getAnimationMode } from "@ui5/webcomponents-base/dist/config/AnimationMode.js";
 import { getTheme, setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
 import { setNoConflict } from "@ui5/webcomponents-base/dist/config/NoConflict.js";
-import { getCompactSize } from "@ui5/webcomponents-base/dist/config/CompactSize.js";
 import { getRTL } from "@ui5/webcomponents-base/dist/config/RTL.js";
 import { getRegisteredNames as getIconNames } from  "@ui5/webcomponents-base/dist/SVGIconRegistry.js"
 const configuration = {
@@ -14,7 +13,6 @@ const configuration = {
 	getTheme,
 	setTheme,
 	setNoConflict,
-	getCompactSize,
 	getRTL,
 };
 export {
