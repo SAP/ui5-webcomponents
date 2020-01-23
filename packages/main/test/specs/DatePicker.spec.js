10c991333bf247b566e300c44afb6fe9d1fa5c10
ilhan <ilhan.myumyun@sap.com>
2020-01-23 12:00:36 +0200

    add test

--- packages/main/test/specs/DatePicker.spec.js
+++ packages/main/test/specs/DatePicker.spec.js
@@ -532,4 +532,23 @@ describe("Date Picker Tests", () => {
 		assert.ok(datepicker.root.getProperty("placeholder"), "The DatePicker has placeholder set");
 		assert.equal(innerInputPlaceholder, placeholder, "The inner input has the placeholder, set by the user");
 	});
+
+	it("navigate to Jan 0001 page", () => {
+		const staringDate = "Feb 1, 0001";
+		const expectedDateAfterNavigtion = "Jan 19, 0001";
+
+		datepicker.id ="#dp0001Page";
+		datepicker.innerInput.setValue(staringDate);
+		datepicker.openPicker();
+
+		// Starting from February we are navigating up to January
+		browser.keys("ArrowUp");
+		browser.keys("ArrowUp");
+		browser.keys("ArrowUp");
+		browser.keys("ArrowUp");
+		browser.keys("Enter");
+
+		const selectedDate = datepicker.innerInput.getValue();
+		assert.strictEqual(selectedDate, expectedDateAfterNavigtion, selectedDate + " has been reached successfully");
+	});
 });
