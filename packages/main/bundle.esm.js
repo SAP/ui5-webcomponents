b1459ce359411291d96e594644fe68cabb6796f9
Vladislav Tasev <vladitasev@users.noreply.github.com>
2020-02-11 14:08:21 +0200

    chore(framework): Allow a workaround to skip theme-base parameters (#1175)

--- packages/main/bundle.esm.js
+++ packages/main/bundle.esm.js
@@ -1,3 +1,5 @@
+// import "@ui5/webcomponents-base/test/dev-helpers/ExternalThemePresent.js";
+
 import "@ui5/webcomponents-base/dist/features/calendar/Buddhist.js";
 import "@ui5/webcomponents-base/dist/features/calendar/Islamic.js";
 import "@ui5/webcomponents-base/dist/features/calendar/Japanese.js";
