72a8a19c89e383ae268cf919ab9e133118bf1ac2
Filip Siderov <filip.siderov@sap.com>
2020-02-12 15:34:24 +0200

    add tests

--- packages/main/test/specs/FileUploader.spec.js
+++ packages/main/test/specs/FileUploader.spec.js
@@ -11,12 +11,24 @@ describe("API", () => {
 	it("File upload with no input", () => {
 		const fileUploader = $("#file-uploader-no-input");
 		const inputField = fileUploader.shadow$("ui5-input");
-		assert.notOk(inputField.isExisting(), "Input should not be rendered");
+		assert.notOk(inputField.isExisting(), "Input should not be rendered.");
 	});
 
 	it("Default slot is working", () => {
 		const fileUploader = $("#file-uploader-no-input");
 		const button = fileUploader.shadow$("ui5-button");
-		assert.notOk(button.isExisting(), "Button should be rendered");
+		assert.notOk(button.isExisting(), "Button should be rendered.");
+	});
+
+	it("Tests disabled file uploader", () => {
+		const fileUploader = $("#disabled");
+		const input = fileUploader.shadow$("input");
+		assert.ok(input.getProperty("disabled"), "Native input is disabled.");
+	});
+
+	it("Tests accept property", () => {
+		const fileUploader = $("#file-uploader-accept");
+		const input = fileUploader.shadow$("input");
+		assert.strictEqual(input.getProperty("accept"), ".txt,.docx", "Native input is has the rignt accept property.");
 	});
 });
