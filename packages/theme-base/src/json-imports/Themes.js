69fce6d8dbaf7e909822725bb1db9617676e9c6d
Martin Hristov <martin.r.hristov@gmail.com>
2020-02-12 17:20:11 +0200

    WIP: belize_hcw theme

--- packages/theme-base/src/json-imports/Themes.js
+++ packages/theme-base/src/json-imports/Themes.js
@@ -3,6 +3,7 @@ import { registerThemeProperties } from "@ui5/webcomponents-base/dist/asset-regi
 import fiori3Dark from "../generated/assets/themes/sap_fiori_3_dark/parameters-bundle.css.json";
 import belize from "../generated/assets/themes/sap_belize/parameters-bundle.css.json";
 import belizeHcb from "../generated/assets/themes/sap_belize_hcb/parameters-bundle.css.json";
+import belizeHcw from "../generated/assets/themes/sap_belize_hcw/parameters-bundle.css.json";
 
 const isInlined = obj => typeof (obj) === "object";
 
@@ -17,3 +18,4 @@ Suggested pattern: "assets\\\/.*\\\.json"`);
 registerThemeProperties("@ui5/webcomponents-theme-base", "sap_fiori_3_dark", fiori3Dark);
 registerThemeProperties("@ui5/webcomponents-theme-base", "sap_belize", belize);
 registerThemeProperties("@ui5/webcomponents-theme-base", "sap_belize_hcb", belizeHcb);
+registerThemeProperties("@ui5/webcomponents-theme-base", "sap_belize_hcw", belizeHcw);
