28ed69a9b3953825d852567e0115db3193cdbe16
Martin <martin.r.hristov@gmail.com>
2020-02-14 15:58:23 +0200

    feat: introduce belize_hcw theme (#1215)

--- packages/theme-base/package-scripts.js
+++ packages/theme-base/package-scripts.js
@@ -5,9 +5,10 @@ module.exports = {
 			default: "nps clean build.src build.less build.postcss",
 			src: `copy-and-watch "src/**/*.js" dist/`,
 			less: {
-				default: "nps build.less.belize build.less.belize_hcb build.less.fiori_3 build.less.fiori_3_dark",
+				default: "nps build.less.belize build.less.belize_hcb build.less.belize_hcw build.less.fiori_3 build.less.fiori_3_dark",
 				belize: "lessc src/themes/sap_belize/parameters-bundle.less dist/themes/sap_belize/parameters-bundle.css",
 				belize_hcb: "lessc src/themes/sap_belize_hcb/parameters-bundle.less dist/themes/sap_belize_hcb/parameters-bundle.css",
+				belize_hcw: "lessc src/themes/sap_belize_hcw/parameters-bundle.less dist/themes/sap_belize_hcw/parameters-bundle.css",
 				fiori_3: "lessc src/themes/sap_fiori_3/parameters-bundle.less dist/themes/sap_fiori_3/parameters-bundle.css",
 				fiori_3_dark: "lessc src/themes/sap_fiori_3_dark/parameters-bundle.less dist/themes/sap_fiori_3_dark/parameters-bundle.css",
 			},
