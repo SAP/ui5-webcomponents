9dab18b152dd4afacc2b3d3bc6dc8afa18d05881
Filip Siderov <filip.siderov@sap.com>
2020-02-18 19:03:39 +0200

    feat(ui5-shellbar): add profile slot (#1222)
    Introduce profile slot, instead of property profile to give more freedom to users, enabling usage of both images and icons for a profile.
    Fixes: https://github.com/SAP/ui5-webcomponents/issues/1099
    
    BREAKING CHANGE: profile property is removed in favour of profile slot

--- packages/fiori/test/specs/ShellBar.spec.js
+++ packages/fiori/test/specs/ShellBar.spec.js
@@ -223,7 +223,7 @@ describe("Component Behavior", () => {
 			});
 
 			it("tests profilePress event", () => {
-				const profileIcon = browser.$("#shellbar").shadow$(".ui5-shellbar-image-button");
+				const profileIcon = browser.$("#shellbar").$("ui5-avatar");
 				const input = browser.$("#press-input");
 
 				profileIcon.click();
@@ -325,7 +325,7 @@ describe("Component Behavior", () => {
 			});
 
 			it("tests profilePress event", () => {
-				const profileIcon = browser.$("#shellbar").shadow$(".ui5-shellbar-image-button");
+				const profileIcon = browser.$("#shellbar").$("ui5-avatar");
 				const input = browser.$("#press-input");
 
 				profileIcon.click();
