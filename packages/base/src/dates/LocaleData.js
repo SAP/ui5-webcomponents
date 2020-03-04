f9150acc406925fa08750c3aa5ab9787ca2820ff
Vladislav Tasev <vladislav.tasev@sap.com>
2020-03-04 13:33:23 +0200

    override LocaleData.prototype.getFirstDayOfWeek

--- packages/base/src/dates/LocaleData.js
+++ packages/base/src/dates/LocaleData.js
@@ -1,4 +1,16 @@
 import LocaleData from "@ui5/webcomponents-utils/dist/sap/ui/core/LocaleData.js";
 import "../shims/Core-shim.js";
+import { getFirstDayOfWeek } from "../config/FormatSettings.js";
+
+// Override getFirstDayOfWeek to favor global configuration over Locale
+const original = LocaleData.prototype.getFirstDayOfWeek;
+LocaleData.prototype.getFirstDayOfWeek = function () {
+	const configuredValue = getFirstDayOfWeek();
+	if (configuredValue !== undefined) {
+		return configuredValue;
+	}
+
+	return original.call(this);
+};
 
 export default LocaleData;
