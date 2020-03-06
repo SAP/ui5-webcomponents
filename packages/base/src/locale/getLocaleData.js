9222cbe39f014a14bc3e7e0566ecb4ef3ad1237d
Vladislav Tasev <vladislav.tasev@sap.com>
2020-03-06 15:37:07 +0200

    add doc

--- packages/base/src/locale/getLocaleData.js
+++ packages/base/src/locale/getLocaleData.js
@@ -4,6 +4,14 @@ import { fetchCldr } from "../asset-registries/LocaleData.js";
 
 const instances = new Map();
 
+/**
+ * Fetches and returns а LocaleData object for the required locale
+ * For more information on this object's API, please see:
+ * https://ui5.sap.com/#/api/sap.ui.core.LocaleData
+ *
+ * @param lang - if left empty, will use the configured/current locale
+ * @returns {LocaleData}
+ */
 const getLocaleData = async lang => {
 	const locale = getLocale(lang);
 	const localeLang = locale.getLanguage();
