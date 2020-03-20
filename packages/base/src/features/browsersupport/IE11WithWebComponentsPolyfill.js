0c1e6764610f3a685ec225312344ddf07ddc19fa
Vladislav Tasev <vladitasev@users.noreply.github.com>
2020-03-20 14:14:57 +0200

    feature(framework): Provide Web Components Polyfill as ES6 import (#1322)

---
+++ packages/base/src/features/browsersupport/IE11WithWebComponentsPolyfill.js
@@ -0,0 +1,5 @@
+// All web components polyfills (including platform) with "window" passed as argument to the iife
+import "../../thirdparty/webcomponents-sd-ce-pf.js";
+
+// All IE11 polyfills, needed by the project itself
+import "./IE11.js";
