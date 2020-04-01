59a8d499e327a9f48376aa9e26668eca84973699
ilhan <ilhan.myumyun@sap.com>
2020-04-01 17:04:45 +0300

    strip classes and sapFontUrl* vars

--- packages/tools/lib/postcss-css-to-esm/index.js
+++ packages/tools/lib/postcss-css-to-esm/index.js
@@ -17,19 +17,20 @@ registerThemeProperties("${packageName}", "${DEFAULT_THEME}", defaultTheme);
 `;
 };
 
+const proccessCSS = css => {
+	css = css.replace(/\.sapThemeMeta[\s\S]*?:root/, ":root");
+	css = css.replace(/\.background-image.*{.*}/, "");
+	css = css.replace(/\.sapContrast[ ]*:root[\s\S]*?}/, "");
+	css = css.replace(/--sapFontUrl.*\);?/, "");
+	return JSON.stringify(css);
+}
+
 module.exports = postcss.plugin('add css to esm transform plugin', function (opts) {
 	opts = opts || {};
 
 	return function (root) {
 		let css = root.toString();
-		
-		// const r = new RegExp(/[\s\S]*(:root{[\s\S]*})/, 'g');
-		// const match = r.exec(css);
-		// if (match) {
-		// 	css = match[1];
-		// }
-
-		css = JSON.stringify(css);
+		css = proccessCSS(css);
 
 		const targetFile = root.source.input.from.replace(`/${opts.toReplace}/`, "/dist/generated/").replace(`\\${opts.toReplace}\\`, "\\dist\\generated\\");
 		mkdirp.sync(path.dirname(targetFile));
--- packages/tools/lib/postcss-css-to-json/index.js
+++ packages/tools/lib/postcss-css-to-json/index.js
@@ -3,25 +3,25 @@ const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 
+const proccessCSS = css => {
+	css = css.replace(/\.sapThemeMeta[\s\S]*?:root/, ":root");
+	css = css.replace(/\.background-image.*{.*}/, "");
+	css = css.replace(/\.sapContrast[ ]*:root[\s\S]*?}/, "");
+	css = css.replace(/--sapFontUrl.*\);?/, "");
+	return css;
+}
+
 module.exports = postcss.plugin('add css to JSON transform plugin', function (opts) {
 	opts = opts || {};
 
 	return function (root) {
 		let css = root.toString();
-
-		// const r = new RegExp(/[\s\S]*(:root{[\s\S]*})/, 'g');
-		// const match = r.exec(css);
-
-		// if (match) {
-		// 	console.log(match[1])
-		// 	css = match[1];
-		// }
+		css = proccessCSS(css);
 
 		const targetFile = root.source.input.from.replace(`/${opts.toReplace}/`, "/dist/generated/assets/").replace(`\\${opts.toReplace}\\`, "\\dist\\generated\\assets\\");
 		mkdirp.sync(path.dirname(targetFile));
 
 		const filePath = `${targetFile}.json`;
 		fs.writeFileSync(filePath, JSON.stringify({_: css}));
-
 	}
 });
