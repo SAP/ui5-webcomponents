54038e40b6be56e5e260df078cc1d11cbe0d03aa
Petar Dimov <32839090+dimovpetar@users.noreply.github.com>
2020-04-03 17:53:26 +0300

    feat(ui5-upload-collection): implement new webcomponent (#1316)
    Background
    ui5-upload-collection is new component located in fiori package,
    which benefits and brings some of ui5-list features, but it shows
    different items of type ui5-upload-collection-item.
    
    It is in the responsibility of the app developer to put ui5-file-uploader
    in the header, create and add new ui5-upload-collection-item(s) to the collection.
    
    Main features:
    - header slot
    - items slot (default)
    - items have special visualisation, suitable for files representation
    - items have "file" property, to allow app developers to set and get file object
    - drag and drop overlay

--- packages/fiori/bundle.esm.js
+++ packages/fiori/bundle.esm.js
@@ -11,3 +11,5 @@ import ProductSwitch from "./dist/ProductSwitch.js";
 import ProductSwitchItem from "./dist/ProductSwitchItem.js";
 import ShellBar from "./dist/ShellBar.js";
 import ShellBarItem from "./dist/ShellBarItem.js";
+import UploadCollection from "./dist/UploadCollection.js";
+import UploadCollectionItem from "./dist/UploadCollectionItem.js";
