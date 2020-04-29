2733e55a19e75a1a51a7554e39b8c14db3e41514
Ivaylo Plashkov <ivaylo.plashkov@sap.com>
2020-04-29 09:51:19 +0300

    fix(ui5-combobox): translate accessibleName

--- packages/main/src/ComboBox.js
+++ packages/main/src/ComboBox.js
@@ -479,6 +479,13 @@ class ComboBox extends UI5Element {
 		return this.i18nBundle.getText(INPUT_SUGGESTIONS_TITLE);
 	}
 
+<<<<<<< Updated upstream
+=======
+	get _iconAccessibleNameText() {
+		return this.i18nBundle.getText(ICON_ACCESSIBLE_NAME);
+	}
+
+>>>>>>> Stashed changes
 	get inner() {
 		return isPhone() ? this.responsivePopover.querySelector(".ui5-input-inner-phone") : this.shadowRoot.querySelector("[inner-input]");
 	}
