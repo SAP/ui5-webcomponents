031b6ca82175de645d60827091b4b89162c829e4
Filip Siderov <filip.siderov@sap.com>
2020-05-14 13:00:20 +0300

    fix(ui5-busyindicator): prevent keyboard events propagation to controls (#1607)

---
+++ packages/main/test/specs/BusyIndicator.spec.js
@@ -0,0 +1,18 @@
+const assert = require("chai").assert;
+
+
+describe("BusyIndicator general interaction", () => {
+	browser.url("http://localhost:8080/test-resources/pages/BusyIndicator.html");
+
+	it("tests event propagation", () => {
+		const busyIndicator = browser.$("#busy-tree");
+		const dynamicItem = busyIndicator.$("ui5-tree").shadow$("ui5-list").$$("ui5-li-tree")[2].shadow$(".ui5-li-tree-toggle-box");
+		const input = browser.$("#tree-input");
+
+		dynamicItem.click();
+		dynamicItem.keys("Space");
+
+		assert.strictEqual(input.getProperty("value"), "0", "itemClick is not thrown");
+	});
+
+});
