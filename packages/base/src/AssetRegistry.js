71ac6eeb6a690bc60f96cb8eab4c852fdef6ed1b
Vladislav Tasev <vladislav.tasev@sap.com>
2020-05-26 16:33:12 +0300

    feat(framework): CLDR location can now be specified

--- packages/base/src/AssetRegistry.js
+++ packages/base/src/AssetRegistry.js
@@ -1,9 +1,10 @@
 import { registerI18nBundle } from "./asset-registries/i18n.js";
-import { registerCldr } from "./asset-registries/LocaleData.js";
+import { registerCldr, _registerMappingFunction as registerCldrMappingFunction } from "./asset-registries/LocaleData.js";
 import { registerThemeProperties } from "./asset-registries/Themes.js";
 
 export {
 	registerCldr,
+	registerCldrMappingFunction,
 	registerThemeProperties,
 	registerI18nBundle,
 };
