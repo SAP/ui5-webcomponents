cec537c55040aab212af2cdd69be2555a0bbb51f
Filip Siderov <filip.siderov@sap.com>
2020-06-09 18:24:13 +0300

    fix item navigation paging behavior

--- packages/base/src/delegate/ItemNavigation.js
+++ packages/base/src/delegate/ItemNavigation.js
@@ -27,6 +27,8 @@ class ItemNavigation extends EventProvider {
 		this.horizontalNavigationOn = autoNavigation || navigationMode === NavigationMode.Horizontal;
 		this.verticalNavigationOn = autoNavigation || navigationMode === NavigationMode.Vertical;
 
+		this.pageSize = options.pageSize;
+
 		this.rootWebComponent = rootWebComponent;
 		this.rootWebComponent.addEventListener("keydown", this.onkeydown.bind(this));
 		this.rootWebComponent._onComponentStateFinalized = () => {
@@ -254,18 +256,17 @@ class ItemNavigation extends EventProvider {
 		if (!this.hasNextPage) {
 			this.currentIndex = items.length - 1;
 		} else {
-			this.currentIndex = 0;
+			this.currentIndex -= this.pageSize;
 		}
 	}
 
 	_handlePrevPage() {
 		this.fireEvent(ItemNavigation.PAGE_TOP);
-		const items = this._getItems();
 
 		if (!this.hasPrevPage) {
 			this.currentIndex = 0;
 		} else {
-			this.currentIndex = (this.pageSize || items.length) - 1;
+			this.currentIndex = this.pageSize + this.currentIndex;
 		}
 	}
 }
