9ac03e382ac98ea853946e781c83852177b8f4c1
ilhan <ilhan.myumyun@sap.com>
2020-06-23 13:54:11 +0300

    feat(ui5-input): add suggestion-scroll event

--- packages/main/src/Popup.js
+++ packages/main/src/Popup.js
@@ -233,6 +233,13 @@ class Popup extends UI5Element {
 		document.body.style.top = "";
 	}
 
+	_scroll(e) {
+		this.fireEvent("scroll", {
+			scrollTop: e.target.scrollTop,
+			targetRef: e.target,
+		});
+	}
+
 	/**
 	 * Focus trapping
 	 * @private
