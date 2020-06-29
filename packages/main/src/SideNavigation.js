c70657097ba8b5e594840811f3c38c0b21e8d883
Filip Siderov <filip.siderov@sap.com>
2020-06-29 18:26:46 +0300

    implement kbh & collapse/expand behaviour

--- packages/main/src/SideNavigation.js
+++ packages/main/src/SideNavigation.js
@@ -1,6 +1,8 @@
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+import ResponsivePopover from "./ResponsivePopover.js";
 import SideNavigationTemplate from "./generated/templates/SideNavigationTemplate.lit.js";
+import SideNavigationItemPopoverContentTemplate from "./generated/templates/SideNavigationItemPopoverContentTemplate.lit.js";
 
 // Styles
 import SideNavigationCss from "./generated/themes/SideNavigation.css.js";
@@ -12,20 +14,50 @@ const metadata = {
 	tag: "ui5-side-navigation",
 	managedSlots: true,
 	properties: /** @lends sap.ui.webcomponents.main.SideNavigation.prototype */ {
-		//
+		/**
+		 * Defines whether the <code>ui5-side-navigation</code> is expanded or collapsed.
+		 *
+		 * @public
+		 * @type {Boolean}
+		 * @defaultvalue false
+		 */
+		collapsed: {
+			type: Boolean,
+		},
 	},
 	slots: /** @lends sap.ui.webcomponents.main.SideNavigation.prototype */ {
+		/**
+		 * Defines the items in the <code>ui5-side-navigation</code>.
+		 *
+		 * @public
+		 */
 		"default": {
 			propertyName: "items",
 			type: HTMLElement,
 		},
 
-		bottomItems: {
+		/**
+		 * Defines the fixed items in the bottom of the <code>ui5-side-navigation</code>.
+		 *
+		 * @public
+		 */
+		fixedItems: {
 			type: HTMLElement,
 		},
 	},
 	events: /** @lends sap.ui.webcomponents.main.SideNavigation.prototype */ {
-		//
+		/**
+		 * Fired when the selection has changed via user interaction
+		 *
+		 * @event sap.ui.webcomponents.main.SideNavigation#selection-change
+		 * @param {HTMLElement} item the clicked item.
+		 * @public
+		 */
+		"selection-change": {
+			item: {
+				type: HTMLElement,
+			},
+		},
 	},
 };
 
@@ -66,8 +98,109 @@ class SideNavigation extends UI5Element {
 		return SideNavigationTemplate;
 	}
 
-	static async onDefine() {
+	static get staticAreaTemplate() {
+		return SideNavigationItemPopoverContentTemplate;
+	}
 
+	static async onDefine() {
+		await Promise.all([
+			ResponsivePopover.define(),
+		]);
+	}
+
+	onBeforeRendering() {
+		this.items.forEach(item => {
+			item._collapsed = this.collapsed;
+
+			return item;
+		});
+
+		this.fixedItems.forEach(item => {
+			item._collapsed = this.collapsed;
+
+			return item;
+		});
+	}
+
+	handleItemClick(event) {
+		const currentItems = this.fixedItems.concat(this.items);
+
+		currentItems.map(item => {
+			item.selected = item === event.target;
+
+			return item;
+		});
+
+		this.fireEvent("selectionChange", {
+			selectedItem: event.target,
+		});
+
+		if (this.collapsed) {
+			this.openPicker(event.target);
+		}
+	}
+
+	getMiddleFocusHelper() {
+		return this.getDomRef().querySelector(".ui5-sn-middle-focus-helper");
+	}
+
+	_focusOtherList() {
+		this.getMiddleFocusHelper().focus();
+	}
+
+	focusNext(event) {
+		const eventTarget = event.target;
+		const isFixedItems = eventTarget.slot === "fixedItems";
+		let currentItems = Array.from(this.querySelectorAll(`ui5-side-navigation-item${isFixedItems ? "[slot='fixedItems']" : ":not([slot])"}`));
+
+		if (eventTarget.expandable && eventTarget.expanded) {
+			eventTarget.items[0].focus();
+		} else if (currentItems.indexOf(eventTarget) > -1 && currentItems.indexOf(eventTarget) < currentItems.length - 1) {
+			if (currentItems[currentItems.indexOf(eventTarget) + 1].getClientRects().length === 0) {
+				currentItems = isFixedItems ? this.fixedItems : this.items;
+			}
+			const nextItem = currentItems[currentItems.indexOf(eventTarget) + 1];
+
+			if (nextItem) {
+				nextItem.focus();
+			}
+		}
+	}
+
+	focusPrevious(event) {
+		const eventTarget = event.target;
+		const isFixedItems = eventTarget.slot === "fixedItems";
+		let currentItems = Array.from(this.querySelectorAll(`ui5-side-navigation-item${isFixedItems ? "[slot='fixedItems']" : ":not([slot])"}`));
+
+		if (eventTarget.expandable && eventTarget.expanded) {
+			const _prevItem = currentItems[currentItems.indexOf(eventTarget) - 1];
+
+			if (_prevItem) {
+				_prevItem.focus();
+			}
+		} else if (currentItems.indexOf(eventTarget) > 0) {
+			if (currentItems[currentItems.indexOf(eventTarget) - 1].getClientRects().length === 0) {
+				currentItems = isFixedItems ? this.fixedItems : this.items;
+			}
+
+			currentItems[currentItems.indexOf(eventTarget) - 1].focus();
+		}
+	}
+
+	async getPicker() {
+		return (await this.getStaticAreaItemDomRef()).querySelector("ui5-responsive-popover");
+	}
+
+	async openPicker(opener) {
+		const responsivePopover = await this.getPicker();
+
+		responsivePopover.open(opener);
+	}
+
+	async closePicker() {
+		const responsivePopover = await this.getPicker();
+
+		responsivePopover.close();
 	}
 }
 
--- packages/main/src/SideNavigationItem.js
+++ packages/main/src/SideNavigationItem.js
@@ -1,5 +1,17 @@
-import ListItem from "./ListItem.js";
 import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
+import {
+	isTabNext,
+	isTabPrevious,
+	isDown,
+	isLeft,
+	isUp,
+	isRight,
+} from "@ui5/webcomponents-base/dist/Keys.js";
+import Title from "./Title.js";
+import Icon from "./Icon.js";
+import "@ui5/webcomponents-icons/dist/icons/navigation-down-arrow.js";
+import "@ui5/webcomponents-icons/dist/icons/navigation-right-arrow.js";
 import SideNavigationItemTemplate from "./generated/templates/SideNavigationItemTemplate.lit.js";
 
 // Styles
@@ -10,6 +22,7 @@ import SideNavigationItemCss from "./generated/themes/SideNavigationItem.css.js"
  */
 const metadata = {
 	tag: "ui5-side-navigation-item",
+	managedSlots: true,
 	properties: /** @lends sap.ui.webcomponents.main.SideNavigationItem.prototype */ {
 		text: {
 			type: String,
@@ -26,6 +39,34 @@ const metadata = {
 		expanded: {
 			type: Boolean,
 		},
+
+		/**
+		 * Defines the current active item.
+		 */
+		selected: {
+			type: Boolean,
+		},
+
+		/**
+		 * @private
+		 */
+		_mouseDown: {
+			type: Boolean,
+		},
+
+		/**
+		 * @private
+		 */
+		_focused: {
+			type: Boolean,
+		},
+
+		/**
+		 * @private
+		 */
+		_collapsed: {
+			type: Boolean,
+		},
 	},
 	slots: /** @lends sap.ui.webcomponents.main.SideNavigationItem.prototype */ {
 		"default": {
@@ -34,7 +75,12 @@ const metadata = {
 		},
 	},
 	events: /** @lends sap.ui.webcomponents.main.SideNavigationItem.prototype */ {
-		//
+		/**
+		 * @private
+		 */
+		_click: {
+
+		},
 	},
 };
 
@@ -54,11 +100,12 @@ const metadata = {
  * @constructor
  * @author SAP SE
  * @alias sap.ui.webcomponents.main.SideNavigationItem
- * @extends ListItem
+ * @extends UI5Element
  * @tagname ui5-side-navigation-item
  * @public
+ * @since 1.0.0-rc.8
  */
-class SideNavigationItem extends ListItem {
+class SideNavigationItem extends UI5Element {
 	static get metadata() {
 		return metadata;
 	}
@@ -76,32 +123,76 @@ class SideNavigationItem extends ListItem {
 	}
 
 	static async onDefine() {
+		await Promise.all([
+			Icon.define(),
+			Title.define(),
+		]);
+	}
 
+	_onkeydown(event) {
+		if (isTabNext(event)) {
+			this.fireEvent("_forward-focus");
+		}
+
+		if (isTabPrevious(event)) {
+			this.fireEvent("_return_focus");
+		}
+
+		if (isDown(event) || isRight(event)) {
+			event.preventDefault();
+			this.fireEvent("_focus_next");
+		}
+
+		if (isUp(event) || isLeft(event)) {
+			event.preventDefault();
+			this.fireEvent("_focus_previous");
+		}
+	}
+
+	_onmousedown(event) {
+		this._mouseDown = true;
+	}
+
+	_onmouseup(event) {
+		this._mouseDown = false;
+		this.fireEvent("_click");
+	}
+
+	_onfocusin(event) {
+		this._focused = true;
+	}
+
+	_onfocusout(event) {
+		this._focused = false;
 	}
 
 	toggleExpanded() {
 		this.expanded = !this.expanded;
 	}
 
-	get aclas() {
+	get classessni() {
 		return {
-			content: {
+			root: {
 				"ui5-sni-content": true,
 				"ui5-sni-padding": !this.icon,
 			},
 			children: {
 				"ui5-sni-children": true,
 				"ui5-sni-children-shown": this.expanded,
-			}
+			},
 		};
 	}
 
-	get getActiveIcon() {
+	get activeIcon() {
 		if (!this.expandable) {
 			return undefined;
 		}
 
 		return this.expanded ? "navigation-down-arrow" : "navigation-right-arrow";
+	}
+
+	get hasOverflow() {
+		return this._collapsed && this.icon && this.items.length > 0;
 	}
 }
 
