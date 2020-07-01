89ee377096c4a18035a1a6da38c9b7088dc9247f
Naydenov <nayden.naydenov@sap.com>
2020-07-01 17:51:06 +0300

    fixed review comments

--- packages/main/test/specs/ProgressIndicator.spec.js
+++ packages/main/test/specs/ProgressIndicator.spec.js
@@ -9,13 +9,13 @@ describe("Properties", () => {
 		const validButton = $("#thirdBtn");
 		const largerButton = $("#seventhBtn");
 
-        validButton.click()
-        assert.strictEqual(progressIndicator.validateValue, 50, "Value visual representation is validate correctly.");
+        validButton.click();
+        assert.strictEqual(progressIndicator.validatedValue, 50, "Value visual representation is validate correctly.");
 
-        negativeButton.click()
-		assert.strictEqual(progressIndicator.validateValue, 0, "Value visual representation is limited to 0 and it validated correctly.");
+        negativeButton.click();
+		assert.strictEqual(progressIndicator.validatedValue, 0, "Value visual representation is limited to 0 and it validated correctly.");
 
-        largerButton.click()
-		assert.strictEqual(progressIndicator.validateValue, 100, "Value visual representation is limited to 100 and it validated correctly.");
+        largerButton.click();
+		assert.strictEqual(progressIndicator.validatedValue, 100, "Value visual representation is limited to 100 and it validated correctly.");
 	});
 });
