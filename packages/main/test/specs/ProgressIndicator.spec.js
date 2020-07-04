1a90bfb2a8626b4433c9471908f723ef92b3d498
Naydenov <nayden.naydenov@sap.com>
2020-07-05 00:26:03 +0300

    validated test

--- packages/main/test/specs/ProgressIndicator.spec.js
+++ packages/main/test/specs/ProgressIndicator.spec.js
@@ -1,21 +1,27 @@
 const assert = require("chai").assert;
 
-describe("Properties", () => {
+const getValidatedValue = (pi) => {
+	return browser.execute((pi) => {
+		return pi.validatedValue;
+	}, pi);
+};
+
+describe("API", () => {
 	browser.url("http://localhost:8080/test-resources/pages/ProgressIndicator.html");
 
-	it("Value validation", () => {
+	it("tests value validation", () => {
 		const progressIndicator = $("#test-progress-indicator");
 		const negativeButton = $("#sixthBtn");
 		const validButton = $("#thirdBtn");
 		const largerButton = $("#seventhBtn");
 
-        validButton.click();
-        assert.strictEqual(progressIndicator.validatedValue, 50, "Value visual representation is validate correctly.");
+		validButton.click();
+		assert.strictEqual(getValidatedValue(progressIndicator), 50, "The value is validated correctly.");
 
-        negativeButton.click();
-		assert.strictEqual(progressIndicator.validatedValue, 0, "Value visual representation is limited to 0 and it validated correctly.");
+		negativeButton.click();
+		assert.strictEqual(getValidatedValue(progressIndicator), 0, "The value is limited to 0 and it is validated correctly.");
 
-        largerButton.click();
-		assert.strictEqual(progressIndicator.validatedValue, 100, "Value visual representation is limited to 100 and it validated correctly.");
+		largerButton.click();
+		assert.strictEqual(getValidatedValue(progressIndicator), 100, "The value is limited to 100 and it is validated correctly.");
 	});
 });
