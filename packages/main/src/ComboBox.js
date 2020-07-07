900ee2732b3c75dcfa897896143a39feaf8523d1
Vladislav Tasev <vladislav.tasev@sap.com>
2020-07-07 18:06:06 +0300

    fix(ui5-combobox): support aria-label/arial-labelledby and fix aria-expanded

--- packages/main/src/ComboBox.js
+++ packages/main/src/ComboBox.js
@@ -2,6 +2,7 @@ import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
 import ValueState from "@ui5/webcomponents-base/dist/types/ValueState.js";
 import { isPhone } from "@ui5/webcomponents-base/dist/Device.js";
+import getEffectiveAriaLabelText from "@ui5/webcomponents-base/dist/util/getEffectiveAriaLabelText.js";
 import "@ui5/webcomponents-icons/dist/icons/slim-arrow-down.js";
 import "@ui5/webcomponents-icons/dist/icons/decline.js";
 import { getI18nBundle } from "@ui5/webcomponents-base/dist/i18nBundle.js";
@@ -171,6 +172,30 @@ const metadata = {
 		 */
 		focused: {
 			type: Boolean,
+		},
+
+		/**
+		 * Defines the aria-label attribute for the combo box
+		 * @type {String}
+		 * @defaultvalue: ""
+		 * @private
+		 * @since 1.0.0-rc.8
+		 */
+		ariaLabel: {
+			type: String,
+			defaultValue: undefined,
+		},
+
+		/**
+		 * Receives id(or many ids) of the elements that label the combo box
+		 * @type {String}
+		 * @defaultvalue ""
+		 * @private
+		 * @since 1.0.0-rc.8
+		 */
+		ariaLabelledby: {
+			type: String,
+			defaultValue: "",
 		},
 
 		_iconPressed: {
@@ -517,6 +542,14 @@ class ComboBox extends UI5Element {
 			"Error": this.i18nBundle.getText(VALUE_STATE_ERROR),
 			"Warning": this.i18nBundle.getText(VALUE_STATE_WARNING),
 		};
+	}
+
+	get open() {
+		return this.responsivePopover ? this.responsivePopover.opened : false;
+	}
+
+	get ariaLabelText() {
+		return getEffectiveAriaLabelText(this);
 	}
 
 	static async onDefine() {
