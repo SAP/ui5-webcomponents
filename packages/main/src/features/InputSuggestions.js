1bb1087ea63666d5c63006f3c721275814dd85ca
Vladislav Tasev <vladislav.tasev@sap.com>
2020-07-21 09:43:23 +0300

    fix(ui5-input): Suggestions can now be arbitrary list items

--- packages/main/src/features/InputSuggestions.js
+++ packages/main/src/features/InputSuggestions.js
@@ -318,7 +318,7 @@ class Suggestions {
 	}
 
 	_getItems() {
-		return [].slice.call(this.responsivePopover.querySelectorAll("ui5-li-groupheader, ui5-li-suggestion-item"));
+		return [...this.responsivePopover.querySelectorAll("ui5-list>*")];
 	}
 
 	_getComponent() {
