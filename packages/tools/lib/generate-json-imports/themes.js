07aebfaf318f34fec2f934041c577b1ee8093f24
Vladislav Tasev <vladitasev@users.noreply.github.com>
2020-07-27 14:41:54 +0300

    fix(tools): Building third-party packages no longer fails (#1994)

--- packages/tools/lib/generate-json-imports/themes.js
+++ packages/tools/lib/generate-json-imports/themes.js
@@ -3,15 +3,24 @@ const path = require('path');
 const mkdirp = require("mkdirp");
 const assets = require("../../assets-meta.js");
 
-const outputFile = path.normalize(`${process.argv[2]}/Themes.js`);
+const inputFolder = path.normalize(process.argv[2]);
+const outputFile = path.normalize(`${process.argv[3]}/Themes.js`);
 
+// All supported optional themes
 const optionalThemes = assets.themes.all.filter(theme => theme !== assets.themes.default);
+
+// All themes present in the file system
+const dirs = fs.readdirSync(inputFolder);
+const themesOnFileSystem = dirs.map(dir => {
+	const matches = dir.match(/sap_.*$/);
+	return matches ? dir : undefined;
+}).filter(key => !!key && optionalThemes.includes(key));
 
 const packageName = JSON.parse(fs.readFileSync("package.json")).name;
 
-const importLines = optionalThemes.map(theme => `import ${theme} from "../assets/themes/${theme}/parameters-bundle.css.json";`).join("\n");
-const isInlinedCondition = optionalThemes.map(theme => `isInlined(${theme})`).join(" || ");
-const registerLines = optionalThemes.map(theme => `registerThemeProperties("${packageName}", "${theme}", ${theme});`).join("\n");
+const importLines = themesOnFileSystem.map(theme => `import ${theme} from "../assets/themes/${theme}/parameters-bundle.css.json";`).join("\n");
+const isInlinedCondition = themesOnFileSystem.map(theme => `isInlined(${theme})`).join(" || ");
+const registerLines = themesOnFileSystem.map(theme => `registerThemeProperties("${packageName}", "${theme}", ${theme});`).join("\n");
 
 // Resulting file content
 const content = `import { registerThemeProperties } from "@ui5/webcomponents-base/dist/asset-registries/Themes.js";
@@ -26,7 +35,7 @@ See rollup-plugin-url or webpack file-loader for more information.
 Suggested pattern: "assets\\\\\\/.*\\\\\\.json"\`);
 }
 
-${registerLines};
+${registerLines}
 `;
 
 mkdirp.sync(path.dirname(outputFile));
