8ba1b2ec0c30669a7dcde1a68a625b002d28a240
ilhan <ilhan.myumyun@sap.com>
2020-07-31 10:31:28 +0300

    fix(ui5-tabcontainer): Fix ARIA posinset and setsize values

--- packages/main/src/TabContainer.js
+++ packages/main/src/TabContainer.js
@@ -266,11 +266,11 @@ class TabContainer extends UI5Element {
 
 	onBeforeRendering() {
 		// Set external properties to items
-		this.items.forEach((item, index) => {
+		this.items.filter(item => !item.isSeparator).forEach((item, index, arr) => {
 			item._isInline = this.tabLayout === TabLayout.Inline;
 			item._mixedMode = this.mixedMode;
 			item._posinset = index + 1;
-			item._setsize = this.items.length;
+			item._setsize = arr.length;
 			item._getTabContainerHeaderItemCallback = _ => {
 				return this.getDomRef().querySelector(`#${item._id}`);
 			};
