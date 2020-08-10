76321ec9f0bbd3e52387dd6fc32c163819ddf3b6
Vladislav Tasev <vladislav.tasev@sap.com>
2020-08-10 16:23:47 +0300

    scan src/

--- packages/tools/lib/scoping/get-all-components.js
+++ packages/tools/lib/scoping/get-all-components.js
@@ -1,5 +1,26 @@
 const fs = require("fs");
 const path = require("path");
+const glob = require("glob");
+
+const getTag = file => {
+	const fileContent = String(fs.readFileSync(file)).replace(/\n/g, "");
+	const matches = fileContent.match(/\btag\b:\s*\"(.*?)\"/);
+	return matches ? matches[1] : undefined;
+};
+
+const getAltTag = file => {
+	const fileContent = String(fs.readFileSync(file)).replace(/\n/g, "");
+	const matches = fileContent.match(/\baltTag\b:\s*\"(.*?)\"/);
+	return matches ? matches[1] : undefined;
+};
+
+const getComponents = (srcDir) => {
+	return glob.sync(path.join(srcDir, "/**/*.js")).map(file => {
+		const tag = getTag(file);
+		const altTag = getAltTag(file);
+		return tag ? {file, tag, altTag} : undefined;
+	}).filter(item => !!item);
+};
 
 /**
  * Returns an array with the metadata files for all dependent packages that ship UI5 Web Components
@@ -9,11 +30,14 @@ const path = require("path");
  */
 const getDepComponentPackages = packageFile => {
 	const packageFileContent = JSON.parse(fs.readFileSync(packageFile));
+	if (!packageFileContent.ui5WebComponentsPackage) {
+		return [];
+	}
 
-	return Object.keys(packageFileContent.dependencies).map(dep => {
+	return Object.keys(packageFileContent.dependencies || []).map(dep => {
 		let file;
 		try {
-			file = require.resolve(path.join(dep, "ui5webcomponents.js"))
+			file = require.resolve(path.join(dep, "package.json"));
 		} catch (e) {
 			file = undefined;
 		}
@@ -24,17 +48,16 @@ const getDepComponentPackages = packageFile => {
 /**
  * Get the list of all components for the purpose of scoping
  *  - include components from dependent packages
- *  - sort descending
  *
- * @param metadataFile
- * @returns {string[]}
+ * @param packageDir
+ * @returns {*}
  */
-const getAllComponents = (metadataFile) => {
-	metadataFile = metadataFile || path.join(process.cwd(), "ui5webcomponents.js");
-	const metadata = require(metadataFile);
+const getAllComponents = (packageDir) => {
+	packageDir = packageDir ? path.dirname(packageDir) : process.cwd();
 
-	const packageFile = path.join(path.dirname(metadataFile), "package.json");
-	return metadata.components.concat(getDepComponentPackages(packageFile).flatMap(getAllComponents)).sort().reverse();
+	const srcDir = path.join(packageDir, "src/");
+	const packageFile = path.join(packageDir, "package.json");
+	return getComponents(srcDir).concat(getDepComponentPackages(packageFile).flatMap(getAllComponents));
 };
 
 module.exports = getAllComponents;
--- packages/tools/lib/scoping/index.js
+++ packages/tools/lib/scoping/index.js
@@ -1,39 +1,42 @@
 const fs = require("fs");
 const path = require("path");
-const mkdirp = require("mkdirp");
 const glob = require("glob");
 const getAllComponents = require("./get-all-components.js");
 
 const root = process.argv[2];
 
 const components = getAllComponents();
-console.log(components);
-process.exit();
 
-const processSourceFile = file => {
-	console.log("Processing source file", file);
+const processSourceFile = (file, component) => {
+	console.log("Processing source file", file, component);
 };
 
-const processTemplate = file => {
-	console.log("Processing template", file);
+const processTemplate = (file, component) => {
+	console.log("Processing template", file, component);
 };
 
-const processCSS = file => {
-	console.log("Processing CSS", file);
+const processCSS = (file, component) => {
+	console.log("Processing CSS", file, component);
 };
 
 // Replace imports for components in other packages, replace tags
 glob.sync(path.join(root, "/*.js")).forEach(file => {
-	processSourceFile(file);
+	components.forEach(component => {
+		processSourceFile(file, component);
+	});
 });
 
 // Replace tags in templates
 glob.sync(path.join(root, "/generated/templates/**/*.js")).forEach(file => {
-	processTemplate(file);
+	components.forEach(component => {
+		processTemplate(file, component);
+	});
 });
 
 // Replace tags in CSS
 glob.sync(path.join(root, "/generated/themes/*.css.js")).forEach(file => {
-	processCSS(file);
+	components.forEach(component => {
+		processCSS(file, component);
+	});
 });
 
