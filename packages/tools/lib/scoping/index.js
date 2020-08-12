d5db6e9cf8a2b2741813e29e3580436075433cce
Vladislav Tasev <vladislav.tasev@sap.com>
2020-08-12 16:23:05 +0300

    fix test pages

--- packages/tools/lib/scoping/index.js
+++ packages/tools/lib/scoping/index.js
@@ -14,7 +14,24 @@ const packages = components.map(item => item.packageName).filter((item, index, a
 const srcFiles = components.map(item => item.file);
 const tags = components.flatMap(item => item.altTag ? [item.tag, item.altTag] : item.tag);
 
-const processSourceFile = (file) => {
+// Replaces tags in HTML content, f.e. <ui5-button> with <ui5-button-ver> or </ui5-button> with </ui5-button-ver>
+const replaceTagsHTML = content => {
+	tags.forEach(tag => {
+		content = content.replace(new RegExp(`(<\/?)(${tag})(\/?[> \t\n])`, "g"), `$1$2-${version}$3`);
+	});
+
+	return content;
+};
+
+const replaceTagsAny = content => {
+	tags.forEach(tag => {
+		content = content.replace(new RegExp(`(^|[^\-_A-Za-z0-9])(${tag})([^\-_A-Za-z0-9]|$)`, "g"), `$1$2-${version}$3`);
+	});
+	return content;
+};
+
+// Replace imports for components in other packages, replace tags
+glob.sync(path.join(root, "/**/*.js")).forEach(file => {
 	let content = String(fs.readFileSync(file));
 
 	// Replace imports
@@ -31,47 +48,36 @@ const processSourceFile = (file) => {
 		}
 	});
 
-	// Replace tags
-	tags.forEach(tag => {
-		content = content.replace(new RegExp(`(${tag})([^\-A-Za-z0-9])`, "g"), `$1-${version}$2`);
-	});
+	content = replaceTagsAny(content);
 
 	fs.writeFileSync(file, content);
-};
-
-const processTemplate = (file) => {
-	let content = String(fs.readFileSync(file));
-
-	// Replace tags
-	tags.forEach(tag => {
-		content = content.replace(new RegExp(`(<\/?)(${tag})([> ])`, "g"), `$1$2-${version}$3`);
-	});
-
-	fs.writeFileSync(file, content);
-};
-
-const processCSS = (file) => {
-	let content = String(fs.readFileSync(file));
-
-	tags.forEach(tag => {
-		content = content.replace(new RegExp(`([^\.\-_])(${tag})([^\-A-Za-z0-9])`, "g"), `$1$2-${version}$3`);
-	});
-
-	fs.writeFileSync(file, content);
-};
-
-// Replace imports for components in other packages, replace tags
-glob.sync(path.join(root, "/*.js")).forEach(file => {
-	processSourceFile(file);
 });
 
 // Replace tags in templates
 glob.sync(path.join(root, "/generated/templates/**/*.js")).forEach(file => {
-	processTemplate(file);
+	let content = String(fs.readFileSync(file));
+	content = replaceTagsHTML(content);
+	fs.writeFileSync(file, content);
 });
 
-// Replace tags in CSS
+// Replace tag names in CSS
 glob.sync(path.join(root, "/generated/themes/*.css.js")).forEach(file => {
-	processCSS(file);
+	let content = String(fs.readFileSync(file));
+	content = replaceTagsAny(content);
+	fs.writeFileSync(file, content);
 });
 
+// Replace both tags and tag names in test pages
+glob.sync(path.join(root, "/test-resources/pages/**/*.html")).forEach(file => {
+	let content = String(fs.readFileSync(file));
+	content = content.replace(/bundle\.(.*?)\.js/g, `bundle.scoped.$1.js`);
+	content = replaceTagsHTML(content);
+	fs.writeFileSync(file, content);
+});
+
+// Replace tag names in test pages scripts
+glob.sync(path.join(root, "/test-resources/pages/**/*.{html,css,js}")).forEach(file => {
+	let content = String(fs.readFileSync(file));
+	content = replaceTagsAny(content);
+	fs.writeFileSync(file, content);
+});
