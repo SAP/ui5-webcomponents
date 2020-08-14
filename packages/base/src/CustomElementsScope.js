5176632da4e3e59cf9a33ed35e4c99acf7a88f76
Vladislav Tasev <vladislav.tasev@sap.com>
2020-08-14 15:46:50 +0300

    check

--- packages/base/src/CustomElementsScope.js
+++ packages/base/src/CustomElementsScope.js
@@ -1,6 +1,10 @@
 let suffix;
 
 const setCustomElementsScopingSuffix = scope => {
+	if (!scope.match(/^[a-zA-Z0-9_-]+$/)) {
+		throw new Error("Only alphanumeric characters and dashes allowed for the scoping suffix");
+	}
+
 	suffix = scope;
 };
 
