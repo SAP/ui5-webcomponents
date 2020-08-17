2b453c990c5cc9d35a43dd91a7892beac9ddf709
Vladislav Tasev <vladislav.tasev@sap.com>
2020-08-17 12:29:35 +0300

    lint 2

--- packages/tools/lib/scoping/lint-src.js
+++ packages/tools/lib/scoping/lint-src.js
@@ -9,7 +9,16 @@ glob.sync(path.join(process.cwd(), "src/**/*.css")).forEach(file => {
 	let content = String(fs.readFileSync(file));
 	tags.forEach(tag => {
 		if (content.match(new RegExp(`(^|[^\-_A-Za-z0-9"])(${tag})([^\-_A-Za-z0-9]|$)`, "g"))) {
-			console.warn(`${tag} found in ${file}`);
+			console.warn(`Warning! ${tag} found in ${file}`);
+		}
+	});
+});
+
+glob.sync(path.join(process.cwd(), "src/**/*.js")).forEach(file => {
+	let content = String(fs.readFileSync(file));
+	tags.forEach(tag => {
+		if (content.match(new RegExp(`querySelector[A-Za-z]*..${tag}`, "g"))) {
+			console.warn(`Warning! querySelector for ${tag} found in ${file}`);
 		}
 	});
 });
