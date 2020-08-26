946296ae01de67ad3a3cb68b5dd718380dacd37a
Boyan Rakilovski <boyan.rakilovski@sap.com>
2020-08-26 17:08:13 +0300

    feat(ui5-date-picker): keyboard handling improvement
    - A calculation regarding the ability to change the month of the "dateValue"
    property of the DatePicker component with "Shift" + "PageDown/PageUp" keyboard
    keys is now adjusted.

--- packages/main/src/DatePicker.js
+++ packages/main/src/DatePicker.js
@@ -580,8 +580,9 @@ class DatePicker extends UI5Element {
 			date.setDate(date.getDate() + incrementStep);
 		} else if (months) {
 			date.setMonth(date.getMonth() + incrementStep);
+			let monthDiff = (date.getFullYear() - oldDate.getFullYear()) * 12 + (date.getMonth() - oldDate.getMonth());
 
-			if (date.getMonth() === oldDate.getMonth() || (date.getMonth() - oldDate.getMonth() > incrementStep)) {
+			if (date.getMonth() === oldDate.getMonth() || monthDiff !== incrementStep) {
 				// first condition example: 31th of March increment month with -1 results in 2th of March
 				// second condition example: 31th of January increment month with +1 results in 2th of March
 				date.setDate(0);
