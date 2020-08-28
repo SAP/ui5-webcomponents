35bfffdda926cf321eb57b44f7d5de572eaef941
Boyan Rakilovski <boyan.rakilovski@sap.com>
2020-08-28 11:40:22 +0300

    Fixed lint errors

--- packages/localization/src/dates/CalendarDate.js
+++ packages/localization/src/dates/CalendarDate.js
@@ -81,7 +81,7 @@ class CalendarDate {
 	 */
 	setMonth(month, date) {
 		checkNumericLike(month, `Invalid month: ${month}`);
-		if (date|| date === 0) {
+		if (date || date === 0) {
 			checkNumericLike(date, `Invalid date: ${date}`);
 			this._oUDate.setUTCMonth(month, date);
 		} else {
