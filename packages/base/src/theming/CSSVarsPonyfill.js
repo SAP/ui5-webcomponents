a01f2a7ed7ac536a132bf0387319d4eb513176b9
Vladislav Tasev <vladislav.tasev@sap.com>
2020-09-23 10:04:06 +0300

    fix comments

--- packages/base/src/theming/CSSVarsPonyfill.js
+++ packages/base/src/theming/CSSVarsPonyfill.js
@@ -26,14 +26,14 @@ const isCompact = () => {
 const getCompactModeVars = () => {
 	const compactVars = {};
 	[...document.querySelectorAll(`[data-ui5-theme-properties]`)].forEach(el => {
-		const cssContent = el.textContent.replace("/n", "");
+		const cssContent = el.textContent.replace("\n", "");
 		let match;
 		const regExp = new RegExp("data-ui5-compact-size[^{]*{(.*?)}", "g");
 		while ((match = regExp.exec(cssContent)) !== null) { // eslint-disable-line
 			const compactCSS = match[1];
 			compactCSS.split(";").forEach(declaration => {
 				const pair = declaration.split(":");
-				compactVars[pair[0]] = pair[1];
+				compactVars[pair[0].trim()] = pair[1].trim();
 			});
 		}
 	});
