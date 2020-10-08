831da572dcfe0d59f056ce9a490baa3deb6ec2f5
Deshev <nikolay.deshev@sap.com>
2020-10-08 09:21:53 +0300

    RangeSlider functionality improvements

--- packages/main/src/Slider.js
+++ packages/main/src/Slider.js
@@ -63,19 +63,21 @@ const metadata = {
 		 */
 		value: {
 			type: Float,
+			defaultValue: 0,
 		},
 		/**
-		 * Determines end point of a selection - position of a second handle on the slider. Allow defining of a <b>selected range</b>.
-		 * When <code>endValue</code> is specified, the <code>value</code> property is used as a "start value" to determine a selected range on the slider.
+		 * Determines end point of a selection - position of a second handle on the slider. Allows defining of a <b>selected range</b>.
+		 * When <code>endValue</code> is specified to > 0, the <code>value</code> property is used as a "start value" to determine a selected range on the slider.
 		 * If the values is lower/higher than the allowed minimum/maximum, they will be set to the corresponding min/max values of the slider.
 		 * <br><br>
 		 *
 		 * @type {Float}
-		 * @defaultvalue [0, 0]
+		 * @defaultvalue 0
 		 * @public
 		 */
 		 endValue: {
 			type: Float,
+			defaultValue: 0,
 		},
 		/**
 		 * Enables tick marks visualization for each step. The step value must not be set to 0
@@ -181,7 +183,7 @@ class Slider extends UI5Element {
 
 	}
 
-	// TODO Include touch events
+	// TODO Include touch/pointer events, keyboard handling
 	/**
 	 * Called when the user starts interacting with the slider
 	 */
@@ -192,11 +194,19 @@ class Slider extends UI5Element {
 			return;
 		}
 
-		this.boundingDOMRect = this.getBoundingClientRect();
+		const oldEndValue = _this.endValue;
+		const oldStartValue = _this.value;
+		const clientX = event.clientX != null ? event.clientX : event.targetTouches[0].clientX;
+
+		_this.boundingDOMRect = _this.getBoundingClientRect();
+		const newValue = _this._calculateValueFromInteraction(event);
+
+		// In case of Range Slider assign the handle clossest to the press point, otherwise - the single handle's DOM
+		_this.handle = _this._getClosestHandle(clientX, newValue);
+
 		const moveHandler = moveEvent => {
 			_this._handleMove(moveEvent);
 		};
-
 		const upHandler = () => {
 			_this._handleUp();
 			_this.removeEventListener("mouseup", upHandler);
@@ -209,7 +219,17 @@ class Slider extends UI5Element {
 		document.body.addEventListener("mousemove", moveHandler);
 		document.body.addEventListener("mouseup", upHandler);
 
-		this._calculateValueFromInteraction(event);
+		// Do not update Slider if press is in range - only for range sliders (meaning that endValue property is set)
+		if (oldEndValue && newValue >= oldStartValue && newValue <= oldEndValue) {
+			return;
+		}
+
+		this._updateUI(newValue);
+		if (this.valueAffected === "startValue") {
+			this._setValue(newValue);
+		} else {
+			this._setEndValue(newValue);
+		}
 	}
 
 	/**
@@ -218,13 +238,54 @@ class Slider extends UI5Element {
 	 */
 	_handleMove(event) {
 		event.preventDefault();
-		// console.warn("move");
 
-		this._calculateValueFromInteraction(event);
+		const value = this._calculateValueFromInteraction(event);
+		const updateValueAndFireEvent = () => {
+			if (this.valueAffected === "startValue") {
+				this._setValue(value);
+			} else {
+				this._setEndValue(value);
+			}
+		}
+
+		// Update Slider UI in real-time (decoupled with rendering)
+		this._updateUI(value);
+		// Prevent re-rendering on every move event fired
+		this.debounce(updateValueAndFireEvent, 100);
 	}
 
 	_handleUp() {
-		// console.warn("up");
+		this.fireEvent("change");
+	}
+
+	_getClosestHandle(clientX, value) {
+		const handleStart = this.shadowRoot.querySelector(".ui5-slider-handle");
+		const handleEnd = this.shadowRoot.querySelector(".ui5-slider-end-handle");
+
+		// If the slider is not a range slider return the single handle
+		if (!this.endValue) {
+			this.valueAffected = "startValue";
+			return handleStart;
+		}
+
+		// Check if the press point is in the bounds of any handle
+		const handleStartDomRect = handleStart.getBoundingClientRect();
+		const handleEndDomRect = handleEnd.getBoundingClientRect();
+		const inHandleStartDom = clientX >= handleStartDomRect.left && clientX <= handleStartDomRect.right;
+		const inHandleEndDom = clientX >= handleEndDomRect.left && clientX <= handleEndDomRect.right;
+		
+		// Return that handle that is closer to the press point
+		// If the two handles are overlapping return the second (end) one as in general the more common drag move is to the right
+		if (inHandleEndDom || value > this.endValue) {
+			this.valueAffected = "endValue";
+			return handleEnd;
+		}
+
+		// If one of the handle is pressed return that one
+		if (inHandleStartDom || value < this.value) {
+			this.valueAffected = "startValue";
+			return handleStart;
+		}
 	}
 
 	/**
@@ -232,24 +293,10 @@ class Slider extends UI5Element {
 	 */
 	_calculateValueFromInteraction(event) {
 		const pageX = this._getPageXValueFromEvent(event);
-		const value = this._computeValueFromPageX(pageX);
-		this._setValue(value, true);
-	}
-
-	/**
-	 * Gets pageX value from event on user interaction with the Slider
-	 */
-	_getPageXValueFromEvent(event) {
-		if (event.targetTouches && event.targetTouches.length > 0) {
-			return event.targetTouches[0].pageX;
-		}
-		return event.pageX;
-	}
-
-	_setValue(value) {
 		const min = this.min;
 		const max = this.max;
 		const step = this.step;
+		let value = this._computeValueFromPageX(pageX);
 
 		// "Stepihfy" the raw value - calculate a step value
 		if (this.step !== 0) {
@@ -264,13 +311,33 @@ class Slider extends UI5Element {
 			value = max;
 		}
 
-		this._updateUI();
-		// this.debounce(() => this.value = value, 100);
+		return value;
 	}
+
+	/**
+	 * Gets pageX value from event on user interaction with the Slider
+	 */
+	_getPageXValueFromEvent(event) {
+		if (event.targetTouches && event.targetTouches.length > 0) {
+			return event.targetTouches[0].pageX;
+		}
+		return event.pageX;
+	}
+
+	_setValue(value) {
+			this.value = value;
+			this.fireEvent("input");
+	}
+
+	_setEndValue(value) {
+		this.endValue = value;
+		this.fireEvent("input");
+}
 
 	/**
 	 * Computes the new value (in %) from the pageX position of the cursor
 	 */
+	// TODO RTL support?
 	_computeValueFromPageX(pageX) {
 		const max = this.max;
 		const min = this.min;
@@ -279,26 +346,77 @@ class Slider extends UI5Element {
 		const xPositionRelative = pageX - this.boundingDOMRect.left;
 		// Calculate the percentage complete (the "progress")
 		const percentageComplete = xPositionRelative / this.boundingDOMRect.width;
-
-		// TODO RTL support?
-
 		// Fit (map) the complete percentage between the min/max value range
 		return min + percentageComplete * (max - min);
 	}
 
-	_updateUI() {
+	// TODO Refactor the 2 functions below
+
+	// Update UI after user interaction
+	_updateUI(value) {
 		const max = this.max;
 		const min = this.min;
-		const value = this.value;
-		const percentageComplete = (value - min) / (max - min);
-		const translatePx = percentageComplete * this.boundingDOMRect.width;
+		const startValue = this.value;
+		const endValue = this.endValue;
+
+		// The value according to which we update the UI can be either the (start) value
+		// or the endValue property in case of a range. Otherwise just the single "value" prop in case
+		// specified (the single handle slider case). It is determined in _getClosestHandle()
+		// depending on to which handle is closer the user interaction. The same goes for the handle.
+		const handleDom = this.handle;
+		const sliderDomRect = this.boundingDOMRect;
+		let percentageComplete = (value - min) / (max - min);
+		let translateProgressPx = percentageComplete * sliderDomRect.width;
+		// Update the progress indication width. In the case of a range slider
+		// Use the already calculated progress of the first handle to determine 
+		// starting point of the progress indicator.
+		if (this.valueAffected === "startValue") {
+			this.shadowRoot.querySelector(".ui5-slider-progress").style.setProperty("transform", `scaleX(${percentageComplete})`);
+		} else {
+			let percentageComplete = (endValue - min) / (max - min);
+			let translateProgressPx = percentageComplete * sliderDomRect.width;
+			const startProgressPoint = translateProgressPx;
+			const translateRangeProgressPx = ((value - startValue) - min) / (max - min);
+			this.shadowRoot.querySelector(".ui5-slider-progress").style.setProperty("transform", `scaleX(${translateRangeProgressPx})`);
+			this.shadowRoot.querySelector(".ui5-slider-progress").style.setProperty("left", `${startProgressPoint}`);
+		}
 
 		// Update the position of the handle depending on the value
-		// Center the Slider Handle position under the cursor/pointer
-		this.shadowRoot.querySelector(".ui5-slider-handle").style.setProperty("left", `${translatePx}px`);
-		this.shadowRoot.querySelector(".ui5-slider-progress").style.setProperty("transform", `scaleX(${percentageComplete})`);
+		handleDom.style.setProperty("left", `${translateProgressPx}px`);
 	}
 
+	// Update Slider UI after entering the DOM
+	// TODO Refactor this
+	_initialUISync() {
+		const max = this.max;
+		const min = this.min;
+		const startValue = this.value;
+		const endValue = this.endValue;
+		const sliderDomRect = this.getBoundingClientRect();
+
+		if (!endValue) {
+			const handleDom = this.shadowRoot.querySelector(".ui5-slider-handle");
+			const percentageComplete = (startValue - min) / (max - min);
+			const translatePx = percentageComplete * sliderDomRect.width;
+
+			// Update the position of the handle depending on the value
+			// Center the Slider Handle position under the cursor/pointer
+			handleDom.style.setProperty("left", `${translatePx}px`);
+			this.shadowRoot.querySelector(".ui5-slider-progress").style.setProperty("transform", `scaleX(${percentageComplete})`);
+		} else {
+			const percentageCompleteSecondHandle = (endValue - min) / (max - min);
+			const percentageComplete = (startValue - min) / (max - min);
+			const translatePx = percentageComplete * sliderDomRect.width;
+			const translatePxSecondHandle = percentageCompleteSecondHandle * sliderDomRect.width;
+			const rangeProgress = ((endValue - startValue) - min) / (max - min);
+			this.shadowRoot.querySelector(".ui5-slider-handle").style.setProperty("left", `${translatePx}px`);
+			this.shadowRoot.querySelector(".ui5-slider-end-handle").style.setProperty("left", `${translatePxSecondHandle}px`);
+			this.shadowRoot.querySelector(".ui5-slider-progress").style.setProperty("left", `${translatePx}px`);
+			this.shadowRoot.querySelector(".ui5-slider-progress").style.setProperty("transform", `scaleX(${rangeProgress})`);
+		}
+	}
+
+	// TODO Allow tickmark labeling?
 	/**
 	 * Calculates and draws the tickmarks with a CSS gradient style
 	 */
@@ -310,7 +428,7 @@ class Slider extends UI5Element {
 		// Calculate how many tickmarks have to be drawn (max - min / stepValue)
 		const tickmarksAmount = `${maxStr - minStr} / ${stepStr}`;
 
-		// TODO* Make it themable?
+		// TODO* Make it themable
 		const tickmarkWidth = "1px";
 
 		// Transparent CSS gradient background
@@ -331,6 +449,10 @@ class Slider extends UI5Element {
 			step = 1;
 		}
 		this.step = step;
+	}
+
+	onEnterDOM() {
+		this._initialUISync() 
 	}
 
 	onBeforeRendering() {
