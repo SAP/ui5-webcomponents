efb0d8ec9c35607687e5d150725dfe0e1e936675
Peter Skelin <petar.skelin@sap.com>
2020-11-04 11:44:07 +0200

    feat(icons-tnt): add new TNT icon collection (#2414)
    based on SAP-icons-TNT

--- packages/tools/lib/create-icons/index.js
+++ packages/tools/lib/create-icons/index.js
@@ -8,25 +8,28 @@ const destDir = path.normalize("dist/");
 
 mkdirp.sync(destDir);
 
-const template = (name, pathData, ltr) => `import { registerIcon } from "@ui5/webcomponents-base/dist/SVGIconRegistry.js";
+const template = (name, pathData, ltr, collection) => `import { registerIcon } from "@ui5/webcomponents-base/dist/SVGIconRegistry.js";
 
 const name = "${name}";
 const pathData = "${pathData}";
 const ltr = ${ltr};
+const collection = "${collection}";
 
-registerIcon(name, { pathData, ltr});
+registerIcon(name, { pathData, ltr, collection});
 
 export default { pathData };`;
 
-const accTemplate = (name, pathData, ltr, accData) => `import { registerIcon } from "@ui5/webcomponents-base/dist/SVGIconRegistry.js";
+
+const accTemplate = (name, pathData, ltr, accData, collection) => `import { registerIcon } from "@ui5/webcomponents-base/dist/SVGIconRegistry.js";
 import { ${accData.key} } from "./generated/i18n/i18n-defaults.js";
 
 const name = "${name}";
 const pathData = "${pathData}";
 const ltr = ${ltr};
 const accData = ${accData.key};
+const collection = "${collection}";
 
-registerIcon(name, { pathData, ltr, accData });
+registerIcon(name, { pathData, ltr, accData, collection });
 
 export default { pathData, accData };`;
 
@@ -40,7 +43,7 @@ const createIcons = (file) => {
 		const ltr = !!iconData.ltr;
 		const acc = iconData.acc;
 
-		const content = acc ? accTemplate(name, pathData, ltr, acc) : template(name, pathData, ltr);
+		const content = acc ? accTemplate(name, pathData, ltr, acc, json.collection) : template(name, pathData, ltr, json.collection);
 
 		fs.writeFileSync(path.join(destDir, `${name}.js`), content);
 	}
