ea17476d7452313db4384d1e2f553675a9b2c58d
Alexandar Mitsev <alexandar.mitsev@sap.com>
2020-12-10 14:34:54 +0200

    fix tabs in test and sample

--- packages/main/test/specs/Dialog.spec.js
+++ packages/main/test/specs/Dialog.spec.js
@@ -114,19 +114,19 @@ describe("Dialog general interaction", () => {
 		closeResizableDialogButton.click();
 	});
 
-	it("initial focus after dynamic dialog creation", () => {​​
-        const openDynamicDialog = browser.$("#dynamic-open");
+	it("initial focus after dynamic dialog creation", () => {
+		const openDynamicDialog = browser.$("#dynamic-open");
 		openDynamicDialog.click();
 
-        const closeButton = browser.$("#dynamic-dialog-close-button");
+		const closeButton = browser.$("#dynamic-dialog-close-button");
 
-        browser.pause(500);
+		browser.pause(500);
 
-        const activeElement = $(browser.getActiveElement());
+		const activeElement = $(browser.getActiveElement());
 		assert.strictEqual(activeElement.getProperty("id"), closeButton.getProperty("id"), "the active element is the close button");
 
-        closeButton.click();
-    }​​);
+		closeButton.click();
+	}​​);
 });
 
 
