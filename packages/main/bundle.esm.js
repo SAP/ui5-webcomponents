b4f836a9865bc5105920150e785c78856b37a406
Vladislav Tasev <vladitasev@users.noreply.github.com>
2021-01-12 09:21:40 +0200

    feat(framework): Create attachDirectionChange function (#2646)

--- packages/main/bundle.esm.js
+++ packages/main/bundle.esm.js
@@ -107,6 +107,7 @@ import { getRTL } from "@ui5/webcomponents-base/dist/config/RTL.js";
 import { getFirstDayOfWeek } from "@ui5/webcomponents-base/dist/config/FormatSettings.js";
 import { getRegisteredNames as getIconNames } from  "@ui5/webcomponents-base/dist/SVGIconRegistry.js";
 import applyDirection from "@ui5/webcomponents-base/dist/locale/applyDirection.js";
+import { attachDirectionChange } from "@ui5/webcomponents-base/dist/locale/directionChange.js";
 import ResizeHandler from "@ui5/webcomponents-base/dist/delegate/ResizeHandler.js";
 
 const testAssets = {
@@ -125,6 +126,7 @@ const testAssets = {
 	},
 	getLocaleData,
 	applyDirection,
+	attachDirectionChange,
 	ResizeHandler,
 	addCustomCSS,
 	attachThemeLoaded,
