772424efe09309017425867dacc60d2ec2a702ed
Tsanislav Gatev <tsanislav.gatev@sap.com>
2021-02-11 08:25:30 +0200

    feat(ui5-color-palette): initial implementation (#2731)
    Implementation of new component called color palette. The control presents a set of colors that you define, from which the user can select.

--- packages/main/bundle.esm.js
+++ packages/main/bundle.esm.js
@@ -47,6 +47,8 @@ import Button from "./dist/Button.js";
 import Card from "./dist/Card.js";
 import Carousel from "./dist/Carousel.js";
 import CheckBox from "./dist/CheckBox.js";
+import ColorPalette from "./dist/ColorPalette.js";
+import ColorPaletteItem from "./dist/ColorPaletteItem.js";
 import ComboBox from "./dist/ComboBox.js";
 import DatePicker from "./dist/DatePicker.js";
 import DateRangePicker from "./dist/DateRangePicker.js";
