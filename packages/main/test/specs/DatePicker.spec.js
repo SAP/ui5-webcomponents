bbc5cd98d964981ca7511cc80719db2db0c1a0dd
Petar Skelin <petar.skelin@sap.com>
2021-02-17 11:46:38 +0200

    fix datepicker inconsistent test

--- packages/main/test/specs/DatePicker.spec.js
+++ packages/main/test/specs/DatePicker.spec.js
@@ -811,9 +811,10 @@ describe("Date Picker Tests", () => {
 		datepicker.root.setAttribute("primary-calendar-type", "Gregorian");
 		datepicker.id = "#dp13";
 		datepicker.openPicker();
-		datepicker.input.click();
 		browser.keys("May 3, 2100");
 		browser.keys("Enter");
+		// open picker after accepting the date
+		datepicker.input.click();
 
 		const data = Array.from(datepicker.getDayPickerDatesRow(2));
 		assert.strictEqual(data[0].getAttribute("aria-label"), "Calendar Week 18", "First columnheader have Week number aria-label");
