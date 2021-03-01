d4f7f92e35a36a400bc9a9532fc956031894aed8
Martin Hristov <martin.r.hristov@gmail.com>
2021-03-01 16:52:31 +0200

    fix(ui5-multi-combobox): adjust timing issue in failing test

--- packages/main/test/specs/MultiComboBox.spec.js
+++ packages/main/test/specs/MultiComboBox.spec.js
@@ -275,44 +275,45 @@ describe("MultiComboBox general interaction", () => {
 			assert.strictEqual(innerInput.getAttribute("aria-describedby"), ariaDescribedBy, "aria-describedby has a reference for the value state and the tokens count");
 		});
 
-		// TODO: Temporary commented as fails on the central build on regular basis
-		// it ("aria-describedby value according to the tokens count", () => {
-		// 	const mcb = $("#mcb-compact");
-		// 	const innerInput = mcb.shadow$("input");
-		// 	const invisibleText = mcb.shadow$(".ui5-hidden-text");
-		// 	const inivisbleTextId = invisibleText.getProperty("id");
-		// 	let tokens = mcb.shadow$$(".ui5-multi-combobox-token");
-		// 	let resourceBundleText = null;
+		it ("aria-describedby value according to the tokens count", () => {
+			const mcb = $("#mcb-compact");
+			const innerInput = mcb.shadow$("input");
+			const invisibleText = mcb.shadow$(".ui5-hidden-text");
+			const inivisbleTextId = invisibleText.getProperty("id");
+			let tokens = mcb.shadow$$(".ui5-multi-combobox-token");
+			let resourceBundleText = null;
 
-		// 	assert.strictEqual(tokens.length, 2, "should have two tokens");
-		// 	assert.strictEqual(innerInput.getAttribute("aria-describedby"), inivisbleTextId, "aria-describedby reference is correct");
-		// 	assert.strictEqual(invisibleText.getText(), "Contains 2 tokens", "aria-describedby text is correct");
+			mcb.scrollIntoView();
+			browser.pause(500)
 
-		// 	mcb.scrollIntoView();
-		// 	innerInput.click();
-		// 	innerInput.keys("Backspace");
-		// 	innerInput.keys("Backspace");
+			assert.strictEqual(tokens.length, 2, "should have two tokens");
+			assert.strictEqual(innerInput.getAttribute("aria-describedby"), inivisbleTextId, "aria-describedby reference is correct");
+			assert.strictEqual(invisibleText.getText(), "Contains 2 tokens", "aria-describedby text is correct");
 
-		// 	tokens = mcb.shadow$$(".ui5-multi-combobox-token");
+			innerInput.click();
+			innerInput.keys("Backspace");
+			innerInput.keys("Backspace");
 
-		// 	resourceBundleText = browser.execute(() => {
-		// 		const mcb = document.getElementById("mcb-compact");
-		// 		return mcb.i18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.TOKENIZER_ARIA_CONTAIN_ONE_TOKEN);
-		// 	});
+			tokens = mcb.shadow$$(".ui5-multi-combobox-token");
 
-		// 	assert.strictEqual(tokens.length, 1, "should have one token");
-		// 	assert.strictEqual(invisibleText.getText(), resourceBundleText, "aria-describedby text is correct");
+			resourceBundleText = browser.execute(() => {
+				const mcb = document.getElementById("mcb-compact");
+				return mcb.i18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.TOKENIZER_ARIA_CONTAIN_ONE_TOKEN);
+			});
 
-		// 	innerInput.keys("Backspace");
+			assert.strictEqual(tokens.length, 1, "should have one token");
+			assert.strictEqual(invisibleText.getText(), resourceBundleText, "aria-describedby text is correct");
 
-		// 	tokens = mcb.shadow$$(".ui5-multi-combobox-token");
-		// 	resourceBundleText = browser.execute(() => {
-		// 		const mcb = document.getElementById("mcb-compact");
-		// 		return mcb.i18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.TOKENIZER_ARIA_CONTAIN_TOKEN);
-		// 	});
+			innerInput.keys("Backspace");
 
-		// 	assert.strictEqual(tokens.length, 0, "should not have tokens");
-		// 	assert.strictEqual(invisibleText.getText(), resourceBundleText, "aria-describedby text is correct");
-		// });
+			tokens = mcb.shadow$$(".ui5-multi-combobox-token");
+			resourceBundleText = browser.execute(() => {
+				const mcb = document.getElementById("mcb-compact");
+				return mcb.i18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.TOKENIZER_ARIA_CONTAIN_TOKEN);
+			});
+
+			assert.strictEqual(tokens.length, 0, "should not have tokens");
+			assert.strictEqual(invisibleText.getText(), resourceBundleText, "aria-describedby text is correct");
+		});
 	});
 });
