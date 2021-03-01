35de38c24fea80bca00940c51b5eb9c07b2978b5
Georgi <georgi.minkov@sap.com>
2021-03-01 14:24:11 +0200

    fix(ui5-upload-collection): add tooltip to edit button (#2886)

--- packages/fiori/src/UploadCollection.js
+++ packages/fiori/src/UploadCollection.js
@@ -73,7 +73,7 @@ const metadata = {
 
 		/**
 		 * By default there will be drag and drop overlay shown over the <code>ui5-upload-collection</code> when files
-		 * are dragged. If you don't intend to use drag and drop, set this property to <code>true</code>
+		 * are dragged. If you don't intend to use drag and drop, set this property.
 		 * <br><br>
 		 * <b>Note:</b> It is up to the application developer to add handler for <code>drop</code> event and handle it.
 		 * <code>ui5-upload-collection</code> only shows an overlay.
