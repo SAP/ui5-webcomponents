44e78d6f663d2771e97be3c871a300ef5fd14acf
Vladislav Tasev <vladislav.tasev@sap.com>
2021-03-18 14:21:21 +0200

    reset

--- packages/main/test/specs/port.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const packageFile = JSON.parse(fs.readFileSync(require.resolve("@ui5/webcomponents/package.json")));
-const port = packageFile.ui5.port;
-
-module.exports = port;
