491c3cdc4a4446a7c0c21f9ae46cb4c84605a6cd
Teodor Taushanov <teodor.taushanov@sap.com>
2021-04-01 11:39:23 +0300

    fix(ui5-dialog): Remove unused variable

--- packages/main/src/Popup.js
+++ packages/main/src/Popup.js
@@ -1,6 +1,5 @@
 import { renderFinished } from "@ui5/webcomponents-base/dist/Render.js";
 import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
-import { getRTL } from "@ui5/webcomponents-base/dist/config/RTL.js";
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 import { getFirstFocusableElement, getLastFocusableElement } from "@ui5/webcomponents-base/dist/util/FocusableElements.js";
 import createStyleInHead from "@ui5/webcomponents-base/dist/util/createStyleInHead.js";
