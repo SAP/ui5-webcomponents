193f015e0789efc28464e9b3dd5e3e5d009c196c
Vladislav Tasev <vladislav.tasev@sap.com>
2021-04-06 09:41:25 +0300

    refactor: remove sap.ui.requireSync from localization code

---
+++ packages/localization/overlay/sap/ui/base/Interface.js
@@ -0,0 +1,17 @@
+/*!
+ * ${copyright}
+ */
+
+// Provides class sap.ui.base.Interface
+sap.ui.define([], function () {
+	"use strict";
+
+	var Interface = function (oObject) {
+
+		return oObject;
+
+	};
+
+	return Interface;
+
+}, /* bExport= */ true);
