4619229abfc4100ba569a2e362128acafd1560ce
Vladislav Tasev <vladislav.tasev@sap.com>
2021-04-21 10:19:56 +0300

    fix if

--- packages/base/src/asset-registries/Icons.js
+++ packages/base/src/asset-registries/Icons.js
@@ -20,10 +20,10 @@ const registerIconLoader = async (collectionName, loader) => {
 
 const _loadIconCollectionOnce = async collectionName => {
 	if (!iconCollectionPromises.has(collectionName)) {
-		const loadIcons = loaders.get(collectionName);
-		if (typeof loadIcons !== "function") {
+		if (!loaders.has(collectionName)) {
 			throw new Error(`No loader registered for the ${collectionName} icons collection. Probably you forgot to import the "AllIcons.js" module for the respective package.`);
 		}
+		const loadIcons = loaders.get(collectionName);
 		iconCollectionPromises.set(collectionName, loadIcons(collectionName));
 	}
 
