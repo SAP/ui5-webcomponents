54a27ecd81ef05f54770bd044d6a4c1565898be5
Dobrin Dimchev <dobrin.dimchev@sap.com>
2021-05-05 15:58:54 +0300

    fix(ui5-avatar-group): click event is fired only once (#3196)

--- packages/main/src/AvatarGroup.js
+++ packages/main/src/AvatarGroup.js
@@ -337,20 +337,23 @@ class AvatarGroup extends UI5Element {
 	}
 
 	_onkeydown(event) {
-		if (isEnter(event)) {
-			this._fireGroupEvent(event.target);
-		}
-
-		if (isSpace(event)) {
-			// prevent scrolling
-			event.preventDefault();
+		// when type is "Individual" the ui5-avatar and ui5-button both
+		// fire "click" event when SPACE or ENTER are pressed and
+		// AvatarGroup "click" is fired in their handlers (_onClick, _onUI5Click).
+		if (this._isGroup) {
+			if (isEnter(event)) {
+				this._fireGroupEvent(event.target);
+			} else if (isSpace(event)) {
+				// prevent scrolling
+				event.preventDefault();
+			}
 		}
 	}
 
 	_onkeyup(event) {
-		if (!event.shiftKey && isSpace(event)) {
-			event.preventDefault();
+		if (!event.shiftKey && isSpace(event) && this._isGroup) {
 			this._fireGroupEvent(event.target);
+			event.preventDefault();
 		}
 	}
 
