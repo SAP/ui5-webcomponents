b90933c5c80de6a317a4d49c55eb128033691779
niyap <38278268+niyap@users.noreply.github.com>
2021-05-12 09:15:24 +0300

    fix(ui5-rating-indicator): Aria-readonly attribute is added when RatingIndicator in not editable (#3219)

--- packages/main/src/RatingIndicator.js
+++ packages/main/src/RatingIndicator.js
@@ -291,6 +291,10 @@ class RatingIndicator extends UI5Element {
 	get _ariaDisabled() {
 		return this.disabled || undefined;
 	}
+
+	get ariaReadonly() {
+		return this.readonly ? "true" : undefined;
+	}
 }
 
 RatingIndicator.define();
