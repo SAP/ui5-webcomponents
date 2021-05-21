0ecb355e6dc37bfaee596a8482788a0f23967202
Tereza Chobanova <tereza.chobanova@sap.com>
2021-05-21 19:27:27 +0300

    The SegmentedButtonItem now extends the ToggleButton
    The unwanted properties, inherited from the Button, are marked as private.

--- packages/main/src/SegmentedButtonItem.js
+++ packages/main/src/SegmentedButtonItem.js
@@ -1,22 +1,7 @@
-import isLegacyBrowser from "@ui5/webcomponents-base/dist/isLegacyBrowser.js";
 import SegmentedButtonItemTemplate from "./generated/templates/SegmentedButtonItemTemplate.lit.js";
-import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
-import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
-import Icon from "./Icon.js";
+import ToggleButton from "./ToggleButton.js";
 import { SEGMENTEDBUTTONITEM_ARIA_DESCRIPTION } from "./generated/i18n/i18n-defaults.js";
-import { isEnter, isSpace } from "@ui5/webcomponents-base/src/Keys";
-import { isPhone, isTablet } from "@ui5/webcomponents-base/src/Device";
-import { getFeature } from "@ui5/webcomponents-base/src/FeaturesRegistry";
-import { getI18nBundle } from "@ui5/webcomponents-base/dist/i18nBundle.js";
-import { getEffectiveAriaLabelText } from "@ui5/webcomponents-base/src/util/AriaLabelHelper";
-
-
-// Styles
-import segmentedButtonItemCss from "./generated/themes/SegmentedButtonItem.css.js";
-import segmentedButtonItemIECss from "./generated/themes/SegmentedButtonItem.ie11.css.js";
-
-let isGlobalHandlerAttached = false;
-let activeButton = null;
+import ButtonDesign from "./types/ButtonDesign";
 
 /**
  * @public
@@ -25,13 +10,63 @@ const metadata = {
 	tag: "ui5-segmentedbutton-item",
 	properties: /** @lends  sap.ui.webcomponents.main.SegmentedButtonItem.prototype */ {
 		/**
-		 * Determines whether the <code>ui5-segmentedbutton-item</code> is displayed as pressed.
+		 * Defines the <code>ui5-button</code> design.
+		 *
+		 * <br><br>
+		 * <b>Note:</b>
+		 *
+		 * <ul>
+		 * <li><code>Default</code></li>
+		 * <li><code>Emphasized</code></li>
+		 * <li><code>Positive</code></li>
+		 * <li><code>Negative</code></li>
+		 * <li><code>Transparent</code></li>
+		 * </ul>
+		 *
+		 * @type {ButtonDesign}
+		 * @defaultvalue "Default"
+		 * @private
+		 */
+		design: {
+			type: ButtonDesign,
+			defaultValue: ButtonDesign.Default,
+		},
+
+		/**
+		 * Defines whether the icon should be displayed after the <code>ui5-button</code> text.
 		 *
 		 * @type {boolean}
 		 * @defaultvalue false
-		 * @public
+		 * @private
 		 */
-		pressed: {
+		iconEnd: {
+			type: Boolean,
+		},
+
+		/**
+		 * Defines the size of the icon inside the <code>ui5-button</code>.
+		 *
+		 * @type {string}
+		 * @defaultvalue undefined
+		 * @private
+		 */
+		iconSize: {
+			type: String,
+			defaultValue: undefined,
+		},
+
+		/**
+		 * When set to <code>true</code>, the <code>ui5-button</code> will
+		 * automatically submit the nearest form element upon <code>press</code>.
+		 * <br><br>
+		 * <b>Important:</b> For the <code>submits</code> property to have effect, you must add the following import to your project:
+		 * <code>import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";</code>
+		 *
+		 * @type {boolean}
+		 * @defaultvalue false
+		 * @private
+		 */
+		submits: {
 			type: Boolean,
 		},
 
@@ -52,179 +87,6 @@ const metadata = {
 		sizeOfSet: {
 			type: String,
 		},
-
-		/**
-		 * Defines the icon to be displayed as graphical element within the <code>ui5-segmentedbutton-item</code>.
-		 * The SAP-icons font provides numerous options.
-		 * <br><br>
-		 * Example:
-		 * <br>
-		 * <pre>ui5-segmentedbutton-item icon="palette"</pre>
-		 *
-		 * See all the available icons in the <ui5-link target="_blank" href="https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html" class="api-table-content-cell-link">Icon Explorer</ui5-link>.
-		 *
-		 * @type {string}
-		 * @defaultvalue ""
-		 * @public
-		 */
-		icon: {
-			type: String,
-		},
-
-		/**
-		 * Defines whether the <code>ui5-segmentedbutton-item</code> is disabled
-		 * (default is set to <code>false</code>).
-		 * A disabled <code>ui5-segmentedbutton-item</code> can't be pressed or
-		 * focused, and it is not in the tab chain.
-		 *
-		 * @type {boolean}
-		 * @defaultvalue false
-		 * @public
-		 */
-		disabled: {
-			type: Boolean,
-		},
-
-		/**
-		 * Defines the tooltip of the SegmentedButtonItem.
-		 * <br>
-		 * <b>Important:</b> Tooltips should only be set to icon only buttons.
-		 * @type {string}
-		 * @defaultvalue: ""
-		 * @private
-		 * @since 1.0.0-rc.11
-		 */
-		title: {
-			type: String,
-		},
-
-		/**
-		 * Used to switch the active state (pressed or not) of the <code>ui5-segmentedbutton-item</code>.
-		 * @private
-		 */
-		active: {
-			type: Boolean,
-		},
-
-		/**
-		 * Defines if a content has been added to the default slot
-		 * @private
-		 */
-		iconOnly: {
-			type: Boolean,
-		},
-
-		/**
-		 * Indicates if the elements is on focus
-		 * @private
-		 */
-		focused: {
-			type: Boolean,
-		},
-
-		/**
-		 * Indicates if the elements has a slotted icon
-		 * @private
-		 */
-		hasIcon: {
-			type: Boolean,
-		},
-
-		/**
-		 * Defines the aria-label attribute for the SegmentedButtonItem
-		 * @type {String}
-		 * @defaultvalue: ""
-		 * @private
-		 * @since 1.0.0-rc.7
-		 */
-		ariaLabel: {
-			type: String,
-			defaultValue: undefined,
-		},
-
-		/**
-		 * Receives id(or many ids) of the elements that label the SegmentedButtonItem
-		 * @type {String}
-		 * @defaultvalue ""
-		 * @private
-		 * @since 1.0.0-rc.7
-		 */
-		ariaLabelledby: {
-			type: String,
-			defaultValue: "",
-		},
-
-		/**
-		 * @type {String}
-		 * @defaultvalue ""
-		 * @private
-		 * @since 1.0.0-rc.8
-		 */
-		ariaExpanded: {
-			type: String,
-		},
-
-		/**
-		 * Indicates if the element if focusable
-		 * @private
-		 */
-		nonInteractive: {
-			type: Boolean,
-		},
-
-		_buttonAccInfo: {
-			type: Object,
-		},
-
-		/**
-		 * Defines the tabIndex of the component.
-		 * @private
-		 */
-		_tabIndex: {
-			type: String,
-			defaultValue: "0",
-			noAttribute: true,
-		},
-
-		/**
-		 * @private
-		 */
-		_isTouch: {
-			type: Boolean,
-		},
-	},
-
-	managedSlots: true,
-
-	slots: /** @lends sap.ui.webcomponents.main.SegmentedButtonItem.prototype */ {
-		/**
-		 * Defines the text of the <code>ui5-segmentedbutton-item</code>.
-		 * <br><br>
-		 * <b>Note:</b> Although this slot accepts HTML Elements, it is strongly recommended that you only use text in order to preserve the intended design.
-		 *
-		 * @type {Node[]}
-		 * @slot
-		 * @public
-		 */
-		"default": {
-			type: Node,
-		},
-	},
-
-	events: /** @lends sap.ui.webcomponents.main.SegmentedButtonItem.prototype */ {
-
-		/**
-		 * Fired when the <code>ui5-segmentedbutton-item</code> is activated either with a
-		 * mouse/tap or by using the Enter or Space key.
-		 * <br><br>
-		 * <b>Note:</b> The event will not be fired if the <code>disabled</code>
-		 * property is set to <code>true</code>.
-		 *
-		 * @event
-		 * @public
-		 * @native
-		 */
-		click: {},
 	},
 };
 
@@ -251,7 +113,7 @@ const metadata = {
  * @tagname ui5-segmentedbutton-item
  * @public
  */
-class SegmentedButtonItem extends UI5Element {
+class SegmentedButtonItem extends ToggleButton {
 	static get metadata() {
 		return metadata;
 	}
@@ -260,161 +122,10 @@ class SegmentedButtonItem extends UI5Element {
 		return SegmentedButtonItemTemplate;
 	}
 
-	static get styles() {
-		return [segmentedButtonItemCss, isLegacyBrowser() && segmentedButtonItemIECss];
-	}
-
-	static get dependencies() {
-		return [Icon];
-	}
-
-	static get render() {
-		return litRender;
-	}
-
-	constructor() {
-		super();
-
-		this._deactivate = () => {
-			if (activeButton) {
-				activeButton.active = false;
-			}
-		};
-
-		if (!isGlobalHandlerAttached) {
-			document.addEventListener("mouseup", this._deactivate);
-
-			isGlobalHandlerAttached = true;
-		}
-
-		this.i18nBundle = getI18nBundle("@ui5/webcomponents");
-	}
-
-	onBeforeRendering() {
-		this.iconOnly = this.isIconOnly;
-		this.hasIcon = !!this.icon;
-	}
-
-	onEnterDOM() {
-		this._isTouch = isPhone() || isTablet();
-	}
-
-	_onfocusout(_event) {
-		if (this.nonInteractive) {
-			return;
-		}
-		this.active = false;
-		this.focused = false;
-	}
-
-	_onfocusin(event) {
-		if (this.nonInteractive) {
-			return;
-		}
-
-		event.isMarked = "button";
-		this.focused = true;
-	}
-
-	_onclick() {
-		if (this.nonInteractive) {
-			return;
-		}
-		event.isMarked = "button";
-		const FormSupport = getFeature("FormSupport");
-		if (FormSupport) {
-			FormSupport.triggerFormSubmit(this);
-		}
-		this.pressed = !this.pressed;
-	}
-
-	_onmousedown(event) {
-		if (this.nonInteractive || this._isTouch) {
-			return;
-		}
-
-		event.isMarked = "button";
-		this.active = true;
-		activeButton = this; // eslint-disable-line
-	}
-
-	_onmouseup(event) {
-		event.isMarked = "button";
-	}
-
-	_onkeydown(event) {
-		event.isMarked = "button";
-
-		if (isSpace(event) || isEnter(event)) {
-			this.active = true;
-		}
-	}
-
-	_onkeyup(event) {
-		if (isSpace(event) || isEnter(event)) {
-			this.active = false;
-		}
-	}
-
-	_ontouchstart(event) {
-		event.isMarked = "button";
-		if (this.nonInteractive) {
-			return;
-		}
-
-		this.active = true;
-	}
-
-	_ontouchend(event) {
-		this.active = false;
-
-		if (activeButton) {
-			activeButton.active = false;
-		}
-	}
-
-	get isIconOnly() {
-		return !Array.from(this.childNodes).filter(node => {
-			return node.nodeType !== Node.COMMENT_NODE
-				&& (node.nodeType !== Node.TEXT_NODE || node.nodeValue.trim().length !== 0);
-		}).length;
-	}
-
-	get tabIndexValue() {
-		const tabindex = this.getAttribute("tabindex");
-
-		if (tabindex) {
-			return tabindex;
-		}
-
-		return this.nonInteractive ? "-1" : this._tabIndex;
-	}
-
-	get accInfo() {
-		return {
-			"ariaExpanded": this.ariaExpanded || (this._buttonAccInfo && this._buttonAccInfo.ariaExpanded),
-			"ariaControls": this._buttonAccInfo && this._buttonAccInfo.ariaControls,
-			"ariaHaspopup": this._buttonAccInfo && this._buttonAccInfo.ariaHaspopup,
-			"title": this.title || (this._buttonAccInfo && this._buttonAccInfo.title),
-		};
-	}
-
-	get styles() {
-		return {
-			icon: {
-				width: this.iconSize,
-				height: this.iconSize,
-			},
-		};
-	}
-
 	get ariaDescription() {
 		return this.i18nBundle.getText(SEGMENTEDBUTTONITEM_ARIA_DESCRIPTION);
 	}
 
-	get ariaLabelText() {
-		return getEffectiveAriaLabelText(this);
-	}
 }
 
 SegmentedButtonItem.define();
