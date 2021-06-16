5d3889c48e052957008c58f599f98c01667ff552
Petar Skelin <petar.skelin@sap.com>
2021-06-16 15:38:07 +0300

    animation mode

--- packages/base/src/config/AnimationMode.js
+++ packages/base/src/config/AnimationMode.js
@@ -1,19 +1,31 @@
+// @ts-check2
 import { getAnimationMode as getConfiguredAnimationMode } from "../InitialConfiguration.js";
 import AnimationMode from "../types/AnimationMode.js";
 
-let animationMode;
+/**
+ * @type {import("../types/AnimationMode.js").default}
+ */
+let curAnimationMode;
 
+/**
+ * Get the animation mode
+ * @returns {import("../types/AnimationMode.js").default} the current animation mode
+ */
 const getAnimationMode = () => {
-	if (animationMode === undefined) {
-		animationMode = getConfiguredAnimationMode();
+	if (curAnimationMode === undefined) {
+		curAnimationMode = getConfiguredAnimationMode();
 	}
 
-	return animationMode;
+	return curAnimationMode;
 };
 
-const setAnimationMode = newAnimationMode => {
-	if (Object.values(AnimationMode).includes(newAnimationMode)) {
-		animationMode = newAnimationMode;
+/**
+ * Set the animation mode
+ * @param {import("../types/AnimationMode.js").default} animationMode
+ */
+const setAnimationMode = animationMode => {
+	if (Object.values(AnimationMode).includes(animationMode)) {
+		curAnimationMode = animationMode;
 	}
 };
 
