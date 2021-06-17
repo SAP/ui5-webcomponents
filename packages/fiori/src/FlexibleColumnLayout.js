4c9da5fa9c0fce8fda3c0ccf176f0091e9d81036
Nayden Naydenov <nnaydenow97@gmail.com>
2021-06-17 18:20:37 +0300

    fix(ui5-flexible-column-layout): correct columns display

--- packages/fiori/src/FlexibleColumnLayout.js
+++ packages/fiori/src/FlexibleColumnLayout.js
@@ -407,18 +407,21 @@ class FlexibleColumnLayout extends UI5Element {
 			columnDOM.style.width = columnWidth;
 
 			// hide column with delay to allow the animation runs entirely
-			setTimeout(() => {
-				columnDOM.classList.add("ui5-fcl-column--hidden");
-			}, FlexibleColumnLayout.ANIMATION_DURATION);
+			columnDOM.addEventListener("transitionend", this.columnResizeHandler);
 
 			return;
 		}
 
 		// show column: from 0 to 33%, from 0 to 25%, etc.
 		if (previouslyHidden) {
+			columnDOM.removeEventListener("transitionend", this.columnResizeHandler);
 			columnDOM.classList.remove("ui5-fcl-column--hidden");
 			columnDOM.style.width = columnWidth;
 		}
+	}
+
+	columnResizeHandler(event) {
+		event.target.classList.add("ui5-fcl-column--hidden");
 	}
 
 	nextLayout(layout, arrowsInfo = {}) {
