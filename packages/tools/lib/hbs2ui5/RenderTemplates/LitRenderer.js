36ec19dd905aa516105f0655d6543512b7fc0066
Vladislav Tasev <vladislav.tasev@sap.com>
2021-06-18 10:05:53 +0300

    remove style-map

--- packages/tools/lib/hbs2ui5/RenderTemplates/LitRenderer.js
+++ packages/tools/lib/hbs2ui5/RenderTemplates/LitRenderer.js
@@ -2,7 +2,7 @@ const buildRenderer = (controlName, litTemplate) => {
 	return `
 /* eslint no-unused-vars: 0 */
 import ifDefined from '@ui5/webcomponents-base/dist/renderer/ifDefined.js';
-import { html, svg, repeat, classMap, styleMap, unsafeHTML, setTags, setSuffix } from '@ui5/webcomponents-base/dist/renderer/LitRenderer.js';
+import { html, svg, repeat, classMap, unsafeHTML, setTags, setSuffix } from '@ui5/webcomponents-base/dist/renderer/LitRenderer.js';
 ${litTemplate}
 
 const main = (context, tags, suffix) => {
@@ -10,7 +10,7 @@ const main = (context, tags, suffix) => {
 	setSuffix(suffix);
 	return block0(context);
 };
- 
+
 export default main;`
 };
 
