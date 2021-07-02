947e5a72d2ebccd95753e3d18031f3f541f7905d
Diana Pazheva <diana.pazheva@sap.com>
2021-07-03 01:02:04 +0300

    feat(ui5-breadcrumbs): Initial implementation
    Fixes SAP#3166

--- packages/main/src/Link.js
+++ packages/main/src/Link.js
@@ -130,6 +130,19 @@ const metadata = {
 			type: String,
 			noAttribute: true,
 		},
+
+		_tabIndex: {
+			type: String,
+			noAttribute: true,
+		},
+
+		/**
+		 * Indicates if the elements is on focus
+		 * @private
+		 */
+		 focused: {
+			type: Boolean,
+		},
 	},
 	slots: /** @lends sap.ui.webcomponents.main.Link.prototype */ {
 		/**
@@ -240,6 +253,9 @@ class Link extends UI5Element {
 	}
 
 	get tabIndex() {
+		if (this._tabIndex) {
+			return this._tabIndex;
+		}
 		return (this.disabled || !this.textContent.length) ? "-1" : "0";
 	}
 
@@ -276,6 +292,11 @@ class Link extends UI5Element {
 
 	_onfocusin(event) {
 		event.isMarked = "link";
+		this.focused = true;
+	}
+
+	_onfocusout(event) {
+		this.focused = false;
 	}
 
 	_onkeydown(event) {
