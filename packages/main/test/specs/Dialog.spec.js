8580fd8e4d40d23cb87651cb5e67ece849b74bf6
Georgi <georgi.minkov@sap.com>
2021-07-06 14:25:16 +0000

    feat(ui5-dialog): add keyboard support for draggable and resizable (#3483)

--- packages/main/test/specs/Dialog.spec.js
+++ packages/main/test/specs/Dialog.spec.js
@@ -51,69 +51,198 @@ describe("Dialog general interaction", () => {
 		assert.ok(!browser.$("ui5-static-area").length, "No static area.");
 	});
 
-	it("draggable", () => {
+	it("draggable - mouse support", () => {
 		browser.url(`http://localhost:${PORT}/test-resources/pages/Dialog.html`);
 
+		// Setup
 		const openDraggableDialogButton = browser.$("#draggable-open");
 		openDraggableDialogButton.click();
 
+		// Assert
 		const dialog = browser.$("#draggable-dialog");
 		const topBeforeDragging = parseInt(dialog.getCSSProperty("top").value);
 		const leftBeforeDragging = parseInt(dialog.getCSSProperty("left").value);
 
 		const header = browser.$("#draggable-dialog").shadow$(".ui5-popup-header-root");
 
-		header.dragAndDrop({ x: -200, y: -200 });
+		// Act
+		header.dragAndDrop({ x: -50, y: -50 });
 
+		// Assert
 		const topAfterDragging = parseInt(dialog.getCSSProperty("top").value);
 		const leftAfterDragging = parseInt(dialog.getCSSProperty("left").value);
-
 		assert.notStrictEqual(topBeforeDragging, topAfterDragging, "top position has changed");
 		assert.notStrictEqual(leftBeforeDragging, leftAfterDragging, "left position has changed");
 
 		const closeDraggableDialogButton = browser.$("#draggable-close");
-		closeDraggableDialogButton.click();
 
+		// Act
+		closeDraggableDialogButton.click();
 		openDraggableDialogButton.click();
 
+		// Assert
 		const topAfterReopening = parseInt(dialog.getCSSProperty("top").value);
 		const leftAfterReopening = parseInt(dialog.getCSSProperty("left").value);
-
 		assert.strictEqual(topBeforeDragging, topAfterReopening, "top position has been reset back to initial");
 		assert.strictEqual(leftBeforeDragging, leftAfterReopening, "left position has been reset back to initial");
 
+		// Clean-up
 		closeDraggableDialogButton.click();
 	});
 
-	it("resizable", () => {
+	it("draggable - keyboard support", () => {
+		// Setup
+		const openDraggableDialogButton = browser.$("#draggable-open");
+		openDraggableDialogButton.click();
+
+		const dialog = browser.$("#draggable-dialog");
+		const initialTop = parseInt(dialog.getCSSProperty("top").value);
+		const initialLeft = parseInt(dialog.getCSSProperty("left").value);
+
+		// Act
+		browser.keys("ArrowUp");
+
+		// Assert
+		let topAfterDragging = parseInt(dialog.getCSSProperty("top").value);
+		let leftAfterDragging = parseInt(dialog.getCSSProperty("left").value);
+		assert.notStrictEqual(initialTop, topAfterDragging, "top position has changed after ArrowUp");
+		assert.strictEqual(initialLeft, leftAfterDragging, "left position has not changed after ArrowUp");
+
+		// Setup
+		const topBeforeDraggingSecondTime = parseInt(dialog.getCSSProperty("top").value);
+		const leftBeforeDraggingSecondTime = parseInt(dialog.getCSSProperty("left").value);
+
+		// Act
+		browser.keys("ArrowLeft");
+
+		// Assert
+		topAfterDragging = parseInt(dialog.getCSSProperty("top").value);
+		leftAfterDragging = parseInt(dialog.getCSSProperty("left").value);
+		assert.strictEqual(topBeforeDraggingSecondTime, topAfterDragging, "top position has not changed after ArrowLeft");
+		assert.notStrictEqual(leftBeforeDraggingSecondTime, leftAfterDragging, "left position has changed after ArrowLeft");
+
+		const closeDraggableDialogButton = browser.$("#draggable-close");
+
+		// Act
+		closeDraggableDialogButton.click();
+		openDraggableDialogButton.click();
+
+		// Assert
+		const topAfterReopening = parseInt(dialog.getCSSProperty("top").value);
+		const leftAfterReopening = parseInt(dialog.getCSSProperty("left").value);
+
+		assert.strictEqual(initialTop, topAfterReopening, "top position has been reset back to initial");
+		assert.strictEqual(initialLeft, leftAfterReopening, "left position has been reset back to initial");
+
+		// Clean-up
+		closeDraggableDialogButton.click();
+	});
+
+	it("resizable - mouse support", () => {
+		// Setup
 		const openResizableDialogButton = browser.$("#resizable-open");
 		openResizableDialogButton.click();
 
 		const dialog = browser.$("#resizable-dialog");
 		const widthBeforeResizing = parseInt(dialog.getCSSProperty("width").value);
 		const heightBeforeResizing = parseInt(dialog.getCSSProperty("height").value);
+		const topBeforeResizing = parseInt(dialog.getCSSProperty("top").value);
+		const leftBeforeResizing = parseInt(dialog.getCSSProperty("left").value);
 
 		const handle = browser.$("#resizable-dialog").shadow$(".ui5-popup-resize-handle");
 
-		handle.dragAndDrop({ x: 200, y: 200});
+		// Act
+		handle.dragAndDrop({ x: 50, y: 50});
 
+		// Assert
 		const widthAfterResizing = parseInt(dialog.getCSSProperty("width").value);
 		const heightAfterResizing = parseInt(dialog.getCSSProperty("height").value);
+		const topAfterResizing = parseInt(dialog.getCSSProperty("top").value);
+		const leftAfterResizing = parseInt(dialog.getCSSProperty("left").value);
 
 		assert.notStrictEqual(widthBeforeResizing, widthAfterResizing, "width has changed");
 		assert.notStrictEqual(heightBeforeResizing, heightAfterResizing, "height has changed");
+		assert.strictEqual(topBeforeResizing, topAfterResizing, "top position has not changed");
+		assert.strictEqual(leftBeforeResizing, leftAfterResizing, "left position has not changed");
 
 		const closeResizableDialogButton = browser.$("#resizable-close");
-		closeResizableDialogButton.click();
 
+		// Act
+		closeResizableDialogButton.click();
 		openResizableDialogButton.click();
 
+		// Assert
 		const widthAfterReopening = parseInt(dialog.getCSSProperty("width").value);
 		const heightAfterReopening = parseInt(dialog.getCSSProperty("height").value);
+		const topAfterReopening = parseInt(dialog.getCSSProperty("top").value);
+		const leftAfterReopening = parseInt(dialog.getCSSProperty("left").value);
 
 		assert.strictEqual(widthBeforeResizing, widthAfterReopening, "width has been reset back to initial");
 		assert.strictEqual(heightBeforeResizing, heightAfterReopening, "height has been reset back to initial");
+		assert.strictEqual(topBeforeResizing, topAfterReopening, "top position has been reset back to initial");
+		assert.strictEqual(leftBeforeResizing, leftAfterReopening, "left position has been reset back to initial");
 
+		// Clean-up
+		closeResizableDialogButton.click();
+	});
+
+	it("resizable - keyboard support", () => {
+		// Setup
+		const openResizableDialogButton = browser.$("#resizable-open");
+		openResizableDialogButton.click();
+
+		const dialog = browser.$("#resizable-dialog");
+		const initialWidth = parseInt(dialog.getCSSProperty("width").value);
+		const initialHeight = parseInt(dialog.getCSSProperty("height").value);
+		const initialTop = parseInt(dialog.getCSSProperty("top").value);
+		const initialLeft = parseInt(dialog.getCSSProperty("left").value);
+	
+		// Act
+		browser.keys(["Shift", "ArrowDown"]);
+
+		// Assert
+		const widthAfterResizing = parseInt(dialog.getCSSProperty("width").value);
+		const heightAfterResizing = parseInt(dialog.getCSSProperty("height").value);
+		const topAfterResizing = parseInt(dialog.getCSSProperty("top").value);
+		const leftAfterResizing = parseInt(dialog.getCSSProperty("left").value);
+
+		assert.strictEqual(initialWidth, widthAfterResizing, "width has not changed after Shift+ArrowDown");
+		assert.notStrictEqual(initialHeight, heightAfterResizing, "height has changed after Shift+ArrowDown");
+		assert.strictEqual(initialTop, topAfterResizing, "top position has not changed after Shift+ArrowDown");
+		assert.strictEqual(initialLeft, leftAfterResizing, "left position has not changed after Shift+ArrowDown");
+
+		// Act
+		browser.keys(["Shift", "ArrowRight"]);
+
+		// Assert
+		const widthAfterResizingSecondTime = parseInt(dialog.getCSSProperty("width").value);
+		const heightAfterResizingSecondTime = parseInt(dialog.getCSSProperty("height").value);
+		const topAfterResizingSecondTime = parseInt(dialog.getCSSProperty("top").value);
+		const leftAfterResizingSecondTime = parseInt(dialog.getCSSProperty("left").value);
+	
+		assert.notStrictEqual(widthAfterResizing, widthAfterResizingSecondTime, "width has changed after Shift+ArrowRight");
+		assert.strictEqual(heightAfterResizing, heightAfterResizingSecondTime, "height has not changed after Shift+ArrowRight")
+		assert.strictEqual(topAfterResizing, topAfterResizingSecondTime, "top position has not changed after Shift+ArrowRight");
+		assert.strictEqual(leftAfterResizing, leftAfterResizingSecondTime, "left position has not changed after Shift+ArrowRight");
+
+		const closeResizableDialogButton = browser.$("#resizable-close");
+
+		// Act
+		closeResizableDialogButton.click();
+		openResizableDialogButton.click();
+
+		// Assert
+		const widthAfterReopening = parseInt(dialog.getCSSProperty("width").value);
+		const heightAfterReopening = parseInt(dialog.getCSSProperty("height").value);
+		const topAfterReopening = parseInt(dialog.getCSSProperty("top").value);
+		const leftAfterReopening = parseInt(dialog.getCSSProperty("left").value);
+
+		assert.strictEqual(initialWidth, widthAfterReopening, "width has been reset back to initial");
+		assert.strictEqual(initialHeight, heightAfterReopening, "height has been reset back to initial");
+		assert.strictEqual(initialTop, topAfterReopening, "top position has been reset back to initial");
+		assert.strictEqual(initialLeft, leftAfterReopening, "left position has been reset back to initial");
+
+		// Clean-up
 		closeResizableDialogButton.click();
 	});
 
