0dc9e6c9d1a3a4ab4aa028a6ee00b40289c9dd98
Vladislav Tasev <vladitasev@users.noreply.github.com>
2021-07-26 10:35:47 +0300

    feat(framework): Expose HTML sanitization utilities (#3525)
    Co-authored-by: ilhan <ilhan.myumyun@sap.com>

---
+++ packages/base/src/util/HTMLSanitizer.js
@@ -0,0 +1,7 @@
+import sanitizeHTML from "../sap/base/security/sanitizeHTML.js";
+import URLListValidator from "../sap/base/security/URLListValidator.js";
+
+export {
+	URLListValidator,
+	sanitizeHTML,
+};
