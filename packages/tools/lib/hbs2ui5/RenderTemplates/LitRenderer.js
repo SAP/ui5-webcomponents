fd132d6c3abfb60f7df86e26a289c83693afc5a2
Vladislav Tasev <vladitasev@users.noreply.github.com>
2021-08-06 14:39:21 +0300

    chore(framework): Generate better lit templates (#3608)

--- packages/tools/lib/hbs2ui5/RenderTemplates/LitRenderer.js
+++ packages/tools/lib/hbs2ui5/RenderTemplates/LitRenderer.js
@@ -1,16 +1,10 @@
 const buildRenderer = (controlName, litTemplate) => {
-	return `
-/* eslint no-unused-vars: 0 */
-import { ifDefined } from "lit-html/directives/if-defined.js";
-import { html, svg, repeat, classMap, styleMap, unsafeHTML, scopeTag } from '@ui5/webcomponents-base/dist/renderer/LitRenderer.js';
+	return `/* eslint no-unused-vars: 0 */
+import { html, svg, repeat, classMap, styleMap, ifDefined, unsafeHTML, scopeTag } from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
 
-const main = (context, tags, suffix) => {
-	${litTemplate}
+${litTemplate}
 
-	return block0(context);
-};
-
-export default main;`;
+export default block0;`;
 };
 
 module.exports = {
