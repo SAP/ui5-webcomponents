bef62af04753ff76fe04599770a30e5cb2008ccd
Vladislav Tasev <vladitasev@users.noreply.github.com>
2021-08-12 10:31:58 +0300

    refactor(framework): UI5Element - improvements and fixes (#3604)

--- packages/base/src/types/DataType.js
+++ packages/base/src/types/DataType.js
@@ -11,6 +11,18 @@ class DataType {
 	static isValid(value) {
 	}
 
+	static attributeToProperty(attributeValue) {
+		return attributeValue;
+	}
+
+	static propertyToAttribute(propertyValue) {
+		return `${propertyValue}`;
+	}
+
+	static valuesAreEqual(value1, value2) {
+		return value1 === value2;
+	}
+
 	static generateTypeAccessors(types) {
 		Object.keys(types).forEach(type => {
 			Object.defineProperty(this, type, {
