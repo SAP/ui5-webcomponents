e5658307fee17ea2f4bae5dfdcd49db52fccae17
Vladislav Tasev <vladislav.tasev@sap.com>
2021-09-16 16:06:09 +0300

    base tests

--- packages/base/test/specs/UI5ElementPropertyValidation.js
+++ packages/base/test/specs/UI5ElementPropertyValidation.js
@@ -1,14 +1,14 @@
 const assert = require("chai").assert;
 
 describe("Properties can only have values, restricted to their types", () => {
-	before(() => {
-		browser.url("http://localhost:9191/test-resources/pages/AllTestElements.html");
+	before(async () => {
+		await browser.url("http://localhost:9191/test-resources/pages/AllTestElements.html");
 	});
 
-	it("String property enforced to string", () => {
-		const el = browser.$("#gen");
-		el.setProperty("strProp", 5);
-		assert.strictEqual(el.getProperty("strProp"), "5", "String property is string");
+	it("String property enforced to string", async () => {
+		const el = await browser.$("#gen");
+		await el.setProperty("strProp", 5);
+		assert.strictEqual(await el.getProperty("strProp"), "5", "String property is string");
 	});
 
 });
