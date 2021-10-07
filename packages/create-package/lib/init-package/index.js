1ba2a85fcd15e8cfeb5ea5aef86bd1196521c359
Vladislav Tasev <vladislav.tasev@sap.com>
2021-10-07 15:07:15 +0300

    adjust script

--- packages/create-package/lib/init-package/index.js
+++ packages/create-package/lib/init-package/index.js
@@ -1,67 +1,25 @@
-console.log("HERER");
-process.exit();
-
 const fs = require("fs");
 const path = require("path");
 const mkdirp = require("mkdirp");
-const commandLineArgs = require('command-line-args');
+const prompts = require("prompts");
 const beautify = require("json-beautify");
 
 // String utils
+const capitalizeFirst = str => str.substr(0,1).toUpperCase() + str.substr(1);
 const kebabToCamelCase = string => toCamelCase(string.split("-"));
 const toCamelCase = parts => {
 	return parts.map((string, index) => {
 		return index === 0 ? string.toLowerCase() : string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 	}).join("");
 };
-const capitalizeFirst = str => str.substr(0,1).toUpperCase() + str.substr(1);
 
-// content of package.json
-let packageContent;
+// Validation of user input
+const isNameValid = name => typeof name === "string" && name.match(/^[a-zA-Z0-9\-_]+$/);
+const isPortValid = port => typeof port === "string" && port.match(/^[0-9]+$/);
+const isTagValid = tag => typeof tag === "string" && tag.match(/^[a-z0-9]+?-[a-zA-Z0-9\-_]+?[a-z0-9]$/);
 
-const DEFAULT_PORT = 8080;
-const DEFAULT_TAG = 'my-first-component';
-
-// from where all the files will be copied
-const RESOURCES_DIR = path.join(`${__dirname}`, `resources/`);
-
-// Command line options
-const options = commandLineArgs([
-	{name: 'port', alias: 'p', type: Number},
-	{name: 'tag', type: String},
-]);
-
-// Ensure there is package.json
-try {
-	packageContent = JSON.parse(fs.readFileSync("package.json"));
-} catch (err) {
-	console.log("No package.json found, please run: 'npm init' first");
-	process.exit(1);
-}
-
-// Ensure correct tag
-const tag = options.tag || DEFAULT_TAG;
-if (!tag.includes("-")) {
-	console.log("tag name should contain at least one dash");
-	process.exit(1);
-}
-if (tag.startsWith("ui5-")) {
-	console.log("The ui5- prefix is reserved for the standard UI5 Web Components due to future possible name clashes.");
-	process.exit(1);
-}
-
-const className = capitalizeFirst(kebabToCamelCase(tag));
-
-// All variables that will be replaced in the content of the resources/
-const vars = {
-	INIT_PACKAGE_VAR_NAME: packageContent.name,
-	INIT_PACKAGE_VAR_PORT: options.port || DEFAULT_PORT,
-	INIT_PACKAGE_VAR_TAG: tag,
-	INIT_PACKAGE_VAR_CLASS_NAME: className,
-};
-console.log(vars);
-
-const replaceVarsInFileContent = content => {
+// Utils for building the file structure
+const replaceVarsInFileContent = (vars, content) => {
 	for (let key in vars) {
 		const re = new RegExp(key, "g");
 		content = content.replace(re, vars[key]);
@@ -69,69 +27,118 @@ const replaceVarsInFileContent = content => {
 	return content;
 };
 
-const replaceVarsInFileName = fileName => {
-	return fileName.replace(/MyFirstComponent/, vars.INIT_PACKAGE_VAR_CLASS_NAME)	;
+const replaceVarsInFileName = (vars, fileName) => {
+	return fileName.replace(/MyFirstComponent/, vars.INIT_PACKAGE_VAR_CLASS_NAME);
 };
 
-const copyFile = (sourcePath, destPath) => {
+const copyFile = (vars, sourcePath, destPath) => {
 	let content = fs.readFileSync(sourcePath, {encoding: "UTF-8"});
-	content = replaceVarsInFileContent(content);
-	destPath = replaceVarsInFileName(destPath);
+	content = replaceVarsInFileContent(vars, content);
+	destPath = replaceVarsInFileName(vars, destPath);
 	fs.writeFileSync(destPath, content);
 	console.log(destPath);
 };
 
-const copyResources = sourcePath => {
+const copyResources = (vars, sourcePath) => {
 	const destPath = sourcePath.substr(RESOURCES_DIR.length);
 	const isDir = fs.lstatSync(sourcePath).isDirectory();
 	if (isDir) {
 		if (destPath) {
 			mkdirp.sync(destPath);
-			console.log(destPath);
 		}
 		fs.readdirSync(sourcePath).forEach(file => {
-			copyResources(path.join(sourcePath, file));
+			copyResources(vars, path.join(sourcePath, file));
 		});
 	} else {
-		copyFile(sourcePath, destPath);
+		copyFile(vars, sourcePath, destPath);
 	}
 };
 
-const updatePackageFile = () => {
-	packageContent.scripts = {
-		"clean": "wc-dev clean",
-		"lint": "wc-dev lint",
-		"start": "wc-dev start",
-		"watch": "wc-dev watch",
-		"serve": "wc-dev serve",
-		"build": "wc-dev build",
-		"test": "wc-dev test",
-		"create-ui5-element": "wc-create-ui5-element",
-		"prepublishOnly": "npm run build"
-	};
+// from where all the files will be copied
+const RESOURCES_DIR = path.join(`${__dirname}`, `resources/`);
 
-	// make convinient exports (no dist in component path)
-	// export the rest that is used directly or via require.resolve in scripts
-	packageContent.exports = {
-		"./.port": "./.port",
-		"./src/*": "./src/*",
-		"./dist/*": "./dist/*",
-		"./package.json": "./package.json",
-		"./bundle.js": "./bundle.js",
-		"./*": "./dist/*"
+const initPackage = async () => {
+	let response;
+
+	// Get the name
+	let name = process.argv[2];
+
+	if (!isNameValid(name)) {
+		response = await prompts({
+			type: "text",
+			name: "name",
+			message: `Enter the name of the new package\n A directory with this name will be created, and this will be the "name" field in "package.json":`,
+			validate: isNameValid,
+		});
+		name = response.name;
 	}
 
-	packageContent.ui5 = {
-		webComponentsPackage: true
+	// Get the port
+	response = await prompts({
+		type: "text",
+		name: "port",
+		message: "Choose the port where the test server will run\n Press Enter for default:",
+		validate: isPortValid,
+		initial: "8080",
+	});
+	const port = response.port;
+
+	// Get the tag
+	response = await prompts({
+		type: "text",
+		name: "tag",
+		message: "Enter the name of your first component (must contain at least one dash)\n Press Enter for default:",
+		initial: "my-first-component",
+		validate: isTagValid,
+	});
+	const tag = response.tag;
+
+	const className = capitalizeFirst(kebabToCamelCase(tag));
+
+	// All variables that will be replaced in the content of the resources/
+	const vars = {
+		INIT_PACKAGE_VAR_NAME: name,
+		INIT_PACKAGE_VAR_PORT: port,
+		INIT_PACKAGE_VAR_TAG: tag,
+		INIT_PACKAGE_VAR_CLASS_NAME: className,
 	};
 
+	const packageContent = {
+		name,
+		version: "0.0.1",
+		ui5: {
+			webComponentsPackage: true
+		},
+		scripts: {
+			"clean": "wc-dev clean",
+			"lint": "wc-dev lint",
+			"start": "wc-dev start",
+			"watch": "wc-dev watch",
+			"serve": "wc-dev serve",
+			"build": "wc-dev build",
+			"test": "wc-dev test",
+			"create-ui5-element": "wc-create-ui5-element",
+			"prepublishOnly": "npm run build",
+		},
+		exports: {
+			"./.port": "./.port",
+			"./src/*": "./src/*",
+			"./dist/*": "./dist/*",
+			"./package.json": "./package.json",
+			"./bundle.js": "./bundle.js",
+			"./*": "./dist/*",
+		},
+	};
+
+	// Copy files
+	copyResources(vars, RESOURCES_DIR);
+
+	// Update package.json
 	fs.writeFileSync("package.json", beautify(packageContent, null, 2, 100));
+
+	console.log("Package successfully initialized.\n\n");
+	console.log(`cd ${name}`);
+	console.log(`npm i`);
 };
 
-// Copy files
-copyResources(RESOURCES_DIR);
-
-// Update package.json
-updatePackageFile();
-
-console.log("Package successfully initialized.");
+initPackage();
