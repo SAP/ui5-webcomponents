36f00d539bc5d14603a825c043c24f853068ae7e
Nikolay Deshev <nikolay.deshev@sap.com>
2021-10-14 09:46:34 +0300

    (ui5-slider, ui5-range-slider) Implement Horizon theme styling (#4106)
    * Adapt Sliders to Horizon theme
    
    * Style the tooltips, add RangeSlider styles
    
    * Fix color of the border hover of the handle and the border radius of the tooltip to match the popover's one by specs
    
    * Call getTheme() once

--- packages/main/src/SliderBase.js
+++ packages/main/src/SliderBase.js
@@ -180,6 +180,7 @@ class SliderBase extends UI5Element {
 			sap_belize: "#bfbfbf",
 			sap_belize_hcw: "#000000",
 			sap_belize_hcb: "#ffffff",
+			sap_horizon: "#d5dadd",
 		};
 	}
 
@@ -673,11 +674,11 @@ class SliderBase extends UI5Element {
 		const maxStr = String(this._effectiveMax);
 		const minStr = String(this._effectiveMin);
 		const stepStr = String(this._effectiveStep);
-		const tickmarkWidth = "1px";
 
 		// There is a CSS bug with the 'currentcolor' value of a CSS gradient that does not
 		// respect the variable for more than one theme. It has to be set here for now.
 		const currentTheme = getTheme();
+		const tickmarkWidth = currentTheme === "sap_horizon" ? "2px" : "1px";
 		const currentColor = SliderBase.TICKMARK_COLOR_MAP[currentTheme];
 
 		this._tickmarksAmount = `${maxStr - minStr} / ${stepStr}`;
