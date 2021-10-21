d66e809020e5feea5412afc783aadd36a3e80e6a
Stanislav Bodurov <stbodurov@icloud.com>
2021-10-21 09:58:31 +0300

    refactor(ui5-input): make suggestion-scroll event protected

--- packages/main/src/Input.js
+++ packages/main/src/Input.js
@@ -418,8 +418,7 @@ const metadata = {
 		 * @event sap.ui.webcomponents.main.Input#suggestion-scroll
 		 * @param {Integer} scrollTop The current scroll position.
 		 * @param {HTMLElement} scrollContainer The scroll container.
-		 * @private
-		 * @deprecated - This event has restricted usage and may be removed in future versions.
+		 * @protected
 		 * @since 1.0.0-rc.8
 		 */
 		"suggestion-scroll": {
