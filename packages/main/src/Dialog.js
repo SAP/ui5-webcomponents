6513cad17ee939ae1b40df6fdee2d124b9a1bdb2
Konstantin Kondov <konstantin.kondov@sap.com>
2021-10-22 08:41:09 +0300

    Addressed code review

--- packages/main/src/Dialog.js
+++ packages/main/src/Dialog.js
@@ -286,10 +286,10 @@ class Dialog extends Popup {
 		this._isRTL = this.effectiveDir === "rtl";
 		this.onPhone = isPhone();
 		this.onDesktop = isDesktop();
+		this._detachResizeHandlers();
 	}
 
 	onAfterRendering() {
-		this._detachResizeHandlers();
 		this._attachResizeHandlers();
 	}
 
@@ -301,11 +301,15 @@ class Dialog extends Popup {
 	_attachResizeHandlers() {
 		ResizeHandler.register(this, this._screenResizeHandler);
 		ResizeHandler.register(document.body, this._screenResizeHandler);
+		this._resizeHandlersAttached = true;
 	}
 
 	_detachResizeHandlers() {
-		ResizeHandler.deregister(this, this._screenResizeHandler);
-		ResizeHandler.deregister(document.body, this._screenResizeHandler);
+		if (this._resizeHandlersAttached) {
+			ResizeHandler.deregister(this, this._screenResizeHandler);
+			ResizeHandler.deregister(document.body, this._screenResizeHandler);
+			this._resizeHandlersAttached = false;
+		}
 	}
 
 	_center() {
