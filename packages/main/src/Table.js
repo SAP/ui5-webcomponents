22f8d03692b06cea94b53f36126f8d2d6b6b11c5
Yavor Ivanov <yavor.ivanov@sap.com>
2021-10-27 15:25:26 +0300

    fix(ui5-table): Add missing dependency to Checkbox
    Fixes: https://github.com/SAP/ui5-webcomponents/issues/4094

--- packages/main/src/Table.js
+++ packages/main/src/Table.js
@@ -12,6 +12,7 @@ import isElementInView from "@ui5/webcomponents-base/dist/util/isElementInView.j
 import TableGrowingMode from "./types/TableGrowingMode.js";
 import BusyIndicator from "./BusyIndicator.js";
 import TableMode from "./types/TableMode.js";
+import "./CheckBox.js"; // Ensure the dependency as it is being used in the renderer
 
 // Texts
 import {
