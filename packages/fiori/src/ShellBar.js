3289da466c137d1bfcfb7adc12351d12d5b50053
Filip Siderov <filip.siderov@sap.com>
2021-11-08 14:09:30 +0200

    fix(ui5-shellbar-item): fix JS error on click (#4287)

--- packages/fiori/src/ShellBar.js
+++ packages/fiori/src/ShellBar.js
@@ -726,7 +726,7 @@ class ShellBar extends UI5Element {
 
 		if (refItemId) {
 			const shellbarItem = this.items.find(item => {
-				return item.__id === refItemId;
+				return this.shadowRoot.querySelector(`#${refItemId}`);
 			});
 
 			const prevented = !shellbarItem.fireEvent("item-click", { targetRef: event.target }, true);
