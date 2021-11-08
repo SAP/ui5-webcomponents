85ee1df3008604e4f7edba37bd3658c76ff31232
codefactor <codefactor@users.noreply.github.com>
2021-11-08 06:21:46 -0800

    feat: addCustomCss should use a shared resource

--- packages/base/src/theming/CustomStyle.js
+++ packages/base/src/theming/CustomStyle.js
@@ -1,7 +1,8 @@
 import { reRenderAllUI5Elements } from "../Render.js";
+import getSharedResource from "@ui5/webcomponents-base/dist/getSharedResource.js";
 import EventProvider from "../EventProvider.js";
 
-const eventProvider = new EventProvider();
+const eventProvider = getSharedResource("CustomStyle.eventProvider", new EventProvider());
 const CUSTOM_CSS_CHANGE = "CustomCSSChange";
 
 const attachCustomCSSChange = listener => {
@@ -16,14 +17,31 @@ const fireCustomCSSChange = tag => {
 	return eventProvider.fireEvent(CUSTOM_CSS_CHANGE, tag);
 };
 
-const customCSSFor = {};
+const customCSSFor = getSharedResource("CustomStyle.customCSSFor", {});
+
+// Listen to the eventProvider, in case other copies of this CustomStyle module fire this
+// event, and this copy would therefore need to reRender the ui5 webcomponents; but
+// don't reRender if it was this copy that fired the event to begin with.
+let skipRerender;
+attachCustomCSSChange(() => {
+	if (!skipRerender) {
+		reRenderAllUI5Elements({ tag });
+	}
+});
 
 const addCustomCSS = (tag, css) => {
 	if (!customCSSFor[tag]) {
 		customCSSFor[tag] = [];
 	}
 	customCSSFor[tag].push(css);
-	fireCustomCSSChange(tag);
+
+	skipRerender = true;
+	try {
+		// The event is fired and the attached event listeners are all called synchronously
+		fireCustomCSSChange(tag);
+	} finally {
+		skipRerender = false;
+	}
 
 	return reRenderAllUI5Elements({ tag });
 };
