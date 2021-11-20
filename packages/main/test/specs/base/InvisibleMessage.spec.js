72f651a580475eb9d7f8133dcf2dc8a2bbc559a9
ilhan <ilhan.myumyun@sap.com>
2021-11-20 19:39:44 +0200

    build: fix failure

--- packages/main/test/specs/base/InvisibleMessage.spec.js
+++ packages/main/test/specs/base/InvisibleMessage.spec.js
@@ -1,34 +1,33 @@
 const assert = require("chai").assert;
-const PORT = require("../_port.js");
 
 describe("InvisibleMessage", () => {
 	before(() => {
 		browser.url(`http://localhost:8080/test-resources/pages/base/InvisibleMessage.html`);
 	});
 
-    it("Initial rendering", () => {
-        const politeSpan = browser.$(".ui5-invisiblemessage-polite");
-        const assertiveSpan = browser.$(".ui5-invisiblemessage-assertive");
+	it("Initial rendering", () => {
+		const politeSpan = browser.$(".ui5-invisiblemessage-polite");
+		const assertiveSpan = browser.$(".ui5-invisiblemessage-assertive");
 
-        assert.ok(politeSpan, "Polite span is rendered");
-        assert.ok(assertiveSpan, "Assertive span is rendered");
-    });
+		assert.ok(politeSpan, "Polite span is rendered");
+		assert.ok(assertiveSpan, "Assertive span is rendered");
+	});
 
-    it("String annoucement", () => {
-        const politeSpan = browser.$(".ui5-invisiblemessage-polite");
-        const assertiveSpan = browser.$(".ui5-invisiblemessage-assertive");
-        const button = browser.$("#announce-button");
-        const checkBox = browser.$("#announce-checkbox");
+	it("String annoucement", () => {
+		const politeSpan = browser.$(".ui5-invisiblemessage-polite");
+		const assertiveSpan = browser.$(".ui5-invisiblemessage-assertive");
+		const button = browser.$("#announce-button");
+		const checkBox = browser.$("#announce-checkbox");
 
-        browser.execute(() => {
+		browser.execute(() => {
 			document.getElementById("announce-textarea").value = "announcement";
 		});
 
-        button.click();
-        checkBox.click();
-        button.click();
+		button.click();
+		checkBox.click();
+		button.click();
 
-        assert.ok(politeSpan.getHTML().indexOf("announcement") > -1, "Value has been rendered.");
-        assert.ok(assertiveSpan.getHTML().indexOf("announcement") > -1, "Value has been rendered.");
-    });
+		assert.ok(politeSpan.getHTML().indexOf("announcement") > -1, "Value has been rendered.");
+		assert.ok(assertiveSpan.getHTML().indexOf("announcement") > -1, "Value has been rendered.");
+	});
 });
