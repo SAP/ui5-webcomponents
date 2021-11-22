ab64ac1e2659004349201e6ab7db09508ae425d9
ilhan <ilhan.myumyun@sap.com>
2021-11-22 18:55:29 +0200

    update

--- packages/fiori/test/specs/ShellBar.spec.js
+++ packages/fiori/test/specs/ShellBar.spec.js
@@ -28,6 +28,15 @@ describe("Component Behavior", () => {
 		await browser.url(`http://localhost:${PORT}/test-resources/pages/ShellBar.html`);
 	});
 
+	describe("Аccessibility", () => {
+		it("tests accessibilityTexts property", async () => {
+			const PROFILE_BTN_CUSTOM_TOOLTIP = "John Dow";
+			const sb = await browser.$("#sbAcc");
+
+			assert.strictEqual(await sb.getProperty("_profileText"), PROFILE_BTN_CUSTOM_TOOLTIP,
+				"Profile button tooltip can be cutomized.");
+		});
+	});
 
 	describe("ui5-shellbar menu", () => {
 		it("tests close on content click", async () => {
@@ -456,14 +465,6 @@ describe("Component Behavior", () => {
 				await cancelButton.click();
 				assert.notOk(await searchField.isDisplayed(), "Search is hidden after clicking on the search icon agian");
 			});
-
-			it("tests accessibilityTexts property", async () => {
-				const profileBtn = await browser.$("#sbAcc").shadow$(".ui5-shellbar-image-button");
-				const PROFILE_BTN_CUSTOM_TOOLTIP = "John Dow";
-
-				assert.strictEqual(await profileBtn.getAttribute("title"),
-					PROFILE_BTN_CUSTOM_TOOLTIP, "Profile button tooltip can be cutomized.");
-			});
-		});s
+		});
 	});
 });
