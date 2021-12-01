e715302d8ccf34357f0c53bdfde2e3a25e3d4bf6
ilhan <ilhan.myumyun@sap.com>
2021-12-01 12:30:00 +0200

    chore: add fiori i18n texts to testAssets

--- packages/fiori/bundle.esm.js
+++ packages/fiori/bundle.esm.js
@@ -1,4 +1,5 @@
 import testAssets from "@ui5/webcomponents/bundle.esm.js";
+import * as defaultFioriTexts from "./dist/generated/i18n/i18n-defaults.js";
 
 // FIORI assets
 import "./dist/Assets.js";
@@ -28,4 +29,5 @@ import ViewSettingsDialog from "./dist/ViewSettingsDialog.js";
 import FilterItem from "./dist/FilterItem.js";
 import FilterItemOption from "./dist/FilterItemOption.js";
 
+testAssets.defaultTexts = {...testAssets.defaultTexts, ...defaultFioriTexts };
 export default testAssets;
