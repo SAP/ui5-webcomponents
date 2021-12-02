bc81b5da4f5abc7593b4ac85af8f2d78376af551
Nia Peeva <nia.peeva@sap.com>
2021-12-02 12:53:32 +0200

    fix(ui5-message-strip): make speech output more comprehensive

--- packages/main/test/specs/MessageStrip.spec.js
+++ packages/main/test/specs/MessageStrip.spec.js
@@ -18,3 +18,38 @@ describe("MessageStrip general interaction", () => {
 		assert.strictEqual(await input.getProperty("value"), "3", "Close should be called 3 times");
 	});
 });
+
+describe("ARIA Support", () => {
+	before(async () => {
+		await browser.url(`http://localhost:${PORT}/test-resources/pages/MessageStrip.html`);
+	});
+
+	it("Test close button title text", async () => {
+
+		const closeButton = await browser.$("#messageStrip").shadow$(".ui5-message-strip-close-button").shadow$("button");
+		let resourceBundleText = null;
+
+		resourceBundleText = await browser.executeAsync(done => {
+			const messageStrip = document.getElementById("messageStrip");
+			done(messageStrip.constructor.i18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_CLOSE_BUTTON));
+		});
+
+		assert.strictEqual(await closeButton.getAttribute("title"), resourceBundleText, "Close button title is correct");
+	});
+
+	it("Test hidden text element content", async () => {
+
+		const messageStrip = await browser.$("#messageStrip");
+		let invisibleText = await messageStrip.shadow$(".ui5-hidden-text");
+		let resourceBundleText = null;
+
+		resourceBundleText = await browser.executeAsync(done => {
+			const messageStrip = document.getElementById("messageStrip");
+			const msgStripi18nBundle = messageStrip.constructor.i18nBundle;
+			done(`${msgStripi18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_INFORMATION)} ${msgStripi18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_CLOSABLE)}`);
+		});
+
+		assert.strictEqual(await invisibleText.getText(), resourceBundleText, "Hidden element content is correct");
+	});
+});
+
