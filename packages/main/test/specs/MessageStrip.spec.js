e3fe05f69acb61728a9b85065bb770d121b0714e
niyap <38278268+niyap@users.noreply.github.com>
2021-12-03 13:07:55 +0200

    fix(ui5-message-strip): make speech output more comprehensive
    * fix(ui5-message-strip): make speech output more comprehensive (#4416)
    
    * fix(ui5-message-strip): make speech output more comprehensive
    
    * use uppercase
    
    * fix failing tests

--- packages/main/test/specs/MessageStrip.spec.js
+++ packages/main/test/specs/MessageStrip.spec.js
@@ -15,3 +15,36 @@ describe("MessageStrip general interaction", () => {
 		assert.strictEqual(input.getProperty("value"), "3", "Close should be called 3 times");
 	});
 });
+
+describe("ARIA Support", () => {
+	browser.url("http://localhost:8080/test-resources/pages/MessageStrip.html");
+
+	it("Test close button title text", () => {
+
+		const closeButton = browser.$("#messageStrip").shadow$(".ui5-messagestrip-close-button").shadow$("button");
+		let resourceBundleText = null;
+
+		resourceBundleText = browser.execute(() => {
+			const messageStrip = document.getElementById("messageStrip");
+			return messageStrip.i18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_CLOSE_BUTTON);
+		});
+
+		assert.strictEqual(closeButton.getAttribute("title"), resourceBundleText, "Close button title is correct");
+	});
+
+	it("Test hidden text element content", () => {
+
+		const messageStrip = browser.$("#messageStrip");
+		let invisibleText = messageStrip.shadow$(".ui5-hidden-text");
+		let resourceBundleText = null;
+
+		resourceBundleText = browser.execute(() => {
+			const messageStrip = document.getElementById("messageStrip");
+			const msgStripi18nBundle = messageStrip.i18nBundle;
+			return `${msgStripi18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_INFORMATION)} ${msgStripi18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_CLOSABLE)}`;
+		});
+
+		assert.strictEqual(invisibleText.getText(), resourceBundleText, "Hidden element content is correct");
+	});
+});
+
