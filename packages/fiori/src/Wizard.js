b5f0b82681382b68e1530a8749d1d68200c968d4
Yavor Ivanov <d3xter666@users.noreply.github.com>
2021-12-08 13:34:07 +0200

    Update packages/fiori/src/Wizard.js
    Co-authored-by: Simona Rangelova <32678370+simonarangelova@users.noreply.github.com>

--- packages/fiori/src/Wizard.js
+++ packages/fiori/src/Wizard.js
@@ -176,7 +176,7 @@ const metadata = {
  * </li>
  * <li>[UP], [RIGHT] <br>
  * 		<ul>
- * 			<li> If focus is on a WizardProgressNavAnchor, focus moves forward through the WizardProgressNavAnchor.</li>
+ * 			<li> If focus is on a WizardProgressNavAnchor, focus moves forward to the WizardProgressNavAnchor.</li>
  * 			<li> If focus is on the last WizardProgressNavAnchor, nothing happens – focus does not loop.</li>
  * 			<li> If the visual estate is to small to present all items, the list scrolls accordingly.</li>
  * 			<li> If there are disabled steps or anchors, the disabled items will also get focused by using the arrow keys. (Consistency to menu navigation, where disabled menu items are als focusable).</li>
