ee6c849821bc4acfcef0751ddf87c16bc2f8bdf3
Nikolay Hristov <53182443+NHristov-sap@users.noreply.github.com>
2021-12-09 16:06:02 +0200

    feat(ui5-dynamic-side-content): initial implementation (#4379)

--- packages/fiori/bundle.common.js
+++ packages/fiori/bundle.common.js
@@ -34,6 +34,7 @@ import "./dist/illustrations/tnt/UnsuccessfulAuth";
 
 // FIORI components
 import Bar from "./dist/Bar.js";
+import DynamicSideContent from "./dist/DynamicSideContent.js";
 import FilterItem from "./dist/FilterItem.js";
 import FilterItemOption from "./dist/FilterItemOption.js";
 import FlexibleColumnLayout from "./dist/FlexibleColumnLayout.js";
