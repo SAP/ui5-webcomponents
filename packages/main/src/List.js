12d3e8a6fed6849cc23905e3a959d0ddeef4c0d6
Plamen Ivanov <plamen.ivanov01@sap.com>
2021-12-10 12:11:31 +0200

    feat(ui5-list): preventable item-click event (#4448)
    * feat(ui5-list): preventable item-click event
    
    In this change we introduce the preventable behaviour of the item-click
    event in the List component.
    
    * fixed name of test
    
    * fixed failing tests
    
    additionally the press and click events are now fired before selection

--- packages/main/src/List.js
+++ packages/main/src/List.js
@@ -265,6 +265,7 @@ const metadata = {
 		 * is set to <code>Inactive</code>.
 		 *
 		 * @event sap.ui.webcomponents.main.List#item-click
+		 * @allowPreventDefault
 		 * @param {HTMLElement} item The clicked item.
 		 * @public
 		 */
@@ -882,6 +883,11 @@ class List extends UI5Element {
 	onItemPress(event) {
 		const pressedItem = event.detail.item;
 
+		this.fireEvent("item-press", { item: pressedItem });
+		if (!this.fireEvent("item-click", { item: pressedItem }, true)) {
+			return;
+		}
+
 		if (!this._selectionRequested && this.mode !== ListMode.Delete) {
 			this._selectionRequested = true;
 			this.onSelectionRequested({
@@ -893,9 +899,6 @@ class List extends UI5Element {
 				},
 			});
 		}
-
-		this.fireEvent("item-press", { item: pressedItem });
-		this.fireEvent("item-click", { item: pressedItem });
 
 		this._selectionRequested = false;
 	}
