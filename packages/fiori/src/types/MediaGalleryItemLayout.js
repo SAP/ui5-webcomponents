262fc46f358a1727a0147e5f099f2f7faf34b2ff
Diana Pazheva <diana.pazheva@sap.com>
2021-12-14 13:13:27 +0200

    Update packages/fiori/src/types/MediaGalleryItemLayout.js
    Co-authored-by: Tanya Kostadinova <tanya.kostadinova@sap.com>

--- packages/fiori/src/types/MediaGalleryItemLayout.js
+++ packages/fiori/src/types/MediaGalleryItemLayout.js
@@ -16,7 +16,7 @@ const MediaGalleryItemLayouts = {
 
 	/**
 	 * Recommended to use when the item contains video content.<br>
-	 * When a thumbnail is selected, makes the corresponding enlarged content appear in a wide display area
+	 * When a thumbnail is selected, it makes the corresponding enlarged content appear in a wide display area
 	 * (stretched to fill all of the available width) for optimal user experiance.
 	 * @public
 	 * @type {Wide}
