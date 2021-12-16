fe3530847ef22d24103e5b5389f16aaf87234c7b
Vladislav Tasev <vladislav.tasev@sap.com>
2021-12-16 12:03:52 +0200

    better messages and bugfix

--- packages/base/src/CustomElementsRegistry.js
+++ packages/base/src/CustomElementsRegistry.js
@@ -16,6 +16,8 @@ const Definitions = new Set();
 let Failures = {};
 let failureTimeout;
 
+const UNKNOWN_RUNTIME = "unknown";
+
 const registerTag = tag => {
 	Definitions.add(tag);
 	Tags.set(tag, getCurrentRuntimeIndex());
@@ -30,7 +32,7 @@ const getAllRegisteredTags = () => {
 };
 
 const recordTagRegistrationFailure = tag => {
-	const tagRegRuntimeIndex = Tags.get(tag);
+	const tagRegRuntimeIndex = Tags.get(tag) || UNKNOWN_RUNTIME; // If the tag is taken, but not registered in Tags, then a version before 1.1.0 defined it => use the "unknown" key
 	Failures[tagRegRuntimeIndex] = Failures[tagRegRuntimeIndex] || new Set();
 	Failures[tagRegRuntimeIndex].add(tag);
 
@@ -49,13 +51,30 @@ const displayFailedRegistrations = () => {
 	}
 
 	const allRuntimes = getAllRuntimes();
-	const logger = new Logger(`There are currently ${allRuntimes.length} known UI5 Web Components instances on this HTML page (versions before 1.1.0 are not detected).`);
-	logger.line(`Loading order: ${allRuntimes.map(ver => ver.descriptor).join(", ")}`);
+	const logger = new Logger(allRuntimes.length === 1
+		? `There are currently at least 2 UI5 Web Components instances on this page.`
+		: `There are currently ${allRuntimes.length} known UI5 Web Components instances on this HTML page (versions before 1.1.0 are not detected).`);
+
+	if (allRuntimes.length > 1) {
+		logger.line(`Loading order: ${allRuntimes.map(ver => ver.descriptor).join(", ")}`);
+	}
 
 	Object.keys(Failures).forEach(otherRuntimeIndex => {
 		const currentRuntime = getRuntime();
-		const otherRuntime = getRuntime(otherRuntimeIndex);
-		const comparison = compareCurrentRuntimeWith(otherRuntimeIndex);
+		let comparison;
+		let otherRuntimeVersionPhrase;
+		let otherRuntimeDescriptor;
+
+		if (otherRuntimeIndex === UNKNOWN_RUNTIME) { // version < 1.1.0 defined the tag
+			comparison = 1; // the current runtime is considered newer
+			otherRuntimeVersionPhrase = "the older version that defined them";
+			otherRuntimeDescriptor = "";
+		} else {
+			const otherRuntime = getRuntime(otherRuntimeIndex);
+			otherRuntimeVersionPhrase = `version ${otherRuntime.version}`;
+			otherRuntimeDescriptor = otherRuntime.descriptor;
+			comparison = compareCurrentRuntimeWith(otherRuntimeIndex);
+		}
 
 		let compareWord;
 		if (comparison > 0) {
@@ -65,11 +84,11 @@ const displayFailedRegistrations = () => {
 		} else {
 			compareWord = "the same";
 		}
-		logger.para(`Runtime "${currentRuntime.descriptor}" failed to define ${Failures[otherRuntimeIndex].size} tag(s) as they were defined by a runtime of ${compareWord} version "${otherRuntime.descriptor}": ${setToArray(Failures[otherRuntimeIndex]).sort().join(", ")}.`);
+		logger.para(`Runtime "${currentRuntime.descriptor}" failed to define ${Failures[otherRuntimeIndex].size} tag(s) as they were defined by a runtime of ${compareWord} version "${otherRuntimeDescriptor}": ${setToArray(Failures[otherRuntimeIndex]).sort().join(", ")}.`);
 		if (comparison > 0) {
-			logger.line(`WARNING! If your code uses features of the above web components, unavailable in version ${otherRuntime.version}, it might not work as expected!`);
+			logger.line(`WARNING! If your code uses features of the above web components, unavailable in ${otherRuntimeVersionPhrase}, it might not work as expected!`);
 		} else {
-			logger.line(`Since the above web components were defined by ${comparison < 0 ? "a newer" : "the same"} version runtime, they should be compatible with your code.`);
+			logger.line(`Since the above web components were defined by ${compareWord} version runtime, they should be compatible with your code.`);
 		}
 	});
 
