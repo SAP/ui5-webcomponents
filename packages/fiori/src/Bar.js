8b02048c63907b4a3fb2ea09fe32a850160e789e
Todor Stoyanov <t.stoyanov@sap.com>
2021-12-20 10:07:46 +0200

    fix(ui5-bar): fix rtl support (#4496)

--- packages/fiori/src/Bar.js
+++ packages/fiori/src/Bar.js
@@ -54,6 +54,7 @@ const metadata = {
 		 */
 		startContent: {
 			type: HTMLElement,
+			propertyName: "startContent",
 		},
 
 		/**
@@ -75,6 +76,7 @@ const metadata = {
 		 */
 		endContent: {
 			type: HTMLElement,
+			propertyName: "endContent",
 		},
 	},
 	events: /** @lends sap.ui.webcomponents.fiori.Bar.prototype */ {
@@ -166,6 +168,12 @@ class Bar extends UI5Element {
 				"ui5-bar-root-shrinked": this._shrinked,
 			},
 		};
+	}
+
+	onBeforeRendering() {
+		// Next two row is specific for IE11. Please remove after stop support and edit css file
+		const content = this.startContent.concat(this.middleContent).concat(this.endContent);
+		content.forEach(e => e.classList.add("ui5-bar-content"));
 	}
 
 	onEnterDOM() {
