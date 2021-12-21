e6646b1c12aecaa13f46050c59b807f19275ab34
Todor Stoyanov <t.stoyanov@sap.com>
2021-12-21 11:11:09 +0200

    fixed missing semicolon

--- packages/fiori/src/Bar.js
+++ packages/fiori/src/Bar.js
@@ -170,7 +170,7 @@ class Bar extends UI5Element {
 
 	onBeforeRendering() {
 		// Next row is specific for IE11. Please remove after stop support and edit css file
-		[...this.startContent, ...this.middleContent, ...this.endContent].forEach(element => element.classList.add("ui5-bar-content"))
+		[...this.startContent, ...this.middleContent, ...this.endContent].forEach(element => element.classList.add("ui5-bar-content"));
 	}
 
 	onEnterDOM() {
