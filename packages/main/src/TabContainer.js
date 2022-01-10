cf6546203dd7c0b6dd73719a8ffb33d98429fbd5
Georgi <georgi.minkov@sap.com>
2022-01-10 14:24:23 +0200

    feat(ui5-tabcontainer): show separators in overflow (#4507)
    Show separators in overflow
    
    Co-authored-by: Konstantin Kondov <konstantin.kondov@sap.com>
    Co-authored-by: Teodor Taushanov <teodor.taushanov@sap.com>

--- packages/main/src/TabContainer.js
+++ packages/main/src/TabContainer.js
@@ -146,8 +146,8 @@ const metadata = {
 		/**
 		 * Defines whether the overflow select list is displayed.
 		 * <br><br>
-		 * The overflow select list represents a list, where all tab filters are displayed
-		 * so that it's easier for the user to select a specific tab filter.
+		 * The overflow select list represents a list, where all tabs are displayed
+		 * so that it's easier for the user to select a specific tab.
 		 *
 		 * @type {boolean}
 		 * @defaultvalue false
@@ -461,14 +461,15 @@ class TabContainer extends UI5Element {
 		const selectedTab = this.items[selectedIndex];
 
 		// update selected items
-		this._getTabs().forEach((item, index) => {
-			const selected = selectedIndex === index;
-			item.selected = selected;
+		this.items
+			.forEach((item, index) => {
+				const selected = selectedIndex === index;
+				item.selected = selected;
 
-			if (item._selected) {
-				item._selected = false;
-			}
-		});
+				if (item._selected) {
+					item._selected = false;
+				}
+			});
 
 		if (this.fixed) {
 			this.selectTab(selectedTab, selectedTabIndex);
@@ -568,7 +569,7 @@ class TabContainer extends UI5Element {
 		if (this.responsivePopover.opened) {
 			this.responsivePopover.close();
 		} else {
-			this.responsivePopover.initialFocus = this.responsivePopover.content[0].items[0].id;
+			this.responsivePopover.initialFocus = this.responsivePopover.content[0].items.filter(item => item.classList.contains("ui5-tab-overflow-item"))[0].id;
 			this.responsivePopover.showAt(button);
 		}
 	}
@@ -586,6 +587,9 @@ class TabContainer extends UI5Element {
 	}
 
 	_handleResize() {
+		if (this.responsivePopover && this.responsivePopover.opened) {
+			this.responsivePopover.close();
+		}
 		this._updateMediaRange();
 		this._setItemsForStrip();
 	}
