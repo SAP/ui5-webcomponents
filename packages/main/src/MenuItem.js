29974c4fdad18209b124c1dd57951d9435e5e506
Nikolay Hristov <n.hristov@sap.com>
2022-02-14 10:43:57 +0200

    feat(ui5-menu): Initial implementation

---
+++ packages/main/src/MenuItem.js
@@ -0,0 +1,187 @@
+import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
+import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+
+// Styles
+import MenuItemCss from "./generated/themes/MenuItem.css.js";
+
+/**
+ * @public
+ */
+const metadata = {
+	tag: "ui5-menu-item",
+	properties: /** @lends sap.ui.webcomponents.main.MenuItem.prototype */ {
+		/**
+		 * Defines the text of the tree item.
+		 *
+		 * @public
+		 * @type {String}
+		 * @defaultValue ""
+		 */
+		text: {
+			type: String,
+		},
+
+		/**
+		 * Defines the icon to be displayed as graphical element within the component.
+		 * The SAP-icons font provides numerous options.
+		 * <br><br>
+		 * Example:
+		 *
+		 * See all the available icons in the <ui5-link target="_blank" href="https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html" class="api-table-content-cell-link">Icon Explorer</ui5-link>.
+		 *
+		 * @type {string}
+		 * @defaultvalue ""
+		 * @public
+		 */
+		icon: {
+			type: String,
+		},
+
+		/**
+		 * Defines whether a visual separator should be rendered before the item.
+		 *
+		 * @type {boolean}
+		 * @defaultvalue false
+		 * @public
+		 */
+		startsSection: {
+			type: Boolean,
+		},
+
+		/**
+		 * Defines whether <code>ui5-li</code> is in disabled state.
+		 * <br><br>
+		 * <b>Note:</b> A disabled <code>ui5-li</code> is noninteractive.
+		 * @type {boolean}
+		 * @defaultvalue false
+		 * @protected
+		 */
+		disabled: {
+			type: Boolean,
+		},
+
+		/**
+		 * Indicates if the any of the element siblings have children items.
+		 * @private
+		 */
+		 _siblingsWithChildren: {
+			type: Boolean,
+			noAttribute: true,
+		},
+
+		/**
+		 * Indicates if the any of the element siblings have icon.
+		 * @private
+		 */
+		 _siblingsWithIcon: {
+			type: Boolean,
+			noAttribute: true,
+		},
+
+		/**
+		 * Stores Menu object with submenu items
+		 * @type {object}
+		 * @private
+		 */
+		 _subMenu: {
+			type: Object,
+		},
+
+		/**
+		 * Defines whether the submenu closing must be prevented
+		 * @type {boolean}
+		 * @private
+		 */
+		 _preventSubMenuClose: {
+			type: Boolean,
+			noAttribute: true,
+		},
+	},
+	managedSlots: true,
+	slots: /** @lends sap.ui.webcomponents.main.MenuItem.prototype */ {
+		/**
+		 * Defines the items of this component.
+		 *
+		 * @type {sap.ui.webcomponents.main.MenuItem[]}
+		 * @slot items
+		 * @public
+		 */
+		 "default": {
+			propertyName: "items",
+			type: HTMLElement,
+			invalidateOnChildChange: true,
+		},
+	},
+};
+
+/**
+ * @class
+ *
+ * <h3 class="comment-api-title">Overview</h3>
+ *
+ * <code>ui5-menu-item</code> is the item to use inside a <code>ui5-menu</code>.
+ * An arbitrary hierarchy structure can be represented by recursively nesting menu items.
+ *
+ * <h3>Usage</h3>
+ *
+ * <code>ui5-menu-item</code> is an abstract element, representing a node in a <code>ui5-menu</code>. The menu itself is rendered as a list,
+ * and each <code>ui5-menu-item</code> is represented by a list item (<code>ui5-li</code>) in that list. Therefore, you should only use
+ * <code>ui5-menu-item</code> directly in your apps. The <code>ui5-li</code> list item is internal for the list, and not intended for public use.
+ *
+ * For the <code>ui5-menu-item</code>
+ * <h3>ES6 Module Import</h3>
+ *
+ * <code>import @ui5/webcomponents/dist/MenuItem.js";</code>
+ *
+ * @constructor
+ * @author SAP SE
+ * @alias sap.ui.webcomponents.main.MenuItem
+ * @extends UI5Element
+ * @tagname ui5-menu-item
+ * @public
+ */
+class MenuItem extends UI5Element {
+	static get metadata() {
+		return metadata;
+	}
+
+	static get render() {
+		return litRender;
+	}
+
+	static get styles() {
+		return MenuItemCss;
+	}
+
+	static get dependencies() {
+		return [];
+	}
+
+	get hasChildren() {
+		return !!this.items.length;
+	}
+
+	get hasDummyIcon() {
+		return this._siblingsWithIcon && !this.icon;
+	}
+
+	get subMenuOpened() {
+		return !!Object.keys(this._subMenu).length;
+	}
+
+	/**
+	 * Returns the parent item of the current item.
+	 * @public
+	 * @returns {object} the item that opened the sub menu containing the current item.
+	 */
+	parent() {
+		if (this.parentElement) {
+			return this.parentElement._openedSubMenuItem ? this.parentElement._parentMenuItem : this.parentElement;
+		}
+		return undefined;
+	}
+}
+
+MenuItem.define();
+
+export default MenuItem;
