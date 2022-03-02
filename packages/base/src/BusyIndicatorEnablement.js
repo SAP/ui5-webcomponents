91f37d3ae3a6c4660b0166bd45e97d2a9503fac7
Nayden Naydenov <nayden.naydenov@sap.com>
2022-03-02 14:11:08 +0200

    fix styles, refactor and lint

--- packages/base/src/BusyIndicatorEnablement.js
+++ packages/base/src/BusyIndicatorEnablement.js
@@ -8,20 +8,20 @@ const busyIndicatorMetadata = {
 		__isBusy: {
 			type: Boolean,
 		},
-	}
+	},
 };
 
 const enrichMetadata = UI5Element => {
 	UI5Element.metadata = merge(UI5Element.metadata, busyIndicatorMetadata);
-}
+};
 
 const enrichMethods = UI5ElementPrototype => {
 	Object.defineProperties(UI5ElementPrototype, {
 		"__redirectFocus": { value: true, writable: true },
 		"__suppressFocusBack": {
-			get: function () {
+			get: function () { // eslint-disable-line
 				return {
-					handleEvent: function (e) {
+					handleEvent: function handleFocusOut(e) {
 						if (isTabPrevious(e)) {
 							const beforeElem = this.shadowRoot.querySelector("[busy-indicator-before-span]");
 							this.__redirectFocus = false;
@@ -31,13 +31,13 @@ const enrichMethods = UI5ElementPrototype => {
 					}.bind(this),
 					capture: true,
 					passive: false,
-				}
-			}
+				};
+			},
 		},
 		"isOpenUI5Component": { get: () => { return true; } },
 	});
 
-	UI5ElementPrototype.__suppressFocusIn = function () {
+	UI5ElementPrototype.__suppressFocusIn = function handleFocusIn() {
 		const busyIndicator = this.shadowRoot.querySelector("[busy-indicator]");
 		if (busyIndicator && this.__redirectFocus) {
 			busyIndicator.focus();
@@ -50,4 +50,4 @@ const enrich = UI5Element => {
 	enrichMethods(UI5Element.prototype);
 };
 
-export { enrich };
+export default { enrich };
--- packages/base/src/features/OpenUI5Enablement.js
+++ packages/base/src/features/OpenUI5Enablement.js
@@ -2,65 +2,63 @@ import { registerFeature } from "../FeaturesRegistry.js";
 
 const getBusyStyles = () => {
 	const busyStyles = `
-	.ui5-busy-indicator-circles-wrapper {
-		line-height: 0;
+	.busy-indicator-wrapper {
+		position: relative;
+		height: 100%;
+		width: 100%;
 	}
-	.ui5-busy-indicator-circle {
-		display: inline-block;
-		background-color: currentColor;
-		border-radius: 50%;
-	}
-	.ui5-busy-indicator-overlay {
+
+	.busy-indicator-overlay {
 		display: var(--ui5_web_components_busy_indicator_display);
 		position: absolute;
 		inset: 0;
-		background: rgba(247, 247, 247, 0.72);
+		background: var(--ui5_web_components_busy_indicator_background-color);
+		z-index: 99;
 	}
-	.ui5-busy-indicator-circle::before {
-		content: "";
-		width: 100%;
-		height: 100%;
-		border-radius: 100%;
-	}
-	.ui5-busy-indicator-circle {
-		width: 1rem;
-		height: 1rem;
-	}
-	.ui5-busy-indicator-root {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		position: relative;
-		background-color: inherit;
-	}
-	.ui5-busy-indicator-busy-area {
+
+	.busy-indicator-busy-area {
 		display: var(--ui5_web_components_busy_indicator_display);
 		position: absolute;
 		z-index: 99;
-		left: 0;
-		right: 0;
-		top: 0;
-		bottom: 0;
+		inset: 0;
 		justify-content: center;
 		align-items: center;
 		background-color: inherit;
 		flex-direction: column;
+		color: var(--_ui5_busy_indicator_color);
 	}
-	.ui5-busy-indicator-busy-area:focus {
-		outline: 1px dotted #000;
+
+	:host([__is-busy]) .busy-indicator-wrapper > :nth-child(2) {
+		--ui5_web_components_busy_indicator_display: none;
+	}
+
+	.busy-indicator-busy-area:focus {
+		outline: var(--_ui5_busy_indicator_focus_outline);
 		outline-offset: -.125rem;
 	}
+
+	.busy-indicator-circle {
+		width: 1rem;
+		height: 1rem;
+		display: inline-block;
+		background-color: currentColor;
+		border-radius: 50%;
+	}
+
 	.circle-animation-0 {
 		animation: grow 1.6s infinite cubic-bezier(0.32, 0.06, 0.85, 1.11);
 	}
+
 	.circle-animation-1 {
 		animation: grow 1.6s infinite cubic-bezier(0.32, 0.06, 0.85, 1.11);
 		animation-delay: 200ms;
 	}
+
 	.circle-animation-2 {
 		animation: grow 1.6s infinite cubic-bezier(0.32, 0.06, 0.85, 1.11);
 		animation-delay: 400ms;
 	}
+
 	@keyframes grow {
 		0%, 50%, 100% {
 			-webkit-transform: scale(0.5);
@@ -68,6 +66,7 @@ const getBusyStyles = () => {
 			-ms-transform: scale(0.5);
 			transform: scale(0.5);
 		}
+
 		25% {
 			-webkit-transform: scale(1);
 			-moz-transform: scale(1);
@@ -83,28 +82,25 @@ const setBusyLit = (html, host, templateResult, forStaticArea) => {
 	if (host.isOpenUI5Component && host.__isBusy && !forStaticArea) {
 		templateResult = html`
 		<style>${getBusyStyles()}</style>
-		<div style="position:relative; height: 100%; width: 100%;">
+		<div class="busy-indicator-wrapper">
 			<span tabindex="0" busy-indicator-before-span @focusin=${host.__suppressFocusIn}></span>
 			${templateResult}
-				<div class="ui5-busy-indicator-overlay" part="busy-indicator"></div>
-				<div
-				part="busy-indicator"
-				busy-indicator
-				class="ui5-busy-indicator-busy-area"
+			<div class="busy-indicator-overlay"></div>
+			<div busy-indicator
+				class="busy-indicator-busy-area"
 				tabindex="0"
 				role="progressbar"
 				@keydown=${host.__suppressFocusBack}
 				aria-valuemin="0"
 				aria-valuemax="100"
-				aria-valuetext="Busy"
-				>
-					<div class="ui5-busy-indicator-circles-wrapper">
-						<div class="ui5-busy-indicator-circle circle-animation-0"></div>
-						<div class="ui5-busy-indicator-circle circle-animation-1"></div>
-						<div class="ui5-busy-indicator-circle circle-animation-2"></div>
-					</div>
+				aria-valuetext="Busy">
+				<div>
+					<div class="busy-indicator-circle circle-animation-0"></div>
+					<div class="busy-indicator-circle circle-animation-1"></div>
+					<div class="busy-indicator-circle circle-animation-2"></div>
 				</div>
-			</div>`;
+			</div>
+		</div>`;
 	}
 
 	return templateResult;
