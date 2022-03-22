1fdf4157bc3313a3ee10ac681ff14aa2f57d0b09
Nayden Naydenov <31909318+nnaydenow@users.noreply.github.com>
2022-03-22 13:21:04 +0200

    fix(ui5-li): correct focus handling (#4935)

--- packages/main/src/ListItemBase.js
+++ packages/main/src/ListItemBase.js
@@ -89,7 +89,7 @@ class ListItemBase extends UI5Element {
 	}
 
 	_onfocusin(event) {
-		if (event.isMarked === "button" || event.isMarked === "link") {
+		if (event.target !== this.getFocusDomRef()) {
 			return;
 		}
 
