91b345ee0d021b92a3c4e49ac4f01b721a6c63ea
Georgi Minkov <georgi.minkov@sap.com>
2022-03-25 16:25:43 +0200

    fix(ui5-card-header): prevent events from action slot to trigger header's click event
    Part of: #4891

--- packages/main/src/CardHeader.js
+++ packages/main/src/CardHeader.js
@@ -241,15 +241,17 @@ class CardHeader extends UI5Element {
 	}
 
 	_headerClick(event) {
-		event.stopImmediatePropagation(); // prevents the native browser "click" event from firing
+		// prevents the native browser "click" event from firing
+		event.preventDefault();
+		event.stopImmediatePropagation();
 
-		if (this.interactive) {
+		if (this.interactive && event.target === event.currentTarget) {
 			this.fireEvent("click");
 		}
 	}
 
 	_headerKeydown(event) {
-		if (!this.interactive) {
+		if (!this.interactive || event.target !== event.currentTarget) {
 			return;
 		}
 
@@ -269,7 +271,7 @@ class CardHeader extends UI5Element {
 	}
 
 	_headerKeyup(event) {
-		if (!this.interactive) {
+		if (!this.interactive || event.target !== event.currentTarget) {
 			return;
 		}
 
