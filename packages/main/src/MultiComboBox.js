77b84df5caa4ca4e09ea38c403486a8b49ecbc03
Martin Hristov <martin.r.hristov@gmail.com>
2022-05-16 11:48:40 +0300

    fix(ui5-multi-combobox): set correct height to items checkboxes
    FIXES: https://github.com/SAP/ui5-webcomponents/issues/5126

--- packages/main/src/MultiComboBox.js
+++ packages/main/src/MultiComboBox.js
@@ -72,6 +72,7 @@ import styles from "./generated/themes/MultiComboBox.css.js";
 import ResponsivePopoverCommonCss from "./generated/themes/ResponsivePopoverCommon.css.js";
 import ValueStateMessageCss from "./generated/themes/ValueStateMessage.css.js";
 import SuggestionsCss from "./generated/themes/Suggestions.css.js";
+import MultiComboBoxPopover from "./generated/themes/MultiComboBoxPopover.css.js";
 
 /**
  * @public
@@ -431,7 +432,7 @@ class MultiComboBox extends UI5Element {
 	}
 
 	static get staticAreaStyles() {
-		return [ResponsivePopoverCommonCss, ValueStateMessageCss, SuggestionsCss];
+		return [ResponsivePopoverCommonCss, ValueStateMessageCss, SuggestionsCss, MultiComboBoxPopover];
 	}
 
 	static get dependencies() {
