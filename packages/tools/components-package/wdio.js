8517ecd23dba6814a3653b6e9382e146defa0f85
Vladislav Tasev <vladislav.tasev@sap.com>
2022-05-18 17:10:38 +0300

    chore: try 15

--- packages/tools/components-package/wdio.js
+++ packages/tools/components-package/wdio.js
@@ -40,7 +40,7 @@ exports.config = {
 	// and 30 processes will get spawned. The property handles how many capabilities
 	// from the same test should run tests.
 	//
-	maxInstances: 5,
+	maxInstances: 15,
 	//
 	// If you have trouble getting all important capabilities together, check out the
 	// Sauce Labs platform configurator - a great tool to configure your capabilities:
