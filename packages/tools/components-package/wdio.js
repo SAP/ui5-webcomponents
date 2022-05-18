3f471c1f704cef0ed2d8f74ca7406d585a75689e
ilhan orhan <ilhan.myumyun@sap.com>
2022-05-18 10:59:03 +0300

    test: run mobile tests on DEV tools emulator (#5107)
    Currently, we test on headless, desktop Chrome and now with this small changes in the set up we can run tests on mobile emulators.
    
    Fixes: #2437
    Closes: #2437

--- packages/tools/components-package/wdio.js
+++ packages/tools/components-package/wdio.js
@@ -99,7 +99,7 @@ exports.config = {
 	// Services take over a specific job you don't want to take care of. They enhance
 	// your test setup with almost no effort. Unlike plugins, they don't add new
 	// commands. Instead, they hook themselves up into the test process.
-	services: ['chromedriver'],
+	services: ['chromedriver', 'devtools'],
 	// options
 	chromeDriverArgs: ['--port=9515'], // default
 	// Framework you want to run your specs with.
