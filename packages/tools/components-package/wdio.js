9bf22d97ba125015de9c20c5104389d16b4d20ea
Vladislav Tasev <vladislav.tasev@sap.com>
2022-05-19 15:39:47 +0300

    chore: run again

--- packages/tools/components-package/wdio.js
+++ packages/tools/components-package/wdio.js
@@ -253,7 +253,7 @@ exports.config = {
 			await browser.executeAsync(function (done) {
 				setTimeout(function() {
 					window["sap-ui-webcomponents-bundle"].renderFinished().then(done);
-				}, 0);
+				}, 1);
 			});
 		}
 	},
@@ -333,7 +333,7 @@ exports.config = {
 			await browser.executeAsync( function (done) {
 				setTimeout(function() {
 					window["sap-ui-webcomponents-bundle"].renderFinished().then(done);
-				}, 0);
+				}, 1);
 			});
 		}
 	},
