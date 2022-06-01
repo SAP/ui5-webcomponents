c64becec8bfe2c91c38ce801ceff5ee27043d477
Vladislav Tasev <vladislav.tasev@sap.com>
2022-06-01 14:04:36 +0300

    chore: flat suite structure

--- packages/main/config/wdio.conf.js
+++ packages/main/config/wdio.conf.js
@@ -3,10 +3,10 @@ const wdio = require("@ui5/webcomponents-tools/components-package/wdio.js");
 wdio.config.suites = {
 	"suite1": [
 		'./test/specs/base/*.spec.js',
-		'./test/specs/suite1/*.spec.js',
+		'./test/specs/[A-D]*.spec.js',
 	],
 	"suite2": [
-		'./test/specs/suite2/*.spec.js',
+		'./test/specs/[^A-D]*.spec.js',
 	],
 };
 
