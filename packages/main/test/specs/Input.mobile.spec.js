edcdd24c8e95047559b49cef4eea938f20e21d97
Nikolay Deshev <nikolay.deshev@sap.com>
2022-06-01 11:12:32 +0300

    fix(ui5-input): fix typeahead on mobile devices (#5292)
    * fix(ui5-input): fix typeahead on mobile devices
    
    The component no longer throws an exception when the input has suggestions
    and the typeahead functionality is adjusted.
    
    * fix(ui5-input): fix typeahead on mobile devices
    
    add tests
    
    * fix(ui5-input): fix typeahead on mobile devices
    
    try different DOM selectors in the mobile tests
    
    * fix(ui5-input): fix typeahead on mobile devices
    
    adjust mobile tests
    
    * fix(ui5-input): fix typeahead on mobile devices
    
    adjust tests
    
    * fix(ui5-input): fix typeahead on mobile devices
    
    adjust mobile tests

---
+++ packages/main/test/specs/Input.mobile.spec.js
@@ -0,0 +1,37 @@
+const assert = require("chai").assert;
+const PORT = require("./_port.js");
+
+describe("Typeahead", () => {
+	before(async () => {
+		await browser.url(`http://localhost:${PORT}/test-resources/pages/Input.html`);
+		await browser.emulateDevice('iPhone X');
+	});
+
+	it("Should autocomplete the first matched suggestion item", async () => {
+		const input = await browser.$("#myInput2");
+		const sExpected = "Cozy";
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#myInput2")
+
+		await input.scrollIntoView();
+		await input.click();
+
+		const dialogInput = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover").$(".ui5-input-inner-phone");
+		await dialogInput.keys("c");
+		assert.strictEqual(await dialogInput.getProperty("value"), sExpected, "Value is autocompleted");
+	});
+
+	it("Should not perform typeahead when it is disabled", async () => {
+		await browser.url(`http://localhost:${PORT}/test-resources/pages/Input.html`);
+
+		const input = await browser.$("#input-disabled-autocomplete");
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#input-disabled-autocomplete")
+
+		await input.scrollIntoView();
+		await input.click();
+
+		const dialogInput = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover").$(".ui5-input-inner-phone");
+		await dialogInput.keys("c");
+
+		assert.strictEqual(await dialogInput.getProperty("value"), "c", "Value is not autocompleted");
+	});
+});
