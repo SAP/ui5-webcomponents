2eebed012dd7b190d2f01bd59378281a46f78f57
Vladislav Tasev <vladislav.tasev@sap.com>
2022-06-01 14:12:40 +0300

    chore: different letters

--- packages/main/config/wdio.conf.js
+++ packages/main/config/wdio.conf.js
@@ -3,10 +3,10 @@ const wdio = require("@ui5/webcomponents-tools/components-package/wdio.js");
 wdio.config.suites = {
 	"suite1": [
 		'./test/specs/base/*.spec.js',
-		'./test/specs/[A-D]*.spec.js',
+		'./test/specs/[A-I]*.spec.js',
 	],
 	"suite2": [
-		'./test/specs/[^A-D]*.spec.js',
+		'./test/specs/[^A-I]*.spec.js',
 	],
 };
 
