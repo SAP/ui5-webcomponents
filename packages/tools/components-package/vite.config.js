97eaa2de8e5968774ed11ef90eca050dd2530c42
Peter Skelin <petar.skelin@sap.com>
2022-06-20 16:51:31 +0300

    fix: more review comments

--- packages/tools/components-package/vite.config.js
+++ packages/tools/components-package/vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
-const { defineConfig } = require('vite')
+const { defineConfig } = require('vite');
 const virtualIndex = require("../lib/dev-server/virtual-index-html-plugin.js");
 
 module.exports = defineConfig(async () => {
