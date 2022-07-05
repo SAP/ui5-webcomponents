9719bcf931f5e89c4ce9fe4fc6503e218f511edb
hinzzx <abvsuxhard@gmail.com>
2022-07-05 08:38:47 +0300

    fixed errors

--- packages/fiori/src/Login.js
+++ packages/fiori/src/Login.js
@@ -13,118 +13,118 @@ import LoginTemplate from "./generated/templates/LoginTemplate.lit.js";
 import LoginCSS from "./generated/themes/Login.css.js";
 
 const metadata = {
-    tag: "ui5-login",
-    properties: {
-        email: {
-            type: Boolean,
-        },
-        headerText: {
-            type: String,
-        },
-        subHeaderText: {
-            type: String,
-        },
-        disabled: {
-            type: Boolean,
-        },
-        error: {
-            type: Boolean,
-        },
-    },
+	tag: "ui5-login",
+	properties: {
+		email: {
+			type: Boolean,
+		},
+		headerText: {
+			type: String,
+		},
+		subHeaderText: {
+			type: String,
+		},
+		disabled: {
+			type: Boolean,
+		},
+		error: {
+			type: Boolean,
+		},
+	},
 
-    managedSlots: true,
+	managedSlots: true,
 
-    slots: {
-        header: {
-            type: HTMLElement,
-        },
-        subHeader: {
-            type: HTMLElement,
-        },
-        errorMessageTitle: {
-            type: HTMLElement,
-        },
-        errorMessage: {
-            type: HTMLElement,
-        },
-    },
+	slots: {
+		header: {
+			type: HTMLElement,
+		},
+		subHeader: {
+			type: HTMLElement,
+		},
+		errorMessageTitle: {
+			type: HTMLElement,
+		},
+		errorMessage: {
+			type: HTMLElement,
+		},
+	},
 
-    events: {},
+	events: {},
 };
 
 class Login extends UI5Element {
-    constructor() {
-        super();
-    }
+	constructor() {
+		super();
+	}
 
-    static get metadata() {
-        return metadata;
-    }
+	static get metadata() {
+		return metadata;
+	}
 
-    static get render() {
-        return litRender;
-    }
+	static get render() {
+		return litRender;
+	}
 
-    static get template() {
-        return LoginTemplate;
-    }
+	static get template() {
+		return LoginTemplate;
+	}
 
-    static get dependencies() {
-        return [Input, Button, Label];
-    }
+	static get dependencies() {
+		return [Input, Button, Label];
+	}
 
-    static get styles() {
-        return LoginCSS;
-    }
+	static get styles() {
+		return LoginCSS;
+	}
 
-    get _displayHeader() {
-        return this.header.length || this.headerText;
-    }
+	get _displayHeader() {
+		return this.header.length || this.headerText;
+	}
 
-    get _displayError() {
-        return this.errorMessage.length || this.error;
-    }
+	get _displayError() {
+		return this.errorMessage.length || this.error;
+	}
 
-    displayUser() {
-        const username = this.shadowRoot.querySelector("#ui5-usernameID").value;
-        const password = this.shadowRoot.querySelector("#ui5-passwordID").value;
+	displayUser() {
+		const username = this.shadowRoot.querySelector("#ui5-usernameID").value;
+		const password = this.shadowRoot.querySelector("#ui5-passwordID").value;
 
-        /** 
+		/** 
 		Checks if email property is passed,
 		if so, then it checks if the username is an email address,
 		if it is an email, then it checks if it is a valid email with @isValidEmail method,
 		if it is not, then it sets error property to true, which displays the error message. 
 		**/
-        if (this.email) {
-            if (isValidEmail(username) && password) {
-                this.error = false;
-            } else if (!isValidEmail(username) && password) {
-                this.error = true;
-            } else if (isValidEmail(username) && !password) {
-                this.error = true;
-            } else {
-                this.error = true;
-            }
-        }
+		if (this.email) {
+			if (isValidEmail(username) && password) {
+				this.error = false;
+			} else if (!isValidEmail(username) && password) {
+				this.error = true;
+			} else if (isValidEmail(username) && !password) {
+				this.error = true;
+			} else {
+				this.error = true;
+			}
+		}
 
-        if (!this.email) {
-            if (username && password) {
-                this.error = false;
-            } else if (!username && password) {
-                this.error = true;
-            } else if (username && !password) {
-                this.error = true;
-            } else {
-                this.error = true;
-            }
-        }
+		if (!this.email) {
+			if (username && password) {
+				this.error = false;
+			} else if (!username && password) {
+				this.error = true;
+			} else if (username && !password) {
+				this.error = true;
+			} else {
+				this.error = true;
+			}
+		}
 
-        function isValidEmail(emailAddress) {
-            var re =
-                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(String(emailAddress).toLowerCase());
-        }
-    }
+		function isValidEmail(emailAddress) {
+			var re =
+				/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(String(emailAddress).toLowerCase());
+		}
+	}
 }
 
 Login.define();
