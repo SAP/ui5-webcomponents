438458ce882caa7a717d9fa3f43bb53b36405397
hinzzx <abvsuxhard@gmail.com>
2022-07-13 16:27:12 +0300

    fix(ui5-combobox): formatting changes

--- packages/main/test/specs/ComboBox.mobile.spec.js
+++ packages/main/test/specs/ComboBox.mobile.spec.js
@@ -1,29 +1,25 @@
 const assert = require("chai").assert;
 
-describe("Typeahead", () => {
+describe("Test for the issue", () => {
 before(async () => {
         await browser.url(`test/pages/ComboBox.html`);
         await browser.emulateDevice("iPhone X");
     });
 
-    it("clicking the close btn should empty the input, if item is not selected or if text is passed without selection", async () => {
+    it ("clicking the close btn should empty the input, if item is not selected or if text is passed without selection", async () => {
     const cb = await browser.$("#combo2");
     const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#combo2");
     
-
     await cb.click();
 
-    const resPopOver = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
-    const dialogInput = await resPopOver.$(".ui5-input-inner-phone");
-    const closeBtn = await resPopOver.$(".ui5-responsive-popover-close-btn");
+    const resPopover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+    const dialogInput = await resPopover.$(".ui5-input-inner-phone");
+    const closeBtn = await resPopover.$(".ui5-responsive-popover-close-btn");
     await dialogInput.click();
     await dialogInput.keys("c");
     await closeBtn.click();
 
     assert.strictEqual(await cb.getProperty("value"), "", "Value is empty");
 
-});
-
-
-
+    });
 });
