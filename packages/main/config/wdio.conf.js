b62a98b99faf3f7bf38e3514e075d44c3d6a30bf
hinzzx <abvsuxhard@gmail.com>
2022-07-20 10:36:38 +0300

    fix(ui5-combobox): fixed additional issues and added tests

--- packages/main/config/wdio.conf.js
+++ packages/main/config/wdio.conf.js
@@ -1,13 +1,14 @@
 const wdio = require("@ui5/webcomponents-tools/components-package/wdio.js");
-
 wdio.config.suites = {
-	"suite1": [
-		'./test/specs/base/*.spec.js',
-		'./test/specs/[A-I]*.spec.js',
-	],
-	"suite2": [
-		'./test/specs/[^A-I]*.spec.js',
-	],
+    "suite1": [
+        './test/specs/base/*.spec.js',
+        './test/specs/[A-I]*.spec.js',
+    ],
+    "suite2": [
+       './test/specs/[^A-I]*.spec.js',
+    ],
 };
 
+// From: [‘--disable-gpu’, ‘--headless’]
+wdio.config.capabilities[0]['goog:chromeOptions'].args = ['--disable-gpu']; 
 module.exports = wdio;
--- packages/tools/components-package/wdio.js
+++ packages/tools/components-package/wdio.js
@@ -127,7 +127,7 @@ exports.config = {
 	// See the full list at http://mochajs.org/
 	mochaOpts: {
 		ui: 'bdd',
-		timeout: 60000
+		timeout: 600000
 	},
 	//
 	// =====
