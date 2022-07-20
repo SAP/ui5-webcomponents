c62adf12faea6b8aba30f51341b356a25f5d144e
hinzzx <abvsuxhard@gmail.com>
2022-07-20 13:26:14 +0300

    fix(ui5-combobox): fixed tabulations

--- packages/main/config/wdio.conf.js
+++ packages/main/config/wdio.conf.js
@@ -1,11 +1,11 @@
 const wdio = require("@ui5/webcomponents-tools/components-package/wdio.js");
 wdio.config.suites = {
-    "suite1": [
-        './test/specs/base/*.spec.js',
-        './test/specs/[A-I]*.spec.js',
-    ],
-    "suite2": [
-       './test/specs/[^A-I]*.spec.js',
-    ],
+	"suite1": [
+		'./test/specs/base/*.spec.js',
+		'./test/specs/[A-I]*.spec.js',
+	],
+	"suite2": [
+		'./test/specs/[^A-I]*.spec.js',
+	],
 };
 module.exports = wdio;
