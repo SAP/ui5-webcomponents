8bb0abbc2821b3e2523a0884095a5f3d4f06186b
Richard Chan <chanrichardsf@gmail.com>
2022-08-04 15:11:06 -0700

    feat: added quotes around filters keys objects

--- packages/fiori/src/ViewSettingsDialog.js
+++ packages/fiori/src/ViewSettingsDialog.js
@@ -683,7 +683,7 @@ class ViewSettingsDialog extends UI5Element {
 	 * The <code>ui5-view-settings-dialog</code> throws an event called "before-open", this can be used as trigger point.
 	 * The object should have the following format:
 	 * <code>
-	 *  {sortOrder: "Ascending", sortBy: "Name", filters: [{Filter 1: ["Some filter 1", "Some filter 2"]}, {Filter 2: ["Some filter 4"]}]}
+	 *  {sortOrder: "Ascending", sortBy: "Name", filters: [{"Filter 1": ["Some filter 1", "Some filter 2"]}, {"Filter 2": ["Some filter 4"]}]}
 	 * </code>
    * @param {Object} settings - predefined settings.
    * @param {string} settings.sortOrder - sort order
