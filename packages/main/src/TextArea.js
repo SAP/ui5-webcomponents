9f593e653de132e4c94961f7f30c332eb79f30db
I521896 <nikolay.deshev@sap.com>
2022-08-24 17:14:46 +0300

    fix(ui5-textarea): revert
    This reverts commit 7200061b374bbc5fbc1e9536ffda81d2efd426e4.

--- packages/main/src/TextArea.js
+++ packages/main/src/TextArea.js
@@ -536,7 +536,7 @@ class TextArea extends UI5Element {
 		if (this.showExceededText) {
 			const maxLength = this.maxlength || 0;
 
-			if (maxLength !== null) {
+			if (maxLength) {
 				leftCharactersCount = maxLength - this.value.length;
 
 				if (leftCharactersCount >= 0) {
