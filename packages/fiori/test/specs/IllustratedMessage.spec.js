51165df780d92404c63e16a09dbfe579c533456c
yanaminkova <32466553+yanaminkova@users.noreply.github.com>
2022-09-20 08:44:53 +0300

    feat(ui5-illustrated-message): accessible-name-ref added (#5765)

--- packages/fiori/test/specs/IllustratedMessage.spec.js
+++ packages/fiori/test/specs/IllustratedMessage.spec.js
@@ -27,4 +27,37 @@ describe("IllustratedMessage 'size' property", () => {
 		// Assert
 		assert.strictEqual(illustratedMsgSize, "Auto", "'size' should be equal to 'Auto' when invalid value is passed");
 	});
+
+
+});
+
+describe("Accessibility", () => {
+	before(async () => {
+		await browser.url(`test/pages/IllustratedMessage.html`);
+	});
+
+
+	it("accessible-name-ref", async () => {
+		// Arrange
+		const illustratedMsg = await browser.$("#illustratedMsg2"),
+			  illustratedMsgSVG = await browser.$("#illustratedMsg2").shadow$('.ui5-illustrated-message-illustration svg'),
+			  EXPECTED_ARIA_LABEL_NAME = "Text from aria-labelledby";
+
+		// Assert
+		assert.strictEqual(await illustratedMsgSVG.getAttribute("aria-label"), EXPECTED_ARIA_LABEL_NAME, "aria-label is set");
+
+		// Act
+		await illustratedMsg.removeAttribute("accessible-name-ref");
+
+		// Assert
+		assert.strictEqual(await illustratedMsgSVG.getAttribute("aria-label"), 'undefined' , "aria-label is removed");
+
+		// Act
+		await illustratedMsg.setAttribute("accessible-name-ref", "lbl");
+
+		// Assert
+		assert.strictEqual(await illustratedMsgSVG.getAttribute("aria-label"), EXPECTED_ARIA_LABEL_NAME, "aria-label is set");
+
+	});
+
 });
