cc9d6066b6067f1a23faa0cd1d55d5e0d317c4e6
Ivaylo Plashkov <ivaylo.plashkov@sap.com>
2022-10-27 14:51:57 +0300

    fix(ui5-table-row): update samples

--- packages/main/test/specs/Table.spec.js
+++ packages/main/test/specs/Table.spec.js
@@ -31,11 +31,6 @@ describe("Table general interaction", () => {
 		assert.ok(await noDataRow.isExisting(), 'noData div is present');
 	});
 
-	it("tests if table with more columns than cells is rendered", async () => {
-		const tblLessCells = await browser.$("#tblLessCells");
-		assert.ok(await tblLessCells.isExisting(), 'table with more columns is rendered without JS errors.');
-	});
-
 	it("tests if popinChange is fired when min-width is reacted (500px)", async () => {
 		let tableLabel = await browser.$("#tableLabel");
 		const btn = await browser.$("#size-btn-500");
