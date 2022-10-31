052e8e3c2adf6337a36c21a431facb368a9a8e60
Plamen Ivanov <plamen.ivanov01@sap.com>
2022-10-31 17:06:41 +0200

    feat(storybook): add script for generating stories (#5898)
    There two new scripts / tasks.
    The first task is 'prepare:samples:initial' which converts UI5 WC samples (*.sample.html) into stories (*.stories.mdx). If a sample isn't present, a placeholder/dummy story is generated.
    The second task is 'prepare:samples:next' which generates argsTypes.js and Description.md for the corresponding *stories.mdx file.

--- packages/playground/build-scripts-storybook/samples-prepare.js
+++ packages/playground/build-scripts-storybook/samples-prepare.js
@@ -1,56 +1,33 @@
 const fs = require('fs/promises');
+const fsDir = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
 
-const EXCLUDE_LIST = [
-	"AvatarGroup.sample.html",
-	"BarcodeScannerDialog.sample.html",
-	"BusyIndicator.sample.html",
-	"Button.sample.html",
-	"Card.sample.html",
-	"Carousel.sample.html",
-	"CheckBox.sample.html",
-	"ComboBox.sample.html",
-	"DateTimePicker.sample.html",
-	"Dialog.sample.html",
-	"FileUploader.sample.html",
-	"FlexibleColumnLayout.sample.html",
-	"IllustratedMessage.sample.html",
-	"Input.sample.html",
-	// "List.sample.html",
-	"MultiInput.sample.html",
-	"MessageStrip.sample.html",
-	"NotificationListGroupItem.sample.html",
-	"Page.sample.html",
-	"NotificationListItem.sample.html",
-	"Panel.sample.html",
-	"Popover.sample.html",
-	"ProductSwitch.sample.html",
-	"ResponsivePopover.sample.html",
-	"ShellBar.sample.html",
-	"SideNavigation.sample.html",
-	"TabContainer.sample.html",
-	"Table.sample.html",
-	"Toast.sample.html",
-	"Tree.sample.html",
-	"UploadCollection.sample.html",
-	"Wizard.sample.html",
-]
+const UI5WC_TO_STORYBOOK_TYPES_MAP = {
+	'string': 'text',
+	'String': 'text',
+	'array': 'object',
+	'Array': 'object',
+	'boolean': 'boolean',
+	'object': 'object',
+	'Object': 'object',
+	'Integer': 'number',
+	'Float': 'number',
+	'Number': 'number',
+	'File': 'file',
+	'HTMLElement': 'text',
+	'CSSColor': 'color'
+}
+
+const STORIES_ROOT_FOLDER_NAME = '_storiesGenerated';
 
 const main = async () => {
-	const template = await fs.readFile(path.join(__dirname, '../stories/template.mdx'), 'utf8');
 	const packages = [
 		"fiori",
 		"main",
 	];
 
 	const components = [];
-
-	// TODO: handle new components 
-	// Add new components here
-	const newComponents = [
-		"Menu",
-	];
 
 	packages.forEach(async package => {
 		const samplesPath = `../${package}/test/samples/`;
@@ -69,17 +46,15 @@ const main = async () => {
 
 	components.sort();
 
+	const baseAPI = JSON.parse((await fs.readFile(`../base/dist/api.json`)).toString());
+
 	packages.forEach(async package => {
 		const samplesPath = `../${package}/test/samples/`;
 		const api = JSON.parse((await fs.readFile(`../${package}/dist/api.json`)).toString());
 
-
 		const files = await fs.readdir(samplesPath);
 
 		files.forEach(async (file) => {
-			if (EXCLUDE_LIST.includes(file)) {
-				return;
-			}
 			console.error('Preparing samples...', file);
 
 			//Copy samples
@@ -89,79 +64,112 @@ const main = async () => {
 			let result = (await fs.readFile(path.join(samplesPath, file))).toString();
 			const $ = cheerio.load(result);
 
-			const snippets = [];
-
-			$('.snippet').each((i, snippet) => {
-				const section = $(snippet).parent();
-				section.find("pre").remove();
-
-				snippets.push(section.html().trim());
-			});
-
-			await fs.writeFile(path.join(process.cwd(), `/docs/components/${file.replace('.sample.html', '.stories.mdx')}`), getStory(api, file.split('.')[0], snippets))
+			await generateStoryDoc(package, file, api);
+			const appendDocs = api.symbols.find(s => s.module === file.split('.')[0]).appenddocs;
+			if (appendDocs) {
+				for (const currSymbol of appendDocs.split(' ')) {
+					await generateStoryDoc(package, currSymbol + '.sample.html', api)
+				}
+			}
 		});
 	});
 
-	function getAPI(api, module) {
-		const args = {};
-		const moduleAPI = api.symbols.find(s => s.module === module);
+	async function generateStoryDoc(package, file, api) {
 
+		const apiData = getAPIData(api, file.split('.')[0]);
+		const storyDir = createStoryDirectory(package, file);
+
+		await fs.writeFile(storyDir + '/Description.md', apiData.storyDescription);
+		await fs.writeFile(storyDir + '/argsTypes.js', `export default ` + apiData.storyArgsTypes);
+	};
+
+	function createStoryDirectory(package, file) {
+		let storyDir = path.join(process.cwd(), `/${STORIES_ROOT_FOLDER_NAME}/${package}`);
+
+		if (!fsDir.existsSync(storyDir)){ // check if the package folder exists and creates one if it doesn't
+			fsDir.mkdirSync(storyDir);
+		}
+
+		storyDir += `/${file.substring(0, file.indexOf('.'))}`;
+
+		if (!fsDir.existsSync(storyDir)){ // check if the component folder exists and creates one if it doesn't
+			fsDir.mkdirSync(storyDir);
+		}
+
+		return storyDir;
+	};
+
+	function getAPIData(api, module) {
+		const moduleAPI = api.symbols.find(s => s.module === module);
+		const args = getArgsTypes(api, moduleAPI);
+
+		return {
+			storyDescription: moduleAPI.description.replaceAll('<br>', '<br/>'),
+			storyArgsTypes: JSON.stringify(args, null, "\t")
+		};
+	}
+
+	function getArgsTypes(api, moduleAPI) {
+		let args = {};
+		if (moduleAPI?.events) {
+			moduleAPI.events.forEach(event => {
+				if (event.visibility && event.visibility === 'public') {
+					args[event.name] = {
+						action: event.name,
+						table: {
+							category: "Events"
+						},
+					};
+				}
+			})
+		}
 		if (moduleAPI?.properties) {
 			moduleAPI.properties.forEach(prop => {
-				args[prop.name] = {
-					description: prop.description,
-					control: prop.type,
-					table: {
-						defaultValue: {
-							summary: prop.defaultValue,
+				if (prop.visibility && prop.visibility === 'public') {
+					const controlType = UI5WC_TO_STORYBOOK_TYPES_MAP[prop.type] || 'select';
+					args[prop.name] = {
+						description: prop.description,
+						control: controlType,
+						table: {
+							defaultValue: {
+								summary: prop.defaultValue
+							},
+							category: "Properties"
+						},
+					};
+					if (controlType === 'select') {
+						const typeEnum = api.symbols.find(s => s.module === 'types/' + prop.type) || baseAPI.symbols.find(s => s.module === 'types/' + prop.type);
+						if (typeEnum && Array.isArray(typeEnum.properties)) {
+							args[prop.name].options = typeEnum.properties.map(a => a.type);
 						}
-					},
-				};
+					}
+				}
 			});
 		}
 
 		if (moduleAPI?.slots) {
-			moduleAPI.slots.forEach(prop => {
-				args[prop.name] = {
-					description: prop.description,
-					control: prop.type,
-				};
+			moduleAPI.slots.forEach(slot => {
+				if (slot.visibility && slot.visibility === 'public') {
+					args[slot.name] = {
+						description: slot.description,
+						control: slot.type,
+						table: {
+							category: "Slots"
+						},
+					};
+				}
 			});
 		}
 
-		return {
-			args,
-			name: moduleAPI.basename,
-			description: moduleAPI.description
-		};
+		// recursively merging the args from the parent/parents
+		const moduleAPIBeingExtended = api.symbols.find(s => s.module === moduleAPI.extends) || baseAPI.symbols.find(s => s.module === moduleAPI.extends);
+		if (moduleAPIBeingExtended) {
+			args = {...args, ...getArgsTypes(api, moduleAPIBeingExtended)};
+		}
+
+		return args;
 	}
 
-	function getStory(api, module, snippets) {
-		const data = getAPI(api, module);
-
-		const stories = snippetsToStories(snippets);
-
-		const storyContent = template
-			.replace(/{{stories}}/g, stories.join("\n"))
-			.replace(/{{name}}/g, data.name)
-			.replace(/{{description}}/g, data.description)
-			.replace(/{{args}}/g, JSON.stringify(data.args), null, 2); // spacing level = 2
-
-		return storyContent;
-	};
-
-	function snippetsToStories(snippets) {
-		return snippets.map((snippet, index) => {
-			let content = snippet
-				.replaceAll('\`', '\\`')
-			return `
-<Preview>
-	<Story name="default-${index}">
-		{html\`${content}\`}
-	</Story>
-</Preview>`
-		});
-	}
 };
 
 main();
