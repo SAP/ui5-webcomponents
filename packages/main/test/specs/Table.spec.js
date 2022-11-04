579fbcd11de6a2d2474f67d78f18b9c4d24d0cde
Zornitsa Dimova <zornitsa.dimova@sap.com>
2022-11-04 16:41:51 +0200

    feat(ui5-table): add inline display property

--- packages/main/test/specs/Table.spec.js
+++ packages/main/test/specs/Table.spec.js
@@ -560,7 +560,7 @@ describe("Table general interaction", () => {
 			assert.notOk(await row.shadow$("tr").getAttribute("aria-current"), "Table row aria-current attribute is not present.");
 		});
 	});
-	// test here
+
 	describe("Display cell inline or block according to property popin-display", () => {
 		before(async () => {
 			await browser.url(`test/pages/Table.html`);
@@ -575,7 +575,7 @@ describe("Table general interaction", () => {
 
 		it("Popin-display should have value Block by default", async () => {
 			const table = await browser.$("#tbl");
-			const column = await table.$$("ui5-table-column")[2];
+			const column = await table.$$("ui5-table-column")[0];
 
 			assert.notOk(await column.getAttribute("popin-display"), "Block", "Table row popin-display property is set by default.");
 		});
