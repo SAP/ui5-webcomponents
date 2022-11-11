ad5c343ff32e3ad7f10819494b389cefb0be7da0
Vladislav Tasev <vladislav.tasev@sap.com>
2022-11-11 16:51:41 +0200

    chore: test

--- packages/base/src/Boot.ts
+++ packages/base/src/Boot.ts
@@ -5,54 +5,53 @@ import { getTheme } from "./config/Theme.js";
 import applyTheme from "./theming/applyTheme.js";
 import { registerCurrentRuntime } from "./Runtimes.js";
 import { getFeature } from "./FeaturesRegistry.js";
+import type OpenUI5Support from "./features/OpenUI5Support.js";
+import type F6Navigation from "./features/F6Navigation.js";
 
-let bootPromise;
+type PromiseResolve = (value: void | PromiseLike<void>) => void;
+
+let bootPromise: Promise<void>;
 
 /**
- * Attach    a callback that will be executed on boot
+ * Attach  a callback that will be executed on boot
  * @public
  * @param listener
  */
-const attachBoot = async listener => {
+const attachBoot = async (listener: () => void) => {
 	await boot();
 	listener();
 };
 
-const boot = async () => {
-	if (bootPromise) {
+const boot = async (): Promise<void> => {
+	if (bootPromise !== undefined) {
 		return bootPromise;
 	}
 
-	/* eslint-disable no-alert, no-async-promise-executor */
-	/*
-		Note(since we disable eslint rule):
-		If an async executor function throws an error, the error will be lost and won't cause the newly-constructed Promise to reject.
-		This could make it difficult to debug and handle some errors.
-	*/
-	bootPromise = new Promise(async resolve => {
+	const bootExecutor = async (resolve: PromiseResolve) => {
 		registerCurrentRuntime();
 
-		const OpenUI5Support = getFeature("OpenUI5Support");
-		const isOpenUI5Loaded = OpenUI5Support ? OpenUI5Support.isLoaded() : false;
-		const F6Navigation = getFeature("F6Navigation");
+		const openUI5Support = getFeature<typeof OpenUI5Support>("OpenUI5Support");
+		const isOpenUI5Loaded = openUI5Support ? openUI5Support.isLoaded() : false;
+		const f6Navigation = getFeature<typeof F6Navigation>("F6Navigation");
 
-		if (OpenUI5Support) {
-			await OpenUI5Support.init();
+		if (openUI5Support) {
+			await openUI5Support.init();
 		}
 
-		if (F6Navigation && !isOpenUI5Loaded) {
-			F6Navigation.init();
+		if (f6Navigation && !isOpenUI5Loaded) {
+			f6Navigation.init();
 		}
 
 		await whenDOMReady();
 		await applyTheme(getTheme());
-		OpenUI5Support && OpenUI5Support.attachListeners();
+		openUI5Support && openUI5Support.attachListeners();
 		insertFontFace();
 		insertSystemCSSVars();
 
 		resolve();
-	});
-	/* eslint-enable no-alert, no-async-promise-executor */
+	};
+
+	bootPromise = new Promise(bootExecutor as (resolve: PromiseResolve) => void);
 
 	return bootPromise;
 };
