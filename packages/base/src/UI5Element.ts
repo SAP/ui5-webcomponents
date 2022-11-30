5d0b945a43f34ba637ff485efe7b413d8326b551
Nayden Naydenov <nayden.naydenov@sap.com>
2022-11-30 16:49:22 +0200

    chore: correct return type for getSlottedNodes method

--- packages/base/src/UI5Element.ts
+++ packages/base/src/UI5Element.ts
@@ -814,8 +814,8 @@ abstract class UI5Element extends HTMLElement {
 	 * Useful when there are transitive slots in nested component scenarios and you don't want to get a list of the slots, but rather of their content.
 	 * @public
 	 */
-	getSlottedNodes(this: Record<string, Array<SlotValue>>, slotName: string) {
-		return getSlottedNodesList(this[slotName]);
+	getSlottedNodes(slotName: string) {
+		return getSlottedNodesList((this as unknown as Record<string, Array<SlotValue>>)[slotName]);
 	}
 
 	/**
