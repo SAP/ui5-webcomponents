5c98e05956feafe158df83d3902400df6a13ff94
ilhan orhan <ilhan.myumyun@sap.com>
2022-12-02 17:08:23 +0200

    build: enable TS in "localization" package (#6137)

--- packages/localization/package-scripts.js
+++ packages/localization/package-scripts.js
@@ -7,15 +7,17 @@ const scripts = {
 	clean: "rimraf dist",
 	lint: "eslint . --config config/.eslintrc.js",
 	build: {
-		"default": "nps lint clean copy.used-modules copy.cldr copy.overlay build.replace-amd build.replace-export-true build.replace-export-false build.amd-to-es6 build.replace-global-core-usage build.esm-abs-to-rel build.jsonImports copy.src",
+		"default": "nps lint clean copy.used-modules copy.cldr copy.overlay build.replace-amd build.replace-export-true build.replace-export-false build.amd-to-es6 build.replace-global-core-usage build.esm-abs-to-rel build.jsonImports build.typescript copy.src",
 		"replace-amd": "replace-in-file sap.ui.define define dist/**/*.js",
 		"replace-export-true": `replace-in-file ", /* bExport= */ true" "" dist/**/*.js`,
 		"replace-export-false": `replace-in-file ", /* bExport= */ false" "" dist/**/*.js`,
 		"amd-to-es6": "amdtoes6 --src=dist --replace --glob=**/*.js",
 		"replace-global-core-usage": `node "${replaceGlobalCore}" dist/`,
 		"esm-abs-to-rel": `node "${esmAbsToRel}" dist/`,
+		typescript: "tsc",
 		jsonImports: "node ./lib/generate-json-imports/cldr.js"
 	},
+	typescript: "tsc",
 	copy: {
 		"used-modules": `node "${copyUsedModules}" ./used-modules.txt dist/`,
 		cldr: `copy-and-watch "../../node_modules/@openui5/sap.ui.core/src/sap/ui/core/cldr/*.json" dist/generated/assets/cldr/`,
@@ -23,7 +25,8 @@ const scripts = {
 		src: `copy-and-watch "src/**/*.js" dist/`,
 	},
 	watch: {
-		default: 'nps watch.src',
+		default: 'concurrently "nps watch.src" "nps watch.typescript" ',
+		typescript: "tsc --watch",
 		src: `nps "copy.src --watch --skip-initial-copy"`,
 	},
 	start: "nps watch",
