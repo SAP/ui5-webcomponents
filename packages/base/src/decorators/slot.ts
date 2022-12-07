4a85e80a19b65dc5858fcb0ce676cba4d241aa74
ilhan orhan <ilhan.myumyun@sap.com>
2022-12-07 16:10:07 +0200

    refactor: remove defaultSlot & add default to slot decorator (#6154)
    With this change:
    - remove "@defaultSlot" decorator
    - use "default" key of "@ slot" decorator to define a default slot

--- packages/base/src/decorators/slot.ts
+++ packages/base/src/decorators/slot.ts
@@ -12,8 +12,19 @@ const slot = (slotData: Slot): PropertyDecorator => {
 		const ctor = target.constructor as typeof UI5Element;
 		const slotMetadata = ctor.getMetadata().getSlots();
 
-		if (!slotMetadata[slotKey as string]) {
-			slotMetadata[slotKey as string] = slotData;
+		if (slotData.default && slotMetadata.default) {
+			throw new Error("Only one slot can be the default slot.");
+		}
+
+		const key = slotData.default ? "default" : slotKey as string;
+
+		if (!slotMetadata[key]) {
+			slotMetadata[key] = slotData;
+		}
+
+		if (slotData.default) {
+			delete slotMetadata.default.default;
+			slotMetadata.default.propertyName = slotKey as string;
 		}
 
 		ctor.getMetadata().metadata.managedSlots = true;
