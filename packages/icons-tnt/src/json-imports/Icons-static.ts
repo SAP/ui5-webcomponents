c6aec0b4c8d9d32dd903e785270e58b9bf6fe98c
ilhan007 <ilhan.myumyun@sap.com>
2022-12-08 09:03:04 +0200

    refactor: migrate 'icons-*' to TS

--- packages/icons-tnt/src/json-imports/Icons-static.ts
+++ packages/icons-tnt/src/json-imports/Icons-static.ts
@@ -1,8 +1,8 @@
-import { registerIconLoader } from "@ui5/webcomponents-base/dist/asset-registries/Icons.js";
+import { registerIconLoader, CollectionData } from "@ui5/webcomponents-base/dist/asset-registries/Icons.js";
 
 import SAPIconsTNTUrl from "../generated/assets/SAP-icons-TNT.json";
 
-const loadIconsBundle = async () => {
+const loadIconsBundle = async (): Promise<CollectionData> => {
 	if (typeof SAPIconsTNTUrl === "object") {
 		// inlined from build
 		throw new Error("[icons-tnt] Inlined JSON not supported with static imports of assets. Use dynamic imports of assets or configure JSON imports as URLs");
--- packages/icons/src/json-imports/Icons.ts
+++ packages/icons/src/json-imports/Icons.ts
@@ -1,7 +1,7 @@
-import { registerIconLoader } from "@ui5/webcomponents-base/dist/asset-registries/Icons.js";
+import { registerIconLoader, CollectionData } from "@ui5/webcomponents-base/dist/asset-registries/Icons.js";
 
-const loadIconsBundle = async (collection) => {
-    let iconData = null;
+const loadIconsBundle = async (collection: string): Promise<CollectionData> => {
+    let iconData: CollectionData;
 
 	if (collection === "SAP-icons-v5") {
 		iconData = (await import(`../generated/assets/v5/SAP-icons.json`)).default;
@@ -9,7 +9,7 @@ const loadIconsBundle = async (collection) => {
 		iconData = (await import(`../generated/assets/v4/SAP-icons.json`)).default;
 	}
 
-    if (typeof iconData === "string" && iconData.endsWith(".json")) {
+    if (typeof iconData === "string" && (iconData as string).endsWith(".json")) {
         throw new Error("[icons] Invalid bundling detected - dynamic JSON imports bundled as URLs. Switch to inlining JSON files from the build or use `import \"@ui5/webcomponents-icons/dist/Assets-static.js\". Check the \"Assets\" documentation for more information.");
     }
     return iconData;
