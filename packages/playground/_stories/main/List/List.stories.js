88d5b0c57043b1893cd47e6772dab48bde568560
Dobrin Dimchev <dobrin.dimchev@sap.com>
2022-12-19 12:03:29 +0200

    fix: restore stories

--- packages/playground/_stories/main/List/List.stories.js
+++ packages/playground/_stories/main/List/List.stories.js
@@ -1,181 +1,232 @@
-import { action } from "@storybook/addon-actions";
 import { html } from "lit-html";
 import { ifDefined } from "lit-html/directives/if-defined.js";
 import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
+
 import argTypes from "./argTypes.js";
 
 export default {
     title: "Components/List",
     component: "ui5-list",
-    subcomponents: {'StandardListItem' : 'ui5-li', 'CustomListItem' : 'ui5-li-custom', 'GroupHeaderListItem' : 'ui5-li-groupheader'},
     argTypes,
 };
 
+const Template = (args) => {
+    return html` <ui5-list
+		mode="${ifDefined(args.mode)}"
+		?busy="${ifDefined(args.busy)}"
+		?indent="${ifDefined(args.indent)}"
+		?growing="${ifDefined(args.growing)}"
+        busy-delay="${ifDefined(args.busyDelay)}"
+        separators="${ifDefined(args.separators)}"
+        header-text="${ifDefined(args.headerText)}"
+        footer-text="${ifDefined(args.footerText)}"
+        no-data-text="${ifDefined(args.noDataText)}"
+		accessible-name="${ifDefined(args.accessibleName)}"
+        accessible-role="${ifDefined(args.accessibleRole)}"
+        @ui5-item-click="${ifDefined(args["ui5-item-click"])}"
+        @ui5-item-delete="${ifDefined(args["ui5-item-delete"])}"
+        @ui5-selection-change="${ifDefined(args["ui5-selection-change"])}"
+    >
+        ${unsafeHTML(args.defaultSlot)}
+</ui5-list>`;
+};
 
-export const Template0 = () => html`
-<h3>Basic List</h3>
-	<div class="snippet">
-		<ui5-list id="myList" class="full-width">
-			<ui5-li icon="nutrition-activity" description="Tropical plant with an edible fruit" additional-text="In-stock" additional-text-state="Success">Pineapple</ui5-li>
-			<ui5-li icon="nutrition-activity" description="Occurs between red and yellow" additional-text="Expires" additional-text-state="Warning">Orange</ui5-li>
-			<ui5-li icon="nutrition-activity" description="The yellow lengthy fruit" additional-text="Re-stock" additional-text-state="Information">Blueberry</ui5-li>
-			<ui5-li icon="nutrition-activity" description="The tropical stone fruit" additional-text="Re-stock" additional-text-state="Error">Mango</ui5-li>
-		</ui5-list>
-	</div>
-`;
+// Basic
+export const Basic = Template.bind({});
+Basic.storyName = "Basic";
+Basic.args = {
+    ["ui5-item-click"]: (e) => action("ui5-item-click")(e.detail),
+    defaultSlot: `<ui5-li
+		icon="nutrition-activity"
+		description="Tropical plant with an edible fruit"
+		additional-text="In-stock"
+		additional-text-state="Success"
+		>Pineapple</ui5-li>
+	<ui5-li
+		icon="nutrition-activity"
+		description="Occurs between red and yellow"
+		additional-text="Expires"
+		additional-text-state="Warning"
+		>Orange</ui5-li>
+	<ui5-li
+		icon="nutrition-activity"
+		description="The yellow lengthy fruit"
+		additional-text="Re-stock"
+		additional-text-state="Information"
+		>Blueberry</ui5-li>
+	<ui5-li
+		icon="nutrition-activity"
+		description="The tropical stone fruit"
+		additional-text="Re-stock"
+		additional-text-state="Error"
+		>Mango</ui5-li>`,
+};
 
-export const Template1 = () => html`
-<h3>List with  growing="Scroll"</h3>
-	<div class="snippet">
-		<ui5-list id="infiniteScrollEx" style="height: 300px" growing="Scroll">
-			<ui5-li icon="nutrition-activity" description="Tropical plant with an edible fruit" additional-text="In-stock" additional-text-state="Success">Pineapple</ui5-li>
-			<ui5-li icon="nutrition-activity" description="Occurs between red and yellow" additional-text="Expires" additional-text-state="Warning">Orange</ui5-li>
-			<ui5-li icon="nutrition-activity" description="The yellow lengthy fruit" additional-text="Re-stock" additional-text-state="Error">Banana</ui5-li>
-			<ui5-li icon="nutrition-activity" description="The tropical stone fruit" additional-text="Re-stock" additional-text-state="Error">Mango</ui5-li>
-			<ui5-li icon="nutrition-activity" description="An apple is a sweet, edible fruit produced by an apple tree " additional-text="In-stock" additional-text-state="Success">Apple</ui5-li>
-			<ui5-li icon="nutrition-activity" description="The peach (Prunus persica) is a deciduous tree native to the region of Northwest China" additional-text="Expires" additional-text-state="Warning">Peach</ui5-li>
-			<ui5-li icon="nutrition-activity" description="The pomelo is the largest citrus fruit from the family Rutaceae and the principal ancestor of the grapefruit" additional-text="Re-stock" additional-text-state="Error">Pomelo</ui5-li>
-			<ui5-li icon="nutrition-activity" description="The pear (/ˈpɛər/) tree and shrub are a species of genus Pyrus, bearing the pomaceous fruit of the same name." additional-text="Re-stock" additional-text-state="Error">Pear</ui5-li>
-		</ui5-list>
-	</div>
-	<script>
-		function template(i) {
-			var li = document.createElement("ui5-li");
-			li.textContent = "Fruit name";
-			li.description = "the description goes here " + i;
-			li.additionalText = "Available";
-			li.additionalTextState = "Success";
-			li.icon = "nutrition-activity" 
-			return li;
+// Growing
+export const Growing = () =>
+    html`<ui5-list id="infiniteScrollEx" style="height: 200px" growing="Scroll">
+	<ui5-li
+		icon="nutrition-activity"
+		description="Tropical plant with an edible fruit"
+		additional-text="In-stock"
+		additional-text-state="Success"
+		>Pineapple</ui5-li
+	>
+	<ui5-li
+		icon="nutrition-activity"
+		description="Occurs between red and yellow"
+		additional-text="Expires"
+		additional-text-state="Warning"
+		>Orange</ui5-li
+	>
+	<ui5-li
+		icon="nutrition-activity"
+		description="The yellow lengthy fruit"
+		additional-text="Re-stock"
+		additional-text-state="Error"
+		>Banana</ui5-li
+	>
+</ui5-list>
+<script>
+	function template(i) {
+		var li = document.createElement("ui5-li");
+		li.textContent = "Fruit name";
+		li.description = "the description goes here " + i;
+		li.additionalText = "Available";
+		li.additionalTextState = "Success";
+		li.icon = "nutrition-activity";
+		return li;
+	}
+	function insertItems(el, num) {
+		for (var i = 0; i < num; i++) {
+			el.appendChild(template(i));
 		}
-		function insertItems(el, num) {
-			for(var i = 0; i < num; i++) {
-				el.appendChild(template(i));
-			}
-		}
-		infiniteScrollEx.addEventListener("load-more", (e) => {
-			var el = infiniteScrollEx;
-			el.busy = true;
-			setTimeout(() => {
-				insertItems(el, 5);
-				el.busy = false;
-			}, 200);
-		});
-	</script>
-`;
+	}
+	infiniteScrollEx.addEventListener("load-more", (e) => {
+		var el = infiniteScrollEx;
+		el.busy = true;
+		setTimeout(() => {
+			insertItems(el, 5);
+			el.busy = false;
+		}, 200);
+	});
+</script>`;
+Growing.storyName = "Growing";
 
-export const Template2 = () => html`
-<h3>List in Single-selection Mode</h3>
-	<div class="snippet">
-		<ui5-list id="country-selector" class="full-width" mode="SingleSelect" header-text="Select a country:">
-			<ui5-li selected="" icon="map" icon-end="">Argentina</ui5-li>
-			<ui5-li icon="map" icon-end="">Bulgaria</ui5-li>
-			<ui5-li icon="map" icon-end="">China</ui5-li>
-			<ui5-li type="Inactive" icon="map" icon-end="">Denmark (ui5-li type='Inactive')</ui5-li>
-		</ui5-list>
-		<script>
-			document.getElementById('country-selector').addEventListener("selection-change", function (event) {
-				var selectedItems = event.detail.selectedItems;
-				alert("The selected item:  " + selectedItems[0].textContent);
-			});
-		</script>
-	</div>
-`;
+// Single Selection
+export const SingleSelection = Template.bind({});
+SingleSelection.storyName = "Single Selection";
+SingleSelection.args = {
+    mode: "SingleSelect",
+    headerText: "Select a country:",
+    ["ui5-selection-change"]: (e) => action("ui5-selection-change")(e.detail),
+    defaultSlot: `
+	<ui5-li selected icon="map" icon-end>Argentina</ui5-li>
+	<ui5-li icon="map" icon-end>Bulgaria</ui5-li>
+	<ui5-li icon="map" icon-end>China</ui5-li>
+	<ui5-li type="Inactive" icon="map" icon-end>Denmark (ui5-li type='Inactive')</ui5-li>`,
+};
 
-export const Template3 = () => html`
-<h3>List in Multi-selection Mode</h3>
-	<div class="snippet">
-		<ui5-list id="myList1" class="samples-margin-bottom full-width" mode="MultiSelect" header-text="Multiple selection is possible">
-				<ui5-li>Pineapple</ui5-li>
-				<ui5-li selected="">Orange</ui5-li>
-				<ui5-li>Banana</ui5-li>
-				<ui5-li>Mango</ui5-li>
-		</ui5-list>
-	</div>
-`;
+// Multi Selection
+export const MultiSelection = Template.bind({});
+MultiSelection.storyName = "Multi Selection";
+MultiSelection.args = {
+    ["ui5-selection-change"]: (e) => action("ui5-selection-change")(e.detail),
+    mode: "MultiSelect",
+    headerText: "Multiple selection is possible",
+    defaultSlot: `
+	<ui5-li>Pineapple</ui5-li>
+	<ui5-li selected="">Orange</ui5-li>
+	<ui5-li>Banana</ui5-li>
+	<ui5-li>Mango</ui5-li>`,
+};
 
-export const Template4 = () => html`
-<h3>Busy List</h3>
-	<div class="snippet">
-		<ui5-list header-text="Fetching data ..." class="full-width" busy=""></ui5-list>
-	</div>
-`;
+// Group Headers
+export const GroupHeaders = Template.bind({});
+GroupHeaders.storyName = "Group Headers";
+GroupHeaders.args = {
+    mode: "MultiSelect",
+    defaultSlot: `<ui5-li-groupheader
+	>Front End Developers</ui5-li-groupheader
+	>
+	<ui5-li
+		image="../assets/images/avatars/woman_avatar_3.png"
+		icon="navigation-right-arrow"
+		icon-end=""
+		>Jennifer</ui5-li
+	>
+	<ui5-li
+		image="../assets/images/avatars/woman_avatar_4.png"
+		icon="navigation-right-arrow"
+		icon-end=""
+		>Lora</ui5-li
+	>
+	<ui5-li
+		image="../assets/images/avatars/woman_avatar_5.png"
+		icon="navigation-right-arrow"
+		icon-end=""
+		>Carlotta</ui5-li
+	>
+	<ui5-li-groupheader>Back End Developers</ui5-li-groupheader>
+	<ui5-li
+		image="../assets/images/avatars/man_avatar_1.png"
+		icon="navigation-right-arrow"
+		icon-end=""
+	>Clark</ui5-li
+	>
+	<ui5-li
+		image="../assets/images/avatars/woman_avatar_1.png"
+		icon="navigation-right-arrow"
+		icon-end=""
+	>Ellen</ui5-li
+	>
+	<ui5-li
+		image="../assets/images/avatars/man_avatar_2.png"
+		icon="navigation-right-arrow"
+		icon-end=""
+	>Adam</ui5-li
+	>`,
+};
 
-export const Template5 = () => html`
-<h3>List With GroupHeaders</h3>
-	<div class="snippet">
-		<ui5-list header-text="Community" mode="MultiSelect">
-			<ui5-li-groupheader>Front End Developers</ui5-li-groupheader>
-			<ui5-li image="../../../assets/images/avatars/woman_avatar_3.png" icon="navigation-right-arrow" icon-end="">Jennifer</ui5-li>
-			<ui5-li image="../../../assets/images/avatars/woman_avatar_4.png" icon="navigation-right-arrow" icon-end="">Lora</ui5-li>
-			<ui5-li image="../../../assets/images/avatars/woman_avatar_5.png" icon="navigation-right-arrow" icon-end="">Carlotta</ui5-li>
-			<ui5-li-groupheader>Back End Developers</ui5-li-groupheader>
-			<ui5-li image="../../../assets/images/avatars/man_avatar_1.png" icon="navigation-right-arrow" icon-end="">Clark</ui5-li>
-			<ui5-li image="../../../assets/images/avatars/woman_avatar_1.png" icon="navigation-right-arrow" icon-end="">Ellen</ui5-li>
-			<ui5-li image="../../../assets/images/avatars/man_avatar_2.png" icon="navigation-right-arrow" icon-end="">Adam</ui5-li>
-			<ui5-li-groupheader>FullStack Developers</ui5-li-groupheader>
-			<ui5-li image="../../../assets/images/avatars/woman_avatar_2.png" icon="navigation-right-arrow" icon-end="">Susan</ui5-li>
-			<ui5-li image="../../../assets/images/avatars/man_avatar_3.png" icon="navigation-right-arrow" icon-end="">David</ui5-li>
-			<ui5-li image="../../../assets/images/avatars/woman_avatar_3.png" icon="navigation-right-arrow" icon-end="">Natalie</ui5-li>
-		</ui5-list>
-	</div>
-`;
+// Delete
+export const Delete = Template.bind({});
+Delete.storyName = "Delete Mode";
+Delete.args = {
+    mode: "Delete",
+    ["ui5-item-delete"]: (e) => action("ui5-item-delete")(e.detail),
+    headerText: "Note: The list items removal is up to application developers",
+    defaultSlot: `
+	<ui5-li>Argentina</ui5-li>
+	<ui5-li>Bulgaria</ui5-li>
+	<ui5-li>China</ui5-li>`,
+};
 
-export const Template6 = () => html`
-<h3>List items using imageContent slot</h3>
-	<div class="snippet">
-		<ui5-list class="full-width">
-			<ui5-li> Avatar inside imageContent slot
-				<div slot="imageContent">
-					<ui5-avatar shape="Square" initials="ABC" color-scheme="Accent2"></ui5-avatar>
-				</div>
-			</ui5-li>
-			<ui5-li> Avatar inside imageContent slot
-				<div slot="imageContent">
-					<ui5-avatar>
-						<img src="./img/woman_avatar_5.png" alt="Woman image">
-					</ui5-avatar>
-				</div>
-			</ui5-li>
-		</ui5-list>
-	</div>
-`;
+// No Data
+export const NoData = Template.bind({});
+NoData.storyName = "No Data";
+NoData.args = {
+    headerText: "Products",
+    noDataText: "No Data Available",
+    separators: "None",
+};
 
-export const Template7 = () => html`
-<h3>List in Delete Mode</h3>
-	<div class="snippet">
-		<ui5-list id="myList5" class="full-width" mode="Delete" header-text="Note: The list items removal is up to application developers">
-			<ui5-li>Argentina</ui5-li>
-			<ui5-li>Bulgaria</ui5-li>
-			<ui5-li>China</ui5-li>
-			<ui5-li>Denmark
-				<div slot="deleteButton">
-					<ui5-button>Custom Delete Button</ui5-button>
-				</div>
-			</ui5-li>
-		</ui5-list>
-	</div>
-`;
-
-export const Template8 = () => html`
-<h3>List with No Data</h3>
-	<div class="snippet">
-		<ui5-list class="full-width" header-text="Products" no-data-text="No Data Available" separators="None"></ui5-list>
-	</div>
-`;
-
-export const Template9 = () => html`
-<h3>List Item Separation Types</h3>
-	<div class="snippet">
-		<ui5-list header-text="No separators" separators="None" class="full-width">
-			<ui5-li icon="product">Item #1</ui5-li>
-			<ui5-li icon="product">Item #2</ui5-li>
-			<ui5-li icon="product">Item #3</ui5-li>
-		</ui5-list>
-		<ui5-list header-text="Inner separators" separators="Inner" class="full-width">
-			<ui5-li icon="shipping-status">Devilered</ui5-li>
-			<ui5-li icon="shipping-status">Pending</ui5-li>
-			<ui5-li icon="shipping-status">Declined</ui5-li>
-		</ui5-list>
-	</div>
-`;
+// Separation Types
+export const SeparationTypes = () =>
+    html` <ui5-list
+	header-text="No separators"
+	separators="None"
+	class="full-width"
+>
+	<ui5-li icon="product">Item #1</ui5-li>
+	<ui5-li icon="product">Item #2</ui5-li>
+	<ui5-li icon="product">Item #3</ui5-li>
+</ui5-list>
+<ui5-list
+	header-text="Inner separators"
+	separators="Inner"
+	class="full-width"
+>
+	<ui5-li icon="shipping-status">Devilered</ui5-li>
+	<ui5-li icon="shipping-status">Pending</ui5-li>
+	<ui5-li icon="shipping-status">Declined</ui5-li>
+</ui5-list>`;
