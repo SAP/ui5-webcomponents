36ef5a6e1f00c86da8dd99f66a487be0dfce277d
gmkv <georgi.minkov@sap.com>
2023-01-09 15:00:05 +0200

    feat(ui5-carousel): allow page indicator to always display numbers (#6245)
    Added a new property pageIndicatorStyle which defines how the page indicator
    is visualized. The previous behavior is now the default - displaying dots
    if there are fewer than 9 pages, and displaying numbers ("1 of 10") when there are more pages.
    
    You can now configure the page indicator to always display numbers
    by setting `page-indicator-style="Numeric"`.
    
    Fixes #5409

--- packages/main/src/Carousel.js
+++ packages/main/src/Carousel.js
@@ -22,6 +22,7 @@ import {
 	CAROUSEL_NEXT_ARROW_TEXT,
 } from "./generated/i18n/i18n-defaults.js";
 import CarouselArrowsPlacement from "./types/CarouselArrowsPlacement.js";
+import CarouselPageIndicatorStyle from "./types/CarouselPageIndicatorStyle.js";
 import CarouselTemplate from "./generated/templates/CarouselTemplate.lit.js";
 import "@ui5/webcomponents-icons/dist/slim-arrow-left.js";
 import "@ui5/webcomponents-icons/dist/slim-arrow-right.js";
@@ -99,7 +100,7 @@ const metadata = {
 		},
 
 		/**
-		 * Defines the visibility of the paging indicator.
+		 * Defines the visibility of the page indicator.
 		 * If set to true the page indicator will be hidden.
 		 * @type {boolean}
 		 * @since 1.0.0-rc.15
@@ -108,6 +109,23 @@ const metadata = {
 		 */
 		hidePageIndicator: {
 			type: Boolean,
+		},
+
+		/**
+		 * Defines the style of the page indicator.
+		 * Available options are:
+		 * <ul>
+		 * <li><code>Default</code> - The page indicator will be visualized as dots if there are fewer than 9 pages. If there are more pages, the page indicator will switch to displaying the current page and the total number of pages. (e.g. X of Y)</li>
+		 * <li><code>Numeric</code> - The page indicator will display the current page and the total number of pages. (e.g. X of Y)</li>
+		 * </ul>
+		 * @type {sap.ui.webc.main.types.CarouselPageIndicatorStyle}
+		 * @since 1.10
+		 * @defaultvalue "Default"
+		 * @public
+		 */
+		pageIndicatorStyle: {
+			type: CarouselPageIndicatorStyle,
+			defaultValue: CarouselPageIndicatorStyle.Default,
 		},
 
 		/**
@@ -612,6 +630,10 @@ class Carousel extends UI5Element {
 	}
 
 	get isPageTypeDots() {
+		if (this.pageIndicatorStyle === CarouselPageIndicatorStyle.Numeric) {
+			return false;
+		}
+
 		return this.pagesCount < Carousel.pageTypeLimit;
 	}
 
