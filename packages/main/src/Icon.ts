bd7c7e65c2b42baa5a661a7edd9b89a73238317f
Ivanov <plamen.ivanov01@sap.com>
2023-01-09 17:39:31 +0200

    feat(ui5-icon): new 'color' property introduced
    With the new property the app developer can chose from several semantic
    color options.
    
    Implements #6019

--- packages/main/src/Icon.ts
+++ packages/main/src/Icon.ts
@@ -10,6 +10,7 @@ import { getI18nBundle, I18nText } from "@ui5/webcomponents-base/dist/i18nBundle
 import { isSpace, isEnter } from "@ui5/webcomponents-base/dist/Keys.js";
 import executeTemplate from "@ui5/webcomponents-base/dist/renderer/executeTemplate.js";
 import IconTemplate from "./generated/templates/IconTemplate.lit.js";
+import IconColor from "./types/IconColor.js";
 
 // Styles
 import iconCss from "./generated/themes/Icon.css.js";
@@ -118,6 +119,34 @@ const PRESENTATION_ROLE = "presentation";
  */
 @event("click")
 class Icon extends UI5Element {
+	/**
+	 * Defines the component semantic color.
+	 *
+	 * <br><br>
+	 * <b>The available values are:</b>
+	 *
+	 * <ul>
+	 * <li><code>Contrast</code></li>
+	 * <li><code>Critical</code></li>
+	 * <li><code>Default</code></li>
+	 * <li><code>Information</code></li>
+	 * <li><code>Marker</code></li>
+	 * <li><code>Negative</code></li>
+	 * <li><code>Neutral</code></li>
+	 * <li><code>NonInteractive</code></li>
+	 * <li><code>Positive</code></li>
+	 * <li><code>Tile</code></li>
+	 * </ul>
+	 *
+	 * @type {sap.ui.webc.main.types.IconColor}
+	 * @name sap.ui.webc.main.Icon.prototype.design
+	 * @defaultvalue "Default"
+	 * @public
+	 * @since 1.9.2
+	 */
+	@property({ type: IconColor, defaultValue: IconColor.Default })
+	color!: IconColor;
+
 	/**
 	 * Defines if the icon is interactive (focusable and pressable)
 	 * @name sap.ui.webc.main.Icon.prototype.interactive
---
+++ packages/main/src/types/IconColor.ts
@@ -0,0 +1,82 @@
+/**
+ * Different Icon semantic colors.
+*
+* @class
+* @enum {string}
+* @public
+* @author SAP SE
+* @alias sap.ui.webc.main.types.IconColor
+*/
+enum IconColor {
+	/**
+	 * Contrast color
+	 * @public
+	 * @type {Contrast}
+	 */
+	Contrast = "Contrast",
+
+	/**
+	 * Critical color
+	 * @public
+	 * @type {Critical}
+	 */
+	Critical = "Critical",
+
+	/**
+	 * Default color (brand color)
+	 * @public
+	 * @type {Default}
+	*/
+	Default = "Default",
+
+	/**
+	 * info type
+	 * @public
+	 * @type {Information}
+	 */
+	Information = "Information",
+
+	/**
+	 * Color for icon used as a marker
+	 * @public
+	 * @type {Marker}
+	 */
+	Marker = "Marker",
+
+	/**
+	 * Negative color
+	 * @public
+	 * @type {Negative}
+	 */
+	Negative = "Negative",
+
+	/**
+	 * Neutral color
+	 * @public
+	 * @type {Neutral}
+	 */
+	Neutral = "Neutral",
+
+	/**
+	 * Color that indicates an icon which isn't interactive
+	 * @public
+	 * @type {NonInteractive}
+	 */
+	NonInteractive = "NonInteractive",
+
+	/**
+	 * Positive color
+	 * @public
+	 * @type {Positive}
+	 */
+	Positive = "Positive",
+
+	/**
+	 * Color for icon used in a Tile
+	 * @public
+	 * @type {Tile}
+	 */
+	Tile = "Tile"
+}
+
+export default IconColor;
