566c46bd47666f8e096ffb9af9195cbe1c27a65a
Plamen Ivanov <plamen.ivanov01@sap.com>
2023-01-11 16:02:09 +0200

    feat(ui5-icon): new 'design' property introduced (#6261)
    * feat(ui5-icon): new 'design' property introduced
    
    With the new property the app developer can chose from several semantic
    design options.
    
    Implements #6019

---
+++ packages/main/src/types/IconDesign.ts
@@ -0,0 +1,68 @@
+/**
+ * Different Icon semantic designs.
+*
+* @class
+* @enum {string}
+* @public
+* @author SAP SE
+* @alias sap.ui.webc.main.types.IconDesign
+*/
+enum IconDesign {
+	/**
+	 * Contrast design
+	 * @public
+	 * @type {Contrast}
+	 */
+	Contrast = "Contrast",
+
+	/**
+	 * Critical design
+	 * @public
+	 * @type {Critical}
+	 */
+	Critical = "Critical",
+
+	/**
+	 * Default design (brand design)
+	 * @public
+	 * @type {Default}
+	*/
+	Default = "Default",
+
+	/**
+	 * info type
+	 * @public
+	 * @type {Information}
+	 */
+	Information = "Information",
+
+	/**
+	 * Negative design
+	 * @public
+	 * @type {Negative}
+	 */
+	Negative = "Negative",
+
+	/**
+	 * Neutral design
+	 * @public
+	 * @type {Neutral}
+	 */
+	Neutral = "Neutral",
+
+	/**
+	 * Design that indicates an icon which isn't interactive
+	 * @public
+	 * @type {NonInteractive}
+	 */
+	NonInteractive = "NonInteractive",
+
+	/**
+	 * Positive design
+	 * @public
+	 * @type {Positive}
+	 */
+	Positive = "Positive"
+}
+
+export default IconDesign;
