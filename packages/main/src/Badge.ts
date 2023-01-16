1989c1ffda06def20681d5f2f16a906025305563
Siyana Todorova <siyana.todorova@sap.com>
2023-01-16 18:48:51 +0200

    refactor(ui5-badge): convert to TS

--- packages/main/src/Badge.ts
+++ packages/main/src/Badge.ts
@@ -1,82 +1,22 @@
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
+import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
+import languageAware from "@ui5/webcomponents-base/dist/decorators/languageAware.js";
+import property from "@ui5/webcomponents-base/dist/decorators/property.js";
+import slot from "@ui5/webcomponents-base/dist/decorators/slot.js";
 import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
 import { getI18nBundle } from "@ui5/webcomponents-base/dist/i18nBundle.js";
+import type { I18nText } from "@ui5/webcomponents-base/dist/i18nBundle.js";
+import type I18nBundle from "@ui5/webcomponents-base/dist/i18nBundle.js";
 import willShowContent from "@ui5/webcomponents-base/dist/util/willShowContent.js";
 
 // Template
 import BadgeTemplate from "./generated/templates/BadgeTemplate.lit.js";
 
+// @ts-ignore
 import { BADGE_DESCRIPTION } from "./generated/i18n/i18n-defaults.js";
 
 // Styles
 import badgeCss from "./generated/themes/Badge.css.js";
-
-/**
- * @public
- */
-const metadata = {
-	tag: "ui5-badge",
-	languageAware: true,
-	properties: /** @lends sap.ui.webc.main.Badge.prototype */  {
-
-		/**
-		 * Defines the color scheme of the component.
-		 * There are 10 predefined schemes. Each scheme applies different values for the <code>background-color</code> and <code>border-color</code>.
-		 * To use one you can set a number from <code>"1"</code> to <code>"10"</code>. The <code>colorScheme</code> <code>"1"</code> will be set by default.
-		 * <br><br>
-		 * <b>Note:</b> Color schemes have no visual representation in High Contrast Black (sap_belize_hcb) theme.
-		 * @type {string}
-		 * @defaultvalue "1"
-		 * @public
-		 */
-		colorScheme: {
-			type: String,
-			defaultValue: "1",
-		},
-
-		/**
-		 * Defines if the badge has an icon.
-		 * @private
-		 */
-		_hasIcon: {
-			type: Boolean,
-		},
-
-		/**
-		 * Defines if the badge has only an icon (and no text).
-		 * @private
-		 */
-		_iconOnly: {
-			type: Boolean,
-		},
-	},
-	managedSlots: true,
-	slots: /** @lends sap.ui.webc.main.Badge.prototype */ {
-		/**
-		 * Defines the text of the component.
-		 * <br><b>Note:</b> Although this slot accepts HTML Elements, it is strongly recommended that you only use text in order to preserve the intended design.
-		 *
-		 * @type {Node[]}
-		 * @slot
-		 * @public
-		 */
-		"default": {
-			type: Node,
-			propertyName: "text",
-		},
-
-		/**
-		 * Defines the icon to be displayed in the component.
-		 *
-		 * @type {sap.ui.webc.main.IIcon}
-		 * @slot
-		 * @public
-		 */
-		icon: {
-			type: HTMLElement,
-		},
-	},
-};
 
 /**
  * @class
@@ -104,10 +44,61 @@ const metadata = {
  * @since 0.12.0
  * @public
  */
+@customElement("ui5-avatar")
+@languageAware
 class Badge extends UI5Element {
-	static get metadata() {
-		return metadata;
-	}
+	/**
+	 * Defines the color scheme of the component.
+	 * There are 10 predefined schemes. Each scheme applies different values for the <code>background-color</code> and <code>border-color</code>.
+	 * To use one you can set a number from <code>"1"</code> to <code>"10"</code>. The <code>colorScheme</code> <code>"1"</code> will be set by default.
+	 * <br><br>
+	 * <b>Note:</b> Color schemes have no visual representation in High Contrast Black (sap_belize_hcb) theme.
+	 * @type {string}
+	 * @name sap.ui.webc.main.Badge.prototype.colorScheme
+	 * @defaultvalue "1"
+	 * @public
+	 */
+	@property({ defaultValue: "1" })
+	colorScheme!: string;
+
+	/**
+	 * Defines if the badge has an icon.
+	 * @private
+	 */
+	@property({ type: Boolean })
+	_hasIcon!: boolean;
+
+	/**
+	 * Defines if the badge has only an icon (and no text).
+	 * @private
+	 */
+	@property({ type: Boolean })
+	_iconOnly!: boolean;
+
+	/**
+	 * Defines the text of the component.
+	 * <br><b>Note:</b> Although this slot accepts HTML Elements, it is strongly recommended that you only use text in order to preserve the intended design.
+	 *
+	 * @type {Node[]}
+	 * @name sap.ui.webc.main.Badge.prototype.default
+	 * @slot
+	 * @public
+	 */
+	@slot({ type: Node, "default": true })
+	text!: Array<HTMLElement>;
+
+	/**
+	 * Defines the icon to be displayed in the component.
+	 *
+	 * @type {sap.ui.webc.main.IIcon}
+	 * @name sap.ui.webc.main.Badge.prototype.icon
+	 * @slot
+	 * @public
+	 */
+	@slot()
+	icon!: Array<HTMLElement>;
+
+	static i18nBundle: I18nBundle;
 
 	static get render() {
 		return litRender;
@@ -143,7 +134,7 @@ class Badge extends UI5Element {
 	}
 
 	get badgeDescription() {
-		return Badge.i18nBundle.getText(BADGE_DESCRIPTION);
+		return Badge.i18nBundle.getText(BADGE_DESCRIPTION as I18nText);
 	}
 }
 
