b7c12174ade29d83e1d5531714f735164025ec2f
ilhan007 <ilhan.myumyun@sap.com>
2023-01-19 14:18:17 +0200

    chore: add @ name to readonly members

--- packages/main/src/TimePicker.ts
+++ packages/main/src/TimePicker.ts
@@ -132,6 +132,7 @@ class TimePicker extends TimePickerBase {
 	 * @readonly
 	 * @type {Date}
 	 * @public
+	 * @name sap.ui.webc.main.TimePicker.prototype.dateValue
 	 */
 	get dateValue() {
 		return this.getFormat().parse(this._effectiveValue as string, undefined as unknown as boolean, undefined as unknown as boolean);
