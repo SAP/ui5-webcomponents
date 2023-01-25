31f2f5694f20b3988df8de8962c70d9c5e1df2d1
Lidiya Georgieva <lidiya.georgieva@sap.com>
2023-01-25 16:55:49 +0200

    code comment fixed

--- packages/main/src/RadioButton.ts
+++ packages/main/src/RadioButton.ts
@@ -471,7 +471,6 @@ class RadioButton extends UI5Element implements IFormElement {
 
 	get classes() {
 		return {
-			main: {},
 			inner: {
 				"ui5-radio-inner--hoverable": !this.disabled && !this.readonly && isDesktop(),
 			},
