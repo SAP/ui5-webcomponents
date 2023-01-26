d6eb545b7f74dc9c293b07593d7af04fbf1f20b6
Dimitar Dimitrov <dimitar.dimitrov11@sap.com>
2023-01-26 10:58:55 +0200

    feat(ui5-input): reflect dynamic changes of labels

--- packages/base/src/util/AriaLabelHelper.ts
+++ packages/base/src/util/AriaLabelHelper.ts
@@ -1,3 +1,8 @@
+import type UI5Element from "../UI5Element.js";
+
+const observers = new WeakMap<Node, MutationObserver>();
+const inputAssociatedLabels = new WeakMap<UI5Element, Array<Node>>();
+
 type AccessibleElement = HTMLElement & {
 	accessibleNameRef: string,
 	accessibleName: string,
@@ -39,11 +44,19 @@ const _getAriaLabelledByTexts = (el: HTMLElement) => {
 };
 
 /**
+ *
+ * @param {HTMLElement} el Defines the HTMLElement, for which you need to get all assosiated labels
+ */
+const _getAssociatedLabels = (el: HTMLElement):NodeList => {
+	return (el.getRootNode() as HTMLElement).querySelectorAll(`[ui5-label][for="${el.id}"],label[for="${el.id}"]`);
+};
+
+/**
  * @param {HTMLElement} el Defines the HTMLElement, for which you need to get all related "label for" texts
  */
 const getAssociatedLabelForTexts = (el: HTMLElement) => {
 	const results: Array<string> = [];
-	const labels = (el.getRootNode() as HTMLElement).querySelectorAll(`[ui5-label][for="${el.id}"],label[for="${el.id}"]`);
+	const labels = _getAssociatedLabels(el);
 
 	labels.forEach((label: Node) => {
 		const labelText = label.textContent;
@@ -57,7 +70,56 @@ const getAssociatedLabelForTexts = (el: HTMLElement) => {
 	return undefined;
 };
 
+const observeAssosiatedLabels = (el: UI5Element, callback: MutationCallback) => {
+	const observerOptions = {
+		childList: true,
+		subtree: true,
+	};
+	const assosiatedLabelsArray: Array<Node> = [];
+	const labels = _getAssociatedLabels(el);
+	labels.forEach((label: Node) => {
+		assosiatedLabelsArray.push(label);
+		const observer = new MutationObserver(callback);
+		observers.set(label, observer);
+		observer.observe(label, observerOptions);
+	});
+	inputAssociatedLabels.set(el, assosiatedLabelsArray);
+};
+
+const disposeAssosiatedLabelsObservers = (el: UI5Element) => {
+	const labels = _getAssociatedLabels(el);
+	labels.forEach((label: Node) => {
+		const observer = observers.get(label);
+		if (observer) {
+			observer.disconnect();
+			observers.delete(label);
+		}
+	});
+	inputAssociatedLabels.delete(el);
+};
+
+const updateInputAssociatedObservers = (el: UI5Element) => {
+	const labelsActual = _getAssociatedLabels(el);
+	const labelsOld = inputAssociatedLabels.get(el) as Array<Node> ?? [];
+	const labelsNew: Array<Node> = [];
+	labelsOld.forEach((label: Node) => {
+		if (!Array.from(labelsActual).find(node => node === label)) {
+			const observer = observers.get(label);
+			if (observer) {
+				observer.disconnect();
+				observers.delete(label);
+			}
+		} else {
+			labelsNew.push(label);
+		}
+	});
+	inputAssociatedLabels.set(el, labelsNew);
+};
+
 export {
 	getEffectiveAriaLabelText,
 	getAssociatedLabelForTexts,
+	observeAssosiatedLabels,
+	disposeAssosiatedLabelsObservers,
+	updateInputAssociatedObservers,
 };
