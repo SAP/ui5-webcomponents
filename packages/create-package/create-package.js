75545d0297baca2deb52f606fcbfe6814afaa909
Nayden Naydenov <nayden.naydenov@sap.com>
2023-01-30 17:35:15 +0200

    chore: remove port

--- packages/create-package/create-package.js
+++ packages/create-package/create-package.js
@@ -80,8 +80,6 @@ const createWebcomponentsPackage = async () => {
 
 	// Get the name
 	let name = argv.name;
-	// Get the port
-	const port = argv || 8080;
 	// Get the tag
 	let tag = argv.tag;
 	// Get the TypeScript support
@@ -116,7 +114,7 @@ const createWebcomponentsPackage = async () => {
 		typescript = response.language === "ts";
 	}
 
-	if (!isTagValid(port)) {
+	if (!isTagValid(tag)) {
 		response = await prompts({
 			type: "text",
 			name: "tag",
@@ -132,7 +130,6 @@ const createWebcomponentsPackage = async () => {
 	// All variables that will be replaced in the content of the resources/
 	const vars = {
 		INIT_PACKAGE_VAR_NAME: name,
-		INIT_PACKAGE_VAR_PORT: port,
 		INIT_PACKAGE_VAR_TAG: tag,
 		INIT_PACKAGE_VAR_CLASS_NAME: className,
 		INIT_PACKAGE_VAR_TYPESCRIPT: typescript
--- packages/create-package/template/package-scripts.js
+++ packages/create-package/template/package-scripts.js
@@ -1,7 +1,6 @@
 const getScripts = require("@ui5/webcomponents-tools/components-package/nps.js"); //eslint-disable-line
 
 const options = {
-	port: INIT_PACKAGE_VAR_PORT,
 	typescript: INIT_PACKAGE_VAR_TYPESCRIPT,
 };
 
