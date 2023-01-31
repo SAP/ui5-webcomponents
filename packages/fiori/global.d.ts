10b4417e8ea4229b6bd2ef5b4957bbf00d155d39
yanaminkova <32466553+yanaminkova@users.noreply.github.com>
2023-01-31 14:44:31 +0200

    feat(ui5-barcode-scanner-dialog): migrated to Typescript (#6338)
    Related to: https://github.com/SAP/ui5-webcomponents/issues/4337

--- packages/fiori/global.d.ts
+++ packages/fiori/global.d.ts
@@ -1,10 +1,17 @@
 // eslint-disable-next-line
 import "@ui5/webcomponents-base/dist/global";
 import { TemplateFunction } from "@ui5/webcomponents-base/dist/renderer/executeTemplate.js";
+import type { BrowserMultiFormatReader, NotFoundException } from "@zxing/library/esm5/index";
 
 export {};
 
 declare global {
+	interface Window {
+		ZXing: {
+			BrowserMultiFormatReader: typeof BrowserMultiFormatReader;
+			NotFoundException: typeof NotFoundException;
+		};
+	}
 	module "*.lit.js" {
 		const content: TemplateFunction;
 		export default content;
