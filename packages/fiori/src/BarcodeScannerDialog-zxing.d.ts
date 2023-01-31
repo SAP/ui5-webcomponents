478203ec6a9c85fe16c4167c6e670a3f5411e022
Peter Skelin <petar.skelin@sap.com>
2023-01-31 10:17:06 +0200

    chore: fix types

--- packages/fiori/src/BarcodeScannerDialog-zxing.d.ts
+++ packages/fiori/src/BarcodeScannerDialog-zxing.d.ts
@@ -1,6 +1,6 @@
 declare module "@zxing/library/umd/index.min.js" {
-	import type { BrowserMultiFormatReader, NotFoundException } from "@zxing/library/esm5/index";
+	import type { BrowserMultiFormatReader as BrowserMultiFormatReaderT, NotFoundException as NotFoundExceptionT} from "@zxing/library/esm5/index";
 
-	const BrowserMultiFormatReader: typeof BrowserMultiFormatReader;
-	const NotFoundException: typeof NotFoundException;
+	const BrowserMultiFormatReader: typeof BrowserMultiFormatReaderT;
+	const NotFoundException: typeof NotFoundExceptionT;
 }
