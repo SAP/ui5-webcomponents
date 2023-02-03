e916daec65bbbbb08812351f56ee72eeb47db9d9
ilhan007 <ilhan.myumyun@sap.com>
2023-02-03 15:00:54 +0200

    chore: revert obsolete change

--- packages/theming/config/postcss.themes/postcss.config.js
+++ packages/theming/config/postcss.themes/postcss.config.js
@@ -19,6 +19,6 @@ module.exports = {
 			},
 		),
 		postcssCSStoJSON({ toReplace: `dist`, packageName: "@ui5/webcomponents-theming" }),
-		postcssCSStoESM({ toReplace: `dist`, packageName: "@ui5/webcomponents-theming", tsMode: true }),
+		postcssCSStoESM({ toReplace: `dist`, packageName: "@ui5/webcomponents-theming" }),
 	]
 };
