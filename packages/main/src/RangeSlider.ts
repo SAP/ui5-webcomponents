61c9230b18647968dfb7a35d9cc89bb832cea559
Nikolay Deshev <nikolay.deshev@sap.com>
2023-02-08 11:10:50 +0200

    fix(ui5-range-slider): fire input event with correct values after swapping (#6385)
    * fix(ui5-range-slider): fire input event with correct values after handle swapping
    
    resolves #6377
    
    Also fixes the visual bugs with the progress bar during value swap when dragging the handles.
    
    * fix(ui5-range-slider): fire input event with correct values after swapping
    
    fix the bug from the testing
    
    * fix(ui5-range-slider): fire input event with correct values after swapping
    
    * fix(ui5-range-slider): fire input event with correct values after swapping
    
    Fix change event firing on keyboard interactions and the swapping bug on keyboard triggered swaps
    
    * fix(ui5-range-slider): fire input event with correct values after swapping
    
    Clean the comment
    
    * fix(ui5-range-slider): fire input event with correct values after swapping
    
    lint
    
    * fix(ui5-range-slider): don't fire change event if values are not changed
    
    * fix(ui5-range-slider): fire input event with correct values after swapping
    
    fix typos

--- packages/main/src/RangeSlider.ts
+++ packages/main/src/RangeSlider.ts
@@ -217,6 +217,14 @@ class RangeSlider extends SliderBase {
 	 *
 	 */
 	onBeforeRendering() {
+		if (this.startValue > this.endValue) {
+			const affectedValue = this._valueAffected === "startValue" ? "endValue" : "startValue";
+
+			this._swapValues();
+			this._setAffectedValue(affectedValue);
+			this.update(affectedValue, this.startValue, this.endValue);
+		}
+
 		if (!this.isCurrentStateOutdated()) {
 			return;
 		}
@@ -311,12 +319,20 @@ class RangeSlider extends SliderBase {
 	*/
 	_onkeyup() {
 		super._onkeyup();
-
-		this._swapValues();
 		this._setAffectedValue(undefined);
+
+		if (this.startValue !== this._startValueAtBeginningOfAction || this.endValue !== this._endValueAtBeginningOfAction) {
+			this.fireEvent("change");
+		}
+
+		this._startValueAtBeginningOfAction = undefined;
+		this._endValueAtBeginningOfAction = undefined;
 	}
 
 	_handleActionKeyPress(e: KeyboardEvent) {
+		this._startValueAtBeginningOfAction = this.startValue;
+		this._endValueAtBeginningOfAction = this.endValue;
+
 		if (isEscape(e)) {
 			this.update(undefined, this._startValueInitial, this._endValueInitial);
 			return;
@@ -411,7 +427,7 @@ class RangeSlider extends SliderBase {
 			this.updateStateStorageAndFireInputEvent("endValue");
 			this._updateHandlesAndRange(0);
 		} else {
-			const newValue = startValue;
+			const newValue = endValue && affectedValue === "endValue" ? endValue : startValue;
 			this._updateHandlesAndRange(newValue || 0);
 
 			if (affectedValue === "startValue") {
@@ -535,21 +551,19 @@ class RangeSlider extends SliderBase {
 	}
 
 	_handleUp() {
-		this._swapValues();
 		this._setAffectedValueByFocusedElement();
 		this._setAffectedValue(undefined);
-
-		this._startValueAtBeginningOfAction = undefined;
-		this._endValueAtBeginningOfAction = undefined;
-		this._setIsPressInCurrentRange(false);
-
-		this.handleUpBase();
-
-		this.rangePressed = false;
 
 		if (this.startValue !== this._startValueAtBeginningOfAction || this.endValue !== this._endValueAtBeginningOfAction) {
 			this.fireEvent("change");
 		}
+
+		this._setIsPressInCurrentRange(false);
+		this.handleUpBase();
+
+		this.rangePressed = false;
+		this._startValueAtBeginningOfAction = undefined;
+		this._endValueAtBeginningOfAction = undefined;
 	}
 
 	/**
@@ -784,24 +798,26 @@ class RangeSlider extends SliderBase {
 	 */
 	_swapValues() {
 		const affectedValue = this._valueAffected;
+		if (!affectedValue) {
+			return;
+		}
 
 		if (affectedValue === "startValue" && this.startValue > this.endValue) {
 			const prevEndValue = this.endValue;
 			this.endValue = this.startValue;
 			this.startValue = prevEndValue;
-
-			this._setValuesAreReversed();
-			this.focusInnerElement();
 		}
 
 		if (affectedValue === "endValue" && this.endValue < this.startValue) {
 			const prevStartValue = this.startValue;
 			this.startValue = this.endValue;
 			this.endValue = prevStartValue;
-
-			this._setValuesAreReversed();
-			this.focusInnerElement();
 		}
+
+		this._setValuesAreReversed();
+		this._updateHandlesAndRange(this[affectedValue]);
+		this.focusInnerElement();
+		this.syncUIAndState();
 	}
 
 	/**
