4e8c7a24e6614ef322400a31111a2f8496641402
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-02-14 16:44:37 +0200

    docs: documentation parser docs

--- packages/playground/build-scripts-storybook/documentation-prepare.js
+++ packages/playground/build-scripts-storybook/documentation-prepare.js
@@ -4,11 +4,35 @@ const path = require("path");
 const rimraf = require("rimraf");
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
+// this script is used to prepare documentation files for storybook
+// it converts md files to mdx and adds metadata to the mdx files
+// e.g
+// import { Meta } from '@storybook/blocks';
+// <Meta title="Docs/Changelog/Changelog" />
+// it also creates sub directories based on the directories config
+// and sorts changelog files by version number
+// it is run before storybook is built
+
+/**
+ * Remove metadata from the md file and return only the content
+ * @param {Buffer} buffer
+ * @returns {string} md content
+ * @example
+ * ---
+ * title: 'Docs/Changelog/Changelog'
+ * metaTitle: 'Changelog'
+ * metaDescription: 'Changelog'
+ * ---
+ */
 const removeMetadata = (buffer) => {
     return buffer.toString().replace(/^---[\s\S]+?---/g, "");
 };
 
-// parseNameNoNumbers: removes numbers and dots, dashes from the name
+/**
+ * Remove numbers and special characters from the file name
+ * @param {string} name
+ * @returns {string} parsed name
+ */
 const parseNameNoNumbers = (name) =>
     capitalize(
         name
@@ -17,25 +41,17 @@ const parseNameNoNumbers = (name) =>
             .replace(/^\s+|\s+$/g, "")
     );
 
-const directories = [
-    {
-        src: "../../../docs",
-        exclude: ["README.md", "images"],
-        parseName: parseNameNoNumbers,
-    },
-    {
-        src: "../docs/changelog",
-        exclude: ["changelog.md"],
-        subDir: () => "99-changelog",
-        parseName: (name) => capitalize(name),
-    },
-];
-
+/**
+ * Returns mdx title in the following formats:
+ * - Docs/Changelog/{articleName}
+ * - Docs/{{articleGroup}}/{articleName}
+ * - Docs/{articleName}
+ *
+ * @param {string} filePath
+ * @param {string} parseName
+ * @returns {string} mdx title
+ */
 const parseMdxTitle = (filePath, parseName) => {
-    // returns mdx title in the following formats:
-    // - Docs/Changelog/{articleName}
-    // - Docs/{{articleGroup}}/{articleName}
-    // - Docs/{articleName}
     const fileName = filePath.split("/").pop();
     const articleName = parseName(fileName);
 
@@ -63,6 +79,11 @@ ${articleContent}`
     );
 };
 
+/**
+ * Recursively get all files from a directory
+ * @param {string} dir
+ * @returns {Promise<string[]>} found files/directories
+ */
 const getFiles = async (dir) => {
     const subdirs = await fsPromises.readdir(dir);
     const files = await Promise.all(
@@ -76,6 +97,15 @@ const getFiles = async (dir) => {
     return files.reduce((a, f) => a.concat(f), []);
 };
 
+/**
+ *
+ * @param {object} obj
+ * @param {string[]} obj.exclude - list of files to exclude, regex
+ * @param {function} obj.parseName - function to parse name used in the mdx's Meta title
+ * @param {function} obj.subDir - function adds sub directory to the path if defined in the directories config
+ * @param {string} obj.filePath - path to the file
+ * @param {string} obj.srcPath - origin path to the source directory defined in the directories config
+ */
 const onFile = ({ exclude, parseName, subDir, filePath, srcPath }) => {
     // handle excluded files
     if (exclude && filePath.match(new RegExp(exclude.join("|")))) {
@@ -116,7 +146,14 @@ const onFile = ({ exclude, parseName, subDir, filePath, srcPath }) => {
     });
 };
 
-const prepareDocs = async () => {
+/**
+ * Process all files from the directories config
+ * and create mdx files in the docs/storybook directory
+ *
+ * @param {object[]} directories
+ * @returns {Promise<void>}
+ */
+const prepareDocs = async (directories) => {
     rimraf.sync(path.join(__dirname, "../docs/storybook/*"));
     const promises = directories.map(async (data) => {
         const srcPath = path.join(__dirname, data.src);
@@ -138,9 +175,10 @@ const prepareDocs = async () => {
     return Promise.all(promises);
 };
 
-// sort files in ../docs/storybook/99-changelog by version number
-// e.g 1.3.2, 1.2.3-rc.3 1.2.3-rc.2 1.2.3-rc.1, 1.2.1, 1.2.0, rc16, rc15
-
+/**
+ * Sort changelog files by version number
+ * e.g 1.3.2, 1.2.3-rc.3 1.2.3-rc.2 1.2.3-rc.1, 1.2.1, 1.2.0, rc16, rc15
+ */
 const sortChangelog = () => {
     console.log("Start sorting changelog files");
     const changelogDir = path.join(__dirname, "../docs/storybook/99-changelog");
@@ -192,8 +230,27 @@ const sortChangelog = () => {
     console.log("Done sorting changelog files");
 };
 
+// directories config
+// src - path to the source directory
+// exclude - list of files to exclude, regex
+// parseName - function to parse name used in the mdx's Meta title
+// subDir - function adds sub directory to the path if defined in the directories config
+const directories = [
+    {
+        src: "../../../docs",
+        exclude: ["README.md", "images"],
+        parseName: parseNameNoNumbers,
+    },
+    {
+        src: "../docs/changelog",
+        exclude: ["changelog.md"],
+        subDir: () => "99-changelog",
+        parseName: (name) => capitalize(name),
+    },
+];
+
 const main = async () => {
-    await prepareDocs();
+    await prepareDocs(directories);
     sortChangelog();
 
     console.log("Done processing!");
