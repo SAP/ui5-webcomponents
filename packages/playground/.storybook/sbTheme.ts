f397a3c77bf006af1c7679b89cea8e6db090d806
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-02-16 10:17:28 +0200

    fix: brandUrl

--- packages/playground/.storybook/sbTheme.ts
+++ packages/playground/.storybook/sbTheme.ts
@@ -4,6 +4,6 @@ export default create({
     base: "light",
     brandTitle: "UI5 Web Components",
     brandImage: `${process.env.STORYBOOK_ASSETS_BASE}/assets/images/sb-logo.png`,
-    brandUrl: "/",
+    brandUrl: process.env.STORYBOOK_ASSETS_BASE,
     brandTarget: "_self",
 });
