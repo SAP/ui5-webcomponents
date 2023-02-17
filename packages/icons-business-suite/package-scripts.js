e77a191bc3f0314088b9b595d7fb64c12c7d10b9
Kralimarkov <stoyan.kralimarkov@sap.com>
2023-02-17 17:24:10 +0200

    feat: support business icons v1 and v2 font ( Horizon )

--- packages/icons-business-suite/package-scripts.js
+++ packages/icons-business-suite/package-scripts.js
@@ -2,6 +2,7 @@ const getScripts = require("@ui5/webcomponents-tools/icons-collection/nps.js");
 
 const options = {
 	collectionName: "SAP-icons-business-suite",
+	versions: ["v1", "v2"],
 	typescript: true,
 };
 
