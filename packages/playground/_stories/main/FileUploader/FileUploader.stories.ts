bc0b1a37f88a03aef4361648b924a9d4fd771416
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-02-17 15:36:35 +0200

    feat(playground): auto generate stories from samples (#6509)
    * feat: update stories
    
    * build: add sb build cmd to build_as_admin script
    
    * fix: closing tag for <br>

---
+++ packages/playground/_stories/main/FileUploader/FileUploader.stories.ts
@@ -0,0 +1,116 @@
+import { html } from "lit-html";
+import type { Meta, StoryFn } from "@storybook/web-components";
+
+import argTypes, { componentInfo } from "./argTypes.js";
+import type { StoryArgsSlots } from "./argTypes.js";
+import type { UI5StoryArgs } from "../../../types.js";
+
+import { DocsPage } from "../../../.storybook/docs";
+
+// @ts-ignore
+import type FileUploader from "@ui5/webcomponents/dist/FileUploader.js";
+
+const component = "ui5-file-uploader";
+
+export default {
+    title: "Main/FileUploader",
+    component,
+    parameters: {
+        docs: {
+          page: DocsPage({ ...componentInfo, component })
+        },
+    },
+    argTypes,
+} as Meta<FileUploader>;
+
+const Template: UI5StoryArgs<FileUploader, StoryArgsSlots> = (args) => html`<div></div>`;
+
+
+export const Template0: StoryFn = () => html`
+<h3>Upload multiple images</h3>
+	<div class="snippet">
+		<ui5-file-uploader id="fileuploader1" accept="image/*" multiple="true">
+			<ui5-button icon="upload">Upload Images</ui5-button>
+		</ui5-file-uploader>
+		<div id="result"></div>
+		<script>
+			var fileUploader = document.querySelector("#fileuploader1"),
+				resultDiv = document.querySelector("#result");
+			fileUploader.addEventListener("change", function(event) {
+				var files = event.target.files;
+				if (!files.length) {
+					resultDiv.innerHTML = "<ui5-label>No Files Selected</ui5-label>";
+				} else {
+					resultDiv.innerHTML = "";
+					resultDiv.style.marginTop = "1rem";
+					for (var i = 0; i < files.length; i++) {
+						var img = document.createElement("img");
+						img.src = URL.createObjectURL(files[i]);
+						img.width = 100;
+						img.height = 100;
+						img.onload = function() {
+							URL.revokeObjectURL(img.src);
+						}
+						resultDiv.appendChild(img);
+					}
+				}
+			})
+		</script>
+	</div>
+`;
+Template0.parameters = {
+	docs: {
+		story: {
+			// Opt-out of inline rendering
+			inline: false,
+		},
+	}
+};
+
+export const Template1: StoryFn = () => html`
+<h3>Upload Single File</h3>
+	<div class="snippet">
+		<ui5-file-uploader>
+			<ui5-button>Upload Single File</ui5-button>
+		</ui5-file-uploader>
+	</div>
+`;
+
+
+export const Template2: StoryFn = () => html`
+<h3>File Uploader With No Input</h3>
+	<div class="snippet">
+		<ui5-file-uploader hide-input="">
+			<ui5-button>Upload File</ui5-button>
+		</ui5-file-uploader>
+	</div>
+`;
+
+
+export const Template3: StoryFn = () => html`
+<h3>Custom File Uploaders</h3>
+	<div class="snippet">
+		<ui5-file-uploader hide-input="">
+			<ui5-avatar icon="upload"></ui5-avatar>
+		</ui5-file-uploader>
+		<ui5-file-uploader hide-input="">
+			<ui5-badge>Upload File</ui5-badge>
+		</ui5-file-uploader>
+	</div>
+`;
+
+
+export const Template4: StoryFn = () => html`
+<h3>Button With Icon File Uploader</h3>
+	<div class="snippet">
+		<ui5-file-uploader>
+			<ui5-button icon="upload">Upload</ui5-button>
+		</ui5-file-uploader>
+		<ui5-file-uploader>
+			<ui5-button icon="upload" icon-end="">Upload</ui5-button>
+		</ui5-file-uploader>
+		<ui5-file-uploader>
+			<ui5-button icon="upload" icon-only=""></ui5-button>
+		</ui5-file-uploader>
+	</div>
+`;
---
+++ packages/playground/_stories/main/RatingIndicator/RatingIndicator.stories.ts
@@ -0,0 +1,64 @@
+import { html } from "lit-html";
+import type { Meta, StoryFn } from "@storybook/web-components";
+
+import argTypes, { componentInfo } from "./argTypes.js";
+import type { StoryArgsSlots } from "./argTypes.js";
+import type { UI5StoryArgs } from "../../../types.js";
+
+import { DocsPage } from "../../../.storybook/docs";
+
+// @ts-ignore
+import type RatingIndicator from "@ui5/webcomponents/dist/RatingIndicator.js";
+
+const component = "ui5-rating-indicator";
+
+export default {
+    title: "Main/RatingIndicator",
+    component,
+    parameters: {
+        docs: {
+          page: DocsPage({ ...componentInfo, component })
+        },
+    },
+    argTypes,
+} as Meta<RatingIndicator>;
+
+const Template: UI5StoryArgs<RatingIndicator, StoryArgsSlots> = (args) => html`<div></div>`;
+
+
+export const Template0: StoryFn = () => html`
+<h3>Basic Rating Indicator</h3>
+	<div class="snippet">
+		<ui5-rating-indicator></ui5-rating-indicator>
+		<ui5-rating-indicator value="3"></ui5-rating-indicator>
+		<ui5-rating-indicator value="3.7"></ui5-rating-indicator>
+	</div>
+`;
+
+
+export const Template1: StoryFn = () => html`
+<h3>Rating Indicator With Different Max Value</h3>
+	<div class="snippet">
+		<ui5-rating-indicator max="10" value="5"></ui5-rating-indicator>
+		<ui5-rating-indicator max="3" value="3"></ui5-rating-indicator>
+	</div>
+`;
+
+
+export const Template2: StoryFn = () => html`
+<h3>Disabled Rating Indicator</h3>
+	<div class="snippet">
+		<ui5-rating-indicator value="4" disabled=""></ui5-rating-indicator>
+		<ui5-rating-indicator max="10" value="5" disabled=""></ui5-rating-indicator>
+		<ui5-rating-indicator max="3" value="3" disabled=""></ui5-rating-indicator>
+	</div>
+`;
+
+
+export const Template3: StoryFn = () => html`
+<h3>Readonly Rating Indicator</h3>
+	<div class="snippet">
+		<ui5-rating-indicator value="4" readonly=""></ui5-rating-indicator>
+		<ui5-rating-indicator max="7" value="5" readonly=""></ui5-rating-indicator>
+	</div>
+`;
