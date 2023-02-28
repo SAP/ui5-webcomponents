22a4b68f719b6568dbbc0d0b326aa7fb05404aad
ilhan007 <ilhan.myumyun@sap.com>
2023-02-28 16:39:22 +0200

    chore: remove templ and staticAreaTemp decorators

--- packages/base/src/decorators/staticAreaTemplate.ts
+++
@@ -1,7 +0,0 @@
-const staticAreaTemplate = (tmpl: any): ClassDecorator => {
-	return (target: any) => {
-		target.staticAreaTemplate = tmpl;
-	};
-};
-
-export default staticAreaTemplate;
--- packages/base/src/decorators/template.ts
+++
@@ -1,7 +0,0 @@
-const template = (tmpl: any): ClassDecorator => {
-	return (target: any) => {
-		target.template = tmpl;
-	};
-};
-
-export default template;
