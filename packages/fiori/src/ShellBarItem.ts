c5926be7d7bb51f82606d2a1edad59765bb7a58a
ilhan007 <ilhan.myumyun@sap.com>
2023-02-28 13:03:46 +0200

    chore: remove unused import

--- packages/fiori/src/ShellBarItem.ts
+++ packages/fiori/src/ShellBarItem.ts
@@ -1,5 +1,4 @@
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
-import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
 import property from "@ui5/webcomponents-base/dist/decorators/property.js";
 import event from "@ui5/webcomponents-base/dist/decorators/event.js";
 import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
