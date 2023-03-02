01e59e02cf5dd150043d6aa5096b7668251f8beb
Nayden Naydenov <nayden.naydenov@sap.com>
2023-03-02 17:07:11 +0200

    chore: lint

--- packages/localization/config/.eslintrc.cjs
+++ packages/localization/config/.eslintrc.cjs
@@ -1,3 +1,3 @@
-import eslintConfig from "@ui5/webcomponents-tools/components-package/eslint.js";
+const config = require("@ui5/webcomponents-tools/components-package/eslint.js");
 
-export default eslintConfig;
+module.exports = config;
