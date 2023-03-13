7aa820e40586b9380fc3d05ad878c5483e15cfbe
Georgi Minkov <georgi.minkov@sap.com>
2023-03-13 14:48:10 +0200

    chore(ui5-notifications): add samples to Storybook

--- packages/fiori/src/NotificationListItem.ts
+++ packages/fiori/src/NotificationListItem.ts
@@ -112,7 +112,7 @@ class NotificationListItem extends NotificationListItemBase {
 	* they truncate by default.
 	*
 	* <br><br>
-	* <b>Note:</b> by default the <code>titleText</code> and <code>decription</code>,
+	* <b>Note:</b> by default the <code>titleText</code> and <code>description</code>,
 	* and a <code>ShowMore/Less</code> button would be displayed.
 	* @type {sap.ui.webc.main.types.WrappingType}
 	* @defaultvalue "None"
