6468f126dae5e59d9058ed06385860cda7e44c68
Diana Petcheva <diana.petcheva@sap.com>
2023-03-29 12:55:49 +0300

    feat(ui5-bar, ui5-timeline, ui5-view-settings-dialog): add stories to storybook

--- packages/playground/_stories/fiori/Bar/Bar.stories.ts
+++ packages/playground/_stories/fiori/Bar/Bar.stories.ts
@@ -1,4 +1,6 @@
 import { html } from "lit-html";
+import { ifDefined } from "lit-html/directives/if-defined.js";
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
 import type { Meta, StoryFn } from "@storybook/web-components";
 
 import argTypes, { componentInfo } from "./argTypes.js";
@@ -7,68 +9,45 @@ import type { UI5StoryArgs } from "../../../types.js";
 
 import { DocsPage } from "../../../.storybook/docs";
 
-// @ts-ignore
 import type Bar from "@ui5/webcomponents-fiori/dist/Bar.js";
+import BarDesign from "@ui5/webcomponents-fiori/dist/types/BarDesign.js";
 
 const component = "ui5-bar";
 
 export default {
-    title: "Fiori/Bar",
-    component,
-    parameters: {
-        docs: {
-          page: DocsPage({ ...componentInfo, component })
-        },
-    },
-    argTypes,
+	title: "Fiori/Bar",
+	component,
+	parameters: {
+		docs: {
+			page: DocsPage({ ...componentInfo, component })
+		},
+	},
+	argTypes,
 } as Meta<Bar>;
 
-const Template: UI5StoryArgs<Bar, StoryArgsSlots> = (args) => html`<div></div>`;
+const Template: UI5StoryArgs<Bar, StoryArgsSlots> = (args) => html`
+<ui5-bar
+	design="${ifDefined(args.design)}"
+>
+	${unsafeHTML(args.default)}
+	${unsafeHTML(args.startContent)}
+	${unsafeHTML(args.endContent)}
+</ui5-bar>`;
 
+export const Basic = Template.bind({});
+Basic.storyName = "With Content and Header Design";
+Basic.args = {
+	design: BarDesign.Header,
+	startContent: `<ui5-button icon="home" tooltip="Go home" design="Transparent" slot="startContent"></ui5-button>`,
+	default: `<ui5-label id="basic-label">Title</ui5-label>`,
+	endContent: `<ui5-button icon="action-settings" tooltip="Go to settings" slot="endContent"></ui5-button>`,
+};
 
-export const Template0: StoryFn = () => html`
-<h3>Header Bar</h3>
-	<div class="snippet">
-		<ui5-bar design="Header">
-			<ui5-button icon="home" tooltip="Go home" design="Transparent" slot="startContent"></ui5-button>
-			<ui5-label id="basic-label">Header Title</ui5-label>
-			<ui5-button icon="action-settings" tooltip="Go to settings" slot="endContent"></ui5-button>
-		</ui5-bar>
-	</div>
-`;
-
-
-export const Template1: StoryFn = () => html`
-<h3>Subheader Bar</h3>
-	<div class="snippet">
-		<ui5-bar design="Subheader">
-			<ui5-button icon="home" tooltip="Go home" slot="startContent"></ui5-button>
-			<ui5-label id="basic-label">Subheader Title</ui5-label>
-			<ui5-button icon="action-settings" tooltip="Go to settings" slot="endContent"></ui5-button>
-		</ui5-bar>
-	</div>
-`;
-
-
-export const Template2: StoryFn = () => html`
-<h3>Footer Bar</h3>
-	<div class="snippet">
-		<ui5-bar design="Footer">
-			<ui5-button design="Positive" slot="endContent">Agree</ui5-button>
-			<ui5-button design="Negative" slot="endContent">Decline</ui5-button>
-			<ui5-button design="Transparent" slot="endContent">Cancel</ui5-button>
-		</ui5-bar>
-	</div>
-`;
-
-
-export const Template3: StoryFn = () => html`
-<h3>FloatingFooter Bar</h3>
-	<div class="snippet">
-		<ui5-bar design="FloatingFooter">
-			<ui5-button design="Positive" slot="endContent">Agree</ui5-button>
-			<ui5-button design="Negative" slot="endContent">Decline</ui5-button>
-			<ui5-button design="Transparent" slot="endContent">Cancel</ui5-button>
-		</ui5-bar>
-	</div>
-`;
+export const Footer = Template.bind({});
+Footer.storyName = "With Content and Footer Design";
+Footer.args = {
+	design: BarDesign.Footer,
+	startContent: `<ui5-button design="Positive" slot="endContent">Agree</ui5-button>`,
+	default: `<ui5-button design="Negative" slot="endContent">Decline</ui5-button>`,
+	endContent: `<ui5-button design="Transparent" slot="endContent">Cancel</ui5-button>`,
+};
--- packages/playground/_stories/fiori/ViewSettingsDialog/ViewSettingsDialog.stories.ts
+++ packages/playground/_stories/fiori/ViewSettingsDialog/ViewSettingsDialog.stories.ts
@@ -1,4 +1,6 @@
 import { html } from "lit-html";
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
+import { ifDefined } from "lit-html/directives/if-defined.js";
 import type { Meta, StoryFn } from "@storybook/web-components";
 
 import argTypes, { componentInfo } from "./argTypes.js";
@@ -7,72 +9,85 @@ import type { UI5StoryArgs } from "../../../types.js";
 
 import { DocsPage } from "../../../.storybook/docs";
 
-// @ts-ignore
 import type ViewSettingsDialog from "@ui5/webcomponents-fiori/dist/ViewSettingsDialog.js";
 
 const component = "ui5-view-settings-dialog";
 
 export default {
-    title: "Fiori/ViewSettingsDialog",
-    component,
-    subcomponents: {'SortItem' : 'ui5-sort-item', 'FilterItem' : 'ui5-filter-item', 'FilterItemOption' : 'ui5-filter-item-option'},
-    parameters: {
-        docs: {
-          page: DocsPage({ ...componentInfo, component })
-        },
-    },
-    argTypes,
+	title: "Fiori/ViewSettingsDialog",
+	component,
+	subcomponents: {'SortItem' : 'ui5-sort-item', 'FilterItem' : 'ui5-filter-item', 'FilterItemOption' : 'ui5-filter-item-option'},
+	parameters: {
+		docs: {
+			page: DocsPage({ ...componentInfo, component })
+		},
+	},
+	argTypes,
 } as Meta<ViewSettingsDialog>;
 
-const Template: UI5StoryArgs<ViewSettingsDialog, StoryArgsSlots> = (args) => html`<div></div>`;
+const Template: UI5StoryArgs<ViewSettingsDialog, StoryArgsSlots> = (args) => html`
+<ui5-view-settings-dialog
+	id="${ifDefined(args.id)}"
+	sort-descending="${ifDefined(args.sortDescending)}"
+>
+	${unsafeHTML(args.sortItems)}
+	${unsafeHTML(args.filterItems)}
+</ui5-view-settings-dialog>`;
 
-
-export const Template0: StoryFn = () => html`
-<h3>Usage</h3>
-	<div class="snippet">
+export const Basic = Template.bind({});
+Basic.storyName = "Example Usage";
+Basic.args = {
+	id: "vsd1",
+	sortDescending: true,
+	sortItems: `
+		<ui5-sort-item slot="sortItems" text="Name" selected=""></ui5-sort-item>
+		<ui5-sort-item slot="sortItems" text="Position"></ui5-sort-item>
+		<ui5-sort-item slot="sortItems" text="Company"></ui5-sort-item>
+		<ui5-sort-item slot="sortItems" text="Department"></ui5-sort-item>
+	`,
+	filterItems: `
+		<ui5-filter-item slot="filterItems" text="Position">
+			<ui5-filter-item-option slot="values" text="CTO"></ui5-filter-item-option>
+			<ui5-filter-item-option slot="values" text="CPO"></ui5-filter-item-option>
+			<ui5-filter-item-option slot="values" text="VP"></ui5-filter-item-option>
+		</ui5-filter-item>
+		<ui5-filter-item slot="filterItems" text="Department">
+			<ui5-filter-item-option slot="values" text="Sales"></ui5-filter-item-option>
+			<ui5-filter-item-option slot="values" text="Management"></ui5-filter-item-option>
+			<ui5-filter-item-option slot="values" text="PR"></ui5-filter-item-option>
+		</ui5-filter-item>
+		<ui5-filter-item slot="filterItems" text="Location">
+			<ui5-filter-item-option slot="values" text="Walldorf"></ui5-filter-item-option>
+			<ui5-filter-item-option slot="values" text="New York"></ui5-filter-item-option>
+			<ui5-filter-item-option slot="values" text="London"></ui5-filter-item-option>
+		</ui5-filter-item>
+		<ui5-filter-item slot="filterItems" text="Reports to">
+			<ui5-filter-item-option slot="values" text="CTO"></ui5-filter-item-option>
+			<ui5-filter-item-option slot="values" text="CPO"></ui5-filter-item-option>
+			<ui5-filter-item-option slot="values" text="VP"></ui5-filter-item-option>
+		</ui5-filter-item>
+	`,
+};
+Basic.decorators = [
+	(story) => html`
 		<ui5-button id="btnOpenDialog1">Open ViewSettingsDialog</ui5-button>
-		<ui5-view-settings-dialog id="vsd1">
-				<ui5-sort-item slot="sortItems" text="Name" selected=""></ui5-sort-item>
-				<ui5-sort-item slot="sortItems" text="Position"></ui5-sort-item>
-				<ui5-sort-item slot="sortItems" text="Company"></ui5-sort-item>
-				<ui5-sort-item slot="sortItems" text="Department"></ui5-sort-item>
-				<ui5-filter-item slot="filterItems" text="Position">
-					<ui5-filter-item-option slot="values" text="CTO"></ui5-filter-item-option>
-					<ui5-filter-item-option slot="values" text="CPO"></ui5-filter-item-option>
-					<ui5-filter-item-option slot="values" text="VP"></ui5-filter-item-option>
-				</ui5-filter-item>
-				<ui5-filter-item slot="filterItems" text="Department">
-					<ui5-filter-item-option slot="values" text="Sales"></ui5-filter-item-option>
-					<ui5-filter-item-option slot="values" text="Management"></ui5-filter-item-option>
-					<ui5-filter-item-option slot="values" text="PR"></ui5-filter-item-option>
-				</ui5-filter-item>
-				<ui5-filter-item slot="filterItems" text="Location">
-					<ui5-filter-item-option slot="values" text="Walldorf"></ui5-filter-item-option>
-					<ui5-filter-item-option slot="values" text="New York"></ui5-filter-item-option>
-					<ui5-filter-item-option slot="values" text="London"></ui5-filter-item-option>
-				</ui5-filter-item>
-				<ui5-filter-item slot="filterItems" text="Reports to">
-					<ui5-filter-item-option slot="values" text="CTO"></ui5-filter-item-option>
-					<ui5-filter-item-option slot="values" text="CPO"></ui5-filter-item-option>
-					<ui5-filter-item-option slot="values" text="VP"></ui5-filter-item-option>
-				</ui5-filter-item>
-			</ui5-view-settings-dialog>
+		${story()}
 		<br/>
 		<br/>
 		<div id="vsdResults"></div>
-	</div>
-	<script>
-		var vsdResults = document.getElementById("vsdResults");
-		btnOpenDialog1.addEventListener("click", function () {
-			vsdResults.innerHTML = "";
-			vsd1.show();
-		});
-		vsd1.addEventListener("confirm", function(evt) {
-			vsdResults.innerHTML = JSON.stringify(evt.detail);
-		});
-	</script>
-`;
-Template0.parameters = {
+		<script>
+			var vsdResults = document.getElementById("vsdResults");
+			btnOpenDialog1.addEventListener("click", function () {
+				vsdResults.innerHTML = "";
+				vsd1.show();
+			});
+			vsd1.addEventListener("confirm", function(evt) {
+				vsdResults.innerHTML = JSON.stringify(evt.detail);
+			});
+		</script>
+	`,
+];
+Basic.parameters = {
 	docs: {
 		story: {
 			// Opt-out of inline rendering
