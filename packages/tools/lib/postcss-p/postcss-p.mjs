8ac166b6f172861131ec0afb2a7f0070acffe2b9
Nayden Naydenov <nnaydenow.work@sap.com>
2023-04-05 10:59:37 +0300

    chore: test2

--- packages/tools/lib/postcss-p/postcss-p.mjs
+++ packages/tools/lib/postcss-p/postcss-p.mjs
@@ -10,5 +10,5 @@ const restArgs = process.argv.slice(2);
 // run all postcss processes in parallel as passing the glob directly to postcss makes them processed sequentially.
 // and the amount of imports give a big speed up when run in parallel
 await Promise.all(inputFiles.map(file => {
-  return $`postcss ${file} --config config/postcss.themes --base src --dir dist/css/ ${restArgs}`;
+  return $`postcss ${file} --config ${path.join(process.cwd(), "config/postcss.themes")} --base src --dir ${path.join(process.cwd(), "config/postcss.themes")} ${restArgs}`;
 }));
