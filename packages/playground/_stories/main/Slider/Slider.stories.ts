4eb682e1840e154706f454fbb24b74beacdf0670
“Evdokia <evdokia.kotova@sap.com>
2023-04-10 07:03:22 +0300

    feat(ui5-panel, ui5-rating-indicator, ui5-slider, ui5-text-area): add storybook samples

--- packages/playground/_stories/main/Slider/Slider.stories.ts
+++ packages/playground/_stories/main/Slider/Slider.stories.ts
@@ -9,6 +9,7 @@ import { DocsPage } from "../../../.storybook/docs";
 
 // @ts-ignore
 import type Slider from "@ui5/webcomponents/dist/Slider.js";
+import { ifDefined } from "lit-html/directives/if-defined.js";
 
 const component = "ui5-slider";
 
@@ -23,36 +24,51 @@ export default {
     argTypes,
 } as Meta<Slider>;
 
-const Template: UI5StoryArgs<Slider, StoryArgsSlots> = (args) => html`<div></div>`;
+const Template: UI5StoryArgs<Slider, StoryArgsSlots> = (args) => html`	<ui5-slider
+value="${ifDefined(args.value)}"
+min="${ifDefined(args.min)}"
+max="${ifDefined(args.max)}"
+step="${ifDefined(args.step)}"
+?show-tickmarks="${ifDefined(args.showTickmarks)}"
+label-interval="${ifDefined(args.labelInterval)}"
+?show-tooltip="${ifDefined(args.showTooltip)}"
+?disabled="${ifDefined(args.disabled)}"
+accessible-name="${ifDefined(args.accessibleName)}"
+></ui5-slider>`;
+
+export const BasicSlider = Template.bind({});
+BasicSlider.args = {};
+BasicSlider.storyName = "Basic Slider";
+
+export const SliderTooltip = Template.bind({});
+SliderTooltip.args = {
+	min: 0,
+	max: 20,
+	showTooltip: true,
+	labelInterval: 5
+
+};
+SliderTooltip.storyName = "Slider with Tooltip";
+
+export const DisabledTickmarksLabel = Template.bind({});
+DisabledTickmarksLabel.args = {
+	min: 20,
+	max: 100,
+	labelInterval: 5,
+	disabled: true,
+	showTickmarks: true
+};
+DisabledTickmarksLabel.storyName = "Disabled Slider with Tickmarks and Labels";
 
 
-export const Template0: StoryFn = () => html`
-<h3>Basic Slider</h3>
-	<div class="snippet">
-		<ui5-slider></ui5-slider>
-	</div>
-`;
-
-
-export const Template1: StoryFn = () => html`
-<h3>Slider with Tooltip</h3>
-	<div class="snippet">
-		<ui5-slider min="0" max="20" show-tooltip=""></ui5-slider>
-	</div>
-`;
-
-
-export const Template2: StoryFn = () => html`
-<h3>Disabled Slider with Tickmarks and Labels</h3>
-	<div class="snippet">
-		<ui5-slider min="20" max="100" label-interval="5" disabled="" show-tickmarks=""></ui5-slider>
-	</div>
-`;
-
-
-export const Template3: StoryFn = () => html`
-<h3>Slider Tooltip, Tickmarks and Labels</h3>
-	<div class="snippet">
-		<ui5-slider min="-20" max="20" step="2" value="12" show-tooltip="" label-interval="2" show-tickmarks=""></ui5-slider>
-	</div>
-`;
+export const TickmarksLabelTooltip = Template.bind({});
+TickmarksLabelTooltip.args = {
+	min: -20,
+	max: 20,
+	step: 2,
+	value: 12,
+	showTooltip: true,
+	labelInterval: 2,
+	showTickmarks: true
+};
+TickmarksLabelTooltip.storyName = "Slider Tooltip, Tickmarks and Labels";
--- packages/playground/_stories/main/TextArea/TextArea.stories.ts
+++ packages/playground/_stories/main/TextArea/TextArea.stories.ts
@@ -1,4 +1,6 @@
 import { html } from "lit-html";
+import { ifDefined } from "lit-html/directives/if-defined.js";
+
 import type { Meta, StoryFn } from "@storybook/web-components";
 
 import argTypes, { componentInfo } from "./argTypes.js";
@@ -9,6 +11,7 @@ import { DocsPage } from "../../../.storybook/docs";
 
 // @ts-ignore
 import type TextArea from "@ui5/webcomponents/dist/TextArea.js";
+import ValueState from "@ui5/webcomponents-base/dist/types/ValueState.js";
 
 const component = "ui5-textarea";
 
@@ -23,29 +26,43 @@ export default {
     argTypes,
 } as Meta<TextArea>;
 
-const Template: UI5StoryArgs<TextArea, StoryArgsSlots> = (args) => html`<div></div>`;
-
-
-export const Template0: StoryFn = () => html`
-<h3>Basic TextArea</h3>
-	<div class="snippet">
-		<ui5-textarea class="textarea-width" placeholder="Type as much text as you wish"></ui5-textarea>
-	</div>
+const Template: UI5StoryArgs<TextArea, StoryArgsSlots> = (args) => html`
+	<ui5-textarea
+		value="${ifDefined(args.value)}"
+		?disabled="${ifDefined(args.disabled)}"
+		?readonly="${ifDefined(args.readonly)}"
+		?required="${ifDefined(args.required)}"
+		placeholder="${ifDefined(args.placeholder)}"
+		value-state="${ifDefined(args.valueState)}"
+		rows="${ifDefined(args.rows)}"
+		maxlength="${ifDefined(args.maxlength)}"
+		?show-exceeded-text="${ifDefined(args.showExceededText)}"
+		?growing="${ifDefined(args.growing)}"
+		growing-max-lines="${ifDefined(args.growingMaxLines)}"
+		name="${ifDefined(args.name)}"
+		accessible-name="${ifDefined(args.accessibleName)}"
+		accessible-name-ref="${ifDefined(args.accessibleNameRef)}"
+		>
+	</ui5-textarea>
 `;
 
-
-export const Template1: StoryFn = () => html`
-<h3>TextArea with Maximum Length</h3>
-	<div class="snippet">
-		<ui5-textarea class="textarea-width" placeholder="Type no more than 10 symbols" maxlength="10" show-exceeded-text=""></ui5-textarea>
-	</div>
-`;
+export const BasicTextArea = Template.bind({});
+BasicTextArea.args = {
+	placeholder: 'Type as much text as you wish',
+};
+BasicTextArea.storyName = "Basic TextArea";
 
 
-export const Template2: StoryFn = () => html`
-<h3>TextArea with Label</h3>
-	<div class="snippet">
-		<ui5-label for="textAreaWithLabelID">Description</ui5-label>
-		<ui5-textarea id="textAreaWithLabelID" class="textarea-width" placeholder="Enter description"></ui5-textarea>
-	</div>
+export const TextAreaMaxLength = Template.bind({});
+TextAreaMaxLength.args = {
+	placeholder: 'Type no more than 10 symbols',
+	maxlength: 10,
+	showExceededText: true
+
+};
+TextAreaMaxLength.storyName = "TextArea with Maximum Length";
+
+export const TextAreaLabel: StoryFn = () => html`
+	<ui5-label for="textAreaWithLabelID">Description</ui5-label>
+	<ui5-textarea id="textAreaWithLabelID" required="" placeholder="Enter description"></ui5-textarea>
 `;
