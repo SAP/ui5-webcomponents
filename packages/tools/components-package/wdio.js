f0ac65a231eae18cedcfdaf80070ee3be0dc2732
Peter Skelin <petar.skelin@sap.com>
2023-04-11 12:01:58 +0300

    chore: test skip

--- packages/tools/components-package/wdio.js
+++ packages/tools/components-package/wdio.js
@@ -99,7 +99,7 @@ exports.config = {
 	// Services take over a specific job you don't want to take care of. They enhance
 	// your test setup with almost no effort. Unlike plugins, they don't add new
 	// commands. Instead, they hook themselves up into the test process.
-	services: ['chromedriver', 'devtools',
+	services: [['chromedriver', {chromedriverCustomPath: process.env.CHROMEWEBDRIVER}], 'devtools',
 		['static-server', {
 			folders: [
 				{ mount: '/', path: './dist' },
