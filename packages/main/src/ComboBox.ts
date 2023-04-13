a3d0cd6c765520ea19be392aa4e6e6a6ea056759
Martin R. Hristov <m.hristov@sap.com>
2023-04-13 17:27:21 +0300

    feat(ui5-combobox): add storybook samples

--- packages/main/src/ComboBox.ts
+++ packages/main/src/ComboBox.ts
@@ -324,7 +324,7 @@ class ComboBox extends UI5Element {
 	 * Defines the filter type of the component.
 	 * Available options are: <code>StartsWithPerTerm</code>, <code>StartsWith</code>, <code>Contains</code> and <code>None</code>.
 	 *
-	 * @type {string}
+	 * @type {sap.ui.webc.main.types.ComboBoxFilter}
 	 * @name sap.ui.webc.main.ComboBox.prototype.filter
 	 * @defaultvalue "StartsWithPerTerm"
 	 * @public
