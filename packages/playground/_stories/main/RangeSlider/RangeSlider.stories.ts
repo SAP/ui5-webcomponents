d64614d2607dfd17bee1cf7b2871c4571c31a28a
“Evdokia <evdokia.kotova@sap.com>
2023-04-19 19:25:51 +0300

    feat(ui5-range-slider): add storybook sample

--- packages/playground/_stories/main/RangeSlider/RangeSlider.stories.ts
+++ packages/playground/_stories/main/RangeSlider/RangeSlider.stories.ts
@@ -9,6 +9,7 @@ import { DocsPage } from "../../../.storybook/docs";
 
 // @ts-ignore
 import type RangeSlider from "@ui5/webcomponents/dist/RangeSlider.js";
+import { ifDefined } from "lit-html/directives/if-defined.js";
 
 const component = "ui5-range-slider";
 
@@ -23,44 +24,59 @@ export default {
     argTypes,
 } as Meta<RangeSlider>;
 
-const Template: UI5StoryArgs<RangeSlider, StoryArgsSlots> = (args) => html`<div></div>`;
+const Template: UI5StoryArgs<RangeSlider, StoryArgsSlots> = (args) => html`
+<ui5-range-slider
+	start-value="${ifDefined(args.startValue)}"
+	end-value="${ifDefined(args.endValue)}"
+	min="${ifDefined(args.min)}"
+	max="${ifDefined(args.max)}"
+	step="${ifDefined(args.step)}"
+	?show-tickmarks="${ifDefined(args.showTickmarks)}"
+	label-interval="${ifDefined(args.labelInterval)}"
+	?show-tooltip="${ifDefined(args.showTooltip)}"
+></ui5-range-slider>`;
 
+export const BasicRangeSlider = Template.bind({});
+BasicRangeSlider.args = {
+	endValue: 20
+};
+BasicRangeSlider.storyName = "Basic Range Slider";
 
-export const Template0: StoryFn = () => html`
-<h3>Basic Range Slider</h3>
-	<div class="snippet">
-		<ui5-range-slider end-value="20"></ui5-range-slider>
-	</div>
-`;
+export const RangeSliderCustomValues = Template.bind({});
+RangeSliderCustomValues.args = {
+	min: 100,
+	max: 200,
+	startValue: 120,
+	endValue: 150
+};
+RangeSliderCustomValues.storyName = "Range Slider with Custom 'min', 'max', 'startValue' and 'endValue' Properties";
 
+export const RangeSliderTooltip = Template.bind({});
+RangeSliderTooltip.args = {
+	startValue: 3,
+	endValue: 13,
+	showTooltip: true
+};
+RangeSliderTooltip.storyName = "Range Slider with Tooltips";
 
-export const Template1: StoryFn = () => html`
-<h3>Range Slider with Custom 'min', 'max', 'startValue' and 'endValue' Properties</h3>
-	<div class="snippet">
-		<ui5-range-slider min="100" max="200" start-value="120" end-value="150"></ui5-range-slider>
-	</div>
-`;
+export const RangeSliderTickmarksCustomStep = Template.bind({});
+RangeSliderTickmarksCustomStep.args = {
+	step: 2,
+	startValue: 12,
+	endValue: 24,
+	showTickmarks: true
+};
+RangeSliderTickmarksCustomStep.storyName = "Range Slider with Tickmarks and Custom Step";
 
-
-export const Template2: StoryFn = () => html`
-<h3>Range Slider with Tooltips</h3>
-	<div class="snippet">
-		<ui5-range-slider start-value="3" end-value="13" show-tooltip=""></ui5-range-slider>
-	</div>
-`;
-
-
-export const Template3: StoryFn = () => html`
-<h3>Range Slider with Tickmarks and Custom Step</h3>
-	<div class="snippet">
-		<ui5-range-slider step="2" start-value="12" end-value="24" show-tickmarks=""></ui5-range-slider>
-	</div>
-`;
-
-
-export const Template4: StoryFn = () => html`
-<h3>Range Slider with Tooltips, Tickmarks and Labels</h3>
-	<div class="snippet">
-		<ui5-range-slider min="0" max="112" step="2" start-value="4" end-value="12" show-tooltip="" label-interval="2" show-tickmarks=""></ui5-range-slider>
-	</div>
-`;
+export const RangeSliderTickmarksTooltipLabel = Template.bind({});
+RangeSliderTickmarksTooltipLabel.args = {
+	min: 0,
+	max: 112,
+	step: 2,
+	startValue: 4,
+	endValue: 12,
+	labelInterval: 2,
+	showTickmarks: true,
+	showTooltip: true
+};
+RangeSliderTickmarksTooltipLabel.storyName = "Range Slider with Tooltips, Tickmarks and Labels";
