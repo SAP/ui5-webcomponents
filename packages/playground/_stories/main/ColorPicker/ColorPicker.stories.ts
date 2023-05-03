d4409741466b008689452ece15b47453539de32f
Diana <diana.petcheva@sap.com>
2023-05-03 12:12:39 +0300

    docs(ui5-color-picker, ui5-color-palette, ui5-color-palette-popover): add samples to storybook (#6927)

--- packages/playground/_stories/main/ColorPicker/ColorPicker.stories.ts
+++ packages/playground/_stories/main/ColorPicker/ColorPicker.stories.ts
@@ -1,4 +1,5 @@
 import { html } from "lit";
+import { ifDefined } from "lit/directives/if-defined.js";
 import type { Meta, StoryFn } from "@storybook/web-components";
 
 import argTypes, { componentInfo } from "./argTypes.js";
@@ -7,28 +8,26 @@ import type { UI5StoryArgs } from "../../../types.js";
 
 import { DocsPage } from "../../../.storybook/docs";
 
-// @ts-ignore
 import type ColorPicker from "@ui5/webcomponents/dist/ColorPicker.js";
 
 const component = "ui5-color-picker";
 
 export default {
-    title: "Main/ColorPicker",
-    component,
-    parameters: {
-        docs: {
-          page: DocsPage({ ...componentInfo, component })
-        },
-    },
-    argTypes,
+	title: "Main/ColorPicker",
+	component,
+	parameters: {
+		docs: {
+			page: DocsPage({ ...componentInfo, component })
+		},
+	},
+	argTypes,
 } as Meta<ColorPicker>;
 
-const Template: UI5StoryArgs<ColorPicker, StoryArgsSlots> = (args) => html`<div></div>`;
+const Template: UI5StoryArgs<ColorPicker, StoryArgsSlots> = (args) => html`<ui5-color-picker
+	color="${ifDefined(args.color)}"
+>Picker</ui5-color-picker>`;
 
-
-export const Template0: StoryFn = () => html`
-<h3>Pick color</h3>
-	<div class="snippet">
-		<ui5-color-picker></ui5-color-picker>
-	</div>
-`;
+export const Basic = Template.bind({});
+Basic.args = {
+	color: "rgba(220, 208, 255, 1)",
+};
