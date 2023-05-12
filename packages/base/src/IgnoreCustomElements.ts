2e3efe45416891cc87b9841cc866fd09bc4f3f00
ilhan007 <ilhan.myumyun@sap.com>
2023-05-12 14:29:41 +0300

    chore: edit jsdoc

--- packages/base/src/IgnoreCustomElements.ts
+++ packages/base/src/IgnoreCustomElements.ts
@@ -1,21 +1,21 @@
 /**
- * The tag preffixes to be ignored.
+ * The tag prefixes to be ignored.
  */
 const tagPrefixes: Array<string> = [];
 
 /**
- * Ignores all custom HTML elements with a given tag preffix to improve the rendering performance of the UI5 Web Components.
+ * Ignores all custom HTML elements with a given tag prefix to improve the rendering performance of the UI5 Web Components.
  * <br>
  *
  * <b>When used:</b> the UI5 Web Components framework treats all custom HTML elements,
- * starting with the given preffix as if they are standard HTML elements, such as: `div`, `span`, etc, without additional processing.
+ * starting with the given prefix as if they are standard HTML elements, such as: `div`, `span`, etc, without additional processing.
  * <br>
  *
  * <b>When not used:</b> the framework waits for the slotted children to be defined and registered first,
  * because the state or visual appearance of the parent may rely on the slotted elements/children.
  *
- * <b>Note:</b> We recommend using `ignoreCustomElements` when slotting custom HTML elements (with only semantic purpose) inside UI5 Web Components,
- * to improve the time to render.
+ * <b>Note:</b> We recommend using `ignoreCustomElements` when slotting custom HTML elements (with only semantic purpose)
+ * inside UI5 Web Components, to improve the time to render.
  *
  * @public
  * @since 1.14.0
@@ -23,7 +23,7 @@ const tagPrefixes: Array<string> = [];
  */
 const ignoreCustomElements = (tagPrefix: string) => {
 	if (typeof tagPrefix !== "string" || !tagPrefix.length) {
-		throw new Error("Only string characters for a tag preffix.");
+		throw new Error("Only string characters for a tag prefix.");
 	}
 
 	tagPrefixes.push(tagPrefix);
@@ -36,7 +36,7 @@ const ignoreCustomElements = (tagPrefix: string) => {
  * @param { string } tag
  */
 const shouldIgnoreCustomElement = (tag: string): boolean => {
-	return tagPrefixes.some(preff => tag.startsWith(preff));
+	return tagPrefixes.some(pref => tag.startsWith(pref));
 };
 
 export {
