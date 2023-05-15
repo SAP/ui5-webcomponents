d076a91bafaf7c1e51a470edbd53e33f630573e8
ilhan007 <ilhan.myumyun@sap.com>
2023-05-15 13:36:55 +0300

    chore: add test-suite-input command

--- packages/tools/components-package/nps.js
+++ packages/tools/components-package/nps.js
@@ -116,6 +116,7 @@ const getScripts = (options) => {
 		"test-suite-1": `node "${LIB}/test-runner/test-runner.js" --suite suite1`,
 		"test-suite-2": `node "${LIB}/test-runner/test-runner.js" --suite suite2`,
 		"test-suite-3": `node "${LIB}/test-runner/test-runner.js" --suite suite3`,
+		"test-suite-input": `node "${LIB}/test-runner/test-runner.js" --suite suite-input`,
 		"test-suite-datetime": `node "${LIB}/test-runner/test-runner.js" --suite suite-datetime`,
 		startWithScope: "nps scope.prepare scope.watchWithBundle",
 		scope: {
