22962e1fd1c403571beaef138de13ed434747e61
ilhan007 <ilhan.myumyun@sap.com>
2023-05-15 20:12:18 +0300

    chore: test more instances option

--- packages/base/config/wdio.conf.cjs
+++ packages/base/config/wdio.conf.cjs
@@ -40,7 +40,7 @@ exports.config = {
 	// and 30 processes will get spawned. The property handles how many capabilities
 	// from the same test should run tests.
 	//
-	maxInstances: 10,
+	maxInstances: 15,
 	//
 	// If you have trouble getting all important capabilities together, check out the
 	// Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -50,7 +50,7 @@ exports.config = {
 		// maxInstances can get overwritten per capability. So if you have an in-house Selenium
 		// grid with only 5 firefox instances available you can make sure that not more than
 		// 5 instances get started at a time.
-		maxInstances: 5,
+		maxInstances: 10,
 		//
 		browserName: 'chrome',
 		'goog:chromeOptions': {
