2b4c8df477a05422b4831a78b3e11ac9c603f724
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-05-18 16:36:28 +0300

    docs(ui5-media-gallery): add storybook sample

--- packages/playground/_stories/fiori/MediaGallery/MediaGallery.stories.ts
+++ packages/playground/_stories/fiori/MediaGallery/MediaGallery.stories.ts
@@ -1,7 +1,8 @@
 import { html } from "lit";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 import { ifDefined } from "lit/directives/if-defined.js";
-import type { Meta, StoryFn } from "@storybook/web-components";
+import type { Meta } from "@storybook/web-components";
+import type { PartialStoryFn } from "@storybook/types";
 
 import argTypes, { componentInfo } from "./argTypes.js";
 import type { StoryArgsSlots } from "./argTypes.js";
@@ -9,9 +10,26 @@ import type { UI5StoryArgs } from "../../../types.js";
 
 import { DocsPage } from "../../../.storybook/docs";
 
-import type MediaGallery from "@ui5/webcomponents-fiori/dist/MediaGallery.js";
+import MediaGallery from "@ui5/webcomponents-fiori/dist/MediaGallery.js";
+import MediaGalleryLayout from "@ui5/webcomponents-fiori/dist/types/MediaGalleryLayout.js";
+import MediaGalleryMenuHorizontalAlign from "@ui5/webcomponents-fiori/dist/types/MediaGalleryMenuHorizontalAlign.js";
 
 const component = "ui5-media-gallery";
+
+const stylesDecorator = (storyFn: PartialStoryFn) => html`
+    <style>
+        ui5-media-gallery-item:not(:defined) {
+            visibility: hidden;
+        }
+
+        @media (min-width: 600px) {
+            ui5-media-gallery {
+                height: 50rem;
+            }
+        }
+    </style>
+    ${storyFn()}
+`;
 
 export default {
     title: "Fiori/MediaGallery",
@@ -22,11 +40,15 @@ export default {
             page: DocsPage({ ...componentInfo, component }),
         },
     },
+    decorators: [stylesDecorator],
     argTypes,
 } as Meta<MediaGallery>;
 
-const Template: UI5StoryArgs<MediaGallery, StoryArgsSlots> = (args) =>
-    html`<ui5-media-gallery
+let index = 0;
+
+const Template: UI5StoryArgs<MediaGallery, StoryArgsSlots> = (args) => {
+    return html` <ui5-media-gallery
+        id="media-gallery-${index++}"
         ?show-all-thumbnails="${ifDefined(args.showAllThumbnails)}"
         ?interactive-display-area="${ifDefined(args.interactiveDisplayArea)}"
         layout="${ifDefined(args.layout)}"
@@ -35,12 +57,12 @@ const Template: UI5StoryArgs<MediaGallery, StoryArgsSlots> = (args) =>
     >
         ${unsafeHTML(args.default)}
     </ui5-media-gallery>`;
-
+};
 export const Basic = Template.bind({});
 Basic.args = {
     showAllThumbnails: true,
     default: `
-	ui5-media-gallery-item>
+	<ui5-media-gallery-item>
 		<img src="../assets/images/HT-1000.jpg" />
 	</ui5-media-gallery-item>
 	<ui5-media-gallery-item>
@@ -60,230 +82,235 @@ Basic.args = {
 	</ui5-media-gallery-item>`,
 };
 
-export const Template1: StoryFn = () => html`
-    <h3>MediaGallery with vertical layout</h3>
-    <div class="snippet">
-        <ui5-media-gallery show-all-thumbnails="" layout="Vertical">
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1000.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1010.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1022.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1030.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-2002.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-2026.jpg" />
-            </ui5-media-gallery-item>
-        </ui5-media-gallery>
-    </div>
-`;
+export const WithVerticalLayout = Template.bind({});
+WithVerticalLayout.args = {
+    showAllThumbnails: true,
+    layout: MediaGalleryLayout.Vertical,
+    default: `
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1000.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1010.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1022.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1030.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-2002.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-2026.jpg" />
+    </ui5-media-gallery-item>`,
+};
 
-export const Template2: StoryFn = () => html`
-    <h3>MediaGallery with thumbnails on the right</h3>
-    <div class="snippet">
-        <ui5-media-gallery
-            show-all-thumbnails=""
-            layout="Horizontal"
-            menu-horizontal-align="Right"
-        >
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1000.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1010.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1022.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1030.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-2002.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-2026.jpg" />
-            </ui5-media-gallery-item>
-        </ui5-media-gallery>
-    </div>
-`;
+export const WithInteractiveDisplayArea = Template.bind({});
+WithInteractiveDisplayArea.args = {
+    showAllThumbnails: true,
+    layout: MediaGalleryLayout.Vertical,
+    menuHorizontalAlign: MediaGalleryMenuHorizontalAlign.Right,
+    default: `
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1000.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1010.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1022.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1030.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-2002.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-2026.jpg" />
+    </ui5-media-gallery-item>`,
+};
 
-export const Template3: StoryFn = () => html`
-    <h3>MediaGallery item with separate image for its thumbnail</h3>
-    <div class="snippet">
-        <ui5-media-gallery>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1000.jpg" />
-                <img
-                    src="../assets/images/HT-1000-small.jpg"
-                    slot="thumbnail"
-                />
-            </ui5-media-gallery-item>
-        </ui5-media-gallery>
-    </div>
-`;
+export const WithMenuHorizontalAlign = Template.bind({});
+WithMenuHorizontalAlign.args = {
+    default: `
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1000.jpg" />
+        <img
+            src="../assets/images/HT-1000-small.jpg"
+            slot="thumbnail"
+        />
+    </ui5-media-gallery-item>`,
+};
 
-export const Template4: StoryFn = () => html`
-    <h3>MediaGallery item with video content</h3>
-    <div class="snippet">
-        <ui5-media-gallery>
-            <ui5-media-gallery-item layout="Wide">
-                <iframe
-                    src="https://www.youtube.com/embed/GxGZG2fv6Aw"
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen=""
-                ></iframe>
-                <img
-                    src="../assets/images/sap-logo-square.svg"
-                    slot="thumbnail"
-                    alt="SAP Video"
-                />
-            </ui5-media-gallery-item>
-        </ui5-media-gallery>
-    </div>
-`;
+export const WithMenuVerticalAlign = Template.bind({});
+WithMenuVerticalAlign.args = {
+    default: `
+    <ui5-media-gallery-item layout="Wide">
+        <iframe
+            src="https://www.youtube.com/embed/GxGZG2fv6Aw"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen=""
+        ></iframe>
+        <img
+            src="../assets/images/sap-logo-square.svg"
+            slot="thumbnail"
+            alt="SAP Video"
+        />
+    </ui5-media-gallery-item>`,
+};
 
-export const Template5: StoryFn = () => html`
-    <h3>MediaGallery with disabled content</h3>
-    <div class="snippet">
-        <ui5-media-gallery>
-            <ui5-media-gallery-item disabled="">
-                <img src="../assets/images/HT-1000.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1010.jpg" />
-            </ui5-media-gallery-item>
-        </ui5-media-gallery>
-    </div>
-`;
+export const WithDisabledContent = Template.bind({});
+WithDisabledContent.args = {
+    default: `
+    <ui5-media-gallery-item disabled="">
+        <img src="../assets/images/HT-1000.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1010.jpg" />
+    </ui5-media-gallery-item>`,
+};
 
-export const Template6: StoryFn = () => html`
-    <h3>MediaGallery with initially selected item</h3>
-    <div class="snippet">
-        <ui5-media-gallery>
-            <ui5-media-gallery-item>
-                <img src="../assets/images/HT-1000.jpg" />
-            </ui5-media-gallery-item>
-            <ui5-media-gallery-item selected="">
-                <img src="../assets/images/HT-1010.jpg" />
-            </ui5-media-gallery-item>
-        </ui5-media-gallery>
-    </div>
-`;
+export const WithInitiallySelected = Template.bind({});
+WithInitiallySelected.args = {
+    default: `
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1000.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item selected="">
+        <img src="../assets/images/HT-1010.jpg" />
+    </ui5-media-gallery-item>`,
+};
 
-export const Template7: StoryFn = () => html`
-    <h3>MediaGallery with thumbnails overflow</h3>
-    <div class="snippet">
-        <div class="container">
-            <ui5-media-gallery id="mediaGallery" interactive-display-area="">
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-1000.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-1010.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-1022.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-1030.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-2002.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-2026.jpg" />
-                </ui5-media-gallery-item>
-            </ui5-media-gallery>
-            <div class="details">
-                <ui5-title level="H1">Item Details</ui5-title>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                quam lectus, tristique semper mi et, faucibus viverra metus.
-                Quisque nec venenatis massa. Ut eu dolor a justo ornare feugiat.
-                Morbi congue diam id enim porttitor, sit amet placerat nunc
-                pulvinar. Vivamus eu feugiat justo. Ut eu lectus mauris. Aliquam
-                erat volutpat. Vestibulum et enim sit amet ipsum tincidunt
-                aliquet nec in dui. Sed dui est, hendrerit non sollicitudin
-                quis, venenatis vel libero. Suspendisse sit amet lorem posuere,
-                egestas neque eget, sodales ipsum. Donec sollicitudin leo ut
-                risus tincidunt tincidunt. Ut vel nisl nisl. Cras leo odio,
-                viverra a ante nec, cursus volutpat lectus. Cras ac metus nisi.
-                Aliquam fermentum nec felis sit amet tristique. Nunc luctus a
-                lacus non semper. Curabitur euismod tellus id massa mattis, in
-                consectetur mi luctus. Mauris dignissim efficitur lobortis.
-                Etiam sit amet nunc commodo, lacinia nisi sagittis, finibus
-                nulla. Proin quis elementum eros. Ut facilisis lacinia viverra.
-            </div>
+export const WithThumbnailOverflow = Template.bind({});
+WithThumbnailOverflow.decorators = [
+    (story) => html`
+<style>
+    @media (min-width: 612px) {
+        .container {
+            display: grid;
+            gap: 1rem;
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+
+    .details {
+		background: var(--sapBaseColor);
+		padding: 1rem;
+	}
+
+	.dialog-footer {
+		display: flex;
+		justify-content: flex-end;
+		width: 100%;
+		padding: 0px 0.5rem 0px 0px;
+	}
+</style>
+<div class="container">
+    ${story()}
+    <div class="details">
+            <ui5-title level="H1">Item Details</ui5-title>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            quam lectus, tristique semper mi et, faucibus viverra metus.
+            Quisque nec venenatis massa. Ut eu dolor a justo ornare feugiat.
+            Morbi congue diam id enim porttitor, sit amet placerat nunc
+            pulvinar. Vivamus eu feugiat justo. Ut eu lectus mauris. Aliquam
+            erat volutpat. Vestibulum et enim sit amet ipsum tincidunt
+            aliquet nec in dui. Sed dui est, hendrerit non sollicitudin
+            quis, venenatis vel libero. Suspendisse sit amet lorem posuere,
+            egestas neque eget, sodales ipsum. Donec sollicitudin leo ut
+            risus tincidunt tincidunt. Ut vel nisl nisl. Cras leo odio,
+            viverra a ante nec, cursus volutpat lectus. Cras ac metus nisi.
+            Aliquam fermentum nec felis sit amet tristique. Nunc luctus a
+            lacus non semper. Curabitur euismod tellus id massa mattis, in
+            consectetur mi luctus. Mauris dignissim efficitur lobortis.
+            Etiam sit amet nunc commodo, lacinia nisi sagittis, finibus
+            nulla. Proin quis elementum eros. Ut facilisis lacinia viverra.
         </div>
-        <ui5-dialog id="mediaGalleryDialog" header-text="Item" stretch="">
-            <ui5-bar design="Header" slot="header">
-                <ui5-label>Item</ui5-label>
-            </ui5-bar>
-            <ui5-media-gallery show-all-thumbnails="">
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-1000.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-1010.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-1022.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-1030.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-2002.jpg" />
-                </ui5-media-gallery-item>
-                <ui5-media-gallery-item>
-                    <img src="../assets/images/HT-2026.jpg" />
-                </ui5-media-gallery-item>
-            </ui5-media-gallery>
-            <div slot="footer" class="dialog-footer">
-                <div style="flex: 1;"></div>
-                <ui5-button id="closeDialogButton">Close</ui5-button>
-            </div>
-        </ui5-dialog>
-        <style>
-            @media (min-width: 1024px) {
-                .container {
-                    display: grid;
-                    gap: 1rem;
-                    grid-template-columns: 1fr 1fr;
-                }
-            }
-        </style>
-        <script>
-            closeDialogButton.addEventListener("click", function () {
-                mediaGalleryDialog.close();
-            });
-            mediaGallery.addEventListener("overflow-click", function (event) {
-                mediaGalleryDialog.show();
-            });
-            mediaGallery.addEventListener(
-                "display-area-click",
-                function (event) {
-                    mediaGalleryDialog.show();
-                }
-            );
-        </script>
     </div>
-`;
-Template7.parameters = {
+</div>
+
+<ui5-dialog id="mediaGalleryDialog" header-text="Item" stretch="">
+    <ui5-bar design="Header" slot="header">
+        <ui5-label>Item</ui5-label>
+    </ui5-bar>
+    <ui5-media-gallery show-all-thumbnails="">
+        <ui5-media-gallery-item>
+            <img src="../assets/images/HT-1000.jpg" />
+        </ui5-media-gallery-item>
+        <ui5-media-gallery-item>
+            <img src="../assets/images/HT-1010.jpg" />
+        </ui5-media-gallery-item>
+        <ui5-media-gallery-item>
+            <img src="../assets/images/HT-1022.jpg" />
+        </ui5-media-gallery-item>
+        <ui5-media-gallery-item>
+            <img src="../assets/images/HT-1030.jpg" />
+        </ui5-media-gallery-item>
+        <ui5-media-gallery-item>
+            <img src="../assets/images/HT-2002.jpg" />
+        </ui5-media-gallery-item>
+        <ui5-media-gallery-item>
+            <img src="../assets/images/HT-2026.jpg" />
+        </ui5-media-gallery-item>
+    </ui5-media-gallery>
+    <div slot="footer" class="dialog-footer">
+        <div style="flex: 1;"></div>
+        <ui5-button id="closeDialogButton">Close</ui5-button>
+    </div>
+</ui5-dialog>
+<script>
+    const mediaGalleryDialog = document.getElementById("mediaGalleryDialog");
+    const mediaGallery = document.querySelector(".container > ui5-media-gallery");
+    const closeDialogButton = document.getElementById("closeDialogButton");
+    
+    closeDialogButton.addEventListener("click", function () {
+        mediaGalleryDialog.close();
+    });
+    mediaGallery.addEventListener("overflow-click", function (event) {
+        mediaGalleryDialog.show();
+    });
+    mediaGallery.addEventListener(
+        "display-area-click",
+        function (event) {
+            mediaGalleryDialog.show();
+        }
+    );
+</script>`,
+];
+
+WithThumbnailOverflow.args = {
+    interactiveDisplayArea: true,
+    default: `
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1000.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1010.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1022.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-1030.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-2002.jpg" />
+    </ui5-media-gallery-item>
+    <ui5-media-gallery-item>
+        <img src="../assets/images/HT-2026.jpg" />
+    </ui5-media-gallery-item>`,
+};
+WithThumbnailOverflow.parameters = {
     docs: {
         story: {
             // Opt-out of inline rendering
             inline: false,
+            iframeHeight: "800px",
         },
     },
 };
