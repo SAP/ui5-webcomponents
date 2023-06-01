74c877b2fd674711a331ab4716c89df3cd50c3fa
Georgieva <lidiya.georgieva@sap.com>
2023-06-01 13:21:31 +0300

    fix(ui5-dialog): add text with instructions for resizing or dragging of dialog fo… (#6989)
    fix(ui5-dialog): add text with instructions for resizing or dragging of dialog for Screen Reader users
    
    Fixes #6659

--- packages/main/src/Dialog.ts
+++ packages/main/src/Dialog.ts
@@ -8,6 +8,8 @@ import {
 	isUpShift, isDownShift, isLeftShift, isRightShift,
 } from "@ui5/webcomponents-base/dist/Keys.js";
 import ValueState from "@ui5/webcomponents-base/dist/types/ValueState.js";
+import { getI18nBundle } from "@ui5/webcomponents-base/dist/i18nBundle.js";
+import type I18nBundle from "@ui5/webcomponents-base/dist/i18nBundle.js";
 import Popup from "./Popup.js";
 import type { PopupBeforeCloseEventDetail as DialogBeforeCloseEventDetail } from "./Popup.js";
 import Icon from "./Icon.js";
@@ -16,6 +18,13 @@ import "@ui5/webcomponents-icons/dist/error.js";
 import "@ui5/webcomponents-icons/dist/alert.js";
 import "@ui5/webcomponents-icons/dist/sys-enter-2.js";
 import "@ui5/webcomponents-icons/dist/information.js";
+
+import {
+	DIALOG_HEADER_ARIA_ROLE_DESCRIPTION,
+	DIALOG_HEADER_ARIA_DESCRIBEDBY_RESIZABLE,
+	DIALOG_HEADER_ARIA_DESCRIBEDBY_DRAGGABLE,
+	DIALOG_HEADER_ARIA_DESCRIBEDBY_DRAGGABLE_RESIZABLE,
+} from "./generated/i18n/i18n-defaults.js";
 
 // Template
 import DialogTemplate from "./generated/templates/DialogTemplate.lit.js";
@@ -242,6 +251,8 @@ class Dialog extends Popup {
 	@slot()
 	footer!: Array<HTMLElement>;
 
+	static i18nBundle: I18nBundle;
+
 	constructor() {
 		super();
 
@@ -254,6 +265,10 @@ class Dialog extends Popup {
 		this._resizeMouseUpHandler = this._onResizeMouseUp.bind(this);
 
 		this._dragStartHandler = this._handleDragStart.bind(this);
+	}
+
+	static async onDefine() {
+		Dialog.i18nBundle = await getI18nBundle("@ui5/webcomponents");
 	}
 
 	static _isHeader(element: HTMLElement) {
@@ -290,6 +305,26 @@ class Dialog extends Popup {
 		}
 
 		return ariaLabelledById;
+	}
+
+	get ariaRoleDescriptionHeaderText() {
+		return (this.resizable || this.draggable) ? Dialog.i18nBundle.getText(DIALOG_HEADER_ARIA_ROLE_DESCRIPTION) : undefined;
+	}
+
+	get effectiveAriaDescribedBy() {
+		return (this.resizable || this.draggable) ? `${this._id}-descr` : undefined;
+	}
+
+	get ariaDescribedByHeaderTextResizable() {
+		return Dialog.i18nBundle.getText(DIALOG_HEADER_ARIA_DESCRIBEDBY_RESIZABLE);
+	}
+
+	get ariaDescribedByHeaderTextDraggable() {
+		return Dialog.i18nBundle.getText(DIALOG_HEADER_ARIA_DESCRIBEDBY_DRAGGABLE);
+	}
+
+	get ariaDescribedByHeaderTextDraggableAndResizable() {
+		return Dialog.i18nBundle.getText(DIALOG_HEADER_ARIA_DESCRIBEDBY_DRAGGABLE_RESIZABLE);
 	}
 
 	get _displayProp() {
