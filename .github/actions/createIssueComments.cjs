217d57621351b1ef63e4e7455157779c7774bce0
ilhan007 <ilhan.myumyun@sap.com>
2023-06-02 15:50:43 +0300

    chore: update issue comment workflow [ci skip]

--- .github/actions/createIssueComments.cjs
+++ .github/actions/createIssueComments.cjs
@@ -1,10 +1,27 @@
+const commitShaRegExp = /commit\/(?<sha>\w{40})/gm;
 
-module.exports = async function run({ github, context, version }) {
-  const relaseInfo = await github.repos.getReleaseByTag({
-	owner: context.repo.owner,
-	repo: context.repo.repo,
-	tag: `v${version}`
-  });
- 
-  console.log(relaseInfo)
-}
+module.exports = async function run({
+	github,
+	context,
+	version
+}) {
+	const {
+		data: release
+	} = await github.repos.getReleaseByTag({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		tag: `v${version}`
+	});
+	release.url = release.html_url;
+
+	const commits = [];
+	let result;
+	do {
+		result = commitShaRegExp.exec(release.body);
+		if (result && result.groups && result.groups.sha) {
+			commits.push({
+				hash: result.groups.sha
+			});
+		}
+	} while (result);
+};
