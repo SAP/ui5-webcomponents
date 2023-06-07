b4da06e623ea98a4167e5c411a8f2b1a6550941b
Stoyan <88034608+hinzzx@users.noreply.github.com>
2023-06-07 15:16:06 +0300

    fix(ui5-calendar): prevent focus loss during arrow navigation with mouse/touch (#6957)
    Currently when clicking/tapping the arrows that navigate to the next/previous month, the focus is lost. It should be on the first day of the newly displayed month if the "Next" button is clicked, and on the last day of the displayed month if the "Prev" button is clicked.
    With this change we apply the focus correctly, when navigating through the Calendar's header using mouse or touch through the arrow buttons (incl. DatePicker, DateRangePicker, DateTimePicker controls that utilize it).

--- packages/main/src/CalendarHeader.ts
+++ packages/main/src/CalendarHeader.ts
@@ -126,11 +126,23 @@ class CalendarHeader extends UI5Element {
 	}
 
 	onPrevButtonClick(e: MouseEvent) {
+		if (this.isPrevButtonDisabled) {
+			e.preventDefault();
+			return;
+		}
+
 		this.fireEvent("previous-press", e);
+		e.preventDefault();
 	}
 
 	onNextButtonClick(e: MouseEvent) {
+		if (this.isNextButtonDisabled) {
+			e.preventDefault();
+			return;
+		}
+
 		this.fireEvent("next-press", e);
+		e.preventDefault();
 	}
 
 	onMonthButtonClick(e: MouseEvent) {
