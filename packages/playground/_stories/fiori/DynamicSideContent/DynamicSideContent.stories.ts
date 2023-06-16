8c340a4f3f5c16b5013fba421363e172861155e0
unknown <abvsuxhard@gmail.com>
2023-06-16 14:46:19 +0300

    docs(ui5-dynamic-side-content): improve sample showcase

--- packages/playground/_stories/fiori/DynamicSideContent/DynamicSideContent.stories.ts
+++ packages/playground/_stories/fiori/DynamicSideContent/DynamicSideContent.stories.ts
@@ -41,20 +41,9 @@ const Template: UI5StoryArgs<DynamicSideContent, StoryArgsSlots> = (args) => htm
 
 export const Basic = Template.bind({});
 Basic.args = {
-	default: `<div>
-	<h1>Main Content</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex mi, elementum et ante commodo, semper sollicitudin magna. Sed dapibus ut tortor quis varius. Sed luctus sem at nunc porta vulputate. Suspendisse lobortis arcu est, quis ultrices ipsum fermentum a. Vestibulum a ipsum placerat ligula gravida fringilla at id ex. Etiam pellentesque lorem sed sagittis aliquam. Quisque semper orci risus, vel efficitur dui euismod aliquet. Morbi sapien sapien, rhoncus et rutrum nec, rhoncus id nisl. Cras non tincidunt enim, id eleifend neque.</p>
-</div>`,
-	sideContent: `<div slot="sideContent">
-	<h1>Side Content</h1>
-	<p>Morbi lorem libero, imperdiet id condimentum ac, tempor ut velit. Integer a laoreet sem. Nunc at sagittis nisi. Sed placerat diam eu porttitor dignissim. Maecenas nec fringilla tortor. Pellentesque ut elit est. Curabitur quis elit at mauris ullamcorper fringilla. Nullam diam mi, porttitor dictum orci nec, molestie luctus metus. Nunc ut ex blandit, elementum erat eget, pulvinar sapien. Donec nec lorem eu nunc eleifend tempor at non tortor. In quam velit, ornare at rutrum ac, porta ac augue. Suspendisse venenatis semper lacus at venenatis. Praesent vestibulum ligula nulla, at tempus lorem consequat suscipit. Aenean consequat dapibus dui, at bibendum mauris porta a.</p>
-</div>`,
-};
-
-export const Position = Template.bind({});
-Position.storyName = "Side Content Position";
-Position.args = {
-	sideContentPosition: SideContentPosition.Start,
+	sideContentFallDown: SideContentFallDown.OnMinimumWidth,
+	sideContentPosition: SideContentPosition.End,
+	sideContentVisibility: SideContentVisibility.ShowAboveS,
 	default: `<div>
 	<h1>Main Content</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex mi, elementum et ante commodo, semper sollicitudin magna. Sed dapibus ut tortor quis varius. Sed luctus sem at nunc porta vulputate. Suspendisse lobortis arcu est, quis ultrices ipsum fermentum a. Vestibulum a ipsum placerat ligula gravida fringilla at id ex. Etiam pellentesque lorem sed sagittis aliquam. Quisque semper orci risus, vel efficitur dui euismod aliquet. Morbi sapien sapien, rhoncus et rutrum nec, rhoncus id nisl. Cras non tincidunt enim, id eleifend neque.</p>
