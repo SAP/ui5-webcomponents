3ae307e08335b091b8e0038a27b998bea8d1c99b
hinzzx <abvsuxhard@gmail.com>
2023-08-04 12:20:36 +0300

    feat(ui5-menu): test push

--- packages/main/test/specs/Menu.spec.js
+++ packages/main/test/specs/Menu.spec.js
@@ -1,136 +1,136 @@
 import { assert } from "chai";
 
 describe("Menu interaction", () => {
-	// it("Menu opens after button click", async () => {
-	// 	await browser.url(`test/pages/Menu.html`);
-	// 	const openButton = await browser.$("#btnOpen");
+	it("Menu opens after button click", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openButton = await browser.$("#btnOpen");
 
-	// 	openButton.click();
-	// 	const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
-	// 	const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+		openButton.click();
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
 
-	// 	assert.strictEqual(await popover.getAttribute("id"), `${staticAreaItemClassName}-menu-rp`, "There is popover for the menu created in the static area");
-	// });
+		assert.strictEqual(await popover.getAttribute("id"), `${staticAreaItemClassName}-menu-rp`, "There is popover for the menu created in the static area");
+	});
 
-	// it("Menu opens after setting of opener and open", async () => {
-	// 	await browser.url(`test/pages/Menu.html`);
-	// 	const openerButton = await browser.$("#btnAddOpener");
-	// 	const openButton = await browser.$("#btnToggleOpen");
+	it("Menu opens after setting of opener and open", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openerButton = await browser.$("#btnAddOpener");
+		const openButton = await browser.$("#btnToggleOpen");
 
-	// 	openerButton.click();
-	// 	openButton.click();
-	// 	const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
-	// 	const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+		openerButton.click();
+		openButton.click();
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
 
-	// 	assert.strictEqual(await popover.getAttribute("id"), `${staticAreaItemClassName}-menu-rp`, "There is popover for the menu created in the static area");
-	// });
+		assert.strictEqual(await popover.getAttribute("id"), `${staticAreaItemClassName}-menu-rp`, "There is popover for the menu created in the static area");
+	});
 
-	// it("Top level menu items appearance", async () => {
-	// 	await browser.url(`test/pages/Menu.html`);
-	// 	const openButton = await browser.$("#btnOpen");
-	// 	const menuItems = await browser.$$("ui5-menu>ui5-menu-item");
+	it("Top level menu items appearance", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openButton = await browser.$("#btnOpen");
+		const menuItems = await browser.$$("ui5-menu>ui5-menu-item");
 
-	// 	openButton.click();
+		openButton.click();
 
-	// 	const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
-	// 	const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
-	// 	const listItems = await popover.$("ui5-list").$$("ui5-li");
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+		const listItems = await popover.$("ui5-list").$$("ui5-li");
 
-	// 	assert.strictEqual(await menuItems.length, 7, "There are proper count of menu items in the top level menu");
-	// 	assert.strictEqual(await listItems.length, 7, "There are proper count of list items in the top level menu popover list");
-	// 	assert.strictEqual(await listItems[0].getAttribute("additional-text"), await menuItems[0].getAttribute("additional-text"), "The first list item has proper additional text set");
-	// 	assert.strictEqual(await listItems[1].getAttribute("disabled"), "true", "The second list item is disabled");
-	// 	assert.strictEqual(await listItems[2].getAttribute("starts-section"), "", "The third list item has separator addded");
-	// 	assert.ok(await listItems[3].$(".ui5-menu-item-icon-end"), "The third list item has sub-items and must have arrow right icon after the text");
-	// 	assert.ok(await listItems[4].$(".ui5-menu-item-dummy-icon"), "The fourth list item has no icon and has dummy div instead of icon");
-	// });
+		assert.strictEqual(await menuItems.length, 7, "There are proper count of menu items in the top level menu");
+		assert.strictEqual(await listItems.length, 7, "There are proper count of list items in the top level menu popover list");
+		assert.strictEqual(await listItems[0].getAttribute("additional-text"), await menuItems[0].getAttribute("additional-text"), "The first list item has proper additional text set");
+		assert.strictEqual(await listItems[1].getAttribute("disabled"), "true", "The second list item is disabled");
+		assert.strictEqual(await listItems[2].getAttribute("starts-section"), "", "The third list item has separator addded");
+		assert.ok(await listItems[3].$(".ui5-menu-item-icon-end"), "The third list item has sub-items and must have arrow right icon after the text");
+		assert.ok(await listItems[4].$(".ui5-menu-item-dummy-icon"), "The fourth list item has no icon and has dummy div instead of icon");
+	});
 
-	// it("Sub-menu creation, opening, closing and destroying", async () => {
-	// 	await browser.url(`test/pages/Menu.html`);
-	// 	const openButton = await browser.$("#btnOpen");
+	it("Sub-menu creation, opening, closing and destroying", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openButton = await browser.$("#btnOpen");
 
-	// 	openButton.click();
+		openButton.click();
 
-	// 	const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
-	// 	const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
-	// 	const listItems = await popover.$("ui5-list").$$("ui5-li");
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+		const listItems = await popover.$("ui5-list").$$("ui5-li");
 
-	// 	listItems[3].click(); // open sub-menu
+		listItems[3].click(); // open sub-menu
 
-	// 	assert.ok(await browser.$(`.${staticAreaItemClassName}`).shadow$(".ui5-menu-submenus").$("ui5-menu"),
-	// 							"The second level sub-menu is being created"); // new ui5-menu element is created for the sub-menu
+		assert.ok(await browser.$(`.${staticAreaItemClassName}`).shadow$(".ui5-menu-submenus").$("ui5-menu"),
+								"The second level sub-menu is being created"); // new ui5-menu element is created for the sub-menu
 
-	// 	await browser.keys("ArrowLeft"); // back to main menu
-	// 	await browser.keys("ArrowDown"); // go to the next menu item (close sub-menu)
+		await browser.keys("ArrowLeft"); // back to main menu
+		await browser.keys("ArrowDown"); // go to the next menu item (close sub-menu)
 
-	// 	assert.strictEqual(await browser.$(`.${staticAreaItemClassName}`).shadow$(".ui5-menu-submenus").$$("ui5-menu").length, 0,
-	// 							"The second level sub-menu is being destroyed"); // sub-menu ui5-menu element is destroyed
-	// });
+		assert.strictEqual(await browser.$(`.${staticAreaItemClassName}`).shadow$(".ui5-menu-submenus").$$("ui5-menu").length, 0,
+								"The second level sub-menu is being destroyed"); // sub-menu ui5-menu element is destroyed
+	});
 
-	// it("Event firing after 'click' on menu item", async () => {
-	// 	await browser.url(`test/pages/Menu.html`);
-	// 	const openButton = await browser.$("#btnOpen");
+	it("Event firing after 'click' on menu item", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openButton = await browser.$("#btnOpen");
 
-	// 	openButton.click();
+		openButton.click();
 
-	// 	const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
-	// 	const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
-	// 	const listItems = await popover.$("ui5-list").$$("ui5-li");
-	// 	const selectionInput = await browser.$("#selectionInput");
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+		const listItems = await popover.$("ui5-list").$$("ui5-li");
+		const selectionInput = await browser.$("#selectionInput");
 
-	// 	await listItems[0].click({x: 1, y: 1});
+		await listItems[0].click({x: 1, y: 1});
 
-	// 	assert.strictEqual(await selectionInput.getAttribute("value"), "New File", "Click on first item fires an event");
-	// });
+		assert.strictEqual(await selectionInput.getAttribute("value"), "New File", "Click on first item fires an event");
+	});
 
-	// it("Event firing after [Space] on menu item", async () => {
-	// 	await browser.url(`test/pages/Menu.html`);
-	// 	const openButton = await browser.$("#btnOpen");
+	it("Event firing after [Space] on menu item", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openButton = await browser.$("#btnOpen");
 
-	// 	openButton.click();
+		openButton.click();
 
-	// 	const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
-	// 	const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
-	// 	const listItems = await popover.$("ui5-list").$$("ui5-li");
-	// 	const selectionInput = await browser.$("#selectionInput");
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+		const listItems = await popover.$("ui5-list").$$("ui5-li");
+		const selectionInput = await browser.$("#selectionInput");
 
-	// 	await browser.keys("Space");
+		await browser.keys("Space");
 
-	// 	assert.strictEqual(await selectionInput.getAttribute("value"), "New File", "Pressing [Space] on first item fires an event");
-	// });
+		assert.strictEqual(await selectionInput.getAttribute("value"), "New File", "Pressing [Space] on first item fires an event");
+	});
 
-	// it("Event firing after [Enter] on menu item", async () => {
-	// 	await browser.url(`test/pages/Menu.html`);
-	// 	const openButton = await browser.$("#btnOpen");
+	it("Event firing after [Enter] on menu item", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openButton = await browser.$("#btnOpen");
 
-	// 	openButton.click();
+		openButton.click();
 
-	// 	const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
-	// 	const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
-	// 	const listItems = await popover.$("ui5-list").$$("ui5-li");
-	// 	const selectionInput = await browser.$("#selectionInput");
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+		const listItems = await popover.$("ui5-list").$$("ui5-li");
+		const selectionInput = await browser.$("#selectionInput");
 
-	// 	await browser.keys("Enter");
+		await browser.keys("Enter");
 
-	// 	assert.strictEqual(await selectionInput.getAttribute("value"), "New File", "Pressing [Enter] on first item fires an event");
-	// });
+		assert.strictEqual(await selectionInput.getAttribute("value"), "New File", "Pressing [Enter] on first item fires an event");
+	});
 
-	// it("Events firing on open/close of the menu", async () => {
-	// 	await browser.url(`test/pages/Menu.html`);
-	// 	const openButton = await browser.$("#btnOpen");
-	// 	const eventLogger = await browser.$("#eventLogger");
+	it("Events firing on open/close of the menu", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openButton = await browser.$("#btnOpen");
+		const eventLogger = await browser.$("#eventLogger");
 
-	// 	openButton.click();
-	// 	await browser.pause(100);
-	// 	await browser.keys("Escape");
+		openButton.click();
+		await browser.pause(100);
+		await browser.keys("Escape");
 
-	// 	const eventLoggerValue = await eventLogger.getValue();
+		const eventLoggerValue = await eventLogger.getValue();
 
-	// 	assert.notEqual(eventLoggerValue.indexOf("before-open"), -1, "'before-open' event is fired");
-	// 	assert.notEqual(eventLoggerValue.indexOf("after-open"), -1, "'after-open' event is fired");
-	// 	assert.notEqual(eventLoggerValue.indexOf("before-close"), -1, "'before-close' event is fired");
-	// 	assert.notEqual(eventLoggerValue.indexOf("after-close"), -1, "'after-close' event is fired");
-	// });
+		assert.notEqual(eventLoggerValue.indexOf("before-open"), -1, "'before-open' event is fired");
+		assert.notEqual(eventLoggerValue.indexOf("after-open"), -1, "'after-open' event is fired");
+		assert.notEqual(eventLoggerValue.indexOf("before-close"), -1, "'before-close' event is fired");
+		assert.notEqual(eventLoggerValue.indexOf("after-close"), -1, "'after-close' event is fired");
+	});
 
 	it("Menu and Menu items busy indication", async () => {
 			await browser.url(`test/pages/Menu.html`);
@@ -161,24 +161,24 @@ describe("Menu interaction", () => {
 		});
 	});
 
-// describe("Menu Accessibility", () => {
-// 	it("Menu and Menu items accessibility attributes", async () => {
-// 		await browser.url(`test/pages/Menu.html`);
-// 		const openButton = await browser.$("#btnOpen");
-// 		const menuItems = await browser.$$("ui5-menu>ui5-menu-item");
+describe("Menu Accessibility", () => {
+	it("Menu and Menu items accessibility attributes", async () => {
+		await browser.url(`test/pages/Menu.html`);
+		const openButton = await browser.$("#btnOpen");
+		const menuItems = await browser.$$("ui5-menu>ui5-menu-item");
 
-// 		openButton.click();
+		openButton.click();
 
-// 		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
-// 		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
-// 		const list = await popover.$("ui5-list");
-// 		const listItems = await popover.$("ui5-list").$$("ui5-li");
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#menu");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-responsive-popover");
+		const list = await popover.$("ui5-list");
+		const listItems = await popover.$("ui5-list").$$("ui5-li");
 
-// 		assert.strictEqual(await list.getAttribute("accessible-role"), "menu", "There is proper 'menu' role for the menu list");
-// 		assert.strictEqual(await listItems[0].getAttribute("accessible-role"), "menuitem", "There is proper 'menuitem' role for the menu list items");
-// 		assert.strictEqual(
-// 			await listItems[0].getAttribute("accessible-name"),
-// 			"New File Opens a file explorer",
-// 			"There is additional description added");
-// 	});
-// });
+		assert.strictEqual(await list.getAttribute("accessible-role"), "menu", "There is proper 'menu' role for the menu list");
+		assert.strictEqual(await listItems[0].getAttribute("accessible-role"), "menuitem", "There is proper 'menuitem' role for the menu list items");
+		assert.strictEqual(
+			await listItems[0].getAttribute("accessible-name"),
+			"New File Opens a file explorer",
+			"There is additional description added");
+	});
+});
