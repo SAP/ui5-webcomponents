32f09e2f0d98141a29ade8725563a8deae618ece
Boyan Rakilovski <boyan.rakilovski@sap.com>
2023-08-08 10:11:03 +0300

    feat(ui5-menu): make the item click event cancellable

--- packages/main/test/specs/Menu.spec.js
+++ packages/main/test/specs/Menu.spec.js
@@ -143,7 +143,7 @@ describe("Menu interaction", () => {
 			const visualCloseItem = await menuPopover.$("ui5-li[accessible-name='Close']");
 
 			visualOpenItem.click();
-			await browser.pause(400);
+			await browser.pause(600);
 			const openSubmenuPopover = await browser.$("ui5-static-area-item:last-of-type").shadow$("ui5-responsive-popover");
 			const openMenuList = await openSubmenuPopover.$("ui5-list");
 
