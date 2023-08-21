167e29398d74bc66d9c1fdf3e660169717035efa
Vladislav Tasev <vladislav.tasev@sap.com>
2023-08-21 16:52:16 +0300

    chore: a new test

--- packages/main/test/specs/SelectMenuConnector.spec.js
+++ packages/main/test/specs/SelectMenuConnector.spec.js
@@ -1,11 +1,11 @@
 import { assert } from "chai";
 
-describe("Select Menu general interaction", () => {
-	before(async () => {
+describe("Select Menu Connector integration", () => {
+	beforeEach(async () => {
 		await browser.url(`test/pages/SelectMenuLifecycle.html`);
 	});
 
-	it("Menu property manipulation", async () => {
+	it("Changing the menu property connects and disconnects", async () => {
 		const select = await browser.$("#sel1");
 		const selectLabel = select.shadow$(".ui5-select-label-root");
 
@@ -23,5 +23,21 @@ describe("Select Menu general interaction", () => {
 
 		selectText = await selectLabel.getText();
 		assert.include(selectText, "Phone", "When connected to another menu, the select should update its label");
+	});
+
+	it("Waiting for a menu to be rendered works", async () => {
+		const select = await browser.$("#sel1");
+		const menu = await browser.$("#menu1");
+		const selectLabel = select.shadow$(".ui5-select-label-root");
+
+		let selectText = await selectLabel.getText();
+		assert.strictEqual(selectText, "", "Select with no options and no menu is empty.");
+
+		// A menu on the page now matches the "menu" property of the select
+		await menu.setAttribute("id", "NO_MENU_WITH_THIS_ID");
+		await browser.pause(1000); // wait for the polling to take effect
+
+		selectText = await selectLabel.getText();
+		assert.include(selectText, "Skirt [3]", "When connected to a menu, the select should update its label");
 	});
 });
