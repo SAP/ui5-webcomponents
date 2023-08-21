e61924d01b220a29474c2055c6bf8c34009e9e5f
Vladislav Tasev <vladislav.tasev@sap.com>
2023-08-21 15:28:10 +0300

    chore: test fixed

--- packages/main/test/specs/SelectMenuConnector.spec.js
+++ packages/main/test/specs/SelectMenuConnector.spec.js
@@ -5,7 +5,7 @@ describe("Select Menu general interaction", () => {
 		await browser.url(`test/pages/SelectMenuLifecycle.html`);
 	});
 
-	it("first option is selected by default", async () => {
+	it("Menu property manipulation", async () => {
 		const select = await browser.$("#sel1");
 		const selectLabel = select.shadow$(".ui5-select-label-root");
 
@@ -13,13 +13,13 @@ describe("Select Menu general interaction", () => {
 		assert.strictEqual(selectText, "", "Select with no options and no menu is empty.");
 
 		// Connect the select to a menu
-		select.setAttribute("id", "menu1");
+		await select.setAttribute("menu", "menu1");
 
 		selectText = await selectLabel.getText();
 		assert.include(selectText, "Skirt [3]", "When connected to a menu, the select should update its label");
 
 		// Connect the select to another menu
-		select.setAttribute("id", "menu2");
+		await select.setAttribute("menu", "menu2");
 
 		selectText = await selectLabel.getText();
 		assert.include(selectText, "Phone", "When connected to another menu, the select should update its label");
