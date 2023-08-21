ef4527b86dc2ede6f33dc14416d79971251d5d75
Vladislav Tasev <vladislav.tasev@sap.com>
2023-08-21 11:38:06 +0300

    chore: do DOM polling

--- packages/base/src/connectToComponent.ts
+++ packages/base/src/connectToComponent.ts
@@ -1,6 +1,10 @@
 import type UI5Element from "./UI5Element.js";
+import { renderDeferred } from "./Render.js";
+import { Interval } from "./types.js";
 
+const MISSING_ELEMENT_POLL_TIMEOUT = 500; // how often to poll for not-yet-in-DOM friend elements
 const connections = new Map<string, HTMLElement>();
+const intervals = new Map<string, Interval>();
 
 type ConnectOptions = {
 	host: UI5Element;
@@ -14,13 +18,13 @@ const connectToComponent = (options: ConnectOptions): HTMLElement | undefined =>
 	const propName = options.propName;
 	const friend = host[propName as keyof typeof host] as HTMLElement | string | undefined;
 
-	let connectedTo: HTMLElement | null;
+	let connectedTo: HTMLElement | undefined;
 	if (friend === undefined) {
-		connectedTo = null;
+		connectedTo = undefined;
 	} else if (friend instanceof HTMLElement) {
 		connectedTo = friend;
 	} else {
-		connectedTo = (host.getRootNode() as Document).getElementById(friend);
+		connectedTo = (host.getRootNode() as Document).getElementById(friend) || undefined;
 	}
 
 	const key = `${host._id}-${propName}`;
@@ -32,14 +36,33 @@ const connectToComponent = (options: ConnectOptions): HTMLElement | undefined =>
 			options.onDisconnect && options.onDisconnect(prevConnectedTo);
 			connections.delete(key);
 		}
+
+		// if friend element not in DOM yet, start polling
+		if (typeof friend === "string" && friend && !intervals.has(key)) {
+			const interval = setInterval(() => {
+				const found = (host.getRootNode() as Document).getElementById(friend);
+				if (found) {
+					clearInterval(intervals.get(key));
+					intervals.delete(key);
+					renderDeferred(host);
+				}
+			}, MISSING_ELEMENT_POLL_TIMEOUT);
+			intervals.set(key, interval);
+		}
+
 		return;
+	}
+
+	// If connected, but still polling, stop polling
+	if (intervals.has(key)) {
+		clearInterval(intervals.get(key));
+		intervals.delete(key);
 	}
 
 	// Connected - either for the first time, or to something else
 	if (prevConnectedTo !== connectedTo) {
 		if (prevConnectedTo) {
 			options.onDisconnect && options.onDisconnect(prevConnectedTo);
-			connections.delete(key);
 		}
 		options.onConnect && options.onConnect(connectedTo);
 		connections.set(key, connectedTo);
