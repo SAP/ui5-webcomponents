dab9f988cb560bcbc94a5eed63a0f2929152fce5
ilhan007 <ilhan.myumyun@sap.com>
2023-09-01 08:43:07 +0300

    chore: update Toolbar test

--- packages/main/test/specs/Toolbar.spec.js
+++ packages/main/test/specs/Toolbar.spec.js
@@ -5,19 +5,6 @@ describe("Toolbar general interaction", () => {
 		await browser.url(`test/pages/Toolbar.html`);
 	});
 
-	it("Should open popover when pressing overflow button", async () => {
-		await browser.setWindowSize(500, 1080);
-		const otb = await browser.$("#otb_a");
-		const overflowButton = await otb.shadow$(".ui5-tb-overflow-btn");
-
-		await overflowButton.click();
-
-		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#otb_a");
-		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-popover");
-		assert.strictEqual(await popover.getAttribute("open"), 'true', "overflow popover created and opened on click on overflow button");
-		await browser.setWindowSize(1920, 1080);
-	});
-
 	it("Should move button with alwaysOverflow priority to overflow popover", async () => {
 
 		const otb = await browser.$("#otb_d");
@@ -32,6 +19,19 @@ describe("Toolbar general interaction", () => {
 
 		const employee_button = await popover.$(`[data-ui5-stable="tb-button-employee-d"]`);
 		assert.ok(await employee_button.hasClass("ui5-tb-popover-item"), "'Employee' button is moved in overflow popover since it has 'Always' priority");
+	});
+
+	it("Should open popover when pressing overflow button", async () => {
+		await browser.setWindowSize(500, 1080);
+		const otb = await browser.$("#otb_a");
+		const overflowButton = await otb.shadow$(".ui5-tb-overflow-btn");
+
+		await overflowButton.click();
+
+		const staticAreaItemClassName = await browser.getStaticAreaItemClassName("#otb_a");
+		const popover = await browser.$(`.${staticAreaItemClassName}`).shadow$("ui5-popover");
+		assert.strictEqual(await popover.getAttribute("open"), 'true', "overflow popover created and opened on click on overflow button");
+		await browser.setWindowSize(1920, 1080);
 	});
 
 	it("Should not move button with neverOverflow priority to overflow popover", async () => {
