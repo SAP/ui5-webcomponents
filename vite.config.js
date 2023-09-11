43c3c58e97786ab3d246a7fccb396cf7172e53a9
Vladislav Tasev <vladislav.tasev@sap.com>
2023-09-11 11:07:12 +0300

    chore: type

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ const os = require("os");
 const { defineConfig } = require('vite');
 const virtualIndex = require("@ui5/webcomponents-tools/lib/dev-server/virtual-index-html-plugin.js");
 
-// do not refresh from .json/.html on MacOS due to false positivesß
+// do not refresh from .json/.html on MacOS due to false positives
 const ignored = os.platform() === "darwin" ? ["**/*.json", "**/*.html"] : [];
 
 module.exports = defineConfig(async () => {
