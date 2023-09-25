847687e029c15b5104cf4ffc9dcd8017686aa49c
Teodor Taushanov <teodor.taushanov@sap.com>
2023-09-25 17:22:39 +0300

    feat(ui5-badge): value state desing types

--- packages/main/test/pages/Badge.js
+++ packages/main/test/pages/Badge.js
@@ -39,33 +39,49 @@ function initializeBadges() {
 		"Grey"
 	];
 
+	const clickEvent = (e) => {
+		console.error("clicked");
+	};
+
+	document.querySelector("#checkboxId").addEventListener("change", e => {
+		document.querySelectorAll("ui5-badge").forEach((item) => {
+			item.interactive = e.currentTarget.checked;
+
+			if (item.interactive) {
+				item.addEventListener("click", clickEvent);
+			} else {
+				item.removeEventListener("click", clickEvent);
+			}
+		})
+	});
+
 	const colorSchemeContent = document.querySelector("#colorSchemeContent");
 	colorSchemes.forEach((value) => {
-		colorSchemeContent.insertAdjacentHTML("beforeend", `<ui5-badge interactive color-scheme="${value}">Color Scheme ${value}</ui5-badge><br>`);
-		colorSchemeContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ColorScheme" color-scheme="${value}">Color Scheme ${value} - design type set</ui5-badge><br>`);
+		colorSchemeContent.insertAdjacentHTML("beforeend", `<ui5-badge color-scheme="${value}">Color Scheme ${value}</ui5-badge>`);
+		colorSchemeContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ColorScheme" color-scheme="${value}">Color Scheme ${value} - design type set</ui5-badge>`);
 
-		colorSchemeContent.insertAdjacentHTML("beforeend", `<ui5-badge color-scheme="${value}"><ui5-icon name="lab" slot="icon"></ui5-icon>Color Scheme ${value}</ui5-badge><br>`);
-		colorSchemeContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ColorScheme" color-scheme="${value}"><ui5-icon name="error" slot="icon"></ui5-icon>Color Scheme ${value} - design type set</ui5-badge><br>`);
+		colorSchemeContent.insertAdjacentHTML("beforeend", `<ui5-badge color-scheme="${value}"><ui5-icon name="lab" slot="icon"></ui5-icon>Color Scheme ${value}</ui5-badge>`);
+		colorSchemeContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ColorScheme" color-scheme="${value}"><ui5-icon name="error" slot="icon"></ui5-icon>Color Scheme ${value} - design type set</ui5-badge>`);
 
-		colorSchemeContent.insertAdjacentHTML("beforeend", "<br>");
+		colorSchemeContent.insertAdjacentHTML("beforeend", "<br><br>");
 	});
 
 	const valueStateContent = document.querySelector("#valueStateContent");
 	valueStates.forEach((value) => {
-		valueStateContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueState" value-state="${value}">${value}</ui5-badge><br>`);
-		valueStateContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueState" value-state="${value}" show-state-icon>${value} default state icon</ui5-badge><br>`);
-		valueStateContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueState" value-state="${value}" show-state-icon><ui5-icon name="lab" slot="icon"></ui5-icon>${value} custom icon</ui5-badge><br>`);
+		valueStateContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueState" value-state="${value}">${value}</ui5-badge>`);
+		valueStateContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueState" value-state="${value}" show-state-icon>${value} default state icon</ui5-badge>`);
+		valueStateContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueState" value-state="${value}" show-state-icon><ui5-icon name="lab" slot="icon"></ui5-icon>${value} custom icon</ui5-badge>`);
 
-		valueStateContent.insertAdjacentHTML("beforeend", "<br>");
+		valueStateContent.insertAdjacentHTML("beforeend", "<br><br>");
 	});
 
 	const valueStateInvertedContent = document.querySelector("#valueStateInvertedContent");
 	valueStates.forEach((value) => {
-		valueStateInvertedContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueStateInverted" value-state="${value}">${value}</ui5-badge><br>`);
-		valueStateInvertedContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueStateInverted" value-state="${value}" show-state-icon>${value} default state icon</ui5-badge><br>`);
-		valueStateInvertedContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueStateInverted" value-state="${value}" show-state-icon><ui5-icon name="lab" slot="icon"></ui5-icon>${value} custom icon</ui5-badge><br>`);
+		valueStateInvertedContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueStateInverted" value-state="${value}">${value}</ui5-badge>`);
+		valueStateInvertedContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueStateInverted" value-state="${value}" show-state-icon>${value} default state icon</ui5-badge>`);
+		valueStateInvertedContent.insertAdjacentHTML("beforeend", `<ui5-badge design-type="ValueStateInverted" value-state="${value}" show-state-icon><ui5-icon name="lab" slot="icon"></ui5-icon>${value} custom icon</ui5-badge>`);
 
-		valueStateInvertedContent.insertAdjacentHTML("beforeend", "<br>");
+		valueStateInvertedContent.insertAdjacentHTML("beforeend", "<br><br>");
 	});
 
 	const indicationColorSet1Content = document.querySelector("#indicationColorSet1Content");
