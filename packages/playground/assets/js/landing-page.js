bcc02e99f8cfdb871b324515e09021f9ca90ac22
Lukas Harbarth <lukas.harbarth@sap.com>
2023-09-29 10:22:13 +0200

    docs(landing-page): dynamically fetch latest ui5wc version

--- packages/playground/assets/js/landing-page.js
+++ packages/playground/assets/js/landing-page.js
@@ -42,3 +42,14 @@ function rotateRight() {
         slides[selectedIndex + 1].classList.add("active");
     }
 }
+
+
+fetch('https://unpkg.com/@ui5/webcomponents@latest/package.json').then(async (response) => {
+    const ui5wcPackageJSON = await response.json();
+    const versionAnchor = document.getElementById('dynamicVersion');
+    if (ui5wcPackageJSON?.version) {
+        const { version } = ui5wcPackageJSON;
+        versionAnchor.textContent = version;
+        versionAnchor.setAttribute('href', `https://github.com/SAP/ui5-webcomponents/releases/tag/v${version}`);
+    }
+});
