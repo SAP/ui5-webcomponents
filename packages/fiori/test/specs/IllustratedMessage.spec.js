45244aacdc4fa846175d8a097ae242256e32a5b4
Diana Pazheva <diana.pazheva@sap.com>
2023-10-10 11:08:47 +0300

    feat(ui5-illustrated-message): enable vertical responsiveness (#7672)
    Fixes: #6492

--- packages/fiori/test/specs/IllustratedMessage.spec.js
+++ packages/fiori/test/specs/IllustratedMessage.spec.js
@@ -81,3 +81,96 @@ describe("Accessibility", () => {
 	});
 
 });
+
+describe("Vertical responsiveness", () => {
+	before(async () => {
+		await browser.url(`test/pages/IllustratedMessage.html`);
+	});
+
+	it("content with auto size shrinks to fit the parent container", async () => {
+
+		const newContainerHeight = 300,
+			expectedMedia = "dialog",
+			illustratedMsg = await browser.$("#illustratedMsg5");
+
+		// Act: apply new height
+		await browser.executeAsync(async (newContainerHeight, done) => {
+			document.getElementById("container").style.height = newContainerHeight;
+			done();
+		}, `${newContainerHeight}px`);
+
+		// Check
+		const contents = await illustratedMsg.shadow$(".ui5-illustrated-message-root"),
+			scrollHeight = await contents.getProperty("scrollHeight"),
+			offsetHeight = await contents.getProperty("offsetHeight"),
+			illustratedMsgMedia = await illustratedMsg.getProperty("media");
+		assert.ok(scrollHeight < newContainerHeight, "root dom fits container");
+		assert.strictEqual(scrollHeight, offsetHeight, "no overflow of content");
+		assert.strictEqual(illustratedMsgMedia, expectedMedia, "correct media");
+	});
+
+	it("content with auto size expands to fit the parent container", async () => {
+
+		const newContainerHeight = 500,
+			expectedMedia = "scene",
+			illustratedMsg = await browser.$("#illustratedMsg5");
+
+		// Act: apply new height
+		await browser.executeAsync(async (newContainerHeight, done) => {
+			document.getElementById("container").style.height = newContainerHeight;
+			done();
+		}, `${newContainerHeight}px`);
+
+		// Check
+		const contents = await illustratedMsg.shadow$(".ui5-illustrated-message-root"),
+			scrollHeight = await contents.getProperty("scrollHeight"),
+			offsetHeight = await contents.getProperty("offsetHeight"),
+			illustratedMsgMedia = await illustratedMsg.getProperty("media");
+		assert.ok(scrollHeight < newContainerHeight, "root dom fits container");
+		assert.strictEqual(scrollHeight, offsetHeight, "no overflow of content");
+		assert.strictEqual(illustratedMsgMedia, expectedMedia, "correct media");
+	});
+
+	it("content with fixed size fits the parent container", async () => {
+
+		const newContainerHeight = 200,
+			expectedMedia = "dialog",
+			illustratedMsg = await browser.$("#illustratedMsg5");
+
+		// set fixed size
+		illustratedMsg.setProperty("size", "Dialog");
+
+		// Act: apply new height
+		await browser.executeAsync(async (newContainerHeight, done) => {
+			document.getElementById("container").style.height = newContainerHeight;
+			done();
+		}, `${newContainerHeight}px`);
+
+		// Check
+		const contents = await illustratedMsg.shadow$(".ui5-illustrated-message-root"),
+			scrollHeight = await contents.getProperty("scrollHeight"),
+			offsetHeight = await contents.getProperty("offsetHeight"),
+			illustratedMsgMedia = await illustratedMsg.getProperty("media");
+		assert.ok(scrollHeight < newContainerHeight, "root dom fits container");
+		assert.strictEqual(scrollHeight, offsetHeight, "no overflow of content");
+		assert.strictEqual(illustratedMsgMedia, expectedMedia, "correct media");
+	});
+
+	it("shows image with unconstrained height when container has auto height", async () => {
+
+		const newContainerHeight = "auto",
+			illustratedMsg = await browser.$("#illustratedMsg5");
+
+		// Act: apply new height
+		await browser.executeAsync(async (newContainerHeight, done) => {
+			document.getElementById("container").style.height = newContainerHeight;
+			done();
+		}, newContainerHeight);
+
+		// Check
+		const illustration = await illustratedMsg.shadow$(".ui5-illustrated-message-illustration svg");
+		const cssHeight = (await illustration.getCSSProperty("height")).value;
+
+		assert.strictEqual(cssHeight, "160px", "svg has expected height");
+	});
+});
