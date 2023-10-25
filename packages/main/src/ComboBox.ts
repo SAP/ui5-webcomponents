0d0bc2df4890f9a9c8a2b394445404be465d4f2b
Nia Peeva <nia.peeva@sap.com>
2023-10-25 14:39:22 +0300

    fix(ui5-combobox): announce value state header on focus

--- packages/main/src/ComboBox.ts
+++ packages/main/src/ComboBox.ts
@@ -769,6 +769,7 @@ class ComboBox extends UI5Element {
 
 		if (this.focused && indexOfItem === -1 && this.hasValueStateText && isOpen) {
 			this._isValueStateFocused = true;
+			this._announceValueStateText();
 			this.focused = false;
 			return;
 		}
@@ -792,6 +793,7 @@ class ComboBox extends UI5Element {
 			this._clearFocus();
 			this._itemFocused = false;
 			this._isValueStateFocused = true;
+			this._announceValueStateText();
 			this._filteredItems[0].selected = false;
 			return;
 		}
@@ -815,6 +817,7 @@ class ComboBox extends UI5Element {
 			this._clearFocus();
 			this._itemFocused = false;
 			this._isValueStateFocused = true;
+			this._announceValueStateText();
 			return;
 		}
 
@@ -838,6 +841,7 @@ class ComboBox extends UI5Element {
 			this._clearFocus();
 			this._itemFocused = false;
 			this._isValueStateFocused = true;
+			this._announceValueStateText();
 			return;
 		}
 
@@ -1070,6 +1074,12 @@ class ComboBox extends UI5Element {
 		} else {
 			announce(`${currentItemAdditionalText} ${itemPositionText}`.trim(), InvisibleMessageMode.Polite);
 		}
+	}
+
+	_announceValueStateText() {
+		const valueStateText = this.valueStateMessageText.map(el => el.textContent).join(" ");
+
+		announce(valueStateText, InvisibleMessageMode.Polite);
 	}
 
 	get _headerTitleText() {
