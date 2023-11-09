f4a2457019111cccca8bfbcb24df3ccf402d11ca
hinzzx <abvsuxhard@gmail.com>
2023-11-09 15:49:19 +0200

    feat(ui5-calendar-legend): remove types for new CEM

--- packages/main/src/types/CalendarLegendItemType.ts
+++ packages/main/src/types/CalendarLegendItemType.ts
@@ -1,155 +1,126 @@
 /**
  * Enum for calendar legend items' types.
- *
- * @readonly
- * @enum {string}
  * @private
- * @author SAP SE
- * @alias sap.ui.webc.main.types.CalendarLegendItemType
  */
 enum CalendarLegendItemType {
 	/**
 	 * Represents the "Today" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Today = "Today",
 	/**
 	 * Represents the "Selected" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Selected = "Selected",
 	/**
 	 * Represents the "Working" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Working = "Working",
 	/**
 	 * Represents the "NonWorking" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	NonWorking = "NonWorking",
 	/**
 	 * Represents the "Type01" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type01 = "Type01",
 	/**
 	 * Represents the "Type02" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type02 = "Type02",
 	/**
 	 * Represents the "Type03" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type03 = "Type03",
 	/**
 	 * Represents the "Type04" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type04 = "Type04",
 	/**
 	 * Represents the "Type05" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type05 = "Type05",
 	/**
 	 * Represents the "Type06" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type06 = "Type06",
 	/**
 	 * Represents the "Type07" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type07 = "Type07",
 	/**
 	 * Represents the "Type08" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type08 = "Type08",
 	/**
 	 * Represents the "Type09" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type09 = "Type09",
 	/**
 	 * Represents the "Type10" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type10 = "Type10",
 	/**
 	 * Represents the "Type11" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type11 = "Type11",
 	/**
 	 * Represents the "Type12" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type12 = "Type12",
 	/**
 	 * Represents the "Type13" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type13 = "Type13",
 	/**
 	 * Represents the "Type14" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type14 = "Type14",
 	/**
 	 * Represents the "Type15" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type15 = "Type15",
 	/**
 	 * Represents the "Type16" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type16 = "Type16",
 	/**
 	 * Represents the "Type17" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type17 = "Type17",
 	/**
 	 * Represents the "Type18" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type18 = "Type18",
 	/**
 	 * Represents the "Type19" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type19 = "Type19",
 	/**
 	 * Represents the "Type20" item in the calendar legend.
 	 * @private
-	 * @type {string}
 	 */
 	Type20 = "Type20",
 }
