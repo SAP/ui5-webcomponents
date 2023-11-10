ecfa2bce505cd8f5832036e32f2c5b75d1501844
Nayden Naydenov <nnaydenow.work@sap.com>
2023-11-10 14:27:46 +0200

    chore: update schema and validation

--- packages/tools/lib/cem/validate.js
+++ packages/tools/lib/cem/validate.js
@@ -3,7 +3,8 @@ const Ajv = require('ajv');
 const path = require('path');
 
 // Load your JSON schema
-const schema = require('./schema.json');
+const extenalSchema = require('./schema.json');
+const internalSchema = require('./schema-internal.json');
 
 // Load your JSON data from the input file
 const inputFilePath = path.join(process.cwd(), "dist/custom-elements.json"); // Update with your file path
@@ -39,15 +40,24 @@ const inputDataExternal = clearProps(JSON.parse(JSON.stringify(inputDataInternal
 
 const ajv = new Ajv({ allowUnionTypes: true, allError: true })
 
-const validate = ajv.compile(schema)
+let validate = ajv.compile(internalSchema)
+
+// Validate the JSON data against the schema
+if (validate(inputDataInternal)) {
+    console.log('Validation internal custom-elements successful');
+} else {
+    console.error('Validation of internal custom-elements failed:', ajv.errorsText(validate.errors));
+    throw new Error("Validation of internal custom-elements failed");
+}
+
+validate = ajv.compile(extenalSchema)
 
 // Validate the JSON data against the schema
 if (validate(inputDataExternal)) {
-    console.log('Validation successful');
+    console.log('Validation external custom-elements successful');
     fs.writeFileSync(inputFilePath, JSON.stringify(inputDataExternal, null, 2), 'utf8');
     fs.writeFileSync(inputFilePath.replace("custom-elements", "custom-elements-internal"), JSON.stringify(inputDataInternal, null, 2), 'utf8');
-    console.log('File updated with cleaned JSON data');
 } else {
-    console.error('Validation of custom-elements.json failed:', ajv.errorsText(validate.errors));
-    throw new Error("Validation of custom-elements.json failed");
+    console.error('Validation of external custom-elements failed:', ajv.errorsText(validate.errors));
+    throw new Error("Validation of external custom-elements failed");
 }
