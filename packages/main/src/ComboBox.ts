575786180bd9a2fa676b2a60f6705c433f321d9d
Martin R. Hristov <m.hristov@sap.com>
2023-12-22 16:50:05 +0200

    fix(ui5-combobox): keep picker open on group selection

--- packages/main/src/ComboBox.ts
+++ packages/main/src/ComboBox.ts
@@ -873,9 +873,13 @@ class ComboBox extends UI5Element {
 		}
 
 		if (isEnter(e)) {
+			const focusedItem = this._filteredItems.find(item => {
+				return item.focused;
+			});
+
 			this._fireChangeEvent();
 
-			if (picker?.opened) {
+			if (picker?.opened && !focusedItem?.isGroupItem) {
 				this._closeRespPopover();
 				this.focused = true;
 			} else if (this.FormSupport) {
