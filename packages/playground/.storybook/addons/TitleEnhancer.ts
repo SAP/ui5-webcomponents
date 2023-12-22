72bd3b918c79f4479239e7d982386fdd0fce483b
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-12-22 13:23:18 +0200

    docs: change event and adjust titles for docs page

--- packages/playground/.storybook/addons/TitleEnhancer.ts
+++ packages/playground/.storybook/addons/TitleEnhancer.ts
@@ -1,22 +1,33 @@
 import { addons } from '@storybook/manager-api';
-import { STORY_RENDERED } from '@storybook/core-events';
+import { CURRENT_STORY_WAS_SET } from '@storybook/core-events';
 
 const ADDON_ID = 'title-enhancer';
 
 addons.register(ADDON_ID, (api) => {
     const setTitle = () => {
         const storyData = api.getCurrentStoryData();
-        const componentName = storyData.name.split('/').pop()?.toLowerCase();
-
-        const componentTag = `ui5-${componentName}`;
-
-        if (storyData) {
-            const data = storyData;
-            document.title = `${data.title} - ${componentTag} - UI5 Web Components | Documentation`;
+        const getComponentTag = (title: string) => {
+            const componentName = title.split('/').pop();
+            return `ui5-${componentName?.split(' ').join('-').toLowerCase()}`;
         }
+
+        let keywords = "";
+
+        if (storyData?.tags?.includes('autodocs')) {
+            // autodocs generated per component
+            keywords = `${storyData.title} - ${getComponentTag(storyData.title)}`;
+        } else if (storyData?.tags?.includes('story')) {
+            // stories generated per component
+            keywords = `${storyData.title} - ${getComponentTag(storyData.title)} - ${storyData.name}`;
+        } else if (storyData?.tags?.includes('docs')) {
+            // docs generated out of the md files
+            keywords = `${storyData.title}`;
+        }
+
+        document.title = `${keywords} | UI5 Web Components | Documentation`
     };
 
-    api.on(STORY_RENDERED, () => {
+    api.on(CURRENT_STORY_WAS_SET, () => {
         setTitle();
     })
 });
