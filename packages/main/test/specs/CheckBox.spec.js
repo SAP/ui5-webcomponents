310c02f4fa0632ed3b544e89a537f0a6961e759f
plamenivanov91 <plamen.ivanov01@sap.com>
2024-02-02 17:38:15 +0200

    fix(ui5-checkbox): improved key down behavior for readonly state - fixed false positive testing

--- packages/main/test/specs/CheckBox.spec.js
+++ packages/main/test/specs/CheckBox.spec.js
@@ -1,4 +1,8 @@
 import { assert } from "chai";
+const KEYS = {
+	ENTER: '\uE007',
+	SPACE: '\ue00D',
+}
 
 describe("CheckBox general interaction", () => {
 	before(async () => {
@@ -25,11 +29,27 @@ describe("CheckBox general interaction", () => {
 	it("tests readonly space and enter keys active state", async () => {
 		const checkBox = await browser.$("#cbReadonly");
 
-		await checkBox.click();
-		await checkBox.keys("Space");
-		await checkBox.keys("Enter");
+		await checkBox.click(); // force focus
 
-		assert.strictEqual(await checkBox.getAttribute("active"), null, "Click, Space or Enter doesn't trigger active attr");
+		// Setup for SPACE Key
+		await browser.performActions([{
+			type: 'key',
+			id: 'keyboard3',
+			actions: [{ type: 'keyDown', value: KEYS.SPACE }],
+			}]);
+		// Action
+		assert.strictEqual(await checkBox.getAttribute("active"), null, "Space doesn't trigger active attr");
+		await browser.releaseActions();
+
+		// Setup for ENTER Key
+		await browser.performActions([{
+			type: 'key',
+			id: 'keyboard3',
+			actions: [{ type: 'keyDown', value: KEYS.ENTER }],
+			}]);
+		// Action
+		assert.strictEqual(await checkBox.getAttribute("active"), null, "Enter doesn't trigger active attr");
+		await browser.releaseActions();
 	});
 
 	it("tests change event not fired, when disabled", async () => {
