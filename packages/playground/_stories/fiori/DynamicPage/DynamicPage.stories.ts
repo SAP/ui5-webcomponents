cfbd2c97f951b4d8f77355210dec0d47ebd4a41f
Dobrin Dimchev <dobrin.dimchev@sap.com>
2024-02-05 11:36:43 +0200

    chore: align docs to new build scripts

--- packages/playground/_stories/fiori/DynamicPage/DynamicPage.stories.ts
+++ packages/playground/_stories/fiori/DynamicPage/DynamicPage.stories.ts
@@ -1,23 +1,17 @@
 import { html } from "lit";
 import { ifDefined } from "lit/directives/if-defined.js";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
-import type { Meta, StoryFn } from "@storybook/web-components";
+import type { Meta } from "@storybook/web-components";
 import type { PartialStoryFn } from "@storybook/types";
 
-import argTypes, { componentInfo } from "./argTypes.js";
+import argTypes from "./argTypes.js";
 import type { StoryArgsSlots } from "./argTypes.js";
 import type { UI5StoryArgs } from "../../../types.js";
-
-import { DocsPage } from "../../../.storybook/docs.js";
 
 import type DynamicPage from "@ui5/webcomponents-fiori/dist/DynamicPage.js";
 
 import DynamicPageTitleTemplate from "./DynamicPageTitleTemplate.js"
 import DynamicPageHeaderTemplate from "./DynamicPageHeaderTemplate.js"
-
-
-
-const component = "ui5-dynamic-page";
 
 const stylesDecorator = (storyFn: PartialStoryFn) => html`
     <style>
@@ -35,11 +29,6 @@ const stylesDecorator = (storyFn: PartialStoryFn) => html`
 export default {
   title: "Fiori/Dynamic page",
   component: "DynamicPage",
-  parameters: {
-    docs: {
-      page: DocsPage({ ...componentInfo, component })
-    },
-  },
   decorators: [stylesDecorator],
   argTypes,
 } as Meta<DynamicPage>;
--- packages/playground/_stories/fiori/DynamicPage/DynamicPageHeader/DynamicPageHeader.stories.ts
+++ packages/playground/_stories/fiori/DynamicPage/DynamicPageHeader/DynamicPageHeader.stories.ts
@@ -2,25 +2,16 @@ import { html } from "lit";
 import type { Meta } from "@storybook/web-components";
 import type { PartialStoryFn } from "@storybook/types";
 
-import argTypes, { componentInfo } from "./argTypes.js";
+import argTypes from "./argTypes.js";
 import type { StoryArgsSlots } from "../argTypes.js";
 import type { UI5StoryArgs } from "../../../../types.js";
-
-import { DocsPage } from "../../../../.storybook/docs.js";
 
 import type DynamicPageHeader from "@ui5/webcomponents-fiori/dist/DynamicPageHeader.js";
 import DynamicPageHeaderTemplate from "../DynamicPageHeaderTemplate.js";
 
-const component = "ui5-dynamic-page-header";
-
 export default {
   title: "Fiori/Dynamic Page/ Dynamic Page Header",
   component: "DynamicPageHeader",
-  parameters: {
-    docs: {
-      page: DocsPage({ ...componentInfo, component, showDefaultStoryOnly: true })
-    },
-  },
   argTypes,
 } as Meta<DynamicPageHeader>;
 
