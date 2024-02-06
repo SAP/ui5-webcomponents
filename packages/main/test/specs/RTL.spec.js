27fa0bbd16a2f61723a4ab055927214784906843
ilhan007 <ilhan.myumyun@sap.com>
2024-02-06 14:26:32 +0200

    chore: update RTL config test

--- packages/main/test/specs/RTL.spec.js
+++ packages/main/test/specs/RTL.spec.js
@@ -4,21 +4,18 @@ describe("RTL", () => {
 	it("language forces RTL, if RTL not specified", async () => {
 		await browser.url(`test/pages/Switch.html?sap-ui-language=he`);
 
-		const swRoot = await browser.$("#switchAccName").shadow$(".ui5-switch-root");
-		assert.strictEqual(await swRoot.getProperty("dir"), "rtl", "dir is correctly set");
+		assert.strictEqual(await browser.$("#switchAccName").getProperty("effectiveDir"), "rtl", "effectiveDir correctly returns 'rtl'");
 	});
 
 	it("config forces RTL", async () => {
 		await browser.url(`test/pages/Switch.html?sap-ui-rtl=true`);
 
-		const swRoot = await browser.$("#switchAccName").shadow$(".ui5-switch-root");
-		assert.strictEqual(await swRoot.getProperty("dir"), "rtl", "dir is correctly set");
+		assert.strictEqual(await await browser.$("#switchAccName").getProperty("effectiveDir"), "rtl", "effectiveDir correctly returns 'rtl'");
 	});
 
 	it("config unsets RTL, although rtl language is used", async () => {
 		await browser.url(`test/pages/Switch.html?sap-ui-rtl=false&sap-ui-language=he`);
 
-		const swRoot = await browser.$("#switchAccName").shadow$(".ui5-switch-root");
-		assert.notOk(await swRoot.getProperty("dir"), "dir is not present");
+		assert.notOk(await browser.$("#switchAccName").getProperty("effectiveDir"), "effectiveDir correctly returns 'undefined'");
 	});
 });
