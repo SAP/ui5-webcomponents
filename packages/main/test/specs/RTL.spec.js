fc1bb98d1646b729de25e3b07b2a0ad0c177facf
ilhan007 <ilhan.myumyun@sap.com>
2024-02-07 15:33:02 +0200

    chore: udpate RTL tests

--- packages/main/test/specs/RTL.spec.js
+++ packages/main/test/specs/RTL.spec.js
@@ -1,21 +1,10 @@
 import { assert } from "chai";
 
 describe("RTL", () => {
-	it("language forces RTL, if RTL not specified", async () => {
-		await browser.url(`test/pages/Switch.html?sap-ui-language=he`);
+	it("tests effectiveDir", async () => {
+		await browser.url(`test/pages/RTL.html`);
 
-		assert.strictEqual(await browser.$("#switchAccName").getProperty("effectiveDir"), "rtl", "effectiveDir correctly returns 'rtl'");
-	});
-
-	it("config forces RTL", async () => {
-		await browser.url(`test/pages/Switch.html?sap-ui-rtl=true`);
-
-		assert.strictEqual(await await browser.$("#switchAccName").getProperty("effectiveDir"), "rtl", "effectiveDir correctly returns 'rtl'");
-	});
-
-	it("config unsets RTL, although rtl language is used", async () => {
-		await browser.url(`test/pages/Switch.html?sap-ui-rtl=false&sap-ui-language=he`);
-
-		assert.notOk(await browser.$("#switchAccName").getProperty("effectiveDir"), "effectiveDir correctly returns 'undefined'");
+		assert.strictEqual(await browser.$("#cbRTL").getProperty("effectiveDir"), "rtl", "effectiveDir correctly returns 'rtl'");
+		assert.strictEqual(await browser.$("#cbLTR").getProperty("effectiveDir"), "ltr", "effectiveDir correctly returns 'ltr'");
 	});
 });
