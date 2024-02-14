e9bb459fad68293425973b389265b4a74279df5d
Stoyan <88034608+hinzzx@users.noreply.github.com>
2024-02-14 16:34:22 +0200

    feat(ui5-calendar-legend): introduce ui5-calendar-legend component (#7706)
    Introducing the new CalendarLegend, CalendarLegendItem, SpecialCalendarDate Web Components.
    The Calendar Legend has four properties:
    *hideToday (hide-today) - Hides the 'Today' item in the Calendar Legend;
    *hideSelectedDay (hide-selected-day) - Hides the 'Selected' item in the Calendar Legend;
    *hideWorkingDay (hide-working-day) - Hides the 'Working Day' item in the Calendar Legend;
    *hideNonWorkingDay (hide-non-working-day) - Hides the 'Non-Working Day' item in the Calendar Legend;
    Accepting <ui5-calendar-legend-item>;
    -------
    The Calendar Legend Item has two properties:
    type - Accepting string value of ("Type01"..."Type20");
    -------
    SpecialCalendarDate accepts the property:
    type - which should be a string value of ("Type01"..."Type20"); emphasizing the special days in the Calendar.

---
+++ packages/main/src/CalendarLegend.ts
@@ -0,0 +1,205 @@
+import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
+import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
+import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+import property from "@ui5/webcomponents-base/dist/decorators/property.js";
+import slot from "@ui5/webcomponents-base/dist/decorators/slot.js";
+import event from "@ui5/webcomponents-base/dist/decorators/event.js";
+import ItemNavigation from "@ui5/webcomponents-base/dist/delegate/ItemNavigation.js";
+import NavigationMode from "@ui5/webcomponents-base/dist/types/NavigationMode.js";
+import {
+	isDown,
+	isUp,
+} from "@ui5/webcomponents-base/dist/Keys.js";
+import ItemNavigationBehavior from "@ui5/webcomponents-base/dist/types/ItemNavigationBehavior.js";
+import CalendarLegendItemType from "./types/CalendarLegendItemType.js";
+import CalendarLegendTemplate from "./generated/templates/CalendarLegendTemplate.lit.js";
+import CalendarLegendItem from "./CalendarLegendItem.js";
+
+// Styles
+import CalendarLegendCss from "./generated/themes/CalendarLegend.css.js";
+
+type CalendarLegendItemSelectionChangeEventDetail = {
+	item: CalendarLegendItem;
+}
+
+/**
+ * @class
+ *
+ * <h3 class="comment-api-title">Overview</h3>
+ *
+ * The <code>ui5-calendar-legend</code> component is designed for use within the <code>ui5-calendar</code> to display a legend.
+ * Each <code>ui5-calendar-legend-item</code> represents a unique date type, specifying its visual style
+ * and a corresponding textual label.
+ *
+ * <h3>ES6 Module Import</h3>
+ *
+ * <code>import "@ui5/webcomponents/dist/CalendarLegend.js";</code>
+ *
+ * @constructor
+ * @extends UI5Element
+ * @public
+ * @since 1.23.0
+ */
+@customElement({
+	tag: "ui5-calendar-legend",
+	renderer: litRender,
+	styles: CalendarLegendCss,
+	template: CalendarLegendTemplate,
+	dependencies: [CalendarLegendItem],
+})
+@event<CalendarLegendItemSelectionChangeEventDetail>("_calendar-legend-selection-change", {
+	detail: {
+		item: { type: CalendarLegendItem },
+	},
+})
+@event("_calendar-legend-focus-out")
+class CalendarLegend extends UI5Element {
+	/**
+	 * Hides the Today item in the legend.
+	 *
+	 * @default false
+	 * @public
+	 */
+	@property({ type: Boolean })
+	hideToday!: boolean;
+
+	/**
+	 * Hides the Selected day item in the legend.
+	 *
+	 * @default false
+	 * @public
+	 */
+	@property({ type: Boolean })
+	hideSelectedDay!: boolean;
+
+	/**
+	 * Hides the Non-Working day item in the legend.
+	 *
+	 * @default false
+	 * @public
+	 */
+	@property({ type: Boolean })
+	hideNonWorkingDay!: boolean;
+
+	/**
+	 * Hides the Working day item in the legend.
+	 *
+	 * @default false
+	 * @public
+	 */
+	@property({ type: Boolean })
+	hideWorkingDay!: boolean;
+
+	/**
+	 * Defines the items of the component.
+	 * @public
+	 */
+	@slot({
+		type: HTMLElement,
+		invalidateOnChildChange: true,
+		individualSlots: true,
+		"default": true,
+	 })
+	items!: Array<CalendarLegendItem>;
+
+	_itemNavigation!: ItemNavigation;
+	_lastFocusedItemIndex: number | null;
+
+	constructor() {
+		super();
+
+		this._lastFocusedItemIndex = null;
+	}
+
+	onAfterRendering(): void {
+		if (!this._itemNavigation) {
+			this._itemNavigation = new ItemNavigation(this, {
+				navigationMode: NavigationMode.Horizontal,
+				behavior: ItemNavigationBehavior.Static,
+				getItemsCallback: () => this.focusableElements,
+			});
+
+			const focusableItemIndex = this._lastFocusedItemIndex !== null ? this._lastFocusedItemIndex : 0;
+			this._itemNavigation.setCurrentItem(this.focusableElements[focusableItemIndex]);
+		}
+	}
+
+	_onMouseDown(e: MouseEvent) {
+		e.stopPropagation();
+		const target = e.target as CalendarLegendItem;
+
+		this._itemNavigation.setCurrentItem(target);
+		this._itemNavigation._focusCurrentItem();
+		this._lastFocusedItemIndex = this.focusableElements.indexOf(target);
+	}
+
+	_onFocusIn(e: FocusEvent) {
+		const target = e.target as CalendarLegendItem;
+
+		this.fireEvent<CalendarLegendItemSelectionChangeEventDetail>("_calendar-legend-selection-change", {
+			item: target,
+		});
+		this._lastFocusedItemIndex = this.focusableElements.indexOf(target);
+	}
+
+	_onFocusOut() {
+		this.fireEvent("_calendar-legend-focus-out");
+	}
+
+	_onItemKeyDown(e: KeyboardEvent) {
+		const items = this.focusableElements;
+		const itemsCount = items.length;
+		const currentItem = e.target as CalendarLegendItem;
+		const currentIndex = items.indexOf(currentItem);
+
+		if (isDown(e)) {
+			e.preventDefault();
+			const nextIndex = currentIndex + 1;
+
+			if (nextIndex < itemsCount) {
+				this._itemNavigation.setCurrentItem(items[nextIndex]);
+				this._itemNavigation._focusCurrentItem();
+				this._lastFocusedItemIndex = nextIndex;
+			}
+		}
+
+		if (isUp(e)) {
+			e.preventDefault();
+			const nextIndex = currentIndex - 1;
+
+			if (nextIndex >= 0) {
+				this._itemNavigation.setCurrentItem(items[nextIndex]);
+				this._itemNavigation._focusCurrentItem();
+				this._lastFocusedItemIndex = nextIndex;
+			}
+		}
+	}
+
+	get focusableElements() {
+		const allFocusableItems = [...this.shadowRoot!.querySelectorAll<CalendarLegendItem>("[ui5-calendar-legend-item]"), ...this.legendItems];
+		return allFocusableItems;
+	}
+
+	get legendItems() {
+		const items = this.getSlottedNodes<CalendarLegendItem>("items");
+		return items.filter(item => item instanceof CalendarLegendItem);
+	}
+
+	get defaultItemsMapping() {
+		const typeMapping = [
+			{ type: [CalendarLegendItemType.Today], hide: this.hideToday },
+			{ type: [CalendarLegendItemType.Selected], hide: this.hideSelectedDay },
+			{ type: [CalendarLegendItemType.Working], hide: this.hideWorkingDay },
+			{ type: [CalendarLegendItemType.NonWorking], hide: this.hideNonWorkingDay },
+		];
+
+		return typeMapping;
+	}
+}
+
+CalendarLegend.define();
+
+export default CalendarLegend;
+export type {
+	CalendarLegendItemSelectionChangeEventDetail,
+};
