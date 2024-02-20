15433954d29730ecd9b445b93eadad52198f16b9
Nayden Naydenov <nnaydenow.work@sap.com>
2024-02-20 17:03:04 +0200

    chore: add Card, Carousel, CheckBox, ColorPalette, ColorPalettePopover, ColorPicker

---
+++ packages/documentation/docs/_samples/main/Card/MoreCards/main.js
@@ -0,0 +1,11 @@
+import "@ui5/webcomponents/dist/Card";
+import "@ui5/webcomponents/dist/CardHeader";
+import "@ui5/webcomponents/dist/List";
+import "@ui5/webcomponents/dist/StandardListItem";
+import "@ui5/webcomponents/dist/Title";
+import "@ui5/webcomponents/dist/Label";
+import "@ui5/webcomponents/dist/Link";
+
+import "@ui5/webcomponents-icons/dist/competitor.js";
+import "@ui5/webcomponents-icons/dist/wallet.js";
+import "@ui5/webcomponents-icons/dist/collaborate.js";
