fe9d1dce999a9ab0c0c4413882bba246cc283126
Boyan Rakilovski <boyan.rakilovski@sap.com>
2024-02-21 18:06:03 +0200

    feat(ui5-menu): enable navigation over disabled items (#8312)
    feat(ui5-menu): enable navigation over disabled items
    
    - There is a new behavior implemented, which enables
    the end users to navigate via mouse and keyboard to a
    disabled ui5-menu-item element. This behavior is aimed
    to improve the accessibility of the ui5-menu component
    by providing screen reader announcements for disabled
    menu items as well.
    
    - The haspopup attribute is now applied to the ui5-menu item's
    with a sub-menu.
    
    - There is a new tooltip property for the ui5-menu-item.
    
    Fixes: #7096
    Fixes: #8214

---
+++ packages/main/src/MenuListItem.ts
@@ -0,0 +1,48 @@
+import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
+import property from "@ui5/webcomponents-base/dist/decorators/property.js";
+import StandardListItem from "./StandardListItem.js";
+import StandardListItemTemplate from "./generated/templates/StandardListItemTemplate.lit.js";
+import MenuItem from "./MenuItem.js";
+import HasPopup from "./types/HasPopup.js";
+
+// Styles
+import menuListItemCss from "./generated/themes/MenuListItem.css.js";
+
+/**
+ * @class
+ *
+ * @constructor
+ * @extends StandardListItem
+ * @since 1.23.0
+ * @private
+ */
+@customElement({
+	tag: "ui5-menu-li",
+	template: StandardListItemTemplate,
+	styles: [StandardListItem.styles, menuListItemCss],
+})
+class MenuListItem extends StandardListItem {
+	/**
+	 * Defines the associated MenuItem instance
+	 * @private
+	 */
+	@property({ type: Object })
+	associatedItem?: MenuItem;
+
+	get _focusable() {
+		return true;
+	}
+
+	get _accInfo() {
+		const accInfoSettings = {
+			role: "menuitem",
+			ariaHaspopup: this.associatedItem?.hasSubmenu ? HasPopup.Menu.toLowerCase() as Lowercase<HasPopup> : undefined,
+		};
+
+		return { ...super._accInfo, ...accInfoSettings };
+	}
+}
+
+MenuListItem.define();
+
+export default MenuListItem;
