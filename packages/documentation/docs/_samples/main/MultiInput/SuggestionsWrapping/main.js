bc8ae5c274a1532dd0cdceab3f235184702ae736
Nayden Naydenov <nnaydenow.work@sap.com>
2024-02-21 17:21:59 +0200

    chore: multiinput

---
+++ packages/documentation/docs/_samples/main/MultiInput/SuggestionsWrapping/main.js
@@ -0,0 +1,3 @@
+import "@ui5/webcomponents/dist/MultiInput.js";
+import "@ui5/webcomponents/dist/SuggestionItem";
+import "@ui5/webcomponents/dist/features/InputSuggestions.js";
---
+++ packages/documentation/docs/_samples/main/MultiInput/Tokens/main.js
@@ -0,0 +1,2 @@
+import "@ui5/webcomponents/dist/MultiInput.js";
+import "@ui5/webcomponents/dist/Token.js";
