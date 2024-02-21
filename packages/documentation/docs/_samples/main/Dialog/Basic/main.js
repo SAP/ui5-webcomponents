7f4c2e35bedd50739270eeef63b24bd0fc6687c6
Nayden Naydenov <nnaydenow.work@sap.com>
2024-02-21 13:57:58 +0200

    chore: datetimepicker, dialog

---
+++ packages/documentation/docs/_samples/main/Dialog/Basic/main.js
@@ -0,0 +1,21 @@
+import "@ui5/webcomponents/dist/Dialog.js";
+import "@ui5/webcomponents/dist/Button.js";
+import "@ui5/webcomponents/dist/Link.js";
+import "@ui5/webcomponents/dist/Label.js";
+
+var dialogOpener = document.getElementById("dialogOpener");
+var dialog = document.getElementById("dialog");
+var dialogClosers = [...dialog.querySelectorAll(".dialogCloser")];
+
+dialogOpener.accessibilityAttributes = {
+    hasPopup: "dialog",
+    controls: dialog.id,
+};
+dialogOpener.addEventListener("click", () => {
+    dialog.open = true;
+});
+dialogClosers.forEach(btn => {
+    btn.addEventListener("click", () => {
+        dialog.open = false;
+    });
+})
