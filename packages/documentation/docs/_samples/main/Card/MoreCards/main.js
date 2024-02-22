debca6a594e801e585902df518e3a8e00e723212
ilhan007 <ilhan.myumyun@sap.com>
2024-02-22 18:11:07 +0200

    chore: polish samples

--- packages/documentation/docs/_samples/main/Card/MoreCards/main.js
+++ packages/documentation/docs/_samples/main/Card/MoreCards/main.js
@@ -1,10 +1,10 @@
-import "@ui5/webcomponents/dist/Card";
-import "@ui5/webcomponents/dist/CardHeader";
-import "@ui5/webcomponents/dist/List";
-import "@ui5/webcomponents/dist/StandardListItem";
-import "@ui5/webcomponents/dist/Title";
-import "@ui5/webcomponents/dist/Label";
-import "@ui5/webcomponents/dist/Link";
+import "@ui5/webcomponents/dist/Card.js";
+import "@ui5/webcomponents/dist/CardHeader.js";
+import "@ui5/webcomponents/dist/List.js";
+import "@ui5/webcomponents/dist/StandardListItem.js";
+import "@ui5/webcomponents/dist/Title.js";
+import "@ui5/webcomponents/dist/Label.js";
+import "@ui5/webcomponents/dist/Link.js";
 
 import "@ui5/webcomponents-icons/dist/competitor.js";
 import "@ui5/webcomponents-icons/dist/wallet.js";
