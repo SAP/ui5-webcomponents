a45f25cf82f17d5c1e2e14c7b73e5edd4346fee6
tsanislavgatev <tseko95@gmail.com>
2024-02-28 13:40:24 +0200

    feat(ui5-button): add accessible-role api

---
+++ packages/main/src/types/ButtonAccessibleRole.ts
@@ -0,0 +1,21 @@
+/**
+ * Button accessible roles.
+ *
+ * @public
+ */
+enum ButtonAccessibleRole {
+
+	/**
+	 * Represents Default (button) ARIA role.
+	 * @public
+	 */
+	Button = "Button",
+
+	/**
+	 * Represents the ARIA role "link".
+	 * @public
+	 */
+	Link = "Link",
+}
+
+export default ButtonAccessibleRole;
