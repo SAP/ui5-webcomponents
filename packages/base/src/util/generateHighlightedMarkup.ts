20722033602d19fd799581daef04b72242aed95e
Nayden Naydenov <nnaydenow.work@sap.com>
2024-03-01 16:15:19 +0200

    chore: fix build

--- packages/base/src/util/generateHighlightedMarkup.ts
+++ packages/base/src/util/generateHighlightedMarkup.ts
@@ -8,11 +8,11 @@ function replaceAll(text: string, find: string, replace: string | ((substring: s
 }
 
 /**
- * Generate markup for a raw string where a particular text is wrapped with some tag, by default `**` (bold) tag.
+ * Generate markup for a raw string where a particular text is wrapped with some tag, by default `<b>` (bold) tag.
  * All inputs to this function are considered literal text, and special characters will always be escaped.
  * @param {string} text The text to add highlighting to
  * @param {string} textToHighlight The text which should be highlighted
- * @return {string} the markup HTML which contains all occurrances of the input text surrounded with a `**` tag.
+ * @return {string} the markup HTML which contains all occurrances of the input text surrounded with a `<b>` tag.
  */
 function generateHighlightedMarkup(text: string, textToHighlight: string) {
 	if (!text || !textToHighlight) {
@@ -34,7 +34,7 @@ function generateHighlightedMarkup(text: string, textToHighlight: string) {
 	// wrap every occurance of the textToHighlight using the open/close tokens (instead of markup at this point)
 	let result = encodeXML(replaceAll(text, textToHighlight, (match: string) => `${openToken}${match}${closeToken}`, true)) as string;
 	// now replace the open and close tokens with the markup that we expect
-	[[openToken, "**"], [closeToken, "**"]].forEach(([find, replace]) => {
+	[[openToken, "<b>"], [closeToken, "</b>"]].forEach(([find, replace]) => {
 		result = replaceAll(result, find, replace, false);
 	});
 	return result;
