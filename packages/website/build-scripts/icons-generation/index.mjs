3d15ab7b546c2b8908dee9f141c18f715107e256
ilhan orhan <ilhan.orhan007@gmail.com>
2024-03-03 10:11:02 +0200

    docs: add copy svg to icon explorer (#8389)

--- packages/website/build-scripts/icons-generation/index.mjs
+++ packages/website/build-scripts/icons-generation/index.mjs
@@ -1,6 +1,34 @@
 import fs from "fs";
 import path from "path";
+
 let counter = 0;
+
+const SAPIconsConfig = {
+    title: "SAP Icons",
+    npmLink: "https://www.npmjs.com/package/@ui5/webcomponents-icons",
+    npmPackage: "@ui5/webcomponents-icons",
+    version: "v5",
+    dir: "icons",
+    componentName: "SAPIcons",
+};
+
+const SAPTNTIconsConfig = {
+    title: "SAP TNT Icons",
+    npmLink: "https://www.npmjs.com/package/@ui5/webcomponents-icons-tnt",
+    npmPackage: "@ui5/webcomponents-icons-tnt",
+    version: "v3",
+    dir: "icons-tnt",
+    componentName: "SAPTNTIcons",
+};
+
+const SAPBSIconsConfig = {
+    title: "SAP Business Suite Icons",
+    npmLink: "https://www.npmjs.com/package/@ui5/webcomponents-icons-business-suite",
+    npmPackage: "@ui5/webcomponents-icons-business-suite",
+    version: "v2",
+    dir: "icons-business-suite",
+    componentName: "SAPBSIcons"
+};
 
 const replaceFirstNumber = (str) => {
     const firstLetter = str.charAt(0);
@@ -16,7 +44,17 @@ const capitalize = (str) => {
     const remainingLetters = str.slice(1);
     const capitalizedWord = firstLetterCap + remainingLetters;
     return capitalizedWord;
-}
+};
+
+const writeFile = (targetDir, content) => {
+    const targetPath = path.resolve(`./${targetDir}`);
+    const targetFile = path.resolve(`${targetPath}/index.js`);
+
+    if (!fs.existsSync(targetPath)) {
+        fs.mkdirSync(targetPath, { recursive: true });
+    }
+    fs.writeFileSync(targetFile, content,{ encoding:'utf8', flag:'w' });
+};
 
 const commonImports = `
 import React from 'react';
@@ -25,7 +63,14 @@ import Heading from '@theme/Heading';
 import Link from '@docusaurus/Link';
 `
 
-const _generateIconsPage = (sourceDir, collection, version) => {
+const additionalImports = `
+import CopySvg from "../local-cdn/local-cdn/icons/dist/v5/copy.svg";
+import AcceptSvg from "../local-cdn/local-cdn/icons/dist/v5/accept.svg";
+import PictureSvg from "../local-cdn/local-cdn/icons/dist/v5/picture.svg";
+`;
+
+
+const _generateIconsPage = (sourceDir, config) => {
     let imports = ``;
     let icons = ``;
 
@@ -36,139 +81,93 @@ const _generateIconsPage = (sourceDir, collection, version) => {
 
             const iconNameImport = `${fileNameImportName}SvgName`;
             const svgImport = `${capitalize(fileNameImportName)}Svg`;
-
-        imports += `
-import ${iconNameImport} from "../local-cdn/local-cdn/${collection}/dist/${fileName}.js";
-import ${svgImport} from "../local-cdn/local-cdn/${collection}/dist/${version}/${fileName}.svg";
-`;
+            
+            imports += `
+            import ${iconNameImport} from "../local-cdn/local-cdn/${config.dir}/dist/${fileName}.js";
+            import ${svgImport} from "../local-cdn/local-cdn/${config.dir}/dist/${config.version}/${fileName}.svg";
+            `;
 
         icons += `
-<div
-    tabIndex="-1"
-    title="Copy the icon name"
-    className="icon__wrapper"
-    onClick={function(e) { 
-        navigator.clipboard.writeText(${iconNameImport}); 
-        const target = e.target;
-        target.classList.add("icon__wrapper--copied");
+        <div
+            tabIndex="-1"
+            title="Copy Icon"
+            className="icon__wrapper"
+            onClick={function(e) {
+                const target = e.target;
 
-        setTimeout(() => {
-            target.classList.remove("icon__wrapper--copied");
-        }, 600)
-    }}>
-    <CopySvg className="icon__wrapper__copy"/>
-    <AcceptSvg className="icon__wrapper__accept"/>
-    <div className="icon__wrapper__svg"><${svgImport} fill="var(--ifm-font-color-base)"/></div>
-    <span className="icon__wrapper__title">{${iconNameImport}}</span>
-</div>`;
+                if (target.classList.contains("icon__button--picture") || target.classList.contains("icon__button--copy")) {
+                    const acceptSVG = document.querySelector("#${svgImport}_accept");
+                    acceptSVG?.classList.add("icon__svg--accept--visible");
+
+                    setTimeout(() => {
+                        acceptSVG.classList.remove("icon__svg--accept--visible");
+                    }, 600);
+                }
+            }}>
+    
+            <AcceptSvg id="${svgImport}_accept" className="icon__svg--accept"/>
+            <div id="${svgImport}_svg"  className="icon__wrapper__svg"><${svgImport} fill="var(--ifm-font-color-base)"/></div>
+            <span className="icon__wrapper__title">{${iconNameImport}.replace("tnt/", "").replace("business-suite/", "")}</span>
+
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <button className="button button--secondary icon__button--copy"
+                    onClick={function(e) {
+                        navigator.clipboard.writeText(${iconNameImport});
+                }}>
+                    <CopySvg className="icon__svg--copy" />
+                </button>
+
+                <button className="button button--secondary icon__button--picture"
+                    onClick={function(e) {
+                        navigator.clipboard.writeText(document.querySelector("#${svgImport}_svg")?.innerHTML);
+                }}>
+                    <PictureSvg className="icon__svg--picture" />
+                </button>
+            </div>
+        </div>`;
         }
     });
 
-    return { imports, icons};
-};
-
-const generateIconsPage = (sourceDir, collection, version) => {
-   const { imports, icons} = _generateIconsPage(sourceDir, collection, version);
-
-    const content =`
-${commonImports}
-
-${imports}
-
-export default function SAPIcons() {
-    return (
-            <div style={{ padding: "2rem 2rem" }}>
-                
-                <Heading as="h2" style={{ marginBottom: "0.125rem" }}>SAP Icons</Heading>
-                <Link to="https://www.npmjs.com/package/@ui5/webcomponents-icons">@ui5/webcomponents-icons</Link>
-
-                <div className="icon__grid">
-                    ${icons}
+    const classDef = `export default function ${config.componentName}() {
+        return (
+                <div style={{
+                        padding: "2rem 2rem",
+                    }}>
+                    
+                    <Heading as="h2" style={{ marginBottom: "0.125rem" }}>${config.title}</Heading>
+                    <Link to="${config.npmLink}">${config.npmPackage}</Link>
+    
+                    <div className="icon__grid">
+                        ${icons}
+                    </div>
                 </div>
-            </div>
-    );
-}`;
+        );
+    }`
 
-    const targetPath = path.resolve(`./${collection}`);
-    const targetFile = path.resolve(`${targetPath}/index.js`);
+    return { imports, classDef};
+};
 
-    if (!fs.existsSync(targetPath)) {
-        fs.mkdirSync(targetPath, { recursive: true });
+const generateIconsPage = (sourceDir, config) => {
+    const { imports, classDef} = _generateIconsPage(sourceDir, config);
+
+    let content
+    if (config.componentName === "SAPIcons" ) {
+        content =`
+${commonImports}
+${imports}
+${classDef}`;
+    } else {
+        content =`
+${commonImports}
+${additionalImports}
+${imports}
+${classDef}`;
     }
-    fs.writeFileSync(targetFile,content,{ encoding:'utf8', flag:'w' });
+   
+    writeFile(config.dir, content);
 };
 
-const generateTNTIconsPage = (sourceDir, collection, version) => {
-    const { imports, icons} = _generateIconsPage(sourceDir, collection, version);
- 
-     const content =`
-${commonImports}
-import CopySvg from "../local-cdn/local-cdn/icons/dist/v5/copy.svg";
-import AcceptSvg from "../local-cdn/local-cdn/icons/dist/v5/accept.svg";
 
-${imports}
- 
- export default function SAPTNTIcons() {
-     return (
-             <div style={{
-                     padding: "2rem 2rem",
-                 }}>
-                 
-                 <Heading as="h2" style={{ marginBottom: "0.125rem" }}>SAP TNT Icons</Heading>
-                 <Link to="https://www.npmjs.com/package/@ui5/webcomponents-icons-tnt">@ui5/webcomponents-icons-tnt</Link>
- 
-                 <div className="icon__grid">
-                     ${icons}
-                 </div>
-             </div>
-     );
- }`;
- 
-     const targetPath = path.resolve(`./${collection}`);
-     const targetFile = path.resolve(`${targetPath}/index.js`);
- 
-     if (!fs.existsSync(targetPath)) {
-         fs.mkdirSync(targetPath, { recursive: true });
-     }
-     fs.writeFileSync(targetFile, content,{ encoding:'utf8', flag:'w' });
-};
-
-const generateBSIconsPage = (sourceDir, collection, version) => {
-    const { imports, icons} = _generateIconsPage(sourceDir, collection, version);
- 
-     const content =`
-${commonImports}
- 
-import CopySvg from "../local-cdn/local-cdn/icons/dist/v5/copy.svg";
-import AcceptSvg from "../local-cdn/local-cdn/icons/dist/v5/accept.svg";
-
-${imports}
- 
- export default function SAPBSIcons() {
-     return (
-             <div style={{
-                     padding: "2rem 2rem",
-                 }}>
-                 
-                 <Heading as="h2" style={{ marginBottom: "0.125rem" }}>SAP Business Suite Icons</Heading>
-                 <Link to="https://www.npmjs.com/package/@ui5/webcomponents-icons-business-suite">@ui5/webcomponents-icons-business-suite</Link>
- 
-                 <div className="icon__grid">
-                     ${icons}
-                 </div>
-             </div>
-     );
- }`;
- 
-     const targetPath = path.resolve(`./${collection}`);
-     const targetFile = path.resolve(`${targetPath}/index.js`);
- 
-     if (!fs.existsSync(targetPath)) {
-         fs.mkdirSync(targetPath, { recursive: true });
-     }
-     fs.writeFileSync(targetFile, content,{ encoding:'utf8', flag:'w' });
-};
-
-generateIconsPage(path.join(path.resolve(), "./local-cdn/local-cdn/icons/dist/v5"), "icons", "v5");
-generateTNTIconsPage(path.join(path.resolve(), "./local-cdn/local-cdn/icons-tnt/dist/v3"), "icons-tnt", "v3");
-generateBSIconsPage(path.join(path.resolve(), "./local-cdn/local-cdn/icons-business-suite/dist/v2"), "icons-business-suite", "v2");
+generateIconsPage(path.join(path.resolve(), "./local-cdn/local-cdn/icons/dist/v5"), SAPIconsConfig);
+generateIconsPage(path.join(path.resolve(), "./local-cdn/local-cdn/icons-tnt/dist/v3"), SAPTNTIconsConfig);
+generateIconsPage(path.join(path.resolve(), "./local-cdn/local-cdn/icons-business-suite/dist/v2"), SAPBSIconsConfig);
