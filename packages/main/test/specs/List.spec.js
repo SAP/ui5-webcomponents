73799b5c65e833bca609d0d469c3a3c508f91fac
yanaminkova <yana.minkova@sap.com>
2024-03-12 10:11:01 +0200

    feat(ui5-li): highligh property added

--- packages/main/test/specs/List.spec.js
+++ packages/main/test/specs/List.spec.js
@@ -615,4 +615,19 @@ describe("List Tests", () => {
 		assert.strictEqual(rootTooltip, newTooltip, "Tooltip of root element is updated correctly at runtime.");
 		assert.strictEqual(rootTooltip, innerTooltip, "Tooltip of root element and title of inner li element are equal after runtime change.");
 	});
+
+	it("Tests the highlight property", async () => {
+		const listItem = await browser.$("#highlight ui5-li:nth-child(1)");
+		const initialValueState = "Error";
+		let highlightValue = await listItem.getProperty("highlight");
+
+		assert.strictEqual(highlightValue, initialValueState, "Highlight property is correctly set to the list item.");
+
+		const newValueState = "Information";
+		await listItem.setProperty("highlight", "Information");
+		highlightValue = await listItem.getProperty("highlight");
+
+		assert.strictEqual(highlightValue, newValueState, "Highlight property is correctly changed.");
+
+	});
 });
