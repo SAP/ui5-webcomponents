ba6829cc0c06a57d51933cb7d2a8399ea98d9b9b
Nayden Naydenov <31909318+nnaydenow@users.noreply.github.com>
2024-03-19 08:05:04 +0000

    chore: fix components API hierarchy (#8468)
    docs: fix components API hierarchy
    
    Co-authored-by: Nayden Naydenov <nnaydenow.work@sap.com>

--- packages/website/build-scripts/api-reference-generation/types.mjs
+++ packages/website/build-scripts/api-reference-generation/types.mjs
@@ -1,37 +1,11 @@
 import fs from "fs/promises"
 import path from "path"
 import { parseDeclaration } from "./component-file.mjs"
+import { findAllImplementations, getInterfaces, getEnums, realPackagesName } from "./manifest.mjs"
 
 const packages = ["main", "fiori"];
-const manifests = {};
 
-await Promise.all(packages.map(async packageName => {
-    manifests[packageName] = JSON.parse(await fs.readFile(path.resolve(`./../${packageName}/dist/custom-elements-internal.json`), { encoding: "utf-8" }))
-}));
-
-
-const findAllImplementations = (interfaceName) => {
-    let declarationNames = [];
-
-    for (let packageName of packages) {
-        for (let _module of manifests[packageName].modules) {
-            for (let _declaration of _module.declarations) {
-                if (_declaration?._ui5implements?.some(reference => reference.name === interfaceName)) {
-                    let fullPackageName = packageName === "main" ? "@ui5/webcomponents" : "@ui5/webcomponents-fiori";
-                    declarationNames.push(`${fullPackageName}/${_declaration.name}`)
-                }
-            }
-        }
-    }
-
-    return declarationNames;
-}
-
-const generateTypes = async () => {
-    await Promise.all(packages.map(async packageName => {
-        manifests[packageName] = JSON.parse(await fs.readFile(path.resolve(`./../${packageName}/dist/custom-elements-internal.json`), { encoding: "utf-8" }))
-    }));
-
+const generateTypes = () => {
     packages.forEach(async (packageName) => {
         await fs.mkdir(`./docs/components/${packageName}/enums`, { recursive: true })
         await fs.mkdir(`./docs/components/${packageName}/interfaces`, { recursive: true })
@@ -46,19 +20,21 @@ import DocCardList from '@theme/DocCardList';
 <DocCardList />`)
         }))
 
-        manifests[packageName].modules.forEach(_module => {
-            _module.declarations.forEach(async (declaration) => {
-                if (declaration.kind === "enum") {
-                    await fs.writeFile(path.join(`./docs/components/${packageName}/enums`, `${declaration.name}.mdx`), parseDeclaration(declaration, packageName))
-                    await fs.writeFile(path.join(`./docs/components/${packageName}/enums`, `_${declaration.name}Declaration.json`), JSON.stringify(declaration))
-                } else if (declaration.kind === "interface") {
-                    declaration._implementations = findAllImplementations(declaration.name);
+        const intefaces = getInterfaces(realPackagesName(packageName));
+        const enums = getEnums(realPackagesName(packageName));
 
-                    await fs.writeFile(path.join(`./docs/components/${packageName}/interfaces`, `${declaration.name}.mdx`), parseDeclaration(declaration, packageName))
-                    await fs.writeFile(path.join(`./docs/components/${packageName}/interfaces`, `_${declaration.name}Declaration.json`), JSON.stringify(declaration))
-                }
-            });
-        });
+
+        intefaces.forEach(async (declaration) => {
+            declaration._implementations = findAllImplementations(declaration);
+
+            await fs.writeFile(path.join(`./docs/components/${packageName}/interfaces`, `${declaration.name}.mdx`), parseDeclaration(declaration, packageName))
+            await fs.writeFile(path.join(`./docs/components/${packageName}/interfaces`, `_${declaration.name}Declaration.json`), JSON.stringify(declaration))
+        })
+
+        enums.forEach(async (declaration) => {
+            await fs.writeFile(path.join(`./docs/components/${packageName}/enums`, `${declaration.name}.mdx`), parseDeclaration(declaration, packageName))
+            await fs.writeFile(path.join(`./docs/components/${packageName}/enums`, `_${declaration.name}Declaration.json`), JSON.stringify(declaration))
+        })
     })
 }
 
