d7bb8ac67d07228e4c72811bb8a5ea93684b30fd
ilhan orhan <ilhan.orhan007@gmail.com>
2024-03-25 14:00:41 +0200

    refactor: remove all Assets-static.js modules (#8526)
    Remove all Assets-static.js modules in favour of their dynamic counterparts - the Assets.js modules.
    
    BREAKING CHANGE: All Assets-static.js modules are removed. If you previously imported any Assets-static.js module from any package:
    ```ts
    import "@ui5/webcomponents/dist/Assets-static.js";
    import "@ui5/webcomponents-icons/dist/Assets-static.js"
    ```
    use the dynamic equivalent of it:
    ```ts
    import "@ui5/webcomponents/dist/Assets.js";
    import "@ui5/webcomponents-icons/dist/Assets.js"
    ```
    
    Related to: https://github.com/SAP/ui5-webcomponents/issues/8461

--- packages/tools/lib/generate-json-imports/themes.js
+++ packages/tools/lib/generate-json-imports/themes.js
@@ -64,7 +64,7 @@ ${dynamicImportLines}
 const loadAndCheck = async (themeName) => {
 	const data = await loadThemeProperties(themeName);
 	if (typeof data === "string" && data.endsWith(".json")) {
-		throw new Error(\`[themes] Invalid bundling detected - dynamic JSON imports bundled as URLs. Switch to inlining JSON files from the build or use 'import ".../Assets-static.js"'. Check the \"Assets\" documentation for more information.\`);
+		throw new Error(\`[themes] Invalid bundling detected - dynamic JSON imports bundled as URLs. Switch to inlining JSON files from the build. Check the \"Assets\" documentation for more information.\`);
 	}
 	return data;
 };
