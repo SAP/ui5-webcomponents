ef7539696cb6732c608a9176f1875e7f1c9fbe47
Teodor Taushanov <teodor.taushanov@sap.com>
2024-03-27 14:30:19 +0200

    docs(ui5-side-navigation): improve docs and samples

--- packages/website/docs/_samples/fiori/SideNavigation/Basic/main.js
+++ packages/website/docs/_samples/fiori/SideNavigation/Basic/main.js
@@ -2,6 +2,7 @@ import "@ui5/webcomponents-fiori/dist/SideNavigation.js";
 import "@ui5/webcomponents-fiori/dist/SideNavigationItem.js";
 import "@ui5/webcomponents-fiori/dist/SideNavigationSubItem.js";
 
+import "@ui5/webcomponents-icons/dist/home.js";
 import "@ui5/webcomponents-icons/dist/chain-link.js";
 import "@ui5/webcomponents-icons/dist/group.js";
 import "@ui5/webcomponents-icons/dist/locate-me.js";
