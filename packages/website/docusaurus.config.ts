7298abd9979ba108bcc9bff02d716a7377d66d94
Peter Skelin <petar.skelin@sap.com>
2024-04-11 07:25:32 +0300

    docs: change to trailing slash urls to skip redirects (#8731)

--- packages/website/docusaurus.config.ts
+++ packages/website/docusaurus.config.ts
@@ -39,6 +39,8 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: BASE_URL,
+  // github pages makes a redirect to a trailing slash url, which prevents pages from being crawled by google
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
