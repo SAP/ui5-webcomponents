5c68f6ba25cc53a304d104605ae1cadbc4ba63ed
Vladislav Tasev <vladislav.tasev@sap.com>
2024-04-12 12:52:29 +0300

    chore: improve stability

--- packages/base/src/features/patchPopup.ts
+++ packages/base/src/features/patchPopup.ts
@@ -1,14 +1,17 @@
+// Mockup of OpenUI5's Element.js
+type Element = {
+	getDomRef: () => HTMLElement | null,
+	addDelegate: (delegate: DelegatesMap) => void,
+	removeDelegate: (delegate: DelegatesMap) => void,
+}
+
 // The lifecycle of Popup.js is open -> _opened -> close -> _closed, we're interested in the first (open) and last (_closed)
 type OpenUI5Popup = {
 	prototype: {
 		open: (...args: any[]) => void,
 		_closed: (...args: any[]) => void,
 		getOpenState: () => "CLOSED" | "CLOSING" | "OPEN" | "OPENING",
-		oContent: {
-			getDomRef: () => HTMLElement,
-			addDelegate: (delegate: DelegatesMap) => void,
-			removeDelegate: (delegate: DelegatesMap) => void,
-		},
+		getContent: () => Element, // this is the OpenUI5 Element/Control instance that opens the Popup (usually sap.m.Popover/sap.m.Dialog)
 	}
 };
 
@@ -17,30 +20,30 @@ const delegatesRegistry = new Map<HTMLElement, DelegatesMap>();
 
 /**
  * Ensures a unique delegate for each element, because removeDelegate expects the same object pased to addDelegate
- * @param el HTML element to use the popover API
+ * @param domRef HTML element to use the popover API
  */
-const getDelegate = (el: HTMLElement) => {
-	if (!delegatesRegistry.has(el)) {
+const getDelegate = (domRef: HTMLElement) => {
+	if (!delegatesRegistry.has(domRef)) {
 		const delegate = {
 			"onAfterRendering": () => {
-				openNativePopover(el);
+				openNativePopover(domRef);
 			},
 		};
-		delegatesRegistry.set(el, delegate);
+		delegatesRegistry.set(domRef, delegate);
 	}
 
-	return delegatesRegistry.get(el)!;
+	return delegatesRegistry.get(domRef)!;
 };
 
-const openNativePopover = (el: HTMLElement) => {
-	el.setAttribute("popover", "manual");
-	el.showPopover();
+const openNativePopover = (domRef: HTMLElement) => {
+	domRef.setAttribute("popover", "manual");
+	domRef.showPopover();
 };
 
-const closeNativePopover = (el: HTMLElement) => {
-	if (el.hasAttribute("popover")) {
-		el.hidePopover();
-		el.removeAttribute("popover");
+const closeNativePopover = (domRef: HTMLElement) => {
+	if (domRef.hasAttribute("popover")) {
+		domRef.hidePopover();
+		domRef.removeAttribute("popover");
 	}
 };
 
@@ -52,23 +55,29 @@ const patchPopup = (Popup: OpenUI5Popup) => {
 		const topLayerAlreadyInUse = !!document.body.querySelector(":popover-open"); // check if there is already something in the top layer
 		const openingInitiated = ["OPENING", "OPEN"].includes(this.getOpenState());
 		if (openingInitiated && topLayerAlreadyInUse) {
-			const el = this.oContent.getDomRef();
-			openNativePopover(el);
-			this.oContent.addDelegate(getDelegate(el)); // add onAfterRendering delegate to restore the "popover" attribute that will be removed by RenderManager on re-rendering
+			const element = this.getContent();
+			if (element) {
+				const domRef = element.getDomRef();
+				if (domRef) {
+					openNativePopover(domRef);
+					element.addDelegate(getDelegate(domRef)); // add onAfterRendering delegate to restore the "popover" attribute that will be removed by RenderManager on re-rendering
+				}
+			}
 		}
 	};
 
 	// 2. Patch _closed (hide the popover after all animations have ended)
 	const _origClosed = Popup.prototype._closed;
 	Popup.prototype._closed = function _closed(...args: any[]) {
-		const el = this.oContent.getDomRef();
-		if (delegatesRegistry.has(el)) {
-			this.oContent.removeDelegate(getDelegate(el)); // remove the onAfterRendering delegate before calling _close to avoid issues
-			delegatesRegistry.delete(el);
+		const element = this.getContent();
+		const domRef = element.getDomRef();
+		if (domRef && delegatesRegistry.has(domRef)) {
+			element.removeDelegate(getDelegate(domRef)); // remove the onAfterRendering delegate before calling _close to avoid issues
+			delegatesRegistry.delete(domRef);
 		}
 		_origClosed.apply(this, args); // only then call _close
-		if (el) {
-			closeNativePopover(el); // unset the popover attribute and close the native popover, but only if still in DOM
+		if (domRef) {
+			closeNativePopover(domRef); // unset the popover attribute and close the native popover, but only if still in DOM
 		}
 	};
 
