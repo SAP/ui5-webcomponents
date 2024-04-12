63a902935961e7f384b6d7aafa8713d960f0d4f4
Duc Vo Ngoc <duc.vo.ngoc@sap.com>
2024-04-12 15:47:37 +0200

    fix(ui5-grid): add missing import in examples

--- packages/website/docs/_samples/main/Grid/ScrollToLoad/main.js
+++ packages/website/docs/_samples/main/Grid/ScrollToLoad/main.js
@@ -1,4 +1,5 @@
 import "@ui5/webcomponents/dist/Grid.js";
+import "@ui5/webcomponents/dist/GridGrowing.js";
 import "@ui5/webcomponents/dist/GridHeaderRow.js";
 import "@ui5/webcomponents/dist/GridHeaderCell.js";
 import "@ui5/webcomponents/dist/Label.js";
