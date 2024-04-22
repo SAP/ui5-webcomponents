d9d2a4b43104bc18d08e272fa2884c78443a424f
Vladislav Tasev <vladislav.tasev@sap.com>
2024-04-22 14:10:34 +0300

    chore: last fixes

--- packages/main/src/MultiInput.ts
+++ packages/main/src/MultiInput.ts
@@ -64,12 +64,14 @@ type MultiInputTokenDeleteEventDetail = {
 	renderer: litRender,
 	template: MultiInputTemplate,
 	styles: [Input.styles, styles],
-	dependencies: [
-		...Input.dependencies,
-		Tokenizer,
-		Token,
-		Icon,
-	],
+	get dependencies() {
+		return [
+			...Input.dependencies,
+			Tokenizer,
+			Token,
+			Icon,
+		];
+	},
 })
 /**
  * Fired when the value help icon is pressed
