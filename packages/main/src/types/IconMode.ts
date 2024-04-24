fa5a488c10bf323c95184e5336175db4109b5a01
yanaminkova <yana.minkova@sap.com>
2024-04-24 09:51:40 +0300

    refactor(ui5-icon): add mode property

---
+++ packages/main/src/types/IconMode.ts
@@ -0,0 +1,25 @@
+/**
+ * Different Icon modes.
+ * @public
+ */
+enum IconMode {
+	/**
+	 * Image mode (by default)
+	 * @public
+	 */
+	Image = "Image",
+
+	/**
+	 * Decorative mode
+	 * @public
+	 */
+	Decorative = "Decorative",
+
+	/**
+	 * Interactive mode
+	 * @public
+	*/
+	Interactive = "Interactive",
+}
+
+export default IconMode;
