9048a99cbdc6fcbd9ffb03285d0b3125f274cf02
ilhan007 <ilhan.myumyun@sap.com>
2024-04-24 16:11:59 +0300

    refactor: move Table to ui5/webcomponents-compat package

---
+++ packages/compat/package-scripts.cjs
@@ -0,0 +1,14 @@
+const getScripts = require("@ui5/webcomponents-tools/components-package/nps.js");
+
+const options = {
+	port: 8080,
+	portStep: 2,
+	noWatchTS: true,
+	dev: true,
+};
+
+const scripts = getScripts(options);
+
+module.exports = {
+	scripts,
+};
