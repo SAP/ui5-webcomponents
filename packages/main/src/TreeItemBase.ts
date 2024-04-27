56061e7ee0c3428b5b879cd8611d1ff8d4185ae0
ilhan007 <ilhan.myumyun@sap.com>
2024-04-27 19:48:10 +0300

    chore: update jsdoc

--- packages/main/src/TreeItemBase.ts
+++ packages/main/src/TreeItemBase.ts
@@ -151,7 +151,7 @@ class TreeItemBase extends ListItem {
 	/**
 	 * Defines the state of the `additionalText`.
 	 *
-	 * Available options are: `"None"` (by default), `"Positive"`, `"Critical"`, `"Information"` and `"Critical"`.
+	 * Available options are: `"None"` (by default), `"Positive"`, `"Critical"`, `"Information"` and `"Negative"`.
 	 * @default "None"
 	 * @public
 	 * @since 1.0.0-rc.15
