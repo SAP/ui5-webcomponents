8235159460d446d0f4df095868f81d86857ec510
yanaminkova <32466553+yanaminkova@users.noreply.github.com>
2024-04-30 08:45:21 +0300

    refactor(ui5-li, ui5-list): changes accessibleRole type (#8825)
    Changes the type of `accessibleRole` property  for both `ui5-li` and `ui5-list`.
    
    BREAKING CHANGE: The `accessibleRole` property for both `ui5-li` and `ui5-list` has been updated from a string type to an enum type. 
    Additionally, the new enums `ListItemAccessibleRole` and `ListAccessibleRole` have been introduced for these properties respectively.
    The available options for the `ui5-li`:
    	`ListItem`- Represents the ARIA role "listitem". (by default)
    	`MenuItem`  -  Represents the ARIA role "menuitem".
    	`TreeItem ` -  Represents the ARIA role "treeitem".
    	`Option ` -  Represents the ARIA role "option".
    	`None` - Represents the ARIA role "none".
    
    The available options for the `ui5-list`:
    	`List`- Represents the ARIA role "list".  (by default)
    	`Menu`  -  Represents the ARIA role "menu".
    	`Tree` -  Represents the ARIA role "tree".
    	`ListBox` - Represents the ARIA role "listbox".
    If you have previously used:
    ```html
    <ui5-li accessible-role="menuitem"> List Item</ui5-li>
    <ui5-list accessible-role="tree"> List </ui5-list>
    ```
    Now use:
    ```html
    <ui5-li accessible-role="MenuItem"> List Item</ui5-li>
    <ui5-list accessible-role="Tree"> List </ui5-list>
    ```
    
    Related to https://github.com/SAP/ui5-webcomponents/issues/8461, https://github.com/SAP/ui5-webcomponents/issues/7887

---
+++ packages/main/src/types/ListItemAccessibleRole.ts
@@ -0,0 +1,40 @@
+/**
+ * ListItem accessible roles.
+ * @public
+ * @since 2.0.0
+ */
+enum ListItemAccessibleRole {
+
+	/**
+	 * Represents the ARIA role "listitem". (by default)
+	 * @public
+	 */
+	ListItem = "ListItem",
+
+	/**
+	 * Represents the ARIA role "menuitem".
+	 * @public
+	 */
+	MenuItem = "MenuItem",
+
+	/**
+	 * Represents the ARIA role "treeitem".
+	 * @public
+	 */
+	TreeItem = "TreeItem",
+
+	/**
+	 * Represents the ARIA role "option".
+	 * @public
+	 */
+	Option = "Option",
+
+	/**
+	 * Represents the ARIA role "none".
+	 * @public
+	 */
+	None = "None"
+
+}
+
+export default ListItemAccessibleRole;
