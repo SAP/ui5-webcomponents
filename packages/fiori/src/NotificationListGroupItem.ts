59415222d433a4519c53cfb18d3bc26e53886287
Teodor Taushanov <teodor.taushanov@sap.com>
2024-05-20 13:57:35 +0300

    chore: disable lint error

--- packages/fiori/src/NotificationListGroupItem.ts
+++ packages/fiori/src/NotificationListGroupItem.ts
@@ -170,6 +170,7 @@ class NotificationListGroupItem extends NotificationListItemBase {
 	}
 
 	get groupCollapsedTooltip() {
+		// eslint-disable-next-line
 		// ToDo: edit and add translation when spec is ready
 		return this.collapsed ? "expand arrow" : "collapse arrow";
 	}
