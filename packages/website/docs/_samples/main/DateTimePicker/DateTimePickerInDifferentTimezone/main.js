95343ed09fac929f3eb1e52424ce84a157ae69be
tsanislavgatev <tseko95@gmail.com>
2024-05-21 14:50:37 +0300

    docs(ui5-datetime-picker): add timezones sample

--- packages/website/docs/_samples/main/DateTimePicker/DateTimePickerInDifferentTimezone/main.js
+++ packages/website/docs/_samples/main/DateTimePicker/DateTimePickerInDifferentTimezone/main.js
@@ -1,1 +1,11 @@
 import "@ui5/webcomponents/dist/DateTimePicker.js";
+import "@ui5/webcomponents/dist/Select.js";
+import "@ui5/webcomponents/dist/Option.js";
+import {setTimezone} from "@ui5/webcomponents-base/dist/config/Timezone.js";
+
+
+sel.addEventListener("change", async (e) => {
+    setTimezone(e.detail.selectedOption.getAttribute("data-timezone"));
+
+    dtp.value = "today";
+});
