5efa1e20271c56b04e88dd3223e2b359785b9eb2
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-22 19:14:45 +0300

    final

--- vite.config.js
+++ vite.config.js
@@ -4,124 +4,66 @@ const customHotUpdate = require("@ui5/webcomponents-tools/lib/dev-server/custom-
 const ssrDomShimLoader = require("@ui5/webcomponents-tools/lib/dev-server/ssr-dom-shim-loader.js");
 const { existsSync } = require('fs');
 const { dirname, join, resolve } = require('path');
-const path = require('path');
 const tsconfigPaths = require('vite-tsconfig-paths').default;
-const {checker} = require('vite-plugin-checker');
+const { checker } = require('vite-plugin-checker');
 
-// use after path.join and path.resolve as they turn paths to windows separators and comparisons and replacements stop working
-const toPosixPath = (pathStr) => {
-	return pathStr.split(path.sep).join(path.posix.sep);
-}
+const toPosixPath = (pathStr) => pathStr.split(path.sep).join(path.posix.sep);
 
-const customResolver = (id, source, options) => {
-	const isIconImporter = source.includes("packages/icons") || source.includes("packages/icons-tnt/") || source.includes("packages/icons-business-suite/")
-	if (isIconImporter && id.startsWith("@ui5/webcomponents-base/dist")) {
-		const importerRoot = source.replace(/packages\/icons.*/, "packages");
-		const resolved = join(importerRoot, "base/src", id.replace("@ui5/webcomponents-base/dist/", "")).replace(".js", ".ts");
-		return resolved;
-	}
+const customResolver = (id, source) => {
+    const isIconImporter = source.includes("packages/icons") ||
+        source.includes("packages/icons-tnt/") ||
+        source.includes("packages/icons-business-suite/");
 
-	if (isIconImporter && id.startsWith("../generated")) {
-		let absoluteId = join(dirname(source), id);
-		// join returns paths with \\ on windows, so the replaces won't work unless converted to posix paths /
-		absoluteId = toPosixPath(absoluteId);
-		const resolved = absoluteId.replace("/dist/", "/src/").replace(/\.js$/, ".ts");
-		return resolved;
-	}
+    const resolvePath = (base, replacement) => join(base, replacement).replace(".js", ".ts");
 
-	// json files are always in dist, this saves a separate copy task
-	if (id.endsWith(".json")) {
-		let absoluteId = join(dirname(source), id);
-		// join returns paths with \\ on windows, so the replaces won't work unless converted to posix paths /
-		absoluteId = toPosixPath(absoluteId);
-		const resolved = absoluteId.replace("/src/", "/dist/");
-		return resolved;
-	}
+    if (isIconImporter) {
+        if (id.startsWith("@ui5/webcomponents-base/dist")) {
+            const importerRoot = source.replace(/packages\/icons.*/, "packages");
+            return resolvePath(importerRoot, `base/src/${id.replace("@ui5/webcomponents-base/dist/", "")}`);
+        }
 
-	if (id.startsWith("./") || id.startsWith("../")) {
-		//   `/sap/base/` and `sap/ui/core/` files imported from `src` are actually in dist
-		//   except 4 files with are ts files in src and could be imported from `dist`
-		let absoluteId = resolve(dirname(source), id);
-		// resolve returns paths with \\ on windows, so the replaces won't work unless converted to posix paths /
-		absoluteId = toPosixPath(absoluteId);
-		if (absoluteId.includes("/sap/base/") || absoluteId.includes("/sap/ui/core/")) {
-			const virtSource = source.replace(/packages\/(\w+)\/src\//, "packages/$1/dist/");
-			let resolved = join(dirname(virtSource), id);
-			// join returns paths with \\ on windows, so the replaces won't work unless converted to posix paths /
-			resolved = toPosixPath(resolved);
-			if (resolved.endsWith("sap/ui/core/Core.js") && resolved.includes("/dist/")) {
-				resolved = resolved.replace("/dist/", "/src/").replace(".js", ".ts");
-			}
-			if (resolved.endsWith("localization/dist/sap/base/i18n/Localization.js")) {
-				resolved = resolved.replace("/dist/", "/src/").replace(".js", ".ts");
-			}
-			if (resolved.endsWith("dist/sap/ui/core/Configuration.js")) {
-				resolved = resolved.replace("/dist/", "/src/").replace(".js", ".ts");
-			}
-			if (resolved.endsWith("dist/sap/ui/core/FormatSettings.js")) {
-				resolved = resolved.replace("/dist/", "/src/").replace(".js", ".ts");
-			}
-			if (resolved.endsWith("dist/sap/base/util/LoaderExtensions.js")) {
-				resolved = resolved.replace("/dist/", "/src/").replace(".js", ".ts");
-			}
-			if (resolved.endsWith("dist/sap/base/util/ObjectPath.js")) {
-				resolved = resolved.replace("/dist/", "/src/").replace(".js", ".ts");
-			}
-			return resolved;
-		}
-	}
+        if (id.startsWith("../generated")) {
+            let absoluteId = join(dirname(source), id);
+            absoluteId = toPosixPath(absoluteId);
+            return absoluteId.replace("/dist/", "/src/").replace(/\.js$/, ".ts");
+        }
+    }
 
-	// relative imports from fiori src that are to a folder starting with `illustrations` are in dist
-	if (source.includes("fiori/src/") && id.includes("/illustrations") && !id.includes("AllIllustrations") && id.startsWith(".")) {
-		let absoluteId = resolve(dirname(source), id);
-		// join returns paths with \\ on windows, so the replaces won't work unless converted to posix paths /
-		absoluteId = toPosixPath(absoluteId);
-		const resolved = absoluteId.replace("/src/", "/dist/");
-		return resolved;
-	}
+    if (id.endsWith(".json")) {
+        let absoluteId = join(dirname(source), id);
+        absoluteId = toPosixPath(absoluteId);
+        return absoluteId.replace("/src/", "/dist/");
+    }
 
-	// generated illustrations search for i18n texts which are in `src/generated`
-	if (source.includes("fiori/dist/illustrations") && id.startsWith("../generated")) {
-		let absoluteId = join(dirname(source), id);
-		// join returns paths with \\ on windows, so the replaces won't work unless converted to posix paths /
-		absoluteId = toPosixPath(absoluteId);
-		const resolved = absoluteId.replace("/dist/", "/src/").replace(/\.js$/, ".ts");
-		return resolved;
-	}
+    if (id.startsWith("./") || id.startsWith("../")) {
+        let absoluteId = resolve(dirname(source), id);
+        absoluteId = toPosixPath(absoluteId);
 
-	if (source.includes("fiori/dist/illustrations") && id.startsWith("@ui5/webcomponents-base/dist")) {
-		const importerRoot = source.replace(/packages\/fiori.*/, "packages");
-		const resolved = join(importerRoot, "base/src", id.replace("@ui5/webcomponents-base/dist/", "")).replace(".js", ".ts");
-		return resolved;
-	}
-}
+        if (absoluteId.includes("/sap/base/") || absoluteId.includes("/sap/ui/core/")) {
+            const virtSource = source.replace(/packages\/(\w+)\/src\//, "packages/$1/dist/");
+            let resolved = join(dirname(virtSource), id);
+            resolved = toPosixPath(resolved);
 
-module.exports = defineConfig(async () => {
-	return {
-		build: {
-			emptyOutDir: false,
-		},
-		plugins: [await virtualIndex(), tsconfigPaths(), customHotUpdate(), ssrDomShimLoader(),
-			checker({
-				// e.g. use TypeScript check
-				typescript: {
-					tsconfigPath: "packages/fiori/tsconfig.json",
-					buildMode: true,
-				},
-		  	}),
-		],
+            const specialCases = [
+                "sap/ui/core/Core.js",
+                "localization/dist/sap/base/i18n/Localization.js",
+                "dist/sap/ui/core/Configuration.js",
+                "dist/sap/ui/core/FormatSettings.js",
+                "dist/sap/base/util/LoaderExtensions.js",
+                "dist/sap/base/util/ObjectPath.js"
+            ];
 
-		resolve: {
-			alias: [
-				// { find: /\@ui5\/webcomponents-base\/dist\/(.*)/, replacement: "../base/src/$1" },
-				// { find: /\@ui5\/webcomponents-icons\/dist\/(.*)/, replacement: "../icons/src/$1" },
-				// { find: /\@ui5\/webcomponents-icons-tnt\/dist\/(.*)/, replacement: "../icons-tnt/src/$1" },
-				// { find: /\@ui5\/webcomponents-icons-business-suite\/dist\/(.*)/, replacement: "../icons-business-suite/src/$1" },
-				{ find: /(\@ui5)(.*)/, replacement: "$1$2", customResolver },
-				{ find: /(..\/generated)(.*)/, replacement: "$1$2", customResolver },
-				// ../sap files are in dist, not src
-				{ find: /^(\..*)/, replacement: "$1", customResolver },
-			],
-		}
-	}
-});
+            for (const specialCase of specialCases) {
+                if (resolved.endsWith(specialCase)) {
+                    return resolved.replace("/dist/", "/src/").replace(".js", ".ts");
+                }
+            }
+
+            return resolved;
+        }
+    }
+
+    if (source.includes("fiori/src/") && id.includes("/illustrations") && !id.includes("AllIllustrations") && id.startsWith(".")) {
+        let absoluteId = resolve(dirname(source), id);
+        absoluteId = toPosixPath(absoluteId);
+        return absoluteId.replace("/src/", "/dist
