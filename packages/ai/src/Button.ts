da0d3eb8856280f485bc539e01b4537e5965b42b
ilhan orhan <ilhan.orhan007@gmail.com>
2024-05-23 17:17:33 +0300

    chore: enable test assets for `ai` test pages (#9056)

--- packages/ai/src/Button.ts
+++ packages/ai/src/Button.ts
@@ -1,5 +1,17 @@
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
+import property from "@ui5/webcomponents-base/dist/decorators/property.js";
+import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+
+// Deps
+import MainButton from "@ui5/webcomponents/dist/Button.js";
+
+// Template
+import template from "./generated/templates/ButtonTemplate.lit.js";
+
+// Styles
+import styles from "./generated/themes/Button.css.js";
+
 /**
  * @class
  * @constructor
@@ -7,8 +19,17 @@ import customElement from "@ui5/webcomponents-base/dist/decorators/customElement
  */
 @customElement({
 	tag: "ui5-ai-button",
+	renderer: litRender,
+	template,
+	styles,
+	dependencies: [MainButton],
 })
 class Button extends UI5Element {
+	@property()
+	icon!: string;
+
+	@property()
+	endIcon!: string;
 }
 
 Button.define();
