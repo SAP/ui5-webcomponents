09c8701050078842ffd2e4841d7f263e8176b8d7
ilhan007 <ilhan.myumyun@sap.com>
2024-05-23 16:09:20 +0300

    chore: enable test assets for `ai` test pages

--- packages/ai/src/Assets.ts
+++ packages/ai/src/Assets.ts
@@ -1,6 +1,6 @@
 // main package assets (transitively base, theming and icons)
 import "@ui5/webcomponents/dist/Assets.js";
 
-// own compat package assets
+// own ai package assets
 import "./generated/json-imports/Themes.js";
 import "./generated/json-imports/i18n.js";
