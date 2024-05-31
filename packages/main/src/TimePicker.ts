60a25131451666df360b479c5cd4bd34c335cbde
hinzzx <abvsuxhard@gmail.com>
2024-05-31 14:27:23 +0300

    fix: add since tag

--- packages/main/src/TimePicker.ts
+++ packages/main/src/TimePicker.ts
@@ -189,11 +189,13 @@ type TimePickerInputEventDetail = TimePickerChangeInputEventDetail;
 })
 /**
  * Fired after the value-help dialog of the component is opened.
+ * @since 2.0.0
  * @public
  */
 @event("open")
 /**
  * Fired after the value-help dialog of the component is closed.
+ * @since 2.0.0
  * @public
  */
 @event("close")
