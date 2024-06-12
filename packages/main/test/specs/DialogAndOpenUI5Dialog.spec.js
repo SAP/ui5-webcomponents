239dedba26c7cdbc521b44d57cf05cf24749ee2a
Teodor Taushanov <teodor.taushanov@sap.com>
2024-06-12 14:48:19 +0300

    chore: fix test

--- packages/main/test/specs/DialogAndOpenUI5Dialog.spec.js
+++ packages/main/test/specs/DialogAndOpenUI5Dialog.spec.js
@@ -6,6 +6,11 @@ describe("Dialog general interaction", () => {
 	});
 
 	it("opening and focusing", async () => {
+		await browser.$("#openUI5Button").waitForExist({
+			timeout: 3000,
+			timeoutMsg: "Busy area must be created after 3000ms"
+		});
+
 		await browser.$("#myButton").click();
 		await browser.$("#dialogButton").click();
 		await browser.$("#openUI5DialogButton").click();
