be77732ca1cc4acfaed90c719b54c1b30d08ab81
Teodor Taushanov <teodor.taushanov@sap.com>
2024-06-13 13:37:23 +0300

    fix(ui5-dialog): fix focusing when dialog is open from OpenUI5 dialog

---
+++ packages/base/src/features/patchPopup.ts
@@ -0,0 +1,27 @@
+// OpenUI5's Element.js subset
+
+type OpenUI5Popup = {
+	setInitialZIndex: (zIndex: number) => void,
+	getNextZIndex: () => number,
+	prototype: {
+		onFocusEvent: (e: FocusEvent) => void,
+	}
+};
+
+const patchFocusEvent = (Popup: OpenUI5Popup) => {
+	const origFocusEvent = Popup.prototype.onFocusEvent;
+	Popup.prototype.onFocusEvent = function onFocusEvent(e: FocusEvent) {
+		const isTypeFocus = e.type === "focus" || e.type === "activate";
+		const target = e.target as HTMLElement;
+		if (!isTypeFocus || !target.closest("[ui5-popover],[ui5-dialog]")) {
+			origFocusEvent.call(this, e);
+		}
+	};
+};
+
+const patchPopup = (Popup: OpenUI5Popup) => {
+	patchFocusEvent(Popup);// Popup.prototype.onFocusEvent
+};
+
+export default patchPopup;
+export type { OpenUI5Popup };
