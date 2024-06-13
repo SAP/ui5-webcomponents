ea55e41ac335bb58f5f7ad13bf400d3c1231e3f9
Duc Vo Ngoc <duc.vo.ngoc@sap.com>
2024-06-13 12:06:13 +0200

    feat(ui5-table): add range selection to selection
    In "Multiple" selection mode, it is now possible to select ranges with a) mouse or b) keyboard.
    
    a) Select a row via checkbox and Shift + Click on another checkbox to select the range.
    b) Select a row and Shift + Up/Down to extend your range selection.

--- packages/main/src/TableSelection.ts
+++ packages/main/src/TableSelection.ts
@@ -1,3 +1,9 @@
+import {
+	isUpShift,
+	isShift,
+	isSpace,
+} from "@ui5/webcomponents-base/dist/Keys.js";
+import getActiveElement from "@ui5/webcomponents-base/dist/util/getActiveElement.js";
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
 import property from "@ui5/webcomponents-base/dist/decorators/property.js";
@@ -5,7 +11,8 @@ import event from "@ui5/webcomponents-base/dist/decorators/event.js";
 import TableSelectionMode from "./types/TableSelectionMode.js";
 import type Table from "./Table.js";
 import type { ITableFeature } from "./Table.js";
-import type TableRow from "./TableRow.js";
+import TableRow from "./TableRow.js";
+import TableHeaderRow from "./TableHeaderRow.js";
 import type TableRowBase from "./TableRowBase.js";
 
 /**
@@ -72,6 +79,7 @@ class TableSelection extends UI5Element implements ITableFeature {
 	selected!: string;
 
 	_table?: Table;
+	_rangeSelection?: {state: boolean, isUp: boolean | null, rows: TableRow[], isMouse: boolean} | null;
 
 	onTableActivate(table: Table) {
 		this._table = table;
@@ -165,16 +173,20 @@ class TableSelection extends UI5Element implements ITableFeature {
 		this.selectedAsArray = [...selectedSet];
 	}
 
-	_informRowSelectionChange(row: TableRow) {
-		const isRowSelected = this.isMultiSelect() ? this.isSelected(row) : true;
+	_selectRow(row: TableRow, state: boolean) {
 		const rowIdentifier = this.getRowIdentifier(row);
-		if (this.selected && this.mode === TableSelectionMode.Multiple) {
+		if (this.mode === TableSelectionMode.Multiple) {
 			const selectedSet = this.selectedAsSet;
-			selectedSet[isRowSelected ? "delete" : "add"](rowIdentifier);
+			selectedSet[state ? "add" : "delete"](rowIdentifier);
 			this.selectedAsSet = selectedSet;
 		} else {
-			this.selected = rowIdentifier;
+			this.selected = state ? rowIdentifier : "";
 		}
+	}
+
+	_informRowSelectionChange(row: TableRow) {
+		const isRowSelected = this.isMultiSelect() ? !this.isSelected(row) : true;
+		this._selectRow(row, isRowSelected);
 		this.fireEvent("change");
 	}
 
@@ -203,6 +215,180 @@ class TableSelection extends UI5Element implements ITableFeature {
 		this._table._invalidate++;
 		this._table.headerRow[0]._invalidate++;
 		this._table.rows.forEach(row => row._invalidate++);
+	}
+
+	_onkeydown(e: KeyboardEvent) {
+		if (!this.isMultiSelect() || !this._table || !e.shiftKey) {
+			return;
+		}
+
+		const focusedElement = getActiveElement(); // Assumption: The focused element is always the "next" row after navigation.
+
+		if (focusedElement === null) {
+			this._stopRangeSelection();
+			return;
+		}
+
+		if (!(focusedElement instanceof TableRow) && !this._rangeSelection?.isMouse) {
+			this._stopRangeSelection();
+			return;
+		}
+
+		if (!this._rangeSelection) {
+			// If no range selection is active, start one
+			this._startRangeSelection(focusedElement as TableRow);
+		} else if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+			const change = isUpShift(e) ? -1 : 1;
+			this._handleRangeSelection(focusedElement as TableRow, change);
+		}
+	}
+
+	_onkeyup(e: KeyboardEvent, eventTarget: HTMLElement) {
+		if (!this._table) {
+			return;
+		}
+
+		if (isSpace(e)) {
+			// Handle selection when SPACE pressed
+			if (this._isHeaderSelector(e)) {
+				this._informHeaderRowSelectionChange();
+			} else if (this._isSelectionCheckbox(e)) {
+				const row = this._findRowInPath(e.composedPath());
+				this._informRowSelectionChange(row);
+			}
+			return;
+		}
+
+		if (!(eventTarget instanceof TableRow) || !this._rangeSelection || isShift(e)) {
+			// Stop range selection if a) Shift is relased or b) the event target is not a row or c) the event is not from the selection checkbox
+			!this._isSelectionCheckbox(e) && this._stopRangeSelection();
+		}
+	}
+
+	_onclick(e: MouseEvent) {
+		if (!this._table) {
+			return;
+		}
+
+		if (this._isHeaderSelector(e)) {
+			this._informHeaderRowSelectionChange();
+			this._stopRangeSelection();
+			return;
+		}
+
+		if (!this._isSelectionCheckbox(e)) {
+			this._stopRangeSelection();
+			return;
+		}
+
+		const row = this._findRowInPath(e.composedPath());
+
+		if (e.shiftKey && this._rangeSelection?.isMouse) {
+			const startRow = this._rangeSelection.rows[0];
+			const startIndex = this._table?.rows.indexOf(startRow);
+			const endIndex = this._table?.rows.indexOf(row);
+
+			if (startIndex === -1 || endIndex === -1 || startIndex === undefined || endIndex === undefined
+				|| row.key === startRow.key || row.key === this._rangeSelection.rows[this._rangeSelection.rows.length - 1].key) {
+				return;
+			}
+
+			const change = endIndex - startIndex;
+			this._handleRangeSelection(row, change);
+		} else if (row) {
+			this.informSelectionChange(row);
+			this._startRangeSelection(row, true);
+		}
+	}
+
+	/**
+	 * Start the range selection and initialises the range selection state
+	 * @param row starting row
+	 * @private
+	 */
+	_startRangeSelection(row: TableRow, isMouse = false) {
+		const state = this.isSelected(row);
+		if (isMouse && !state) {
+			// Do not initiate range selection if the row is not selected
+			return;
+		}
+
+		this._rangeSelection = {
+			state,
+			isUp: null,
+			rows: [row],
+			isMouse,
+		};
+	}
+
+	/**
+	 * Handles the keyboard range selection.
+	 * @param e keyboard event
+	 * @param row focused row
+	 */
+	_handleRangeSelection(targetRow: TableRow, change: number) {
+		if (!this._rangeSelection) {
+			return;
+		}
+
+		const isUp = change > 0;
+		let selectionChanged = false;
+		this._rangeSelection.isUp ??= isUp;
+		if (isUp !== this._rangeSelection.isUp && !this._rangeSelection.isMouse) {
+			// Only reverse selection for keyboard
+			if (this.isSelected(targetRow)) {
+				selectionChanged = true;
+			}
+			// Changing direction "reverse" the selection
+			this._reverseRangeSelection();
+		} else {
+			const endIndex = this._table!.rows.indexOf(targetRow);
+			const startIndex = endIndex - change;
+
+			this._table?.rows.slice(Math.min(startIndex, endIndex), Math.max(startIndex, endIndex) + 1).forEach(row => {
+				if (!this._rangeSelection?.rows.includes(row)) {
+					this._rangeSelection?.rows.push(row);
+				}
+
+				if (this.isSelected(row) !== this._rangeSelection!.state) {
+					selectionChanged = true;
+				}
+
+				// Workaround required due to bug with mouse, where the checkbox stays unchecked, but the row is selected.
+				// Even invalidation does not change the state of the checkbox. Maybe an issue with lit's 'intelligent' rendering?
+				this._rangeSelection?.isMouse && row.shadowRoot?.querySelector("#selection-component")?.setAttribute("checked", "");
+				this._selectRow(row, this._rangeSelection!.state);
+			});
+		}
+
+		selectionChanged && this._fireEvent("change");
+	}
+
+	_stopRangeSelection() {
+		this._rangeSelection = null;
+	}
+
+	_reverseRangeSelection() {
+		const row = this._rangeSelection?.rows.pop();
+		if (row) {
+			this._selectRow(row, false);
+		}
+
+		if (this._rangeSelection?.rows.length === 1) {
+			this._rangeSelection.isUp = null;
+		}
+	}
+
+	_isSelectionCheckbox(e: Event) {
+		return e.composedPath().some((el: EventTarget) => (el as HTMLElement).id === "selection-component");
+	}
+
+	_isHeaderSelector(e: Event) {
+		return this._isSelectionCheckbox(e) && e.composedPath().some((el: EventTarget) => el instanceof TableHeaderRow);
+	}
+
+	_findRowInPath(composedPath: Array<EventTarget>) {
+		return composedPath.find((el: EventTarget) => el instanceof TableRow) as TableRow;
 	}
 }
 
