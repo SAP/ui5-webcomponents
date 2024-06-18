8cf9eefc916b92fa3be641b1a961d4aa52bc70d2
tsanislavgatev <tseko95@gmail.com>
2024-06-18 10:00:29 +0300

    refactor(ui5-special-date): remove calendar date inheritance

--- packages/main/src/SpecialCalendarDate.ts
+++ packages/main/src/SpecialCalendarDate.ts
@@ -1,6 +1,5 @@
 import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
 import property from "@ui5/webcomponents-base/dist/decorators/property.js";
-import CalendarDate from "./CalendarDate.js";
 import CalendarLegendItemType from "./types/CalendarLegendItemType.js";
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 
