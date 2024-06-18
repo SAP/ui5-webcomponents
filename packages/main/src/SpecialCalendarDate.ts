4d272e857ce8f2299e951921a1ac7c81405450fb
tsanislavgatev <tseko95@gmail.com>
2024-06-18 10:00:20 +0300

    refactor(ui5-special-date): remove calendar date inheritance

--- packages/main/src/SpecialCalendarDate.ts
+++ packages/main/src/SpecialCalendarDate.ts
@@ -2,6 +2,7 @@ import customElement from "@ui5/webcomponents-base/dist/decorators/customElement
 import property from "@ui5/webcomponents-base/dist/decorators/property.js";
 import CalendarDate from "./CalendarDate.js";
 import CalendarLegendItemType from "./types/CalendarLegendItemType.js";
+import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 
 /**
  * @class
@@ -11,13 +12,20 @@ import CalendarLegendItemType from "./types/CalendarLegendItemType.js";
  * The `ui5-special-date` component defines a special calendar date to be used inside `ui5-calendar`,
  * which is visually distinguished from the rest of the dates.
  * @constructor
- * @extends CalendarDate
  * @abstract
  * @public
  * @since 1.23.0
  */
 @customElement("ui5-special-date")
-class SpecialCalendarDate extends CalendarDate {
+class SpecialCalendarDate extends UI5Element {
+	/**
+	 * The date formatted according to the `formatPattern` property
+	 * of the `ui5-calendar` that hosts the component.
+	 * @default ""
+	 * @public
+	 */
+	@property()
+	value!: string;
 	/**
 	 * Defines the type of the special date.
 	 * @default "None"
