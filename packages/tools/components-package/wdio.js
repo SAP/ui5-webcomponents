03820aaa17e71fda621b9e9ae6dcf41dafedfc2f
tsanislavgatev <tseko95@gmail.com>
2024-06-19 16:13:19 +0300

    fix(ui5-segmented-button): fix wdio file

--- packages/tools/components-package/wdio.js
+++ packages/tools/components-package/wdio.js
@@ -59,16 +59,16 @@ exports.config = {
 		'goog:chromeOptions': {
 			// to run chrome headless the following flags are required
 			// (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-			// args: [
-			// 	'--headless',
-			// 	'--start-maximized',
-			// 	'--no-sandbox',
-			// 	'--disable-gpu',
-			// 	'--disable-infobars',
-			// 	'--disable-extensions',
-			// 	'--disable-dev-shm-usage',
-			// ],
-			args: ['--disable-gpu'],
+			args: [
+				'--headless',
+				'--start-maximized',
+				'--no-sandbox',
+				'--disable-gpu',
+				'--disable-infobars',
+				'--disable-extensions',
+				'--disable-dev-shm-usage',
+			],
+			// args: ['--disable-gpu'],
 		}
 	}],
 	//
