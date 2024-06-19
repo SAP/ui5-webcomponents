25a6ebc4b9c10b54aa79053607120a5032c96458
Duc Vo Ngoc <duc.vo.ngoc@sap.com>
2024-06-19 17:28:32 +0200

    fix: access property in type guards

--- packages/main/src/TableUtils.ts
+++ packages/main/src/TableUtils.ts
@@ -3,15 +3,15 @@ import type TableCellBase from "./TableCellBase";
 import type TableRowBase from "./TableRowBase";
 
 const isInstanceOfTable = (obj: any): obj is Table => {
-	return "isTable" in obj;
+	return "isTable" in obj && !!obj.isTable;
 };
 
 const isInstanceOfTableCellBase = (obj: any): obj is TableCellBase => {
-	return "isTableCellBase" in obj;
+	return "isTableCellBase" in obj && !!obj.isTableCellBase;
 };
 
 const isInstanceOfTableRowBase = (obj: any): obj is TableRowBase => {
-	return "isTableRowBase" in obj;
+	return "isTableRowBase" in obj && !!obj.isTableRowBase;
 };
 
 export {
