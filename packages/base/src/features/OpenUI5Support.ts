0cbba780fa1c66071271f81da1d5869211164ba9
Vladislav Tasev <vladitasev@users.noreply.github.com>
2024-06-20 08:57:55 +0300

    chore: adapt OpenUI5 version check (#9245)

--- packages/base/src/features/OpenUI5Support.ts
+++ packages/base/src/features/OpenUI5Support.ts
@@ -70,6 +70,9 @@ type Locale = {
 
 class OpenUI5Support {
 	static isAtLeastVersion116() {
+		if (!window.sap.ui!.version) {
+			return true; // sap.ui.version will be removed in newer OpenUI5 versions
+		}
 		const version = window.sap.ui!.version as string;
 		const parts = version.split(".");
 		if (!parts || parts.length < 2) {
