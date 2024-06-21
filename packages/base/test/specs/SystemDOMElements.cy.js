a26364edb01f9d426bada38299511ec0afede2e8
Nayden Naydenov <nnaydenow.work@gmail.com>
2024-06-21 11:17:47 +0300

    chore: base cleanup

--- packages/base/test/specs/SystemDOMElements.cy.js
+++
@@ -1,8 +0,0 @@
-import "../../bundle.esm.js";
-
-describe("Shared Resources", () => {
-	it("Tests the presense of the shared resources 'meta' element", async () => {
-		cy.get(`meta[name="ui5-shared-resources"]`)
-			.should("exist");
-	});
-});
