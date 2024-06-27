33ecc8e23c7e1a915c072088247bec723d4cbd38
Diana <diana.petcheva@sap.com>
2024-06-27 10:50:12 +0300

    docs(ui5-date-range): add documentation (#9179)
    Adding documentation for CalendarDateRange, and ICalendarSelectedDates.

--- packages/main/src/Calendar.ts
+++ packages/main/src/Calendar.ts
@@ -59,6 +59,12 @@ interface ICalendarPicker {
 	_lastYear?: number,
 }
 
+/**
+ * Interface for components that may be slotted inside a `ui5-calendar`.
+ *
+ * **Note:** Use with `ui5-date` or `ui5-date-range` as calendar date selection types.
+ * @public
+ */
 interface ICalendarSelectedDates extends UI5Element {
 	value?: string,
 	startValue?: string,
@@ -278,7 +284,8 @@ class Calendar extends CalendarPart {
 
 	/**
 	 * Defines the selected date or dates (depending on the `selectionMode` property)
-	 * for this calendar as instances of `ui5-date`.
+	 * for this calendar as instances of `ui5-date` or `ui5-date-range`.
+	 * Use `ui5-date` for single or multiple selection, and `ui5-date-range` for range selection.
 	 * @public
 	 */
 	@slot({ type: HTMLElement, invalidateOnChildChange: true, "default": true })
