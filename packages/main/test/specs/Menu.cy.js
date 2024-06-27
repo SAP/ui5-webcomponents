95083fb670fdd43000ac993e0c00b3a6daddaac2
Boyan Rakilovski <boyan.rakilovski@sap.com>
2024-06-27 18:10:38 +0300

    fix: add test

--- packages/main/test/specs/Menu.cy.js
+++ packages/main/test/specs/Menu.cy.js
@@ -301,6 +301,27 @@ describe("Menu interaction", () => {
 
 	})
 
+	it("Focus restored to the menu opener", () => {
+        cy.mount(html`<ui5-button id="btnOpen">Open Menu</ui5-button>
+        <ui5-menu id="menu" opener="btnOpen">
+            <ui5-menu-item text="New File"></ui5-menu-item>
+        </ui5-menu>`)
+ 
+        cy.get("[ui5-button]")
+            .realClick();
+ 
+        cy.get("[ui5-menu]")
+            .ui5MenuOpen({ opener: "btnOpen" });
+ 
+        cy.get("[ui5-menu]")
+            .ui5MenuOpened();
+ 
+        cy.realPress("Escape");
+ 
+        cy.get("[ui5-button]")
+            .should("be.focused");
+    });
+
 	describe("Accessibility", () => {
 		it("Menu and Menu items accessibility attributes", () => {
 			cy.mount(html`<ui5-button id="btnOpen">Open Menu</ui5-button>
