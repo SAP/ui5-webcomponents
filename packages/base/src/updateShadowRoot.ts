5954fddf6dc6e14896a0fb1174c1d97284be3544
Nayden Naydenov <nnaydenow.work@gmail.com>
2024-07-03 00:34:13 +0300

    fix: lint

--- packages/base/src/updateShadowRoot.ts
+++ packages/base/src/updateShadowRoot.ts
@@ -1,6 +1,10 @@
 import getConstructableStyle from "./theming/getConstructableStyle.js";
 import type UI5Element from "./UI5Element.js";
 
+interface OwnDefinedRendering extends UI5Element {
+	render(): object
+}
+
 /**
  * Updates the shadow root of a UI5Element or its static area item
  * @param element
@@ -8,7 +12,7 @@ import type UI5Element from "./UI5Element.js";
 const updateShadowRoot = (element: UI5Element) => {
 	const ctor = element.constructor as typeof UI5Element;
 	const shadowRoot = element.shadowRoot;
-	const renderResult = "render" in ctor.prototype ? (element as any).render() : element._originalRender(); // this is checked before calling updateShadowRoot
+	const renderResult = "render" in ctor.prototype ? (element as OwnDefinedRendering).render() : element._originalRender(); // this is checked before calling updateShadowRoot
 
 	if (!shadowRoot) {
 		console.warn(`There is no shadow root to update`); // eslint-disable-line
