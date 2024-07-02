597367bbfe62e95bf4aeed821e49f1d8247de674
Nayden Naydenov <31909318+nnaydenow@users.noreply.github.com>
2024-07-02 15:39:53 +0300

    chore: remove css modules setup (#9369)
    Icon styles related to the `ui5-input` component will now be implemented by enhancing slotted elements (see: https://github.com/SAP/ui5-webcomponents/pull/8995). This change eliminates the need for CSS modules setup, as there are no remaining candidates requiring this approach.
    
    Additionally, the configuration to use CSS modules in somed technologies is excluded by default, making the setup process more complex (e.g., requiring specific configurations for webpack or other bundlers). In most cases, such styles are intended to be utilized via CDN if available, further reducing the necessity for CSS modules.

--- packages/tools/components-package/nps.js
+++ packages/tools/components-package/nps.js
@@ -77,10 +77,9 @@ const getScripts = (options) => {
 			default: "nps prepare lint build.bundle", // build.bundle2
 			templates: `mkdirp src/generated/templates && ${tsCrossEnv} node "${LIB}/hbs2ui5/index.js" -d src/ -o src/generated/templates`,
 			styles: {
-				default: `concurrently "nps build.styles.themes" "nps build.styles.components" "nps build.styles.componentStyles"`,
+				default: `concurrently "nps build.styles.themes" "nps build.styles.components"`,
 				themes: `node "${LIB}/css-processors/css-processor-themes.mjs"`,
 				components: `node "${LIB}/css-processors/css-processor-components.mjs"`,
-				componentStyles: `node "${LIB}/css-processors/css-processor-component-styles.mjs"`,
 			},
 			i18n: {
 				default: "nps build.i18n.defaultsjs build.i18n.json",
@@ -114,10 +113,9 @@ const getScripts = (options) => {
 			props: 'nps "copy.props --watch --safe --skip-initial-copy"',
 			bundle: `node ${LIB}/dev-server/dev-server.js ${viteConfig}`,
 			styles: {
-				default: 'concurrently "nps watch.styles.themes" "nps watch.styles.components"  "nps watch.styles.componentStyles" ',
+				default: 'concurrently "nps watch.styles.themes" "nps watch.styles.components"',
 				themes: 'nps "build.styles.themes -w"',
 				components: `nps "build.styles.components -w"`,
-				componentStyles: `nps "build.styles.componentStyles -w"`,
 			},
 			templates: 'chokidar "src/**/*.hbs" -c "nps build.templates"',
 			i18n: 'chokidar "src/i18n/messagebundle.properties" -c "nps build.i18n.defaultsjs"'
