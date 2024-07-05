cc452053531bfe064ea77babbccdeeff6a6e5a73
Nikolay Deshev <nikolay.deshev@sap.com>
2024-07-05 15:18:33 +0300

    chore: update cypress config path for windows
    detect dirname conditionally for windows and unix

--- packages/main/config/cypress.config.js
+++ packages/main/config/cypress.config.js
@@ -1,7 +1,8 @@
 import cypressConfig from "@ui5/webcomponents-tools/components-package/cypress.config.js";
 import path from "path";
 
-const __dirname = path.parse(import.meta.url)['dir'].replace('file:///','');
+const isWin = process.platform === "win32";
+const __dirname = isWin ? path.parse(import.meta.url)['dir'].replace('file:///','') : path.dirname(new URL(import.meta.url).pathname);
 cypressConfig.component.supportFile = path.join(__dirname, "cypress/support/component.js");
 
 export default cypressConfig;
