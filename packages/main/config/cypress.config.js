c9b194ca57e77d91062b04465fdcdc2bd2ace8ae
Nikolay Deshev <nikolay.deshev@sap.com>
2024-07-05 15:01:26 +0300

    chore: update cypress config path for windows
    The Cypress config path module adds a leading slash to the dirname
    like "/C:/..." which results in error on Windows

--- packages/main/config/cypress.config.js
+++ packages/main/config/cypress.config.js
@@ -1,8 +1,7 @@
 import cypressConfig from "@ui5/webcomponents-tools/components-package/cypress.config.js";
 import path from "path";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
+const __dirname = path.parse(import.meta.url)['dir'].replace('file:///','');
 cypressConfig.component.supportFile = path.join(__dirname, "cypress/support/component.js");
 
 export default cypressConfig;
