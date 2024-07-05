66a3063d7c972b041d4a32dc3a6e4aa38d03d00c
Nayden Naydenov <31909318+nnaydenow@users.noreply.github.com>
2024-07-05 13:12:57 +0300

    chore: add missing types in cem (#9398)

--- packages/fiori/src/UploadCollectionItem.ts
+++ packages/fiori/src/UploadCollectionItem.ts
@@ -140,7 +140,7 @@ class UploadCollectionItem extends ListItem {
 	 * @public
 	 */
 	@property({ type: Boolean })
-	declare disableDeleteButton;
+	declare disableDeleteButton: boolean;
 
 	/**
 	 * Hides the delete button.
