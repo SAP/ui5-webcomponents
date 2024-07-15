b8548528bf0f2379c683c6651c04f19c7faaf75f
plamenivanov91 <plamen.ivanov01@sap.com>
2024-07-15 13:27:12 +0300

    fix(ui5-li-group): drag and drop enabled - fixed review comments (added move and move-over events description)

--- packages/main/src/ListItemGroup.ts
+++ packages/main/src/ListItemGroup.ts
@@ -1,4 +1,5 @@
 import slot from "@ui5/webcomponents-base/dist/decorators/slot.js";
+import event from "@ui5/webcomponents-base/dist/decorators/event.js";
 import property from "@ui5/webcomponents-base/dist/decorators/property.js";
 import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
 import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
@@ -51,6 +52,52 @@ type ListItemGroupMoveEventDetail = {
 	styles: [ListItemGroupCss],
 	dependencies: [ListItemStandard, ListItemGroupHeader, DropIndicator],
 })
+
+/**
+ * Fired when a movable list item is moved over a potential drop target during a dragging operation.
+ *
+ * If the new position is valid, prevent the default action of the event using `preventDefault()`.
+ * @param {object} source Contains information about the moved element under `element` property.
+ * @param {object} destination Contains information about the destination of the moved element. Has `element` and `placement` properties.
+ * @public
+ * @allowPreventDefault
+ */
+
+@event<ListItemGroupMoveEventDetail>("move-over", {
+	detail: {
+		/**
+		 * @public
+		 */
+		source: { type: Object },
+		/**
+		 * @public
+		 */
+		destination: { type: Object },
+	},
+})
+
+/**
+ * Fired when a movable list item is dropped onto a drop target.
+ *
+ * **Note:** `move` event is fired only if there was a preceding `move-over` with prevented default action.
+ * @param {object} source Contains information about the moved element under `element` property.
+ * @param {object} destination Contains information about the destination of the moved element. Has `element` and `placement` properties.
+ * @public
+ * @allowPreventDefault
+ */
+@event<ListItemGroupMoveEventDetail>("move", {
+	detail: {
+		/**
+		 * @public
+		 */
+		source: { type: Object },
+		/**
+		 * @public
+		 */
+		destination: { type: Object },
+	},
+})
+
 class ListItemGroup extends UI5Element {
 	/**
 	 * Defines the header text of the <code>ui5-li-group</code>.
