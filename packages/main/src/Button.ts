a17b20e86e1f2f2602b46e29695e054427f85631
Tsanislav Gatev <tsanislav.gatev@sap.com>
2024-07-16 13:10:46 +0300

    fix(ui5-segmented-button): prevent focus for disabled items (#9469)
    fixes: #9236
    
    Info: We're removing two tests in the button, as we remove the pointer-events, hence the click is impossible and the tests are failing with : "element click intercepted: Element ... is not clickable at point (42, 62)"

--- packages/main/src/Button.ts
+++ packages/main/src/Button.ts
@@ -516,6 +516,10 @@ class Button extends UI5Element implements IFormElement, IButton {
 	}
 
 	get tabIndexValue() {
+		if (this.disabled) {
+			return;
+		}
+
 		const tabindex = this.getAttribute("tabindex");
 
 		if (tabindex) {
--- packages/main/src/SegmentedButton.ts
+++ packages/main/src/SegmentedButton.ts
@@ -130,7 +130,7 @@ class SegmentedButton extends UI5Element {
 		super();
 
 		this._itemNavigation = new ItemNavigation(this, {
-			getItemsCallback: () => this.getSlottedNodes<SegmentedButtonItem>("items"),
+			getItemsCallback: () => this.navigatableItems,
 		});
 		this.hasPreviouslyFocusedItem = false;
 	}
@@ -227,6 +227,10 @@ class SegmentedButton extends UI5Element {
 		const eventTarget = e.target as SegmentedButtonItem;
 		const isTargetSegmentedButtonItem = eventTarget.hasAttribute("ui5-segmented-button-item");
 
+		if (eventTarget.disabled || eventTarget === this.getDomRef() || !isTargetSegmentedButtonItem) {
+			return;
+		}
+
 		if (isTargetSegmentedButtonItem) {
 			eventTarget.focus();
 			this._itemNavigation.setCurrentItem(eventTarget);
@@ -278,6 +282,12 @@ class SegmentedButton extends UI5Element {
 
 	get ariaDescription() {
 		return SegmentedButton.i18nBundle.getText(SEGMENTEDBUTTON_ARIA_DESCRIPTION);
+	}
+
+	get navigatableItems() {
+		return this.getSlottedNodes<SegmentedButtonItem>("items").filter(item => {
+			return !item.disabled;
+		});
 	}
 }
 
