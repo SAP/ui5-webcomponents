6f318911492f0e038ff082935b81a7dd89d93c90
Nikoleta Terzieva <nikoleta.ivanova.terzieva@sap.com>
2024-07-19 12:23:02 +0300

    fix(ui5-input, ui5-multi-input): fire change event properly on mobile devices
    Fire change event only once when a user selects a suggestion.
    Do not fire change event if the user closes the suggestions dialog from the "x" button.
    
    Fixes #9453

---
+++ packages/main/test/specs/MultiInput.mobile.spec.js
@@ -0,0 +1,42 @@
+import { assert } from "chai";
+
+describe("MultiInput events", () => {
+	before(async () => {
+		await browser.emulateDevice('iPhone X');
+	});
+
+	it("Selecting an item from the suggestion list fires change event only once", async () => {
+		await browser.url(`test/pages/MultiInput.html`);
+
+		const mi = await browser.$("#suggestion-token");
+
+		await mi.scrollIntoView();
+		await mi.click();
+		await mi.keys("A");
+
+		await mi.$$("ui5-suggestion-item")[0].click();
+
+		const counter = await browser.$("#suggestion-token-counter");
+
+		//assert
+		assert.strictEqual(await counter.getValue(), "1", "The change event is fired once");
+	});
+
+	it("Cancelling input does not fire a change event", async () => {
+		await browser.url(`test/pages/MultiInput_Suggestions.html`);
+
+		const mi = await browser.$("#suggestion-token");
+
+		await mi.click();
+		await mi.keys("A");
+
+		const popover = await mi.shadow$("ui5-responsive-popover");
+		const closeButton = popover.$(".ui5-responsive-popover-close-btn");
+		await closeButton.click();
+
+		const counter = await browser.$("#suggestion-token-counter");
+
+		//assert
+		assert.strictEqual(await counter.getValue(), "0", "The change event is not fired");
+	});
+});
