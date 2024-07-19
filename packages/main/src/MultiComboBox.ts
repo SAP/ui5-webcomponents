8dc5e5192d273663116eb8e00541ebe952ceb685
Elena Stoyanova <elena.stoyanova@sap.com>
2024-07-19 09:40:08 +0300

    fix(ui5-multi-combobox, ui5-combobox, ui5-input): improve popup announcement (#9513)
    Co-authored-by: Elena Stoyanova <e.nikolova@sap.com>

--- packages/main/src/MultiComboBox.ts
+++ packages/main/src/MultiComboBox.ts
@@ -84,6 +84,7 @@ import {
 	SELECT_OPTIONS,
 	SHOW_SELECTED_BUTTON,
 	MULTICOMBOBOX_DIALOG_OK_BUTTON,
+	COMBOBOX_AVAILABLE_OPTIONS,
 	VALUE_STATE_ERROR_ALREADY_SELECTED,
 	MCB_SELECTED_ITEMS,
 	INPUT_CLEAR_ICON_ACC_NAME,
@@ -1988,6 +1989,10 @@ class MultiComboBox extends UI5Element implements IFormInputElement {
 		const selected = items.filter(item => item.selected);
 
 		return MultiComboBox.i18nBundle.getText(MCB_SELECTED_ITEMS, selected.length, items.length);
+	}
+
+	get _popupLabel() {
+		return MultiComboBox.i18nBundle.getText(COMBOBOX_AVAILABLE_OPTIONS);
 	}
 
 	get classes(): ClassMap {
