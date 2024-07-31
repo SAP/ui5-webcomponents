cf93d8317ef5dba28336acad8cbe4cfd0b2edab9
yanaminkova <yana.minkova@sap.com>
2024-07-31 10:00:16 +0300

    fix(ui5-shellbar): remove aria attribute from search button

--- packages/fiori/src/ShellBar.ts
+++ packages/fiori/src/ShellBar.ts
@@ -320,7 +320,7 @@ class ShellBar extends UI5Element {
 	 * - **notifications** - `notifications.expanded` and `notifications.hasPopup`.
 	 * - **profile** - `profile.expanded`, `profile.hasPopup` and `profile.name`.
 	 * - **product** - `product.expanded` and `product.hasPopup`.
-	 * - **search** - `search.expanded` and `search.hasPopup`.
+	 * - **search** - `search.hasPopup`.
 	 * - **overflow** - `overflow.expanded` and `overflow.hasPopup`.
 	 *
 	 * The accessibility attributes support the following values:
@@ -1159,7 +1159,6 @@ class ShellBar extends UI5Element {
 	}
 
 	get accInfo() {
-		const searchExpanded = this.accessibilityAttributes.search?.expanded;
 		const overflowExpanded = this.accessibilityAttributes.overflow?.expanded;
 
 		return {
@@ -1188,7 +1187,6 @@ class ShellBar extends UI5Element {
 				"title": this._searchText,
 				"accessibilityAttributes": {
 					hasPopup: this.accessibilityAttributes.search?.hasPopup,
-					expanded: searchExpanded === undefined ? this.showSearchField : searchExpanded,
 				},
 			},
 			overflow: {
