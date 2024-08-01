8435bd9375f4fcdb95a5a57016590714d0428d09
Diana Petcheva <diana.petcheva@sap.com>
2024-08-01 17:41:20 +0300

    fix: update sample

--- packages/website/docs/_samples/main/FileUploader/FileSizeLimit/main.js
+++ packages/website/docs/_samples/main/FileUploader/FileSizeLimit/main.js
@@ -5,7 +5,7 @@ import "@ui5/webcomponents/dist/Label.js";
 import "@ui5/webcomponents-icons/dist/upload.js";
 
 const fileUploader = document.querySelector("#fileuploader");
-const resultDiv = document.querySelector("#file-exceed-result");
+const resultDiv = document.querySelector("#file-exceeded-result");
 
 fileUploader.addEventListener("fileSizeExceeded", (event) => {
     const maxSize = fileUploader.maxFileSize;
@@ -14,6 +14,8 @@ fileUploader.addEventListener("fileSizeExceeded", (event) => {
     resultDiv.innerHTML = "<ui5-label>File too big! Max file size is " + maxSize + " MB, this file is " + fileSize + " MB</ui5-label>";
 })
 
-fileUploader.addEventListener("change", () => {
-    resultDiv.innerHTML = "";
+fileUploader.addEventListener("change", (event) => {
+    if (event.detail.files.length) {
+        resultDiv.innerHTML = "";
+    }
 })
