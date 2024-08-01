6f2ceb0bf93fe5d716b0f8bbc87bb1c1dd66908d
Peter Skelin <petar.skelin@sap.com>
2024-08-01 15:16:28 +0300

    chore: adjust test to new behaviour

--- packages/base/test/specs/UI5ElementPropertyValidation.spec.js
+++ packages/base/test/specs/UI5ElementPropertyValidation.spec.js
@@ -8,7 +8,7 @@ describe("Properties can only have values, restricted to their types", () => {
 	it("String property enforced to string", async () => {
 		const el = await browser.$("#gen");
 		await el.setProperty("strProp", 5);
-		assert.strictEqual(await el.getProperty("strProp"), "5", "String property is string");
+		assert.strictEqual(await el.getProperty("strProp"), 5, "String property is not coerced, value is left as is");
 	});
 
 });
