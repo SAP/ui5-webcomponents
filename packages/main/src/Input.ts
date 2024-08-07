83c5545ce7d9ee34924ce1df363f44f2d267f33d
Shen Lin <slin-sap@outlook.com>
2024-08-07 16:43:16 -0700

    update

--- packages/main/src/Input.ts
+++ packages/main/src/Input.ts
@@ -186,11 +186,6 @@ type InputSuggestionScrollEventDetail = {
  *
  * `import "@ui5/webcomponents/dist/features/InputSuggestions.js";` (optional - for input suggestions support)
  *
- * @csspart root - Used to style the root DOM element of the Input component
- * @csspart input - Used to style the native input element
- * @csspart clear-icon - Used to style the clear icon, which can be pressed to clear user input text
- *
- *
  * @constructor
  * @extends UI5Element
  * @public
