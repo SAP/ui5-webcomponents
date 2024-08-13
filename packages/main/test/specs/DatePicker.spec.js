6cf5b187341e1cad734978f3c3d553c0dc2392c4
Boyan Rakilovski <boyan.rakilovski@sap.com>
2024-08-13 16:15:50 +0300

    fix(ui5-date-picker): adjust unstable test (#9663)
    Fixes: #9034

--- packages/main/test/specs/DatePicker.spec.js
+++ packages/main/test/specs/DatePicker.spec.js
@@ -720,6 +720,7 @@ describe("Date Picker Tests", () => {
 	});
 
 	it("placeholder, set by the user", async () => {
+		await browser.url(`test/pages/DatePicker_test_page.html?sap-ui-language=en`);
 		datepicker.id = "#dp15";
 
 		const placeholder = "Delivery date";
@@ -734,6 +735,7 @@ describe("Date Picker Tests", () => {
 	});
 
 	it("Going under the minimum date changes value state", async () => {
+		await browser.url(`test/pages/DatePicker_test_page.html?sap-ui-language=en`);
 		datepicker.id = "#dp33";
 
 		const innerInput = await datepicker.getInnerInput();
@@ -749,6 +751,7 @@ describe("Date Picker Tests", () => {
 	});
 
 	it("Going over the maximum date changes value state", async () => {
+		await browser.url(`test/pages/DatePicker_test_page.html?sap-ui-language=en`);
 		datepicker.id = "#dp33";
 
 		const innerInput = await datepicker.getInnerInput();
