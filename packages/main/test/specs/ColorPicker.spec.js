1094e3a39cd1c271fcb78b1046d613c9f318e6e3
Boyan Rakilovski <boyan.rakilovski@sap.com>
2024-08-27 11:01:29 +0300

    fix: add test

--- packages/main/test/specs/ColorPicker.spec.js
+++ packages/main/test/specs/ColorPicker.spec.js
@@ -81,6 +81,20 @@ describe("Color Picker general interaction", () => {
 		assert.strictEqual(await stepInput.getAttribute("value"), "2", "Change event gets fired on hue slider change");
 	});
 
+	it("Change hue slider and then alpha slider", async () => {
+		const colorPicker = await browser.$("#cp1");
+		const alphaSliderHandle = await colorPicker.shadow$(".ui5-color-picker-alpha-slider").shadow$(".ui5-slider-handle");
+		const hueSliderHandle = await colorPicker.shadow$(".ui5-color-picker-hue-slider").shadow$(".ui5-slider-handle");
+
+		await colorPicker.scrollIntoView();
+		await colorPicker.setAttribute("value", "rgba(183, 61, 61, 1)");
+
+		await alphaSliderHandle.dragAndDrop({ x: 200, y: 0 });
+		await hueSliderHandle.dragAndDrop({ x: 200, y: 0 });
+
+		assert.strictEqual(await colorPicker.getAttribute("value"), "rgba(183, 61, 184, 0.83)", "Alpha value propely changed");
+	});
+
 	it("tests color property", async () => {
 		const colorPicker = await browser.$("#cp3");
 		const hexInput = await colorPicker.shadow$(".ui5-color-picker-hex-input");
