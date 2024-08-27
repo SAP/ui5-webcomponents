7bb9f3bef22c50210009e58bbdbcc81c296b644e
Nayden Naydenov <nnaydenow.work@gmail.com>
2024-08-27 13:46:31 +0300

    fix: build

--- packages/base/src/util/getEffectiveScrollbarStyle.ts
+++ packages/base/src/util/getEffectiveScrollbarStyle.ts
@@ -1,5 +1,8 @@
 const NO_SCROLLBAR_STYLE_CLASS = "ui5-content-native-scrollbars";
+const isSSR = typeof document === "undefined";
 
-const getEffectiveScrollbarStyle = () => document.body.classList.contains(NO_SCROLLBAR_STYLE_CLASS);
+const getEffectiveScrollbarStyle = () => {
+	return isSSR ? true : document.body.classList.contains(NO_SCROLLBAR_STYLE_CLASS);
+};
 
 export default getEffectiveScrollbarStyle;
