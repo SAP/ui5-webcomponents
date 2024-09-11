e132d5f153c5733a97438cb1543eb79133b14da1
Teodor Taushanov <teodor.taushanov@sap.com>
2024-09-11 16:21:30 +0300

    chore(ui5-notification-list): improve popover example

--- packages/website/docs/_samples/fiori/NotificationList/InShellBar/main.js
+++ packages/website/docs/_samples/fiori/NotificationList/InShellBar/main.js
@@ -5,6 +5,7 @@ import "@ui5/webcomponents/dist/Title.js";
 import "@ui5/webcomponents/dist/Button.js";
 import "@ui5/webcomponents/dist/Menu.js";
 import "@ui5/webcomponents/dist/Bar.js";
+import "@ui5/webcomponents/dist/MessageStrip.js";
 import "@ui5/webcomponents-icons/dist/action-settings.js"
 import "@ui5/webcomponents-icons/dist/sort.js"
 
@@ -17,9 +18,12 @@ import "@ui5/webcomponents-icons/dist/employee.js";
 import "@ui5/webcomponents-icons/dist/message-error.js";
 import "@ui5/webcomponents-icons/dist/accept.js";
 
-var shellbar = document.querySelector("ui5-shellbar");
-var notificationsPopover = document.querySelector("ui5-popover");
-var notificationList = document.querySelector("ui5-notification-list");
+const shellbar = document.querySelector("ui5-shellbar");
+const notificationsPopover = document.querySelector(".notificationsPopover");
+const notificationList = document.querySelector(".notificationsPopoverList");
+const notificationsPopoverMessageStrip = document.querySelector(".notificationsMessageStrip");
+const btnShowMessageStrip = document.querySelector("#show-message-strip");
+const notificationsListGroupGrowing = document.querySelector("#notificationsListGroupGrowing");
 
 notificationList.addEventListener("item-close", e => {
     var visibleItems = 0;
@@ -43,4 +47,40 @@ shellbar.addEventListener("notifications-click", e => {
     e.preventDefault();
     notificationsPopover.opener = e.detail.targetRef;
 	notificationsPopover.open = true;
+});
+let itemsLoaded = 0;
+const itemToLoad = 5;
+
+const notificationListItemTemplate = (index) => {
+	var notificationLi = document.createElement("ui5-li-notification");
+	notificationLi.titleText = "New notification";
+	notificationLi.state = "Critical";
+
+	return notificationLi;
+}
+
+const insertItems = (list) => {
+	for (var i = itemsLoaded; i < itemsLoaded + itemToLoad; i++) {
+		list.appendChild(notificationListItemTemplate(i));
+	}
+
+	itemsLoaded += itemToLoad;
+};
+
+notificationsListGroupGrowing.addEventListener("load-more", (e) => {
+	const focusIndex = notificationsListGroupGrowing.items.length;
+
+	notificationsListGroupGrowing.loading = true; // as in the ui5-list (the whole list gets 'busy')
+	setTimeout(() => {
+		insertItems(notificationsListGroupGrowing);
+		notificationsListGroupGrowing.loading = false;
+
+		setTimeout(() => {
+			notificationsListGroupGrowing.items[focusIndex].focus();
+		}, 500);
+	}, 500);
+});
+
+btnShowMessageStrip.addEventListener("click", function() {
+	notificationsPopoverMessageStrip.style.display = "inline-block";
 });
