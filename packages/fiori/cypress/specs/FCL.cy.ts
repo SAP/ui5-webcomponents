83bf27cff8d9b7acf5b54da89301025bf916da52
Diana Pazheva <diana.pazheva@sap.com>
2024-09-13 09:40:15 +0300

    fix(ui5-flexible-column-layout): add test

---
+++ packages/fiori/cypress/specs/FCL.cy.ts
@@ -0,0 +1,42 @@
+import { html } from "lit";
+import "../../src/FlexibleColumnLayout.js";
+
+describe("Columns resize", () => {
+	beforeEach(() => {
+		cy.mount(html`
+<ui5-flexible-column-layout id="fcl" style="height: 300px;" layout="TwoColumnsMidExpanded">
+	<div class="column" id="startColumn" slot="startColumn">some content</div>
+	<div class="column" id="midColumn" slot="midColumn">some content</div>
+</ui5-flexible-column-layout>
+`);
+	});
+
+	it("disables column pointer events during separator drag'n'drop", () => {
+		cy.get("[ui5-flexible-column-layout]")
+			.shadow()
+			.find(".ui5-fcl-column--start")
+			.should("have.css", "pointer-events", "auto");
+
+		cy.get("[ui5-flexible-column-layout]")
+			.shadow()
+			.find(".ui5-fcl-separator-start")
+			.realMouseDown()
+			.realMouseMove(10, 0); // offset;
+
+		cy.get("[ui5-flexible-column-layout]")
+			.shadow()
+			.find(".ui5-fcl-column--start")
+			.should("have.css", "pointer-events", "none");
+
+		cy.get("[ui5-flexible-column-layout]")
+			.shadow()
+			.find(".ui5-fcl-separator-start")
+			.realMouseUp()
+			.then(() => {
+				cy.get("[ui5-flexible-column-layout]")
+					.shadow()
+					.find(".ui5-fcl-column--start")
+					.should("have.css", "pointer-events", "auto");
+			});
+	});
+});
