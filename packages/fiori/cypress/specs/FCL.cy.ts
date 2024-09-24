d594ae7c4b89849f0cd296cac29fdab7b5c2a8c0
Diana Pazheva <diana.pazheva@sap.com>
2024-09-24 10:30:59 +0300

    fix(ui5-flexible-column-layout): iframe content no longer hinders resize (#9859)
    * fix(flexible-column-layout): iframe content no longer hinders resize
    
    * fix(ui5-flexible-column-layout): rename new property to match existing disableResizing property
    
    * fix(ui5-flexible-column-layout): add test
    
    * fix(ui5-flexible-column-layout): correct test
    
    * fix(ui5-flexible-column-layout): correct test
    
    * fix(ui5-flexible-column-layout): correct test
    
    * fix(ui5-flexible-column-layout): correct test
    
    * fix(ui5-flexible-column-layout): correct test

---
+++ packages/fiori/cypress/specs/FCL.cy.ts
@@ -0,0 +1,41 @@
+import { html } from "lit";
+import "../../src/FlexibleColumnLayout.js";
+
+describe("Columns resize", () => {
+	beforeEach(() => {
+		cy.mount(html`
+<ui5-flexible-column-layout id="fcl" style="height: 300px;" layout="TwoColumnsMidExpanded">
+	<div class="column" id="startColumn" slot="startColumn">some content</div>
+	<div class="column" id="midColumn" slot="midColumn">some content</div>
+</ui5-flexible-column-layout>
+`);
+	});
+
+	it("toggles _resizing property during separator drag'n'drop", () => {
+		cy.get("[ui5-flexible-column-layout]")
+			.as("fcl");
+
+		cy.get("[ui5-flexible-column-layout]")
+			.shadow()
+			.find(".ui5-fcl-separator-start")
+			.as("separator");
+
+		// act: mock the user starting to drag the separator
+		cy.get("@separator")
+			.realMouseDown();
+
+		// assert that the property is set
+		cy.get("@fcl")
+			.invoke("prop", "_resizing")
+			.should("be.equal", true)
+			// act: mock the user releasing the mouse button; use "then" to ensure it happens after the above check has completed
+			.then(() => {
+				cy.get("@separator")
+					.realMouseUp();
+				// assert that the property is reset
+				cy.get("@fcl")
+					.invoke("prop", "_resizing")
+					.should("be.equal", false);
+			});
+	});
+});
