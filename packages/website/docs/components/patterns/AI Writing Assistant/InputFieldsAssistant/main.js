aca264d96a97d0d78bd3235ac5a0861ee25f0fba
Evdokia Yordanova <evdokia.kotova@sap.com>
2024-09-30 14:56:13 +0300

    feat(writingAssistant): introduce AI Writing Assistant for Input fields

--- packages/website/docs/components/patterns/AI Writing Assistant/InputFieldsAssistant/main.js
+++ packages/website/docs/components/patterns/AI Writing Assistant/InputFieldsAssistant/main.js
@@ -1,66 +1,62 @@
-import '@ui5/webcomponents/dist/Button.js';
 import '@ui5/webcomponents/dist/Label.js';
 import '@ui5/webcomponents/dist/Input.js';
 import "@ui5/webcomponents/dist/Menu.js";
 import "@ui5/webcomponents/dist/MenuItem.js";
-import "@ui5/webcomponents/dist/Button.js";
 import "@ui5/webcomponents-icons/dist/ai.js";
 import "@ui5/webcomponents-icons/dist/stop.js";
 import "@ui5/webcomponents-icons/dist/navigation-left-arrow.js";
 import "@ui5/webcomponents-icons/dist/navigation-right-arrow.js";
 
 const input = document.getElementById('ai-wa-input');
-const menu = document.getElementById('ai-wa-menu');
+const icon = document.getElementById('ai-wa-icon');
+const menuGenerate = document.getElementById('ai-wa-menu-generate');
+const menuRegenerate = document.getElementById('ai-wa-menu-regenerate');
 
-let loadTimeout;
-
-let aiIcon = document.getElementById('ai-wa-icon');
-let stopIcon = document.getElementById('stop-wa-icon');
-let isInputFocused = false;
-//let isHovered = false;
-let isIconHovered = false;
 let inputValues = [];
-let currentPage = 1;
-let totalPages = inputValues.length;
-let isGenerateClickAttached = false;
 let isItemClickAttached = false;
+let loadTimeout;
 
 
 input.addEventListener('focus', () => {
-	if (!isInputFocused) {
-		isInputFocused = true;
-		updateIconVisibility();
+    updateIconVisibility(true);
+    icon.name = "ai";
+}); 
+
+input.addEventListener('focusout', e => {
+  if(!menuGenerate.open && !menuRegenerate.open){
+    icon.name = "ai";
+    //input.classList.remove("loading-input");
+    updateIconVisibility(false);
+  }
+});
+
+icon.addEventListener( "mousedown", e => {
+	if(icon.name === "stop"){
+		stopLoading();
+		return;
 	}
+  if(input.value.length){
+ 	updateMenuVisibility(false);
+   	menuRegenerate.open = true;
+
+  } else{
+	 updateMenuVisibility(true);
+   	menuGenerate.open = true;
+  }
 });
 
-input.addEventListener('focusout', () => {
-	if (!isIconHovered) {
-		isInputFocused = false;
-		updateIconVisibility();
-	}
+menuGenerate.addEventListener("before-close", e => {
+  if(icon.name === "stop"){
+     return;
+  }
+  updateIconVisibility(false); 
 });
 
-input.addEventListener('click', () => {
-	if (!isInputFocused) {
-		isInputFocused = true;
-		updateIconVisibility();
-	}
-});
-
-
-input.addEventListener('focusout', () => {
-	isInputFocused = false;
-	updateIconVisibility();
-});
-
-input.addEventListener('blur', () => {
-	isInputFocused = false;
-	updateIconVisibility();
-});
-
-aiIcon.addEventListener('mouseenter', () => {
-	isIconHovered = true;
-	updateIconVisibility();
+menuRegenerate.addEventListener("before-close", e => {
+  if(icon.name === "stop"){
+     return;
+  }
+   updateIconVisibility(false);
 });
 
 aiIcon.addEventListener('mouseleave', () => {
@@ -70,29 +66,33 @@ aiIcon.addEventListener('mouseleave', () => {
 	}
 });
 
-function updateIconVisibility() {
-	if (isInputFocused) {
-		aiIcon.classList.add('show-icon');
-		aiIcon.classList.remove('hide-icon');
-	} else {
-		aiIcon.classList.remove('show-icon');
-		aiIcon.classList.add('hide-icon');
-	}
+
+menuGenerate.addEventListener("item-click", () => {
+  updateIconVisibility(true);
+  icon.name = "stop";
+  transitionToLoadState()
+});
+
+menuRegenerate.addEventListener("item-click", e => {
+   icon.name = "stop";
+});
+
+function stopLoading() {
+	clearTimeout(loadTimeout);
+	let lastInputValue = inputValues[inputValues.length - 1] || '';
+	updateInputState(true, lastInputValue, 'Placeholder');
+	icon.name = "ai"
 }
 
-function updateIconsOnLoad(isLoading) {
-	if (isLoading) {
-		stopIcon.classList.add('show-icon');
-		stopIcon.classList.remove('hide-icon');
-		aiIcon.classList.remove('show-icon');
-		aiIcon.classList.add('hide-icon');
+const updateIconVisibility = (visible) => {
+	if(visible){
+	   icon.classList.remove("hidden");
+	   icon.classList.add("show");
 	} else {
-		aiIcon.classList.add('show-icon');
-		aiIcon.classList.remove('hide-icon');
-		stopIcon.classList.remove('show-icon');
-		stopIcon.classList.add('hide-icon');
+	  icon.classList.remove("show");
+		icon.classList.add("hidden");
 	}
-}
+  };
 
 function updateInputState(isEditable, value, placeholder) {
 	if (isEditable) {
@@ -107,8 +107,6 @@ function updateInputState(isEditable, value, placeholder) {
 function addInputValue(value) {
 	if (value && value !== "Analyzing request...") {
 		inputValues.push(value);
-		currentPage = inputValues.length;
-		totalPages = inputValues.length;
 		console.log("Current inputValues:", inputValues);
 	}
 }
@@ -116,155 +114,34 @@ function addInputValue(value) {
 function transitionToLoadState() {
 	addInputValue(input.value);
 
-	updateIconsOnLoad(true);
 	updateInputState(false, 'Analyzing request...', 'Analyzing request...');
-	input.classList.add('generating-input');
+	input.classList.add('loading-input');
 
 	loadTimeout = setTimeout(() => {
 		updateInputState(true, 'AI Design Fellowship', 'Placeholder');
-		updateIconsOnLoad(false);
-		loadMenuItems(false);
+		updateMenuVisibility(false);
+		icon.name = "ai"
 	}, 2000);
 }
 
-function aiIconClickHandler() {
-	if (input.value === "") {
-		loadMenuItems(true);
+function updateMenuVisibility(isInitialItemVisible) {
+	if (isInitialItemVisible) {
+		menuGenerate.classList.add("show");
+		menuGenerate.classList.remove("hide");
+		menuRegenerate.classList.add("hide");
+		menuRegenerate.classList.remove("show");
 	} else {
-		loadMenuItems(false);
-	}
-	menu.open = true;
-}
-
-function stopIconClickHandler() {
-	stopLoading();
-}
-
-function stopLoading() {
-	clearTimeout(loadTimeout);
-	let lastInputValue = inputValues[inputValues.length - 1] || '';
-	updateInputState(true, lastInputValue, 'Placeholder');
-	updateIconsOnLoad(false);
-	loadMenuItems(true);
-}
-
-
-function loadMenuItems(isInitialItemVisible) {
-	for (let itemIndex = 0; itemIndex < menu.items.length; itemIndex++) {
-		const item = menu.items[itemIndex];
-		totalPages = inputValues.length;
-		currentPage = totalPages;
-
-		if (itemIndex === 0 && isInitialItemVisible) {
-			item.classList.remove("hide-item");
-			item.classList.add("show-item");
-			item.addEventListener('click', () => {
-				transitionToLoadState();
+		menuGenerate.classList.remove("show");
+		menuGenerate.classList.add("hide");
+		menuRegenerate.classList.remove("hide");
+		menuRegenerate.classList.add("show");
+		if (!isItemClickAttached) {
+			menuRegenerate.addEventListener('item-click', function (evt) {
+				if (evt.detail.item.getAttribute('text') === 'Regenerate') {
+					transitionToLoadState();
+				}
 			});
-			if (!isGenerateClickAttached) {
-				item.addEventListener('click', transitionToLoadState);
-				isGenerateClickAttached = true;
-			}
-		} else if (!isInitialItemVisible && itemIndex > 0 && itemIndex < menu.items.length - 1) {
-			item.classList.remove("hide-item");
-			item.classList.add("show-item");
-		} else if (!isInitialItemVisible && itemIndex === menu.items.length - 1 && totalPages >= 1) {
-			item.classList.remove("hide-item");
-			item.classList.add("show-item");
-			addPaginationControls();
-		} else {
-			item.classList.remove("show-item");
-			item.classList.add("hide-item");
+			isItemClickAttached = true;
 		}
 	}
-
-	if (!isItemClickAttached) {
-		menu.addEventListener('item-click', function (evt) {
-			if (evt.detail.item.getAttribute('text') === 'Regenerate') {
-				transitionToLoadState();
-			}
-		});
-		isItemClickAttached = true;
-	}
 }
-function addPaginationControls() {
-	const paginationItem = document.getElementById('pagination-menu-item');
-	const rightArrowIcon = document.getElementById('right-arrow-icon');
-	const leftArrowIcon = document.getElementById('pagination-menu-item');
-
-	paginationItem.addEventListener('click', (event) => {
-		event.stopPropagation();
-		menu.open= true;
-		if (event.target.getAttribute('icon') === 'navigation-left-arrow' && !leftArrowIcon.disabled && currentPage > 1) {
-			currentPage--;
-			let lastInputValue = inputValues[currentPage-1] || '';
-			updateInputState(true, lastInputValue, 'Placeholder');
-			updatePaginationText(paginationItem, rightArrowIcon);
-		}
-	});
-
-	rightArrowIcon.addEventListener('click', (event) => {
-		event.stopPropagation();
-		menu.open= true;
-		if (currentPage < totalPages) {
-			currentPage++;
-			let nextInputValue = inputValues[currentPage] || '';
-			updateInputState(true, nextInputValue, 'Placeholder');
-			updatePaginationText(paginationItem, rightArrowIcon);
-		}
-	});
-
-	updatePaginationText(paginationItem, rightArrowIcon);
-}
-
-function updatePaginationText(paginationItem, rightArrowIcon) {
-	paginationItem.setAttribute('text', `${currentPage} / ${totalPages}`);
-
-	if (currentPage === 1) {
-        paginationItem.disabled = true;
-    } else {
-        paginationItem.disabled = false; 
-    }
-
-	if(currentPage === totalPages) {
-		rightArrowIcon.setAttribute('disabled', true);
-	} else {
-		rightArrowIcon.removeAttribute('disabled');
-	}
-
-}
-
-
-aiIcon.addEventListener('mousedown', (event) => {
-	event.stopPropagation();
-	aiIconClickHandler();
-});
-
-stopIcon.addEventListener('mousedown', (event) => {
-	event.stopPropagation();
-	stopIcon.classList.add("icon-clicked")
-	stopIconClickHandler();
-});
-
-aiIcon.addEventListener('click', (event) => {
-	aiIcon.classList.add("icon-clicked")
-	event.stopPropagation();
-});
-
-aiIcon.addEventListener('mouseleave', (event) => {
-	aiIcon.classList.remove("icon-clicked")
-});
-
-stopIcon.addEventListener('mouseleave', (event) => {
-	stopIcon.classList.remove("icon-clicked")
-});
-
-
-
-//TODO:
-//onclick stop => return state of the menu to initial? when click stop - should input become in initial state (not blue)
-//create versioning functionality - nope
-// ai progress indicator - input value set Analyzing ?!?
-//icon twice click?
-//shown focus on generate should be there - SPEC shows it, but it's strange - Navigation flow - no focus/ MOUSE/TOUCH interaction there
-
