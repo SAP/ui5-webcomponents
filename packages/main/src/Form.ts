46f1da590c918d20e32b039c3bcab1237e9a0114
ilhan007 <ilhan.myumyun@sap.com>
2024-10-11 11:48:13 +0300

    refactor(ui5-form): change default columns number in XL

--- packages/main/src/Form.ts
+++ packages/main/src/Form.ts
@@ -83,7 +83,7 @@ type ItemsInfo = {
  * - **S** (< 600px) – 1 column is recommended (default: 1)
  * - **M** (600px - 1022px) – up to 2 columns are recommended (default: 1)
  * - **L** (1023px - 1439px) - up to 3 columns are recommended (default: 2)
- * - **XL** (> 1439px) – up to 6 columns are recommended (default: 2)
+ * - **XL** (> 1439px) – up to 6 columns are recommended (default: 3)
  *
  * To change the layout, use the `layout` property - f.e. layout="S1 M2 L3 XL6".
  *
@@ -156,13 +156,13 @@ class Form extends UI5Element {
 	 * - `S` - 1 column by default (1 column is recommended)
 	 * - `M` - 1 column by default (up to 2 columns are recommended)
 	 * - `L` - 2 columns by default (up to 3 columns are recommended)
-	 * - `XL` - 2 columns by default (up to 6 columns  are recommended)
+	 * - `XL` - 3 columns by default (up to 6 columns  are recommended)
 	 *
 	 * @default "S1 M1 L2 XL2"
 	 * @public
 	 */
 	@property()
-	layout = "S1 M1 L2 XL2"
+	layout = "S1 M1 L2 XL3"
 
 	/**
 	 * Defines the width proportion of the labels and fields of a FormItem by breakpoint.
@@ -246,7 +246,7 @@ class Form extends UI5Element {
 	labelSpanL = 4;
 
 	@property({ type: Number })
-	columnsXl = 2;
+	columnsXl = 3;
 	@property({ type: Number })
 	labelSpanXl = 4;
 
