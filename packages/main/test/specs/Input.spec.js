058c47c8edc9b1959cd3f27a01414cccbc128678
Evdokia Yordanova <evdokia.kotova@sap.com>
2024-10-15 00:15:19 +0300

    fix(ui5-input): properly change numeric input value using the arrow keys
    Downport #9616

--- packages/main/test/specs/Input.spec.js
+++ packages/main/test/specs/Input.spec.js
@@ -255,6 +255,21 @@ describe("Input general interaction", () => {
 		await input.keys("Enter");
 	});
 
+	it("Input event is fired when user uses arrow keys to increase/decrease numeric value", async () => {
+		await browser.url(`test/pages/Input.html`);
+
+		const input = await browser.$("#input-number3");
+		await input.scrollIntoView();
+		await input.click();
+
+		await input.keys("ArrowUp");
+		await input.keys("ArrowDown");
+
+		const inputChangeCount = await browser.$("#input-number3-change-count");
+
+		assert.strictEqual( await inputChangeCount.getProperty("value"), "2", "Input event is fired when navigating with arrow keys");
+	});
+
 	it("tests value removal when Input type is 'Number'", async () => {
 		const input = await browser.$("#input-number3");
 		const btn = await browser.$("#input-number3-focusout");
