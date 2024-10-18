7fb4f9c860b7295c6f948de5490b2332a0e99ba8
Peter Skelin <petar.skelin@sap.com>
2024-10-18 14:14:46 +0300

    chore: simplify jsx-runtime files

--- packages/base/src/jsx-dev-runtime.ts
+++ packages/base/src/jsx-dev-runtime.ts
@@ -1,14 +1,25 @@
 // eslint-disable-next-line import/extensions
 import { jsxDEV as _jsxDEV, Fragment as _Fragment } from "preact/jsx-runtime";
-import type { JSX } from "preact/jsx-runtime";
-import UI5Element from "./UI5Element.js";
+import type { JSX as _JSX } from "preact/jsx-runtime";
+import type UI5Element from "./UI5Element.js";
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace JSX {
+    export interface IntrinsicElements extends _JSX.IntrinsicElements {}
+    export type ElementClass = unknown;
+    export interface ElementAttributesProperty {
+		_jsxProps: any;
+	}
+	export interface HTMLAttributes extends _JSX.HTMLAttributes {}
+	export interface SVGAttributes extends _JSX.SVGAttributes {}
+}
 
 export function Fragment(props: Record<string, any>, context?: any) {
 	return _Fragment(props, context);
 }
 export function jsxDEV(type: string | typeof UI5Element, props: Record<string, any>, key: string) {
 	let tag = type;
-	if (typeof type === "function" && type.prototype instanceof UI5Element) {
+	if (typeof type === "function" && "getMetadata" in type) {
 		tag = type.getMetadata().getTag();
 	}
 	return _jsxDEV(tag as string, props, key);
--- packages/main/src/TextTemplate.tsx
+++ packages/main/src/TextTemplate.tsx
@@ -1,8 +1,5 @@
 import type Text from "./Text.js";
 import Icon from "./Icon.js";
-import { jsxDEV } from "preact/jsx-dev-runtime";
-import { jsx } from "preact/jsx-runtime";
-console.log({jsxDEV, jsx})
 declare module "preact/jsx-runtime" {
 	namespace JSX {
 	  interface IntrinsicElements {
@@ -16,16 +13,16 @@ type ElementProps<I> = Partial<Omit<I, keyof HTMLElement>>;
 function UI5Icon(props: ElementProps<Icon>) {
 	return <ui5-icon {...props}></ui5-icon>
 }
-function GenUI5Icon(props: ElementProps<Icon>) {
-	return jsx(Icon._metadata.getTag(), props);
-}
+
+// class Icon2{}
 
 export default (props: Text) => {
 	return <>
 		<ui5-icon onTabChange={()=>{}} name="account" showTooltip={false}></ui5-icon>
 		<Icon name="appointment" design="Critical" mode="Decorative"></Icon>
+		{/* <Icon2 name="appointment" design="Critical" mode="Decorative"></Icon2> */}
 		<UI5Icon name="home" mode="Image"></UI5Icon>
-		<GenUI5Icon name="home" mode="Image"></GenUI5Icon>
+		{/* <GenUI5Icon name="home" mode="Image"></GenUI5Icon> */}
 		<span onClick={e => console.log(e)}>
 			<span> { props.emptyIndicatorMode } </span>
 			{ props._renderEmptyIndicator ?
