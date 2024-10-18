9cdff676b496f0ac8079d94387e941c774f32611
Teodor Taushanov <teodor.taushanov@sap.com>
2024-10-18 13:56:10 +0300

    chore: update example

--- packages/website/docs/_samples/fiori/NavigationLayout/Basic/main.js
+++ packages/website/docs/_samples/fiori/NavigationLayout/Basic/main.js
@@ -3,12 +3,12 @@ import "@ui5/webcomponents/dist/ToggleButton.js";
 
 import "@ui5/webcomponents-fiori/dist/NavigationLayout.js";
 import "@ui5/webcomponents-fiori/dist/SideNavigation.js";
+import "@ui5/webcomponents-fiori/dist/SideNavigationGroup.js";
 import "@ui5/webcomponents-fiori/dist/SideNavigationItem.js";
 import "@ui5/webcomponents-fiori/dist/SideNavigationSubItem.js";
 import "@ui5/webcomponents-fiori/dist/ShellBar.js";
 import "@ui5/webcomponents-fiori/dist/ShellBarItem.js";
 
-import "@ui5/webcomponents-icons/dist/da.js";
 import "@ui5/webcomponents-icons/dist/home.js";
 import "@ui5/webcomponents-icons/dist/group.js";
 import "@ui5/webcomponents-icons/dist/menu.js";
@@ -20,6 +20,9 @@ import "@ui5/webcomponents-icons/dist/background.js";
 import "@ui5/webcomponents-icons/dist/activity-assigned-to-goal.js";
 import "@ui5/webcomponents-icons/dist/action-settings.js";
 import "@ui5/webcomponents-icons/dist/chain-link.js";
+import "@ui5/webcomponents-icons/dist/document-text.js";
+import "@ui5/webcomponents-icons/dist/compare.js";
+import "@ui5/webcomponents-icons/dist/locked.js";
 
 document.querySelector("#startButton").addEventListener("click", function (event) {
 	nl1.sideCollapsed = !nl1.sideCollapsed;
