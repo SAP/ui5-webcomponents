cd1880096dfa88544e6df354adfdeaa3b60d8eba
Nayden Naydenov <nnaydenow.work@gmail.com>
2024-10-21 14:32:41 +0300

    chore: clean

---
+++ packages/testing/src/eslint.js
@@ -0,0 +1,32 @@
+export default [{
+	"files": ["**/cypress/**/*.ts"],
+
+	"plugins": [
+		"cypress"
+	],
+	extends: [
+		"plugin:cypress/recommended"
+	],
+	"env": {
+		"cypress/globals": true
+	},
+	"rules": {
+		"max-nested-callbacks": 0,
+		"@typescript-eslint/no-namespace": "off",
+		"cypress/no-assigning-return-values": "error",
+		"cypress/no-unnecessary-waiting": "error",
+		"cypress/assertion-before-screenshot": "warn",
+		"cypress/no-force": "warn",
+		"cypress/no-async-tests": "error",
+		"cypress/no-async-before": "error",
+		"cypress/no-pause": "error",
+		"import/no-extraneous-dependencies": [
+			"error",
+			{
+				"devDependencies": [
+					"**/cypress/**/*.ts"
+				]
+			}
+		]
+	}
+}];
