f27de5792665b082ec5d2ee94462b3622cd41ce8
Nayden Naydenov <nnaydenow.work@gmail.com>
2024-10-25 17:01:00 +0300

    chore: fix import

--- packages/main/cypress.config.js
+++ packages/main/cypress.config.js
@@ -1,3 +1,3 @@
-import { defineConfig } from "@ui5/webcomponents-testing/cypress/config.js";
+import { defineCypressConfig } from "@ui5/webcomponents-testing/cypress/config.js";
 
-export default defineConfig({});
+export default defineCypressConfig({});
