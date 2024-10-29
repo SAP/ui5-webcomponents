e3f2476195717079aa8e6dbeae58aa17c0ae537f
ilhan007 <ilhan.myumyun@sap.com>
2024-10-29 16:10:30 +0200

    chore: new test of issue-handling workflow

--- .github/actions/commentOnFixedIssues.mjs
+++ .github/actions/commentOnFixedIssues.mjs
@@ -1,10 +1,11 @@
 import { promises as fs } from 'node:fs';
 import { success as issueCommenter } from '@semantic-release/github';
 
-const getRelease = async (github, version) => {
+const getRelease = async (github, context, version) => {
+	const { owner, repo } = context.repo;
 	const releaseInfo = await github.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
-		owner: "SAP",
-		repo: "ui5-webcomponents",
+		owner,
+		repo,
 		tag: `v${version}`
 	});
 	const release = releaseInfo.data;
@@ -41,16 +42,17 @@ const getOctokitShim = (github) => {
  * Publishes comments to issues that are fixed and released.
  * @param options {object}
  * @param options.github {import("@octokit/rest/dist-types/index.d.ts").Octokit}
+ * @param options.context
  */
-export default async function run({ github }) {
+export default async function run({ github, context }) {
   const lerna = await fs.readFile(new URL('../../lerna.json', import.meta.url), 'utf8');
   const { version } = JSON.parse(lerna);
-  const release = await getRelease(github, version);
+  const release = await getRelease(github, context, version);
   const commits = getReleaseCommits(release);
   const Octokit = getOctokitShim(github);
 
   await issueCommenter({}, {
-    options: { repositoryUrl: `https://github.com/SAP/ui5-webcomponents/`},
+    options: { repositoryUrl: `https://github.com/${context.repo.owner}/${context.repo.repo}/`},
     commits,
     nextRelease: { version: `v${version}` },
     releases: [release],
