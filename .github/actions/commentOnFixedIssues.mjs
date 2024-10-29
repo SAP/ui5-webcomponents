16c6f42a933165b2103d80e40d7ad6c83592abcb
ilhan orhan <ilhan.orhan007@gmail.com>
2024-10-29 18:04:19 +0200

    ci: add issues commenting after release (#10107) [ci skip]

--- .github/actions/commentOnFixedIssues.mjs
+++ .github/actions/commentOnFixedIssues.mjs
@@ -1,30 +1,18 @@
 import { promises as fs } from 'node:fs';
 import { success as issueCommenter } from '@semantic-release/github';
 
-const getRelease = async (github, version) => {
-	const releaseInfo = await github.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
-		owner: context.repo.owner,
-		repo: context.repo.repo,
-		tag: `v${version}`
-	});
-	const release = releaseInfo.data;
-	release.url = release.html_url;
+const getReleaseCommits = (releaseBody) => {
+	const commits = new Set();
 
-	return release;
-};
-
-const getReleaseCommits = (release) => {
-	const commits = [];
-
-	let result;
-	do {
-		result = /commit\/(?<sha>\w{40})/gm.exec(release.body);
-		if (result?.groups?.sha) {
-			commits.push({ hash: result.groups.sha });
+	const shaRegex = /commit\/(?<sha>\w{40})/g;
+	for (const match of releaseBody.matchAll(shaRegex)) {
+		if (match.groups?.sha) {
+			commits.add({ hash: match.groups.sha });
 		}
-	} while (result);
+	}
 
-	return commits;
+	// Converting Set back to an array to get unique commits as array
+	return Array.from(commits);
 };
 
 const getOctokitShim = (github) => {
@@ -37,23 +25,46 @@ const getOctokitShim = (github) => {
 
 /**
  * Publishes comments to issues that are fixed and released.
- * @param options.github
+ * @param options {object}
+ * @param options.github {import("@octokit/rest/dist-types/index.d.ts").Octokit}
  * @param options.context
- * @returns {Promise<void>}
  */
 export default async function run({ github, context }) {
-  const lerna = await fs.readFile(new URL('../../lerna.json', import.meta.url), 'utf8');
-  const { version } = JSON.parse(lerna);
-  const release = await getRelease(version);
-  const commits = getReleaseCommits(release);
-  const Octokit = getOctokitShim(github);
+	const lerna = await fs.readFile(new URL('../../lerna.json', import.meta.url), 'utf8');
+	const { version } = JSON.parse(lerna);
 
-  await issueCommenter({}, {
-    options: { repositoryUrl: `https://github.com/${context.repo.owner}/${context.repo.repo}`},
-    commits,
-    nextRelease: { version: `v${version}` },
-    releases: [release],
-    logger: console,
-    env: process.env
-  }, { Octokit });
+	const { owner, repo } = context.repo;
+	const releaseInfo = await github.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
+		owner,
+		repo,
+		tag: `v${version}`
+	});
+	const release = releaseInfo.data;
+	release.url = release.html_url;
+	
+
+	const commits = await getReleaseCommits(release.body);
+	
+	
+	try {
+		const semanticReleaseContext = {
+			options: { 
+				repositoryUrl: `https://github.com/${owner}/${repo}` 
+			},
+			commits,
+			nextRelease: { version: `v${version}` },
+			releases: [release],
+			logger: console,
+			env: process.env
+		};
+		const Octokit = getOctokitShim(github);
+
+		console.log('Commits:', commits.toString());
+		console.log('semanticReleaseContext:', semanticReleaseContext);
+
+		await issueCommenter({}, semanticReleaseContext, { Octokit });
+
+	} catch (error) {
+		console.error('Error in posting comment:', error);
+	}
 }
