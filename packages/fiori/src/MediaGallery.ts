0d8472bdbe4405be5c79d1e015a51f6d77c548c0
Peter Skelin <petar.skelin@sap.com>
2024-11-01 13:10:51 +0200

    chore: check build

--- packages/fiori/src/MediaGallery.ts
+++ packages/fiori/src/MediaGallery.ts
@@ -247,7 +247,7 @@ class MediaGallery extends UI5Element {
 
 	_updateSelection() {
 		let itemToSelect = this.items.find(item => item.selected);
-		if (!itemToSelect || !this._isSelectableItem(itemToSelect)) {
+		if (!itemToSelect || !this._isSelectableItem(itemToSelect!)) {
 			itemToSelect = this._findSelectableItem();
 		}
 		if (itemToSelect && itemToSelect !== this._selectedItem) {
