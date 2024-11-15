37469d0263a592591232a550578907f586ebf5c6
Felix Schubert <felix.schubert@sap.com>
2024-11-15 13:12:44 +0100

    feat(ui5-table): table row actions - rebase
    First push after the last rebase

--- packages/main/src/TableRow.ts
+++ packages/main/src/TableRow.ts
@@ -3,11 +3,15 @@ import slot from "@ui5/webcomponents-base/dist/decorators/slot.js";
 import property from "@ui5/webcomponents-base/dist/decorators/property.js";
 import { isEnter } from "@ui5/webcomponents-base/dist/Keys.js";
 import getActiveElement from "@ui5/webcomponents-base/dist/util/getActiveElement.js";
+import Button from "./Button.js";
+import Popover from "./Popover.js";
+import RadioButton from "./RadioButton.js";
+import TableRowAction from "./TableRowAction.js";
 import TableRowTemplate from "./generated/templates/TableRowTemplate.lit.js";
 import TableRowBase from "./TableRowBase.js";
 import TableRowCss from "./generated/themes/TableRow.css.js";
 import TableCell from "./TableCell.js";
-import RadioButton from "./RadioButton.js";
+import Menu from "./Menu.js";
 
 /**
  * @class
@@ -30,7 +34,7 @@ import RadioButton from "./RadioButton.js";
 	tag: "ui5-table-row",
 	styles: [TableRowBase.styles, TableRowCss],
 	template: TableRowTemplate,
-	dependencies: [...TableRowBase.dependencies, RadioButton, TableCell],
+	dependencies: [...TableRowBase.dependencies, RadioButton, TableCell, Button, Popover, TableRowAction, Menu],
 })
 class TableRow extends TableRowBase {
 	/**
@@ -50,6 +54,38 @@ class TableRow extends TableRowBase {
 		},
 	})
 	cells!: Array<TableCell>;
+
+	/**
+	 * Defines the actions of the component.
+	 *
+	 * **Note:** Use `ui5-table-row-action` for the intended design.
+	 *
+	 * @public
+	 */
+	@slot({
+		type: HTMLElement,
+		individualSlots: true,
+		invalidateOnChildChange: true,
+	})
+	actions!: Array<TableRowAction>;
+
+	/**
+	 * Defines the visible actions of the component.
+	 *
+	 * **Note:** Use `ui5-table-row-action` for the intended design.
+	 *
+	 * @public
+	 */
+	visibleActions : Array<TableRowAction> = [];
+
+	/**
+	 * Defines the visible actions of the component.
+	 *
+	 * **Note:** Use `ui5-table-row-action` for the intended design.
+	 *
+	 * @public
+	 */
+	popoverActions : Array<TableRowAction> = [];
 
 	/**
 	 * Unique identifier of the row.
@@ -103,6 +139,7 @@ class TableRow extends TableRowBase {
 
 	onBeforeRendering() {
 		super.onBeforeRendering();
+		this._updateActions();
 		this.toggleAttribute("_interactive", this._isInteractive);
 		if (this.position !== -1) {
 			this.setAttribute("aria-rowindex", `${this.position + 1}`);
@@ -143,6 +180,22 @@ class TableRow extends TableRowBase {
 		}
 	}
 
+	_onTableRowActionClick(e: MouseEvent) {
+		e?.stopImmediatePropagation();
+
+		this._table?._onTableRowActionPress(e.currentTarget as TableRowAction);
+	}
+
+	_handleRowActionPopoverClick(e: KeyboardEvent) {
+		const tableRowActionPopoverButton = e.currentTarget as HTMLElement;
+		const tableRowActionPopover = tableRowActionPopoverButton.nextSibling as Menu;
+
+		if (tableRowActionPopoverButton && tableRowActionPopover) {
+			tableRowActionPopover.opener = tableRowActionPopoverButton;
+			tableRowActionPopover.open = !tableRowActionPopover.open;
+		}
+	}
+
 	_onkeyup() {
 		this.removeAttribute("_active");
 	}
@@ -151,8 +204,59 @@ class TableRow extends TableRowBase {
 		this.removeAttribute("_active");
 	}
 
+	_updateActions() {
+		if (this.visibleActions.length === 0 && this.popoverActions.length === 0) { // onBeforeRendering is called twice
+			const actionsLength = this.actions.filter(action => { return action.type !== "Navigation"; }).length;
+			const visibleActionsCount = this._visibleActionsCount;
+			let actionCounter = 0;
+			while (this.visibleActions.length < visibleActionsCount && actionCounter < this.actions.length && this.visibleActions.length < actionsLength) {
+				if (this.actions[actionCounter].type !== "Navigation") {
+					this.visibleActions.push(this.actions[actionCounter]);
+				}
+				actionCounter++;
+			}
+			for (let i = visibleActionsCount; i < this.actions.length; i++) {
+				if (this.actions[i].type !== "Navigation") {
+					this.popoverActions.push(this.actions[i]);
+				}
+			}
+			this.popoverActions.forEach(action => { action.menuItem = true; });
+		}
+	}
+
+	get _visibleActionsCount() {
+		const actionsLength = this.actions.filter(action => { return action.type !== "Navigation"; }).length;
+		const visibleActionsPerRowDefault = 2;
+		// handle case with only one visible rowaction
+		const actionlengthRowActionCount = (actionsLength > 1 ? 0 : 1);
+		let rowActionCount = this._table?.rowActionCount ?? visibleActionsPerRowDefault;
+		rowActionCount = rowActionCount === 1 ? actionlengthRowActionCount : rowActionCount;
+
+		let visibleActionsCount = Math.min(rowActionCount, actionsLength, visibleActionsPerRowDefault);
+		if (this._hasRowActionNavigation && visibleActionsCount === 2 && actionsLength > 2) {
+			visibleActionsCount = 1;
+		}
+		return visibleActionsCount;
+	}
+
 	get _isInteractive() {
 		return this.interactive;
+	}
+
+	get _hasRowActions() {
+		return this._table?.querySelector("ui5-table-row-action") !== null;
+	}
+
+	get _hasRowActionNavigation() {
+		return this.actions.find(action => action.type === "Navigation") !== undefined;
+	}
+
+	get _hasRowActionPopover() {
+		return this.popoverActions.length !== 0;
+	}
+
+	get _actionCount() {
+		return this.actions.length;
 	}
 }
 
