b1ffd45e264a6a3ea5b8eb92b1c6ff2f21c5012e
Petar Dimov <petar.dimov@sap.com>
2024-11-21 15:52:08 +0200

    test: add tests for popover

--- packages/main/cypress/specs/ExpandableText.cy.ts
+++ packages/main/cypress/specs/ExpandableText.cy.ts
@@ -1,6 +1,11 @@
 import { html } from "lit";
 import "../../src/ExpandableText.js";
-import "../../src/Label.js";
+
+// Test keyboard support
+// Test SR attributes
+// Test RTL
+// Test empty indicator
+// Test mobile popover
 
 describe("ExpandableText", () => {
 	describe("Rendering and Interaction", () => {
@@ -89,6 +94,63 @@ describe("ExpandableText", () => {
 				.contains(text.substring(0, maxCharacters))
 				.should("exist");
 		});
+	});
 
+	describe("Rendering and Interaction with overflowMode=Popover", () => {
+		it("Toggling 'Show More' and 'Show Less'", () => {
+			const text = "This is a very long text that should be displayed";
+			const maxCharacters = 5;
+
+			cy.mount(html`<ui5-expandable-text text=${text} max-characters="${maxCharacters}" overflow-mode="Popover"></ui5-expandable-text>`);
+
+			cy.get("[ui5-expandable-text]")
+				.shadow()
+				.find("[ui5-text]")
+				.contains(text.substring(0, maxCharacters))
+				.should("exist");
+
+			cy.get("[ui5-expandable-text]")
+				.shadow()
+				.find(".ui5-exp-text-ellipsis")
+				.contains("... ")
+				.should("exist");
+
+			cy.get("[ui5-expandable-text]")
+				.shadow()
+				.find("[ui5-link].ui5-exp-text-toggle")
+				.contains("Show More")
+				.realClick();
+
+			cy.get("[ui5-expandable-text]")
+				.shadow()
+				.find("[ui5-link].ui5-exp-text-toggle")
+				.invoke("attr", "id")
+				.as("expectedOpenerId");
+
+			cy.get("[ui5-expandable-text]")
+				.shadow()
+				.find("[ui5-responsive-popover]")
+				.should("exist")
+				.should("have.attr", "open");
+
+			cy.get("[ui5-expandable-text]")
+				.shadow()
+				.find("[ui5-responsive-popover]")
+				.invoke("attr", "opener")
+				.then(function testOpenerId(opener) {
+					expect(opener).to.equal(this.expectedOpenerId);
+				});
+
+			cy.get("[ui5-expandable-text]")
+				.shadow()
+				.find("[ui5-link].ui5-exp-text-toggle")
+				.contains("Show Less")
+				.realClick();
+
+			cy.get("[ui5-expandable-text]")
+				.shadow()
+				.find("[ui5-responsive-popover]")
+				.should("not.have.attr", "open");
+		});
 	});
 });
