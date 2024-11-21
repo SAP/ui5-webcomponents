aae938eb4cf2e5021fd15c7152445fff53b2cb16
Petar Dimov <petar.dimov@sap.com>
2024-11-21 16:45:01 +0200

    feat: take Show More and Show Less from messagebundle

--- packages/main/src/ExpandableText.ts
+++ packages/main/src/ExpandableText.ts
@@ -1,12 +1,18 @@
 import UI5Element from "@ui5/webcomponents-base/dist/UI5Element.js";
 import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
 import property from "@ui5/webcomponents-base/dist/decorators/property.js";
+import i18n from "@ui5/webcomponents-base/dist/decorators/i18n.js";
 import litRender from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+import type I18nBundle from "@ui5/webcomponents-base/dist/i18nBundle.js";
 import Text from "./Text.js";
 import Link from "./Link.js";
 import ResponsivePopover from "./ResponsivePopover.js";
 import ExpandableTextOverflowMode from "./types/ExpandableTextOverflowMode.js";
 import type TextEmptyIndicatorMode from "./types/TextEmptyIndicatorMode.js";
+import {
+	EXPANDABLE_TEXT_SHOW_LESS,
+	EXPANDABLE_TEXT_SHOW_MORE,
+} from "./generated/i18n/i18n-defaults.js";
 
 // Template
 import ExpandableTextTemplate from "./generated/templates/ExpandableTextTemplate.lit.js";
@@ -78,11 +84,9 @@ class ExpandableText extends UI5Element {
 
 	@property({ type: Boolean })
 	_expanded = false;
-	_popoverClosed = false;
 
-	onAfterRendering() {
-		this._popoverClosed = false;
-	}
+	@i18n("@ui5/webcomponents")
+	static i18nBundle: I18nBundle;
 
 	getFocusDomRef(): HTMLElement | undefined {
 		if (this._usePopover) {
@@ -116,27 +120,15 @@ class ExpandableText extends UI5Element {
 		return "... ";
 	}
 
-	get _toggleText() {
-		return this._expanded ? "Show Less" : "Show More";
-	}
-
-	_handleShowLessClick() {
-		this._expanded = false;
-	}
-
-	_handleShowMoreClick() {
-		this._expanded = true;
+	get _textForToggle() {
+		return this._expanded ? ExpandableText.i18nBundle.getText(EXPANDABLE_TEXT_SHOW_LESS) : ExpandableText.i18nBundle.getText(EXPANDABLE_TEXT_SHOW_MORE);
 	}
 
 	_handlePopoverClose() {
 		this._toggleExpanded();
-		this._popoverClosed = true;
 	}
 
 	_toggleExpanded() {
-		if (this._popoverClosed) {
-			return;
-		}
 		this._expanded = !this._expanded;
 	}
 }
