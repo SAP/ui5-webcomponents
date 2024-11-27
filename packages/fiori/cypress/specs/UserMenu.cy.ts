cd0f2eeba8a148af162cc88903d830f76fb3cb7e
Adrian Bobev <adrian.bobev@sap.com>
2024-11-27 23:59:39 +0200

    chore: add tests for events

--- packages/fiori/cypress/specs/UserMenu.cy.ts
+++ packages/fiori/cypress/specs/UserMenu.cy.ts
@@ -12,12 +12,12 @@ describe("Initial rendering", () => {
 		cy.mount(html`<ui5-button id="openUserMenuBtn">Open User Menu</ui5-button>
 						<ui5-user-menu open opener="openUserMenuBtn">
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").shadow().find("[ui5-responsive-popover]").as("responsivePopover");
 		cy.get("@responsivePopover").should("exist");
-		cy.get("@responsivePopover").find("ui5-button").contains("Sign Out");
-		cy.get("@responsivePopover").find("ui5-button").should("have.length", 1);
+		cy.get("@responsivePopover").find("[ui5-button]").contains("Sign Out");
+		cy.get("@responsivePopover").find("[ui5-button]").should("have.length", 1);
 	});
 
 	it("tests config show-manage-account", () => {
@@ -29,12 +29,12 @@ describe("Initial rendering", () => {
 												   description="Delivery Manager, SAP SE">
 							</ui5-user-menu-account>
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").shadow().find("[ui5-responsive-popover]").as("responsivePopover");
 		cy.get("@responsivePopover").should("exist");
-		cy.get("@responsivePopover").find("ui5-button").contains("Manage account");
-		cy.get("@responsivePopover").find("ui5-button").should("have.length", 2);
+		cy.get("@responsivePopover").find("[ui5-button]").contains("Manage account");
+		cy.get("@responsivePopover").find("[ui5-button]").should("have.length", 2);
 	});
 
 	it("tests config show-other-accounts", () => {
@@ -52,12 +52,12 @@ describe("Initial rendering", () => {
 												   additional-text="test.alian.chevalier@sap.com">
 							</ui5-user-menu-account>
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").shadow().find("[ui5-responsive-popover]").as("responsivePopover");
 		cy.get("@responsivePopover").should("exist");
-		cy.get("@responsivePopover").find("ui5-panel").contains("Other accounts (1)");
-		cy.get("@responsivePopover").find("ui5-button").should("have.length", 1);
+		cy.get("@responsivePopover").find("[ui5-panel]").contains("Other accounts (1)");
+		cy.get("@responsivePopover").find("[ui5-button]").should("have.length", 1);
 	});
 
 	it("tests config show-add-account", () => {
@@ -75,12 +75,12 @@ describe("Initial rendering", () => {
 												   additional-text="test.alian.chevalier@sap.com">
 							</ui5-user-menu-account>
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").shadow().find("[ui5-responsive-popover]").as("responsivePopover");
 		cy.get("@responsivePopover").should("exist");
 		cy.get("@responsivePopover").find(".ui5-pm-add-account-btn").should("exist");
-		cy.get("@responsivePopover").find("ui5-button").should("have.length", 2);
+		cy.get("@responsivePopover").find("[ui5-button]").should("have.length", 2);
 	});
 });
 
@@ -97,7 +97,7 @@ describe("Menu configuration", () => {
 							<ui5-user-menu-item text="Product-specific account action"
 												data-id="account-action2"></ui5-user-menu-item>
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").find("[ui5-user-menu-item]").as("userMenuItems");
 		cy.get("@userMenuItems").should("exist");
@@ -120,7 +120,7 @@ describe("Menu configuration", () => {
 								<ui5-user-menu-item text="Terms of Use" data-id="terms-of-use"></ui5-user-menu-item>
 							</ui5-user-menu-item>
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").find("[ui5-user-menu-item]").as("userMenuItems");
 		cy.get("@userMenuItems").should("exist");
@@ -139,7 +139,7 @@ describe("Menu configuration", () => {
 							</ui5-user-menu-account>
 							<ui5-user-menu-item icon="action-settings" text="Setting" data-id="setting"></ui5-user-menu-item>
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").find("[ui5-user-menu-item]").as("userMenuItems");
 		cy.get("@userMenuItems").should("exist");
@@ -158,7 +158,7 @@ describe("Avatar configuration", () => {
 								   description="Delivery Manager, SAP SE">
 			</ui5-user-menu-account>
 		</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").shadow().find("[ui5-avatar]").as("avatar");
 		cy.get("@avatar").should("exist");
@@ -176,7 +176,7 @@ describe("Avatar configuration", () => {
 												   description="Delivery Manager, SAP SE">
 							</ui5-user-menu-account>
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").shadow().find("[ui5-avatar]").as("avatar");
 		cy.get("@avatar").should("exist");
@@ -194,7 +194,7 @@ describe("Avatar configuration", () => {
 												   description="Delivery Manager, SAP SE">
 							</ui5-user-menu-account>
 						</ui5-user-menu>`);
-		cy.get("ui5-user-menu").as("userMenu");
+		cy.get("[ui5-user-menu]").as("userMenu");
 		cy.get("@userMenu").should("exist");
 		cy.get("@userMenu").shadow().find("[ui5-avatar]").as("avatar");
 		cy.get("@avatar").should("exist");
@@ -202,5 +202,144 @@ describe("Avatar configuration", () => {
 		cy.get("@avatar").find("img").as("image");
 		cy.get("@image").should("have.length", 1);
 		cy.get("@image").should("have.attr", "src", "./../../test/pages/img/man_avatar_1.png");
+	});
+});
+
+describe("Events", () => {
+	it("tests avatar-click event", () => {
+		cy.mount(html`<ui5-button id="openUserMenuBtn">Open User Menu</ui5-button>
+						<ui5-user-menu open opener="openUserMenuBtn">
+							<ui5-user-menu-account slot="accounts"
+												   title-text="Alain Chevalier 1"
+							</ui5-user-menu-account>
+						</ui5-user-menu>`);
+		cy.get("[ui5-user-menu]").as("userMenu");
+		cy.get("@userMenu")
+			.shadow()
+			.find("[ui5-responsive-popover]")
+			.find("[ui5-avatar]")
+			.as("avatar");
+
+		cy.get("@userMenu")
+			.then($avatar => {
+				$avatar.get(0).addEventListener("avatar-click", cy.stub().as("clicked"));
+			});
+
+		cy.get("@avatar").click();
+
+		cy.get("@clicked").should("have.been.calledOnce");
+	});
+
+	it("tests manage-account-click event", () => {
+		cy.mount(html`<ui5-button id="openUserMenuBtn">Open User Menu</ui5-button>
+						<ui5-user-menu open opener="openUserMenuBtn" show-manage-account>
+							<ui5-user-menu-account slot="accounts"
+												   title-text="Alain Chevalier 1"
+							</ui5-user-menu-account>
+						</ui5-user-menu>`);
+		cy.get("[ui5-user-menu]").as("userMenu");
+		cy.get("@userMenu")
+			.shadow()
+			.find("[ui5-button]")
+			.eq(0)
+			.as("manageAccountBtn");
+
+		cy.get("@userMenu").then($userMenu => {
+			$userMenu.get(0).addEventListener("manage-account-click", cy.stub().as("clicked"));
+		});
+
+		cy.get("@manageAccountBtn").click();
+
+		cy.get("@clicked").should("have.been.calledOnce");
+	});
+
+	it("tests add-account-click event", () => {
+		cy.mount(html`<ui5-button id="openUserMenuBtn">Open User Menu</ui5-button>
+						<ui5-user-menu open opener="openUserMenuBtn" show-add-account show-other-accounts>
+						</ui5-user-menu>`);
+		cy.get("[ui5-user-menu]").as("userMenu");
+		cy.get("@userMenu")
+			.shadow()
+			.find(".ui5-pm-add-account-btn")
+			.as("addAccountBtn");
+
+		cy.get("@userMenu")
+			.then($userMenu => {
+				$userMenu.get(0).addEventListener("add-account-click", cy.stub().as("clicked"));
+			});
+
+		cy.get("@addAccountBtn").click();
+
+		cy.get("@clicked").should("have.been.calledOnce");
+	});
+
+	it("tests change-account event", () => {
+		cy.mount(html`<ui5-button id="openUserMenuBtn">Open User Menu</ui5-button>
+						<ui5-user-menu open opener="openUserMenuBtn" show-other-accounts>
+							<ui5-user-menu-account slot="accounts" title-text="Alain Chevalier 1">
+							</ui5-user-menu-account>
+							<ui5-user-menu-account slot="accounts" title-text="Alain Chevalier 2">
+							</ui5-user-menu-account>
+						</ui5-user-menu>`);
+		cy.get("[ui5-user-menu]").as("userMenu");
+		cy.get("@userMenu")
+			.shadow()
+			.find("[ui5-panel]")
+			.as("otherAccounts");
+
+		cy.get("@userMenu").then($userMenu => {
+			$userMenu.get(0).addEventListener("change-account", cy.stub().as("changedAccount"));
+		});
+
+		cy.get("@otherAccounts")
+			.shadow()
+			.find("[ui5-button]")
+			.click();
+
+		cy.get("@otherAccounts")
+			.find("[ui5-li-custom]")
+			.click();
+
+		cy.get("@changedAccount").should("have.been.calledOnce");
+		cy.get("@changedAccount").its("args.0.0.detail.prevSelectedAccount").should("have.property", "titleText", "Alain Chevalier 1");
+		cy.get("@changedAccount").its("args.0.0.detail.selectedAccount").should("have.property", "titleText", "Alain Chevalier 2");
+	});
+
+	it("tests item-click event", () => {
+		cy.mount(html`<ui5-button id="openUserMenuBtn">Open User Menu</ui5-button>
+						<ui5-user-menu open opener="openUserMenuBtn">
+							<ui5-user-menu-item text="Setting" data-id="setting"></ui5-user-menu-item>
+						</ui5-user-menu>`);
+		cy.get("[ui5-user-menu]").as("userMenu");
+		cy.get("@userMenu")
+			.find("[ui5-user-menu-item]")
+			.as("userMenuItem");
+
+		cy.get("@userMenu")
+			.then($userMenu => {
+				$userMenu.get(0).addEventListener("item-click", cy.stub().as("clicked"));
+			});
+
+		cy.get("@userMenuItem").click();
+
+		cy.get("@clicked").should("have.been.calledOnce");
+		cy.get("@clicked").its("args.0.0.detail.item").should("have.property", "text", "Setting");
+	});
+
+	it("tests sign-out-click event", () => {
+		cy.mount(html`<ui5-button id="openUserMenuBtn">Open User Menu</ui5-button>
+						<ui5-user-menu open opener="openUserMenuBtn">
+						</ui5-user-menu>`);
+		cy.get("[ui5-user-menu]").as("userMenu");
+		cy.get("@userMenu").shadow().find("[ui5-button]").as("signOutBtn");
+
+		cy.get("@userMenu")
+			.then($userMenu => {
+				$userMenu.get(0).addEventListener("sign-out-click", cy.stub().as("clicked"));
+			});
+
+		cy.get("@signOutBtn").click();
+
+		cy.get("@clicked").should("have.been.calledOnce");
 	});
 });
