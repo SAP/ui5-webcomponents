6430b8ed1f62c240ddb1f624b40b6a9ce1527963
Peter Skelin <petar.skelin@sap.com>
2024-12-02 22:46:32 +0200

    chore: improve error message when eventDetails is missing

--- packages/base/src/decorators/event-strict.ts
+++ packages/base/src/decorators/event-strict.ts
@@ -1,9 +1,15 @@
 import type UI5Element from "../UI5Element.js";
 
-type IsAny<T> = 0 extends (1 & T) ? true : false;
-type EventDetailsValues<T extends UI5Element> = T["eventDetails"][keyof T["eventDetails"]];
-type HasOwnEventDetails<T extends UI5Element> = IsAny<EventDetailsValues<T>> extends true ? false : true;
-type ExtractEventKeys<T extends typeof UI5Element> = HasOwnEventDetails<InstanceType<T>> extends true ? keyof InstanceType<T>["eventDetails"] : "eventDetails missing or has value with type any"
+// type IsAny<T> = 0 extends (1 & T) ? true : false;
+// type EventDetailsValues<T extends UI5Element> = T["eventDetails"][keyof T["eventDetails"]];
+// type HasOwnEventDetails<T extends UI5Element> = IsAny<EventDetailsValues<T>> extends true ? false : true;
+// type ExtractEventKeys<T extends typeof UI5Element> = HasOwnEventDetails<InstanceType<T>> extends true ? keyof InstanceType<T>["eventDetails"] : "eventDetails missing or has value with type any"
+
+// type IsAny<T> = 0 extends (1 & T) ? true : false;
+// type EventDetailsValues<T extends UI5Element> = T["eventDetails"][keyof T["eventDetails"]];
+// type HasOwnEventDetails<T extends UI5Element> = IsAny<EventDetailsValues<T>> extends true ? false : true;
+type EventDetailKeys<T extends typeof UI5Element> = keyof InstanceType<T>["eventDetails"];
+type ExtractEventKeys<T extends typeof UI5Element> = EventDetailKeys<T> extends never ? "event name not found in `eventDetails` field" : keyof InstanceType<T>["eventDetails"];
 
 /**
  * Returns an event class decorator.
