fd439ce1898afe7be90351ad060e88874d64f808
ilhan007 <ilhan.myumyun@sap.com>
2024-12-02 19:48:12 +0200

    chore: drop indicator

---
+++ packages/main/src/DropIndicatorTemplate.tsx
@@ -0,0 +1,10 @@
+import clsx from "clsx";
+import MovePlacement from "@ui5/webcomponents-base/dist/types/MovePlacement.js";
+import type DropIndicator from "./DropIndicator.js";
+
+export default function (this: DropIndicator) {
+	return <div class={clsx({
+			"ui5-di-rect": this.placement === MovePlacement.On,
+			"ui5-di-needle": this.placement !== MovePlacement.On,
+	})}></div>
+};
