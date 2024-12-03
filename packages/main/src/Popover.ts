a8874fe3ee7456bf2c0a0e8066d7f870208472d7
Petar Dimov <petar.dimov@sap.com>
2024-12-03 11:04:33 +0200

    chore: update popover workaround

--- packages/main/src/Popover.ts
+++ packages/main/src/Popover.ts
@@ -303,7 +303,16 @@ class Popover extends Popup {
 		}
 
 		const rootNode = this.getRootNode();
-		const openerHTMLElement = rootNode instanceof DocumentFragment ? rootNode.getElementById(opener) : document.getElementById(opener);
+
+		if (!(rootNode instanceof ShadowRoot || rootNode instanceof Document)) {
+			return;
+		}
+
+		let openerHTMLElement = rootNode.getElementById(opener);
+
+		if (rootNode instanceof ShadowRoot && !openerHTMLElement) {
+			openerHTMLElement = document.getElementById(opener);
+		}
 
 		if (openerHTMLElement && this._isUI5Element(openerHTMLElement)) {
 			return openerHTMLElement.getFocusDomRef();
