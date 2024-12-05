4562ab3035e7564712616d1b3206abbd0c32d279
Stefan Dimitrov <stefan.dimitrov02@sap.com>
2024-12-05 09:41:25 +0200

    feat(ui5-message-strip): fix tests

--- packages/main/test/specs/MessageStrip.spec.js
+++ packages/main/test/specs/MessageStrip.spec.js
@@ -76,16 +76,24 @@ describe("ARIA Support", () => {
 		await browser.url(`test/pages/MessageStrip.html`);
 	});
 
-	it("Test close button title text", async () => {
+	it("Test hidden text element content", async () => {
 
-		const closeButton = await browser.$("#messageStrip").shadow$(".ui5-message-strip-close-button").shadow$("button");
+		const messageStrip = await browser.$("#messageStrip");
+		const customMessageStrip = await browser.$("#colorSet1ColorScheme1");
+		let invisibleText = await messageStrip.shadow$(".ui5-hidden-text");
+		let customMessageStripInvisibleText = await customMessageStrip.shadow$(".ui5-hidden-text");
 		let resourceBundleText = null;
 
 		resourceBundleText = await browser.executeAsync(done => {
 			const messageStrip = document.getElementById("messageStrip");
-			done(messageStrip.constructor.i18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_CLOSE_BUTTON_INFORMATION));
+			const msgStripi18nBundle = messageStrip.constructor.i18nBundle;
+			done({
+				information: `${msgStripi18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_INFORMATION)} ${msgStripi18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_CLOSABLE)}`,
+				custom: `${msgStripi18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_CUSTOM)} ${msgStripi18nBundle.getText(window["sap-ui-webcomponents-bundle"].defaultTexts.MESSAGE_STRIP_CLOSABLE)}`,
+			});
 		});
 
-		assert.strictEqual(await closeButton.getAttribute("title"), resourceBundleText, "Close button title is correct");
+		assert.strictEqual(await invisibleText.getText(), resourceBundleText.information, "Hidden element content is correct");
+		assert.strictEqual(await customMessageStripInvisibleText.getText(), resourceBundleText.custom, "Hidden element content is correct");
 	});
 });
