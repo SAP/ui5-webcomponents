93a8381331413965ef867e50898fedbeb14a77d4
Nayden Naydenov <nnaydenow.work@gmail.com>
2024-12-13 17:04:49 +0200

    chore(ui5-side-navigation): correct type definitions

--- packages/fiori/src/SideNavigation.ts
+++ packages/fiori/src/SideNavigation.ts
@@ -24,6 +24,7 @@ import {
 } from "@ui5/webcomponents-base/dist/Keys.js";
 import NavigationMode from "@ui5/webcomponents-base/dist/types/NavigationMode.js";
 import type SideNavigationItemBase from "./SideNavigationItemBase.js";
+import { isInstanceOfSideNavigationSelectableItemBase } from "./SideNavigationSelectableItemBase.js";
 import { isInstanceOfSideNavigationItemBase } from "./SideNavigationItemBase.js";
 import type SideNavigationSelectableItemBase from "./SideNavigationSelectableItemBase.js";
 import SideNavigationItem, { isInstanceOfSideNavigationItem } from "./SideNavigationItem.js";
@@ -480,7 +481,7 @@ class SideNavigation extends UI5Element {
 
 		itemsHeight = overflowItem.offsetHeight;
 
-		const selectedItem = overflowItems.find(item => item._selected);
+		const selectedItem = overflowItems.filter(isInstanceOfSideNavigationSelectableItemBase).find(item => item._selected);
 
 		if (selectedItem) {
 			const selectedItemDomRef = selectedItem.getDomRef();
@@ -539,10 +540,10 @@ class SideNavigation extends UI5Element {
 		return this._getSelectableItems(items).find(item => item._selected);
 	}
 
-	get overflowItems() : Array<SideNavigationItem> {
+	get overflowItems() : Array<HTMLElement> {
 		return (this.items as Array<SideNavigationItem | SideNavigationGroup>).reduce((result, item) => {
 			return result.concat(item.overflowItems);
-		}, new Array<SideNavigationItem>());
+		}, new Array<HTMLElement>());
 	}
 
 	_handleItemClick(e: KeyboardEvent | MouseEvent, item: SideNavigationSelectableItemBase) {
@@ -582,7 +583,7 @@ class SideNavigation extends UI5Element {
 		const result: Array<SideNavigationItem> = [];
 
 		this.overflowItems.forEach(item => {
-			if (item.hasAttribute("ui5-side-navigation-item") && item.classList.contains(overflowClass)) {
+			if (isInstanceOfSideNavigationItem(item) && item.classList.contains(overflowClass)) {
 				 result.push(item);
 			}
 		});
