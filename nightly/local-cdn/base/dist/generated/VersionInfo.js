4a1f1c6a7c48a6915e9cd15ea8a04d425200d9f3
ilhan007 <ilhan007@users.noreply.github.com>
2024-12-25 00:09:40 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents@8109bfd85d54374f007564c66df1404fef1a340c 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.2",
     isNext: false,
-    buildTime: 1734998777,
+    buildTime: 1735085158,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
