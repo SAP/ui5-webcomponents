c65a9055554299947c4501ec73bc57908fc2f66d
Vladislav Tasev <vladislav.tasev@sap.com>
2025-01-03 10:55:00 +0200

    chore: rework the more colors feature

--- packages/main/src/features/ColorPaletteMoreColors.ts
+++ packages/main/src/features/ColorPaletteMoreColors.ts
@@ -1,5 +1,6 @@
 import { ComponentFeature, registerComponentFeature } from "@ui5/webcomponents-base/dist/FeaturesRegistry.js";
 import type I18nBundle from "@ui5/webcomponents-base/dist/i18nBundle.js";
+import ColorPaletteMoreColorsTemplate from "./ColorPaletteMoreColorsTemplate.js";
 
 import {
 	COLOR_PALETTE_DIALOG_CANCEL_BUTTON,
@@ -9,6 +10,10 @@ import {
 
 class ColorPaletteMoreColors extends ComponentFeature {
 	static i18nBundle: I18nBundle;
+
+	get template() {
+		return ColorPaletteMoreColorsTemplate;
+	}
 
 	get colorPaletteDialogTitle() {
 		return ColorPaletteMoreColors.i18nBundle.getText(COLOR_PALETTE_DIALOG_TITLE);
