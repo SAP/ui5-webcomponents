d5cabdea6884391899af5f5e9d4984cde25465bd
Adrian Bobev <adrian.bobev@sap.com>
2025-01-05 23:10:04 +0200

    chore: position busy indicator inside the setting item

--- packages/fiori/src/SettingItemTemplate.tsx
+++ packages/fiori/src/SettingItemTemplate.tsx
@@ -6,13 +6,31 @@ import Tab from "@ui5/webcomponents/dist/Tab.js";
 import Title from "@ui5/webcomponents/dist/Title.js";
 
 export default function SettingItemTemplate(this: SettingItem) {
+	const content = this._shouldHaveTabs ?
+		<TabContainer content-background-design="Transparent" class="ui5-setting-item-tabs">
+			{this.views.map(view =>
+				<Tab text={view.text}>
+					<slot name={view._individualSlot} />
+				</Tab>
+			)}
+		</TabContainer>
+		:
+		<>
+			{this.views.map(view =>
+				<div class="ui5-setting-item-view-container">
+					<slot name={view._individualSlot} />
+				</div>
+			)}
+		</>;
+	const loadingState = <div class="ui5-setting-item-loading-container">
+		<BusyIndicator
+			active={this.loading}
+			class="ui5-setting-item-busy">
+		</BusyIndicator>
+	</div>;
+
 	return (
 		<div class="ui5-setting-item">
-			<BusyIndicator
-				active={this.loading}
-				class="ui5-setting-item-busy">
-			</BusyIndicator>
-
 			<header class="ui5-setting-item-header-container">
 				<div class="ui5-setting-item-header">
 					<Button icon="nav-back" design="Transparent" onClick={this._handleCollapseClick} class="ui5-setting-item-collapse-btn"></Button>
@@ -27,23 +45,7 @@ export default function SettingItemTemplate(this: SettingItem) {
 				</div>
 			</header>
 
-			{this._shouldHaveTabs ?
-				<TabContainer content-background-design="Transparent" class="ui5-setting-item-tabs">
-					{this.views.map(view =>
-						<Tab text={view.text}>
-							<slot name={view._individualSlot} />
-						</Tab>
-					)}
-				</TabContainer>
-				:
-				<>
-					{this.views.map(view =>
-						<div class="ui5-setting-item-view-container">
-							<slot name={view._individualSlot} />
-						</div>
-					)}
-				</>
-			}
+			{this.loading ? loadingState : content}
 		</div>
 	);
 }
