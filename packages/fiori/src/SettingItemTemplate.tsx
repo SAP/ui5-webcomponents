5df9a9e5f5a4b84d2572f61b7e259db6f9c196b7
Adrian Bobev <adrian.bobev@sap.com>
2025-01-05 22:30:55 +0200

    chore: update lineending and tabs

--- packages/fiori/src/SettingItemTemplate.tsx
+++ packages/fiori/src/SettingItemTemplate.tsx
@@ -6,44 +6,44 @@ import Tab from "@ui5/webcomponents/dist/Tab.js";
 import Title from "@ui5/webcomponents/dist/Title.js";
 
 export default function SettingItemTemplate(this: SettingItem) {
-    return (
-        <div class="ui5-setting-item">
-            <BusyIndicator
-            active={this.loading}
-            class="ui5-setting-item-busy">
-        </BusyIndicator>
+	return (
+		<div class="ui5-setting-item">
+			<BusyIndicator
+				active={this.loading}
+				class="ui5-setting-item-busy">
+			</BusyIndicator>
 
-        <header class="ui5-setting-item-header-container">
-            <div class="ui5-setting-item-header">
-                <Button icon="nav-back" design="Transparent" onClick={this._handleCollapseClick} class="ui5-setting-item-collapse-btn"></Button>
-            {!this._isPhone &&
-                <>
-                    {this.showBackNavigation &&(
-                    <Button icon="nav-back" design="Transparent" onClick={this._handleBackNavigationClick}></Button>
-                    )}
+			<header class="ui5-setting-item-header-container">
+				<div class="ui5-setting-item-header">
+					<Button icon="nav-back" design="Transparent" onClick={this._handleCollapseClick} class="ui5-setting-item-collapse-btn"></Button>
+					{!this._isPhone &&
+						<>
+							{this.showBackNavigation && (
+								<Button icon="nav-back" design="Transparent" onClick={this._handleBackNavigationClick}></Button>
+							)}
+						</>
+					}
+					<Title level="H4" size="H4">{this.headerTitle}</Title>
+				</div>
+			</header>
+
+			{this._shouldHaveTabs ?
+				<TabContainer content-background-design="Transparent" class="ui5-setting-item-tabs">
+					{this.views.map(view =>
+						<Tab text={view.text}>
+							<slot name={view._individualSlot} />
+						</Tab>
+					)}
+				</TabContainer>
+				:
+				<>
+					{this.views.map(view =>
+						<div class="ui5-setting-item-view-container">
+							<slot name={view._individualSlot} />
+						</div>
+					)}
 				</>
-            }
-                <Title level="H4" size="H4">{this.headerTitle}</Title>
-            </div>
-        </header>
-
-            { this._shouldHaveTabs ?
-                <TabContainer content-background-design="Transparent" class="ui5-setting-item-tabs">
-                    { this.views.map(view =>
-                    <Tab text={view.text}>
-                        <slot name={view._individualSlot} />
-                    </Tab>
-                    )}
-                </TabContainer>
-                    :
-                    <>
-                    { this.views.map(view =>
-                        <div class="ui5-setting-item-view-container">
-                            <slot name={view._individualSlot} />
-                        </div>
-                        )}
-                    </>
-                }
-        </div>
-    );
+			}
+		</div>
+	);
 }
--- packages/fiori/src/SettingsTemplate.tsx
+++ packages/fiori/src/SettingsTemplate.tsx
@@ -10,47 +10,47 @@ import Toolbar from "@ui5/webcomponents/dist/Toolbar.js";
 import search from "@ui5/webcomponents-icons/dist/search.js";
 
 export default function SettingsTemplate(this: Settings) {
-    return (
+	return (
 
-        <Dialog class="ui5-sd-dialog" open={this.open} onui5-_collapse={this._handleCollapseClick} accessibleName={this.accessibleNameText}>
-            <div class="ui5-sd-root">
-                <div class="ui5-sd-side" aria-orientation="vertical" aria-roledescription="{{ariaRoleDescList}}">
-                    <div class="ui5-sd-side-header">
-                        {this.headerTitle &&
-                             <Title level="H4" size="H4">{this.headerTitle}</Title>
-                        }
-                        {this.showSearchField &&
-                            <Input placeholder="Search" type="Search" class="ui5-sd-side-search" onInput={this._handleInput}>
-                                <Icon id="searchFieldIcon" slot="icon" name={search}></Icon>
-                            </Input>
-                        }
-                    </div>
+		<Dialog class="ui5-sd-dialog" open={this.open} onui5-_collapse={this._handleCollapseClick} accessibleName={this.accessibleNameText}>
+			<div class="ui5-sd-root">
+				<div class="ui5-sd-side" aria-orientation="vertical" aria-roledescription="{{ariaRoleDescList}}">
+					<div class="ui5-sd-side-header">
+						{this.headerTitle &&
+							<Title level="H4" size="H4">{this.headerTitle}</Title>
+						}
+						{this.showSearchField &&
+							<Input placeholder="Search" type="Search" class="ui5-sd-side-search" onInput={this._handleInput}>
+								<Icon id="searchFieldIcon" slot="icon" name={search}></Icon>
+							</Input>
+						}
+					</div>
 
-                    <List accessibleRole="Menu" onItemClick={this.setSelectedItem} class="ui5-sd-side-items">
-                        <>
-                        {this._normalItems.map(normalItem => (
-                            <ListItemStandard icon={normalItem._icon} tooltip={normalItem._tooltip} ref={this.captureRef.bind(normalItem)} selected={normalItem.selected} disabled={normalItem.disabled} accessibleName={normalItem.ariaLabelledByText}>{normalItem.text}</ListItemStandard>
-                         ))}
-                        </>
-                    </List>
+					<List accessibleRole="Menu" onItemClick={this.setSelectedItem} class="ui5-sd-side-items">
+						<>
+							{this._normalItems.map(normalItem => (
+								<ListItemStandard icon={normalItem._icon} tooltip={normalItem._tooltip} ref={this.captureRef.bind(normalItem)} selected={normalItem.selected} disabled={normalItem.disabled} accessibleName={normalItem.ariaLabelledByText}>{normalItem.text}</ListItemStandard>
+							))}
+						</>
+					</List>
 
-                        {this._fixedItems.length > 0 &&
-							<List accessibleRole="Menu" onItemClick={this.setSelectedItem} class="ui5-sd-side-fixedItems">
-                                 {this._fixedItems.map(fixedItem =>(
-                                    <ListItemStandard icon={fixedItem._icon} tooltip={fixedItem._tooltip} ref={this.captureRef.bind(fixedItem)} selected={fixedItem.selected} disabled={fixedItem.disabled} accessibleName={fixedItem.ariaLabelledByText}>{fixedItem.text}</ListItemStandard>
-                                ))}
-                            </List>
-                        }
-                </div>
+					{this._fixedItems.length > 0 &&
+						<List accessibleRole="Menu" onItemClick={this.setSelectedItem} class="ui5-sd-side-fixedItems">
+							{this._fixedItems.map(fixedItem => (
+								<ListItemStandard icon={fixedItem._icon} tooltip={fixedItem._tooltip} ref={this.captureRef.bind(fixedItem)} selected={fixedItem.selected} disabled={fixedItem.disabled} accessibleName={fixedItem.ariaLabelledByText}>{fixedItem.text}</ListItemStandard>
+							))}
+						</List>
+					}
+				</div>
 
-                <div class="ui5-sd-content">
-                        <slot name={this._selectedItemSlotName}></slot>
-                    </div>
-                </div>
+				<div class="ui5-sd-content">
+					<slot name={this._selectedItemSlotName}></slot>
+				</div>
+			</div>
 
-            <Toolbar slot="footer" design="Transparent">
-                <ToolbarButton text={this.closeButtonText} onClick={this._onClose} />
-            </Toolbar>
-        </Dialog>
-);
+			<Toolbar slot="footer" design="Transparent">
+				<ToolbarButton text={this.closeButtonText} onClick={this._onClose} />
+			</Toolbar>
+		</Dialog>
+	);
 }
