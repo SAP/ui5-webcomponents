c913a2baffa2df36d8288df90ad15324488d903a
Vladislav Tasev <vladislav.tasev@sap.com>
2025-01-09 18:02:53 +0200

    chore: document the deprecated feature file

--- packages/main/src/features/ColorPaletteMoreColors.ts
+++ packages/main/src/features/ColorPaletteMoreColors.ts
@@ -1,1 +1,3 @@
+// This module is now deprecated and should no longer be imported
+// When imported, it will only pre-load the optional template for the showMoreColors feature
 import "./ColorPaletteMoreColorsTemplate.js";
