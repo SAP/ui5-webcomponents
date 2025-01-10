5ed39c17ff3143598c086c758147eaf488b90960
ilhan007 <ilhan.myumyun@sap.com>
2025-01-10 22:17:15 +0200

    chore: update console log

--- packages/tools/lib/create-new-component/index.js
+++ packages/tools/lib/create-new-component/index.js
@@ -76,7 +76,7 @@ const generateFiles = (componentName, tagName, library, packageName) => {
 	// Change the color of the output
 	console.warn('\x1b[33m%s\x1b[0m', `
 Now, import the component via: "import ${componentName} from ./${componentName}.js";
-And, add it to your HTML: <${componentName}></${componentName}>.`);
+And, add it to your HTML: <${tagName}></${tagName}>.`);
 }
 
 // Main function
