2b7b903cae2e60390d911c109a2779e1b92bd339
Nikoleta Terzieva <nikoleta.ivanova.terzieva@sap.com>
2025-01-11 17:32:25 +0200

    fix(ui5-combobox): fire selection-change event when typed word fully matches item
    Fixes #10239
    
    When combobox is in noTypeahead mode, if the user writes a text that matches exactly
    a list item, selection-change event was not fired if the user selects the item by
    clicking on it.
    Now the selection0change event fires when item text is exactly typed and
    immediately selected from the items list.

--- packages/main/cypress/specs/ComboBox.cy.ts
+++ packages/main/cypress/specs/ComboBox.cy.ts
@@ -1,6 +1,6 @@
 import { html } from "lit";
-import "../../src/ComboBox.js";
 import "../../src/ComboBoxItem.js";
+import type ComboBox from "../../src/ComboBox.js";
 
 describe("Security", () => {
 	it("tests setting malicious text to items", () => {
@@ -18,5 +18,38 @@ describe("Security", () => {
 			.should("have.text", "<b onmouseover=alert('XSS')></b>");
 		cy.get("ui5-cb-item").eq(2).shadow().find(".ui5-li-title")
 			.should("have.text", "Albania<button onClick='alert(1)'>alert</button>");
+	});
+});
+
+describe("Events testing", () => {
+	it("Selection-change: event fires when item is fully typed and selected from the items list", () => {
+		cy.mount(html`
+			<ui5-combobox no-typeahead>
+				<ui5-cb-item text="Albania"></ui5-cb-item>
+				<ui5-cb-item text="Bulgaria"></ui5-cb-item>
+			</ui5-combobox>
+		`);
+
+		cy.get("ui5-combobox").as("combobox");
+
+		cy.get<ComboBox>("@combobox")
+			.then($combobox => {
+				$combobox.get(0).addEventListener("selection-change", cy.stub().as("selectionChange"));
+			});
+
+		cy.get<ComboBox>("@combobox").shadow().find("input").as("inner");
+
+		cy.get("@inner").realClick();
+		cy.get("@inner").type("Bulgaria");
+
+		cy.get("@combobox")
+			.find("ui5-cb-item")
+			.last()
+			.click();
+
+		cy.get("@selectionChange")
+			.should("have.been.calledOnce");
+		cy.get("@selectionChange")
+			.should("have.been.calledWithMatch", { detail: { item: { text: "Bulgaria" } } });
 	});
 });
