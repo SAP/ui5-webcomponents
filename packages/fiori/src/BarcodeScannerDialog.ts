4a73eaf40c0d35bcf5f3c9edaef2649bde63cc06
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-01-13 11:07:08 +0200

    chore: lint

--- packages/fiori/src/BarcodeScannerDialog.ts
+++ packages/fiori/src/BarcodeScannerDialog.ts
@@ -7,7 +7,6 @@ import customElement from "@ui5/webcomponents-base/dist/decorators/customElement
 import property from "@ui5/webcomponents-base/dist/decorators/property.js";
 import event from "@ui5/webcomponents-base/dist/decorators/event-strict.js";
 import i18n from "@ui5/webcomponents-base/dist/decorators/i18n.js";
-import { getI18nBundle } from "@ui5/webcomponents-base/dist/i18nBundle.js";
 import type { Result, Exception } from "@zxing/library";
 import type { Interval } from "@ui5/webcomponents-base/dist/types.js";
 // eslint-disable-next-line import/no-extraneous-dependencies
