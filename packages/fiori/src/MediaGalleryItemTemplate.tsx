02aeabfd95bba37f07dacf0d6a146fb30f56fa92
Diana Pazheva <diana.pazheva@sap.com>
2025-01-22 12:29:56 +0200

    chore(ui5-media-gallery): formatting corrected

--- packages/fiori/src/MediaGalleryItemTemplate.tsx
+++ packages/fiori/src/MediaGalleryItemTemplate.tsx
@@ -2,19 +2,20 @@ import type MediaGalleryItem from "./MediaGalleryItem.js";
 import Icon from "@ui5/webcomponents/dist/Icon.js";
 
 export default function MediaGalleryItemTemplate(this: MediaGalleryItem) {
-    return (<div class="ui5-media-gallery-item-root"
-            tabindex={this.effectiveTabIndex}
-            data-sap-focus-ref
-            onKeyDown={this._onkeydown}
-            onKeyUp={this._onkeyup}
-            role={this._role}
-        >
-            <div class="ui5-media-gallery-item-mask-layer"></div>
-            <div class="ui5-media-gallery-item-wrapper" style={this.styles.wrapper}>
-                {this._showBackgroundIcon && <Icon name="background"></Icon>}
-                {this._useContent && <slot />}
-                {this._useThumbnail && <slot name="thumbnail" />}
-            </div>
-        </div>
-    );
+	return (<div class="ui5-media-gallery-item-root"
+			tabindex={this.effectiveTabIndex}
+			data-sap-focus-ref
+			onKeyDown={this._onkeydown}
+			onKeyUp={this._onkeyup}
+			role={this._role}
+		>
+			<div class="ui5-media-gallery-item-mask-layer"></div>
+			<div class="ui5-media-gallery-item-wrapper" style={this.styles.wrapper}>
+				{this._showBackgroundIcon && <Icon name="background"></Icon>}
+				{this._useContent && <slot />}
+				{this._useThumbnail && <slot name="thumbnail" />}
+			</div>
+		</div>
+	);
 }
+
