95ae60a041861572d0a3610228874fde764e9a7f
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-01-24 10:08:02 +0200

    chore: wdio to use new headless

--- packages/tools/components-package/wdio.js
+++ packages/tools/components-package/wdio.js
@@ -60,7 +60,6 @@ exports.config = {
 			// to run chrome headless the following flags are required
 			// (see https://developers.google.com/web/updates/2017/04/headless-chrome)
 			args: [
-				'--headless=old',
 				'--disable-search-engine-choice-screen',
 				'--start-maximized',
 				'--no-sandbox',
