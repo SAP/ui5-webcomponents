056a89134705e502bfd880d837ba2486491312f8
Petar Dimov <petar.dimov@sap.com>
2025-01-27 14:05:13 +0200

    docs(ui5-side-navigation): add sample with unselectable parent items

---
+++ packages/website/docs/_samples/fiori/SideNavigation/UnselectableParentItems/main.js
@@ -0,0 +1,9 @@
+import "@ui5/webcomponents-fiori/dist/SideNavigation.js";
+import "@ui5/webcomponents-fiori/dist/SideNavigationItem.js";
+import "@ui5/webcomponents-fiori/dist/SideNavigationGroup.js";
+import "@ui5/webcomponents-fiori/dist/SideNavigationSubItem.js";
+
+import "@ui5/webcomponents-icons/dist/home.js";
+import "@ui5/webcomponents-icons/dist/group.js";
+import "@ui5/webcomponents-icons/dist/history.js";
+import "@ui5/webcomponents-icons/dist/locate-me.js";
