0369bce1344559ccd0cdee4c8922be0b51695c1b
ilhan007 <ilhan.myumyun@sap.com>
2025-01-28 19:11:58 +0200

    chore: update

--- packages/tools/components-package/cypress/support/component.js
+++ packages/tools/components-package/cypress/support/component.js
@@ -1,8 +1,8 @@
 import '@cypress/code-coverage/support'
-import { render, createContext } from "@ui5/webcomponents-base/dist/thirdparty/preact/preact.module.js";
-import { jsx } from "@ui5/webcomponents-base/dist/thirdparty/preact/jsxRuntime.module.js";
 import { setupHooks, getContainerEl} from '@cypress/mount-utils';
 import "./commands.js";
+import render from "./cypress-ct-preact.js";
+
 
 function cleanup() {
 	render(null, getContainerEl())
@@ -10,17 +10,15 @@ function cleanup() {
 
 function mount(component, options = {}) {
 	const root = getContainerEl();
-	const configurationScript = document.head.querySelector("script[data-ui5-config]")
+	const configurationScript = document.head.querySelector("script[data-ui5-config]");
+
 	cleanup();
 
 	if (options.ui5Configuration) {
 		configurationScript.innerHTML = JSON.stringify(options.ui5Configuration);
 	}
 
-	// REFACTOR THIS PART
-	const ctx = createContext(this);
-	const vnode = jsx(ctx.Provider, { value: component, children: component });
-	return render(vnode, root);
+	return render(component, root);
 }
 
 setupHooks(cleanup);
---
+++ packages/tools/components-package/cypress/support/cypress-ct-preact.js
@@ -0,0 +1,11 @@
+import { render as preactRender, createContext } from "@ui5/webcomponents-base/dist/thirdparty/preact/preact.module.js";
+import { jsx } from "@ui5/webcomponents-base/dist/thirdparty/preact/jsxRuntime.module.js";
+// extract preact in @ui5/webcomponents-render package to cut off the base package dependency
+
+const ctx = createContext(null);
+
+function render(component, container) {
+	return preactRender(jsx(ctx.Provider, { children: component }), container);
+}
+
+export default render;
