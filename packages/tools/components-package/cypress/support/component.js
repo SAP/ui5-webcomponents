be3bb2ea404c846ff874c7057756b6649e99a1ce
ilhan007 <ilhan.myumyun@sap.com>
2025-01-28 20:50:44 +0200

    chore: fallback to lit mount

--- packages/tools/components-package/cypress/support/component.js
+++ packages/tools/components-package/cypress/support/component.js
@@ -1,24 +1,41 @@
-import '@cypress/code-coverage/support'
-import { setupHooks, getContainerEl} from '@cypress/mount-utils';
+import "@cypress/code-coverage/support";
+import { setupHooks, getContainerEl} from "@cypress/mount-utils";
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
+import { mount as preactMount } from "./cypress-ct-preact.js";
+import { mount as litMount } from "cypress-ct-lit";
 import "./commands.js";
-import render from "./cypress-ct-preact.js";
 
-
-function cleanup() {
-	render(null, getContainerEl())
-}
-
-function mount(component, options = {}) {
-	const root = getContainerEl();
+function applyConfiguration(options) {
 	const configurationScript = document.head.querySelector("script[data-ui5-config]");
-
-	cleanup();
 
 	if (options.ui5Configuration) {
 		configurationScript.innerHTML = JSON.stringify(options.ui5Configuration);
 	}
+}
 
-	return render(component, root);
+function cleanup() {
+	preactMount(null, getContainerEl());
+}
+
+function mount(component, options = {}) {
+	const container = getContainerEl();
+
+	// Apply custom configuration
+	applyConfiguration(options);
+
+	// Mount string - remove after migrating all
+	if (typeof component === "string") {
+		return litMount(unsafeHTML(component), options)
+	}
+	
+	// Mount lit template -  - remove after migrating all
+	const legacyMount = component?.strings?.length > 0;
+	if (legacyMount) {
+		return litMount(component, options);
+	}
+
+	// Mount JSX template
+	return preactMount(component, container);
 }
 
 setupHooks(cleanup);
--- packages/tools/components-package/cypress/support/cypress-ct-preact.js
+++ packages/tools/components-package/cypress/support/cypress-ct-preact.js
@@ -1,11 +1,13 @@
-import { render as preactRender, createContext } from "@ui5/webcomponents-base/dist/thirdparty/preact/preact.module.js";
+import { render, createContext } from "@ui5/webcomponents-base/dist/thirdparty/preact/preact.module.js";
 import { jsx } from "@ui5/webcomponents-base/dist/thirdparty/preact/jsxRuntime.module.js";
 // extract preact in @ui5/webcomponents-render package to cut off the base package dependency
 
 const ctx = createContext(null);
 
-function render(component, container) {
-	return preactRender(jsx(ctx.Provider, { children: component }), container);
+function mount(component, container) {
+	return render(jsx(ctx.Provider, { children: component }), container);
 }
 
-export default render;
+export {
+	mount,
+};
