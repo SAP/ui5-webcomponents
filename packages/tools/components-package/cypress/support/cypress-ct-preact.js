b19b95538f45bde9f3beb8bce683613a3e954b13
ilhan007 <ilhan.myumyun@sap.com>
2025-01-29 11:49:32 +0200

    chore: use standard preact directly

--- packages/tools/components-package/cypress/support/cypress-ct-preact.js
+++ packages/tools/components-package/cypress/support/cypress-ct-preact.js
@@ -1,11 +1,9 @@
-import { render, createContext } from "@ui5/webcomponents-base/dist/thirdparty/preact/preact.module.js";
-import { jsx } from "@ui5/webcomponents-base/dist/thirdparty/preact/jsxRuntime.module.js";
-// extract preact in @ui5/webcomponents-render package to cut off the base package dependency
+import { render } from 'preact';
 
-const ctx = createContext(null);
+
 
 function mount(component, container) {
-	return render(jsx(ctx.Provider, { children: component }), container);
+	return render(component, container);
 }
 
 export {
