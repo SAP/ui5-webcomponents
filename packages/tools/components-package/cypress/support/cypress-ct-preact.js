1d33a7159580f4b5fd53530238c6033b1b708c04
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-01-31 11:22:47 +0200

    chore: cleanup

--- packages/tools/components-package/cypress/support/cypress-ct-preact.js
+++ packages/tools/components-package/cypress/support/cypress-ct-preact.js
@@ -3,10 +3,6 @@ import { h, render } from 'preact';
 function mount(component, container) {
 	render(null, container);
 
-	if (typeof component === "string") {
-		return render(h("div", { dangerouslySetInnerHTML: { __html: component } }), container);
-	}
-
 	return render(component, container);
 }
 
