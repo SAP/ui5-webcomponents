b4c4e15965079c8d230d414c17d684400f6d882d
Nikola Anachkov <nikola.anachkov@sap.com>
2025-02-02 19:58:34 +0200

    refactor: remove grip container and simplify grip icon rendering

--- packages/fiori/src/FlexibleColumnLayoutTemplate.tsx
+++ packages/fiori/src/FlexibleColumnLayoutTemplate.tsx
@@ -101,12 +101,11 @@ function arrowStart(this: FlexibleColumnLayout) {
 function gripStart(this: FlexibleColumnLayout) {
 	return (
 		<div
-			class="ui5-fcl-grip-container"
+			class="ui5-fcl-grip ui5-fcl-grip--start"
 			style={{ display: this.showStartSeparatorGrip ? "flex" : "none" }}>
 			<Icon
 				name={verticalGripIcon}
-				class="ui5-fcl-grip ui5-fcl-grip--start"
-
+				class="ui5-fcl-grip-icon"
 			/>
 		</div>
 	);
@@ -115,11 +114,11 @@ function gripStart(this: FlexibleColumnLayout) {
 function gripEnd(this: FlexibleColumnLayout) {
 	return (
 		<div
-			class="ui5-fcl-grip-container"
+			class="ui5-fcl-grip ui5-fcl-grip--end"
 			style={{ display: this.showEndSeparatorGrip ? "flex" : "none" }}>
 			<Icon
 				name={verticalGripIcon}
-				class="ui5-fcl-grip ui5-fcl-grip--end"
+				class="ui5-fcl-grip-icon"
 			/>
 		</div>
 	);
