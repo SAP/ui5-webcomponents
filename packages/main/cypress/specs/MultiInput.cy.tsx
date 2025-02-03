69f91ce94221454296e1a878fca6e442c5c52a91
ilhan007 <ilhan.myumyun@sap.com>
2025-02-03 14:58:36 +0200

    chore: revert MultiInput test

--- packages/main/cypress/specs/MultiInput.cy.tsx
+++ packages/main/cypress/specs/MultiInput.cy.tsx
@@ -1,31 +1,10 @@
 import SuggestionItem from "../../src/SuggestionItem.js";
 import MultiInput from "../../src/MultiInput.js";
 
-const createTokenFromText = (text: string): HTMLElement => {
-	const token = document.createElement("ui5-token");
-	token.setAttribute("text", text);
-	token.setAttribute("slot", "tokens");
-	return token;
-};
-
-const handleKeyDown = (event: KeyboardEvent): void => {
-	const inputElement = event.target as HTMLInputElement;
-	if (event.key === "Enter" && inputElement.value) {
-		const token = createTokenFromText(inputElement.value);
-		inputElement.appendChild(token);
-		inputElement.value = "";
-	}
-};
-
 describe("MultiInput Web Component", () => {
 	it("creates only one token when typing 'ad' and pressing Enter", () => {
 		cy.mount(
-			<MultiInput
-				id="multi-inp-suggestion-token"
-				showSuggestions={true}
-				showValueHelpIcon={true}
-				onKeyDown={handleKeyDown}
-			>
+			<MultiInput showSuggestions={true} showValueHelpIcon={true} id="suggestion-token">
 				<SuggestionItem text="Aute"></SuggestionItem>
 				<SuggestionItem text="ad"></SuggestionItem>
 				<SuggestionItem text="exercitation"></SuggestionItem>
@@ -37,13 +16,31 @@ describe("MultiInput Web Component", () => {
 			</MultiInput>
 		);
 
-		cy.get("#multi-inp-suggestion-token")
+		cy.get("#suggestion-token").then(multiInput => {
+			const createTokenFromText = (text: string): HTMLElement => {
+				const token = document.createElement("ui5-token");
+				token.setAttribute("text", text);
+				token.setAttribute("slot", "tokens");
+				return token;
+			};
+
+			multiInput[0].addEventListener("keydown", (event: KeyboardEvent) => {
+				const inputElement = multiInput[0] as HTMLInputElement;
+				if (event.key === "Enter" && inputElement.value) {
+					const token = createTokenFromText(inputElement.value);
+					inputElement.appendChild(token);
+					inputElement.value = "";
+				}
+			});
+		});
+
+		cy.get("#suggestion-token")
 			.shadow()
 			.find("input")
-			.realType("ad{enter}");
+			.type("ad{enter}");
 
-		cy.get("#multi-inp-suggestion-token")
-			.find("[ui5-token]")
+		cy.get("ui5-multi-input")
+			.find("ui5-token")
 			.should("have.length", 1)
 			.and("have.attr", "text", "ad");
 	});
