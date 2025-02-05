35e7988ae5239edf15546389f7cae1283e9cb5b1
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-02-05 18:22:13 +0200

    chore: fix build

--- packages/main/cypress.config.js
+++ packages/main/cypress.config.js
@@ -2,8 +2,19 @@
 import { defineCypressConfig } from "@ui5/webcomponents-testing";
 import viteConfig from "../../vite.config.js";
 
+const suites = {
+	SUITE1: [
+		"**/specs/base/*.cy.{jsx,tsx}",
+		"**/specs/[A-I]*.cy.{js,jsx,ts,tsx}",
+	],
+	SUITE2: [
+		"**/specs/[^A-I]*.cy.{js,jsx,ts,tsx}",
+	],
+};
+
 export default defineCypressConfig({
 	component: {
+		specPattern: suites[process.env.TEST_SUITE] || ["**/specs/**/*.cy.{js,ts,jsx,tsx}"],
 		devServer: {
 			viteConfig,
 		},
--- packages/testing/cypress.config.js
+++ packages/testing/cypress.config.js
@@ -3,21 +3,11 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { existsSync } from 'fs';
 import merge from "deepmerge";
-import coverageTask from '@cypress/code-coverage/task';
+import coverageTask from '@cypress/code-coverage/task.js';
 
 const __processDir = process.cwd();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const suites = {
-	SUITE1: [
-		"**/specs/base/*.cy.{jsx,tsx}",
-		"**/specs/[A-I]*.cy.{js,jsx,ts,tsx}",
-	],
-	SUITE2: [
-		"**/specs/[^A-I]*.cy.{js,jsx,ts,tsx}",
-	],
-};
 
 const indexHtmlFilePath = "./cypress/support/component-index.html"; // relative to the config file
 
@@ -27,7 +17,7 @@ const intialConfig = () => ({
 			coverageTask(on, config)
 			return config
 		},
-		specPattern: suites[process.env.TEST_SUITE] || ["**/specs/**/*.cy.{js,ts,jsx,tsx}"],
+		specPattern: ["**/specs/**/*.cy.{js,ts,jsx,tsx}"],
 		devServer: {
 			bundler: 'vite',
 		}
@@ -45,7 +35,7 @@ const defineCypressConfig = (externalConrfig = {}) => {
 
 	if (externalConrfig.component?.indexHtmlFile) {
 		indexHtmlFile = path.join(__processDir, externalConrfig.component.indexHtmlFile)
-	} else if (!existsSync(path.join(__processDir, "cypress/", indexHtmlFilePath))) {
+	} else if (!existsSync(path.join(__processDir, indexHtmlFilePath))) {
 		indexHtmlFile = path.join(__dirname, indexHtmlFilePath);
 	}
 
