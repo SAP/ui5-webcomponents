66a79b17011d4882728457f9e07e2b183785258f
ilhan007 <ilhan.myumyun@sap.com>
2025-02-05 21:35:57 +0200

    chore: spacings

--- packages/base/src/ssr-dom-shim.ts
+++ packages/base/src/ssr-dom-shim.ts
@@ -17,55 +17,54 @@ globalThis.FileList ??= FileListShim as object as typeof FileList;
 const adoptedSheetsStore = new WeakMap();
 
 if (globalThis.Document && !("adoptedStyleSheets" in Document.prototype)) {
-	Object.defineProperty(Document.prototype, "adoptedStyleSheets", {
-		get() {
-			return adoptedSheetsStore.get(this) || [];
-		},
-		set(sheets: CSSStyleSheet[]) {
-			adoptedSheetsStore.set(this, sheets);
-		},
-	});
+  Object.defineProperty(Document.prototype, "adoptedStyleSheets", {
+    get() {
+      return adoptedSheetsStore.get(this) || [];
+    },
+    set(sheets: CSSStyleSheet[]) {
+      adoptedSheetsStore.set(this, sheets);
+    },
+  });
 }
 
 if (globalThis.ShadowRoot && !("adoptedStyleSheets" in ShadowRoot.prototype)) {
-	Object.defineProperty(ShadowRoot.prototype, "adoptedStyleSheets", {
-		get() {
-			return adoptedSheetsStore.get(this) || [];
-		},
-		set(sheets: CSSStyleSheet[]) {
-			adoptedSheetsStore.set(this, sheets);
-		},
-	});
+  Object.defineProperty(ShadowRoot.prototype, "adoptedStyleSheets", {
+    get() {
+      return adoptedSheetsStore.get(this) || [];
+    },
+    set(sheets: CSSStyleSheet[]) {
+      adoptedSheetsStore.set(this, sheets);
+    },
+  });
 }
 
 // Polyfill CSSStyleSheet to provide `replaceSync`
 if (globalThis.CSSStyleSheet && !("replaceSync" in CSSStyleSheet.prototype)) {
-	Object.defineProperty(CSSStyleSheet.prototype, "replaceSync", {
-		value(cssText: string) {
-		this.cssText = cssText;
-		return cssText;
-		},
-	});
+    Object.defineProperty(CSSStyleSheet.prototype, "replaceSync", {
+      value(cssText: string) {
+        this.cssText = cssText;
+        return cssText;
+      },
+    });
   }
 
 // Empty resize observer
 globalThis.ResizeObserver = class ResizeObserver {
-	observe() {
-		// do nothing
-	}
-	unobserve() {
-		// do nothing
-	}
-	disconnect() {
-		// do nothing
-	}
+    observe() {
+      // do nothing
+    }
+    unobserve() {
+      // do nothing
+    }
+    disconnect() {
+      // do nothing
+    }
 };
 
 // empty showPopover method
 globalThis.HTMLElement.prototype.showPopover = function () {};
 
-
 //  ElementInternals
 globalThis.HTMLElement.prototype.attachInternals = function() {
-  return new ElementInternals();
+    return new ElementInternals();
 }
