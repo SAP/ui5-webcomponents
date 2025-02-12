769c8fc1c4006ae2bb5cd076d0cfffd06762e727
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-02-12 20:19:24 +0200

    chore: add comment

--- packages/create-package/template/vite.config.js
+++ packages/create-package/template/vite.config.js
@@ -1,1 +1,13 @@
-export { default } from "@ui5/webcomponents-tools/components-package/vite.config.js"; //eslint-disable-line
+import viteConfig from "@ui5/webcomponents-tools/components-package/vite.config.js"; //eslint-disable-line
+
+// Modifying the default Vite configuration provided by the @ui5/webcomponents-tools package.
+// You can directly access and update the properties you need to change.
+// Ensure that the property exists before modifying it to avoid unintended errors.
+// For available configuration options, refer to: https://vite.dev/config/#configuring-vite
+//
+// viteConfig.build = {
+// 	...viteConfig.build,
+// 	sourcemap: true, // For example: Enable source maps for better debugging
+// };
+
+export default viteConfig;
