060e66e5ab51c0688b7cb3158503d9a6c4f621de
Duc Vo Ngoc <duc.vo.ngoc@sap.com>
2025-02-18 11:29:20 +0100

    docs: apply suggestions
    Co-authored-by: Katja  <53079257+KvM2@users.noreply.github.com>

--- packages/main/src/TableHeaderCell.ts
+++ packages/main/src/TableHeaderCell.ts
@@ -103,8 +103,8 @@ class TableHeaderCell extends TableCellBase {
 	/**
 	 * Defines if the column is hidden in the popin.
 	 *
-	 * **Note:** Please be aware, that hiding the column in the popin might lead to accessibility issues, as
-	 * users might not be able to access the content of the column on small screen sizes.
+	 * **Note:** Please be aware that hiding the column in the popin might lead to accessibility issues as
+	 * users might not be able to access the content of the column on small screens.
 	 *
 	 * @default false
 	 * @since 2.8.0
