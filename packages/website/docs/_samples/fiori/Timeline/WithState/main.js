e71abfaad7b935d9383b9ce69ad9f78fc2bd3758
Stoyan <88034608+hinzzx@users.noreply.github.com>
2025-02-28 09:28:58 +0200

    chore(ui5-timeline): add missing import in sample (#10956)
    There was a missing import in the Timeline's sample, leading to missing sample. With this import, we fix the issue.

--- packages/website/docs/_samples/fiori/Timeline/WithState/main.js
+++ packages/website/docs/_samples/fiori/Timeline/WithState/main.js
@@ -1,6 +1,7 @@
 import "@ui5/webcomponents/dist/Label.js";
 
 import "@ui5/webcomponents-fiori/dist/Timeline.js";
+import "@ui5/webcomponents-fiori/dist/TimelineGroupItem.js"
 import "@ui5/webcomponents-fiori/dist/TimelineItem.js";
 
 import "@ui5/webcomponents-icons/dist/message-information.js";
