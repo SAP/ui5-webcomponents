a0e1789e5a008c5cedff7931eebbf04efaf4db29
Siyana Todorova <siyana.todorova@sap.com>
2025-03-12 14:25:18 +0200

    chore(ui5-side-navigation): adjust method of closing and add more keyboard support

--- packages/website/docs/_samples/fiori/SideNavigation/OverlayMode/main.js
+++ packages/website/docs/_samples/fiori/SideNavigation/OverlayMode/main.js
@@ -1,3 +1,6 @@
+/* playground-hide */
+import "./playground-support.js";
+/* playground-hide-end */
 import "@ui5/webcomponents-fiori/dist/SideNavigation.js";
 import "@ui5/webcomponents-fiori/dist/SideNavigationItem.js";
 import "@ui5/webcomponents-fiori/dist/SideNavigationGroup.js";
@@ -6,6 +9,8 @@ import "@ui5/webcomponents-fiori/dist/SideNavigationGroup.js";
 import "@ui5/webcomponents-fiori/dist/ShellBar.js";
 import "@ui5/webcomponents/dist/ResponsivePopover.js"
 import "@ui5/webcomponents-fiori/dist/Page.js"
+import "@ui5/webcomponents/dist/Text.js";
+import "@ui5/webcomponents/dist/Title.js";
 
 import "@ui5/webcomponents-icons/dist/home.js";
 import "@ui5/webcomponents-icons/dist/chain-link.js";
@@ -15,14 +20,26 @@ import "@ui5/webcomponents-icons/dist/calendar.js";
 import "@ui5/webcomponents-icons/dist/history.js";
 import "@ui5/webcomponents-icons/dist/customer.js"; 
 import "@ui5/webcomponents-icons/dist/menu.js";
+import "@ui5/webcomponents-icons/dist/write-new.js";
+import "@ui5/webcomponents-icons/dist/widgets.js";
+import "@ui5/webcomponents-icons/dist/compare.js";
 
-startButton.addEventListener("click", function () {
-	respPopover.open = true;
+menuBtn.addEventListener("click", function () {
+	respPopover.open = !respPopover.open;
 });
 
 //By design on clicking on an element the popover should close
-nl1.querySelectorAll("ui5-side-navigation-item").forEach(element => {
-	element.addEventListener("click", function () {
-		respPopover.open=false;	
+sideNavigation.addEventListener("selection-change", function (event) {
+	if (event.detail.item.getAttribute("target")) {
+		respPopover.open=false;
+		return;
+	}
+
+	const contentItems = document.querySelectorAll(".contentItem");
+	contentItems.forEach(item => {
+		item.classList.remove("contentItemVisible");
 	});
+	document.getElementById(event.detail.item.getAttribute("href").replace("#", ""))?.classList.add("contentItemVisible");
+
+	respPopover.open=false;
 });
