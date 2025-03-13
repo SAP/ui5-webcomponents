76898950875fafebaa949c68600d748f87907fdc
Konstantin Gogov <konstantin.gogov@sap.com>
2025-03-13 09:58:21 +0200

    feat(ui5-li): add text wrapping support with expandable text
    The standard list item now supports content wrapping through a new "wrapping"
    property. When enabled, long text content (title and description) will wrap to
    multiple lines instead of truncating with an ellipsis.
    
    Key features:
    - Responsive behavior based on screen size:
      - On mobile (size S): Text truncates after 100 characters
      - On tablets/desktop (size M/L): Text truncates after 300 characters
    - "Show More/Less" functionality for very long content using ui5-expandable-text
    - Works for both title and description fields
    - CSS styling to ensure proper layout in different states
    
    Implementation details:
    - Added "wrapping" boolean property to ListItemStandard component
    - Conditionally render content using ExpandableText component when wrapping is enabled
    - Added _maxCharacters getter to determine truncation point based on media range
    - Updated ListItemStandardTemplate to handle wrapped content rendering
    - Added CSS styles to support proper wrapping behavior
    - Updated HTML test page with examples
    
    Documentation:
    - Added new section in List.mdx explaining the wrapping behavior
    - Created detailed sample in WrappingBehavior folder
    - Added JSDoc descriptions for the new property
    - Added note about deprecated usage of default slot in favor of text property
    
    Tests:
    - Added desktop test for 300 character limit
    - Added mobile-specific test for 100 character limit
    - Added test for toggling wrapping property
    
    NOTE: This change also promotes the use of the "text" property (added in v2.9.0)
    instead of the default slot content for setting list item text. The default slot
    usage is now deprecated and will be removed in a future version.

--- packages/main/src/ListItemStandardTemplate.tsx
+++ packages/main/src/ListItemStandardTemplate.tsx
@@ -2,6 +2,7 @@ import Icon from "./Icon.js";
 import type ListItemStandard from "./ListItemStandard.js";
 import ListItemTemplate from "./ListItemTemplate.js";
 import type { ListItemHooks } from "./ListItemTemplate.js";
+import ExpandableText from "./ExpandableText.js";
 
 const predefinedHooks: Partial<ListItemHooks> = {
 	imageBegin,
@@ -19,27 +20,73 @@ export default function ListItemStandardTemplate(this: ListItemStandard, hooks?:
 function listItemContent(this: ListItemStandard) {
 	return <>
 		<div class="ui5-li-text-wrapper">
-			<span part="title" class="ui5-li-title">
-				<slot></slot>
-			</span>
-			{this.description && (
-				<div class="ui5-li-description-info-wrapper">
-					<span part="description" class="ui5-li-desc">{this.description}</span>
-					{
-						this.additionalText && (
-							<span part="additional-text" class="ui5-li-additional-text">{this.additionalText}</span>
-						)
-					}
-				</div>
-			)}
+			{renderTitle.call(this)}
+			{renderDescription.call(this)}
 
 			{!this.typeActive && <span class="ui5-hidden-text">{this.type}</span>}
 		</div>
 
-		{!this.description && this.additionalText && (
-			<span part="additional-text" class="ui5-li-additional-text">{this.additionalText}</span>
-		)}
+		{!this.description && renderAdditionalText.call(this)}
 	</>;
+}
+
+function renderTitle(this: ListItemStandard) {
+	if (this.wrapping) {
+		return (
+			<span part="title" class="ui5-li-title">
+				<ExpandableText
+					class={{
+						"ui5-li-title-exp-text": true
+					}}
+					text={this._textContent}
+					maxCharacters={this._maxCharacters}>
+				</ExpandableText>
+			</span>
+		);
+	}
+
+	return (
+		<span part="title" class="ui5-li-title">
+			{this.text ? this.text : <slot></slot>}
+		</span>
+	);
+}
+
+function renderDescription(this: ListItemStandard) {
+	if (!this.description) {
+		return null;
+	}
+
+	if (this.wrapping) {
+		return (
+			<div class="ui5-li-description-info-wrapper">
+				<span part="description" class="ui5-li-desc">
+					<ExpandableText
+						class={{
+							"ui5-li-desc-exp-text": true
+						}}
+						text={this.description}
+						maxCharacters={this._maxCharacters}>
+					</ExpandableText>
+				</span>
+				{renderAdditionalText.call(this)}
+			</div>
+		);
+	}
+
+	return (
+		<div class="ui5-li-description-info-wrapper">
+			<span part="description" class="ui5-li-desc">{this.description}</span>
+			{renderAdditionalText.call(this)}
+		</div>
+	);
+}
+
+function renderAdditionalText(this: ListItemStandard) {
+	if (!this.additionalText) {
+		return null;
+	}
+	return <span part="additional-text" class="ui5-li-additional-text">{this.additionalText}</span>;
 }
 
 function imageBegin(this: ListItemStandard) {
