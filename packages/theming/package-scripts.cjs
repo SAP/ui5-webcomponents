f5cc43229939aa31d32e44ddb4f07cbee37c7d1d
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-03-13 15:18:06 +0200

    chore: usage of build scripts

--- packages/theming/package-scripts.cjs
+++ packages/theming/package-scripts.cjs
@@ -1,11 +1,12 @@
-const resolve = require("resolve");
-const assets = require('@ui5/webcomponents-tools/assets-meta.js');
 const path = require('path');
 
-const jsonImportsScript = resolve.sync("@ui5/webcomponents-tools/lib/generate-json-imports/themes.js");
-const generateReportScript = resolve.sync("@ui5/webcomponents-theming/lib/generate-css-vars-usage-report/index.js");
 
-const LIB = path.join(__dirname, `../tools/lib/`);
+const CURRENT_LIB = path.join(__dirname, `./lib/`);
+const TOOLS_LIB = path.join(__dirname, `../tools/lib/`);
+
+const jsonImportsScript = path.join(TOOLS_LIB, "./generate-json-imports/themes.js");
+const generateReportScript = path.join(CURRENT_LIB, "./generate-css-vars-usage-report/index.js");
+
 
 module.exports = {
 	scripts: {
@@ -15,7 +16,7 @@ module.exports = {
 			default: `cross-env UI5_TS=true nps clean build.src build.postcss build.jsonImports build.typescript generateReport`,
 			src: `copy-and-watch "src/**/*.{json}" dist/`,
 			typescript: "tsc",
-			postcss: `node "${LIB}/css-processors/css-processor-themes.mjs"`,
+			postcss: `node "${TOOLS_LIB}/css-processors/css-processor-themes.mjs"`,
 			jsonImports: `node "${jsonImportsScript}" dist/generated/assets/themes src/generated/json-imports`,
 		},
 		generateReport: `node "${generateReportScript}"`,
