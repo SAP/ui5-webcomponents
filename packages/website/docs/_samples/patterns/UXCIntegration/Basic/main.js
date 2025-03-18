be13971ce2512fd194a0659a789a98748effa903
Diana Pazheva <diana.pazheva@sap.com>
2025-03-18 11:01:43 +0200

    chore: add uxc integration pattern (#11000)
    * chore: add uxc integration pattern
    
    * chore: adapt sample to match UX specification

---
+++ packages/website/docs/_samples/patterns/UXCIntegration/Basic/main.js
@@ -0,0 +1,45 @@
+import "@ui5/webcomponents/dist/Avatar.js";
+import "@ui5/webcomponents/dist/Button.js";
+import "@ui5/webcomponents/dist/Input.js";
+import "@ui5/webcomponents/dist/ListItemStandard.js";
+import "@ui5/webcomponents/dist/Popover.js";
+import "@ui5/webcomponents/dist/Title.js";
+import "@ui5/webcomponents/dist/ToggleButton.js";
+import "@ui5/webcomponents/dist/List.js";
+import "@ui5/webcomponents/dist/Tag.js";
+import "@ui5/webcomponents/dist/Text.js";
+
+import "@ui5/webcomponents-fiori/dist/ShellBar.js";
+import "@ui5/webcomponents-fiori/dist/ShellBarItem.js";
+import "@ui5/webcomponents-fiori/dist/NavigationLayout.js";
+import "@ui5/webcomponents-fiori/dist/SideNavigation.js";
+import "@ui5/webcomponents-fiori/dist/SideNavigationItem.js";
+import "@ui5/webcomponents-fiori/dist/SideNavigationSubItem.js";
+
+import "@ui5/webcomponents-icons/dist/da.js";
+import "@ui5/webcomponents-icons/dist/da-2.js";
+import "@ui5/webcomponents-icons/dist/menu2.js";
+import "@ui5/webcomponents-icons/dist/sys-find.js";
+import "@ui5/webcomponents-icons/dist/settings.js";
+import "@ui5/webcomponents-icons/dist/edit.js";
+import "@ui5/webcomponents-icons/dist/sys-help.js";
+import "@ui5/webcomponents-icons/dist/log.js";
+import "@ui5/webcomponents-icons/dist/home.js";
+import "@ui5/webcomponents-icons/dist/favorite-list.js";
+import "@ui5/webcomponents-icons/dist/account.js";
+import "@ui5/webcomponents-icons/dist/business-by-design.js";
+
+const shellbar = document.getElementById("shellbar");
+const actionPopover = document.getElementById("action-popover");
+
+shellbar.addEventListener("ui5-profile-click", (event) => {
+    actionPopover.opener = event.detail.targetRef;
+	actionPopover.open = true;
+});
+
+[...document.querySelectorAll("ui5-toggle-button")].forEach(el => {
+	el.addEventListener("click", event => {
+		const toggleButton = event.target;
+		toggleButton.icon = toggleButton.pressed ? "sap-icon://da-2" : "sap-icon://da";
+	});
+});
