f902ec48673bde679d62c81ddb8685905f3b2483
ilhan orhan <ilhan.orhan007@gmail.com>
2025-03-26 17:48:19 +0200

    chore: api and docs improvements (#11179)

--- packages/website/docs/_samples/main/Select/IconOnly/main.js
+++
@@ -1,4 +0,0 @@
-import "@ui5/webcomponents/dist/Select.js";
-import "@ui5/webcomponents/dist/Icon.js";
-import "@ui5/webcomponents/dist/Option.js";
-import "@ui5/webcomponents-icons/dist/employee.js";
