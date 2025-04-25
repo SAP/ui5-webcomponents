46532edc15e9aefb26fa69eea0825cf06634aaf9
Siyana Todorova <siyana.todorova@sap.com>
2025-04-25 15:20:29 +0300

    chore: add tests

--- packages/fiori/cypress/specs/SideNavigationWithGroups.cy.tsx
+++ packages/fiori/cypress/specs/SideNavigationWithGroups.cy.tsx
@@ -28,6 +28,39 @@ describe("Component Behavior", () => {
 				.should("not.exist");
 		});
 
+		it("Tests that visualization is correct when two groups are next to each other", async () => {
+			cy.mount(
+				<SideNavigation id="sn1" collapsed={false}>
+					<SideNavigationGroup id="group1" text="Group 1">
+						<SideNavigationItem text="Item 1.1" icon="group"></SideNavigationItem>
+					</SideNavigationGroup>
+					<SideNavigationGroup id="group2" text="Group 2">
+						<SideNavigationItem text="Item 2.1" icon="locate-me" selected></SideNavigationItem>
+					</SideNavigationGroup>
+					<SideNavigationItem text="Item" icon="locate-me"></SideNavigationItem>
+					<SideNavigationGroup id="group3" text="Group 3">
+						<SideNavigationItem text="Item 3.1" icon="locate-me"></SideNavigationItem>
+					</SideNavigationGroup>
+				</SideNavigation>	
+			);
+		
+			cy.get("#group2")
+				.should("have.prop", "belowGroup", true);
+
+			cy.get("#group2")
+				.shadow()
+				.find(".ui5-sn-item-separator").eq(0)
+				.should("not.be.visible");
+
+			cy.get("#group3")
+				.should("have.prop", "belowGroup", false);
+			
+			cy.get("#group3")
+				.shadow()
+				.find(".ui5-sn-item-separator").eq(0)
+				.should("be.visible");
+		});
+
 		it("collapse/expand", async () => {
 			cy.mount(
 				<SideNavigation style="height: 90vh; " id="sn1">
@@ -45,7 +78,7 @@ describe("Component Behavior", () => {
 				.shadow()
 				.find(".ui5-sn-item")
 				.realClick();
-			cy.get("#group1").should("not.have.prop", "expanded");
+			cy.get("#group1").should("have.prop", "expanded", false);
 
 			cy.get("#group1")
 				.shadow()
