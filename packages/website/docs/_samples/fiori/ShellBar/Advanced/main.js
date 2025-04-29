86f55b5b03458966f8c9de3881465ab73d19ffe9
Dobrin Dimchev <dobrin.dimchev@sap.com>
2025-04-29 23:40:47 +0300

    docs: adjust samples

--- packages/website/docs/_samples/fiori/ShellBar/Advanced/main.js
+++ packages/website/docs/_samples/fiori/ShellBar/Advanced/main.js
@@ -8,6 +8,9 @@ import "@ui5/webcomponents-fiori/dist/UserMenuAccount.js";
 import "@ui5/webcomponents-fiori/dist/UserMenuItem.js";
 
 import "@ui5/webcomponents-fiori/dist/ShellBar.js";
+import "@ui5/webcomponents-fiori/dist/ShellBarSearch.js";
+import "@ui5/webcomponents-fiori/dist/SearchItem.js";
+import "@ui5/webcomponents-fiori/dist/SearchScope.js";
 
 import "@ui5/webcomponents-icons/dist/da.js";
 import "@ui5/webcomponents-icons/dist/da-2.js";
@@ -28,4 +31,44 @@ shellbar.addEventListener("ui5-profile-click", (event) => {
 		const toggleButton = event.target;
 		toggleButton.icon = toggleButton.pressed ? "sap-icon://da-2" : "sap-icon://da";
 	});
+});
+
+const scopeData = [
+    { name: "Laptop", scope: "products" },
+    { name: "Leave Requests", scope: "apps" },
+    { name: "Log work", scope: "apps" },
+    { name: "Manage Products", scope: "apps" },
+    { name: "Mobile Phones", scope: "products" },
+    { name: "Tablet", scope: "products" },
+];
+
+function createScopeItems(scope) {
+    let filterData = [];
+
+    if (!scope) {
+        filterData = scopeData;
+    } else {
+        filterData = scopeData.filter(item => item.scope === scope);
+    }
+
+    filterData.forEach(item => {
+        const searchItem = document.createElement("ui5-search-item");
+        searchItem.text = item.name;
+        searchItem.scopeName = item.scope;
+        searchScope.appendChild(searchItem);
+    });
+}
+
+const searchScope = document.getElementById("search-scope");
+
+createScopeItems();
+
+searchScope.addEventListener("ui5-scope-change", (event) => {
+    const scope = event.detail.scope.text === "All" ? "" : event.detail.scope.text.toLowerCase();
+    
+    searchScope.items.forEach(item => {
+        item.remove();
+    });
+
+    createScopeItems(scope);
 });
