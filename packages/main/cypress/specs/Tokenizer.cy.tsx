915a6f5cca0db14e1cd7e32b62890b4918bfcebf
Stefan Dimitrov <stefan.dimitrov02@sap.com>
2025-05-20 15:24:13 +0300

    fix(ui5-combobox/tokenizer/wizard/input): extract button texts to variables for translation

--- packages/main/cypress/specs/Tokenizer.cy.tsx
+++ packages/main/cypress/specs/Tokenizer.cy.tsx
@@ -1,6 +1,7 @@
 import Tokenizer from "../../src/Tokenizer.js";
 import Token from "../../src/Token.js";
 import type { UI5CustomEvent } from "@ui5/webcomponents-base/dist/index.js";
+import { TOKENIZER_DIALOG_OK_BUTTON, TOKENIZER_DIALOG_CANCEL_BUTTON } from "../../src/generated/i18n/i18n-defaults.js";
 
 const onTokenDelete = (event: UI5CustomEvent<Tokenizer, "token-delete">) => {
 	event.detail.tokens.forEach(token => {
@@ -207,3 +208,48 @@ describe("Tokenizer - multi-line and Clear All", () => {
 			.should("be.visible");
 	});
 });
+describe("Phone mode", () => {
+		it("verifies OK and Cancel button texts in mobile dialog", () => {
+		cy.mount(
+			<Tokenizer style={{ width: "50%" }}>
+				<Token text="Andora"></Token>
+				<Token text="Bulgaria"></Token>
+				<Token text="Canada"></Token>
+				<Token text="Denmark"></Token>
+				<Token text="Estonia"></Token>
+			</Tokenizer>
+		);
+
+		cy.ui5SimulateDevice("phone");
+
+		cy.get("ui5-tokenizer")
+			.as("tokenizer");
+
+		cy.get("@tokenizer")
+			.shadow()
+			.find(".ui5-tokenizer-more-text")
+			.click();
+
+		cy.get("@tokenizer")
+			.shadow()
+			.find("ui5-responsive-popover")
+			.as("popover");
+
+		cy.get("@popover")
+			.should("have.attr", "open");
+
+		cy.get("@popover")
+			.find(".ui5-responsive-popover-footer")
+			.as("footer");
+
+		cy.get("@footer")
+			.find("ui5-button")
+			.eq(0)
+			.should("have.text", TOKENIZER_DIALOG_OK_BUTTON.defaultText);
+
+		cy.get("@footer")
+			.find("ui5-button")
+			.eq(1)
+			.should("have.text", TOKENIZER_DIALOG_CANCEL_BUTTON.defaultText);
+	});
+})
