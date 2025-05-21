06533e5b886920eaa927b09e058bcf8971481128
tsanislavgatev <tseko95@gmail.com>
2025-05-21 23:14:20 +0300

    feat(ui5-ddr): add tests

--- packages/main/cypress/specs/DynamicDateRange.cy.tsx
+++ packages/main/cypress/specs/DynamicDateRange.cy.tsx
@@ -1,15 +1,11 @@
 import DynamicDateRange from '../../src/DynamicDateRange.js';
-// import "../../dist/Assets.js";
-import DynamicDateOptionDate from '../../src/dynamic-date-range-options/DynamicDateRangeOptionDate.js';
-import DynamicDateOptionDateRange from '../../src/dynamic-date-range-options/DynamicDateRangeOptionDateRange.js';
-import DynamicDateOptionToday from '../../src/dynamic-date-range-options/DynamicDateRangeOptionToday.js';
+import SingleDate from '../../src/dynamic-date-range-options/SingleDate.js';
+import DateRange from '../../src/dynamic-date-range-options/DateRange.js';
+import Today from '../../src/dynamic-date-range-options/Today.js';
 
 describe('DynamicDateRange Component', () => {
     beforeEach(() => {
-       cy.mount(<DynamicDateRange>
-            <DynamicDateOptionDate/>
-            <DynamicDateOptionToday/>
-            <DynamicDateOptionDateRange/>
+       cy.mount(<DynamicDateRange options="TODAY, DATE, DATERANGE">
         </DynamicDateRange>
        );
     });
@@ -27,9 +23,9 @@ describe('DynamicDateRange Component', () => {
 
     it('displays all options correctly', () => {
         const mockOptions = [
-            new DynamicDateOptionDate(),
-            new DynamicDateOptionToday(),
-            new DynamicDateOptionDateRange()
+            new Today(),
+            new SingleDate(),
+            new DateRange()
         ];
         cy.get('[ui5-dynamic-date-range]').as("ddr");
         cy.get("@ddr").shadow().find('[ui5-input]').as("input");
@@ -77,5 +73,61 @@ describe('DynamicDateRange Component', () => {
         cy.get("@input").realType("Today");
         cy.get("@input").realPress("Enter");
         cy.get("@input").should('have.value', 'Today');
+    });
+
+    it('selects the Date option and updates the current value', () => {
+        cy.window().then((win) => {
+            cy.stub(win.Date, 'now').returns(new Date(2025, 4, 15).getTime());
+          });
+
+        cy.get('[ui5-dynamic-date-range]').as("ddr");
+        cy.get("@ddr").shadow().find('[ui5-input]').as("input");
+    
+        cy.get("@input").should('exist');
+    
+        cy.get("@input").find('[ui5-icon]').as("icon");
+        cy.get("@icon").realClick();
+    
+        cy.get("@ddr").shadow().find("[ui5-responsive-popover]").as("popover");
+        cy.get("@popover").should('exist');
+
+        cy.get("@popover").find("[ui5-list]").as("list");
+        cy.get("@list").find("[ui5-li]").contains('Date').realClick();
+    
+        cy.get("@popover").find("[ui5-calendar]").as("calendar");
+        cy.get("@calendar").should('exist');
+    
+        cy.get("@calendar").shadow().find("ui5-daypicker").as("dayPicker");
+        cy.get("@dayPicker").shadow().find("div[data-sap-timestamp='1747785600']").realClick();
+
+        cy.get("@popover").find("[ui5-button][design='Emphasized']").as("submitButton");
+        cy.get("@submitButton").should('exist').realClick();
+
+        cy.get("@input").shadow().find("input").should('have.value', 'May 21, 2025');
+    });
+
+    it('writes a date in the input and verifies it is selected in the calendar for the Date option', () => {
+        cy.get('[ui5-dynamic-date-range]').as("ddr");
+        cy.get("@ddr").shadow().find('[ui5-input]').as("input");
+    
+        cy.get("@input").should('exist');
+    
+        cy.get("@input").shadow().find("input").clear().realType('May 15, 2025');
+        cy.realPress("Enter");
+
+        cy.get("@input").find('[ui5-icon]').as("icon");
+        cy.get("@icon").realClick();
+
+        cy.get("@ddr").shadow().find("[ui5-responsive-popover]").as("popover");
+        cy.get("@popover").should('exist');
+
+        cy.get("@popover").find("[ui5-list]").as("list");
+        cy.get("@list").find("[ui5-li]").contains('Date').realClick();
+    
+        cy.get("@popover").find("[ui5-calendar]").as("calendar");
+        cy.get("@calendar").should('exist');
+    
+        cy.get("@calendar").shadow().find("ui5-daypicker").as("dayPicker");
+        cy.get("@dayPicker").shadow().find("div[data-sap-timestamp='1747267200']").should('have.class', 'ui5-dp-item--selected'); // Timestamp for May 15, 2025
     });
 });
