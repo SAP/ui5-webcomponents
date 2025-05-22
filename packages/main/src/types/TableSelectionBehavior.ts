56b113bee378db91e8df69cbf6b4dd0bb903dad5
Cahit Gürgüc <cahit.guerguec@sap.com>
2025-05-23 00:16:59 +0200

    feat(ui5-table): behavior property added for selections (#11520)
    * feat(ui5-table): behavior property added for selections
    
    - RowSelector: In this default mode, rows can only be selected using the row selector column.
    - RowOnly: Rows can only be selected by clicking directly on the row, as the row selector column
      is hidden.
    
    Fixes: #10893
    
    * feat(ui5-table): behavior property added for selections
    
    - RowSelector: In this default mode, rows can only be selected using the row selector column.
    - RowOnly: Rows can only be selected by clicking directly on the row, as the row selector column
      is hidden.
    - Documentation fixes
    
    Fixes: #10893

---
+++ packages/main/src/types/TableSelectionBehavior.ts
@@ -0,0 +1,24 @@
+/**
+ * Selection behavior of the `ui5-table` selection components.
+ *
+ * @public
+ * @since 2.11
+ */
+enum TableSelectionBehavior {
+	/**
+	 * Rows can only be selected by using the row selector column.
+	 * @public
+	 */
+	RowSelector = "RowSelector",
+
+	/**
+	 * Rows can only be selected by clicking directly on the row, as the row selector column is hidden.
+	 *
+	 * **Note:** In this mode, the `row-click` event of the `ui5-table` component is not fired.
+	 *
+	 * @public
+	 */
+	RowOnly = "RowOnly",
+}
+
+export default TableSelectionBehavior;
