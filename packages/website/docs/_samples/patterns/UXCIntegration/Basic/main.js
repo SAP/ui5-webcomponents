67d06215774f27a2595d962eebd770eb5cd749ad
Dobromira Boycheva <dobromira.boycheva@sap.com>
2025-05-29 11:36:08 +0300

    docs(UXC Pattern): extend with user settings dialog

--- packages/website/docs/_samples/patterns/UXCIntegration/Basic/main.js
+++ packages/website/docs/_samples/patterns/UXCIntegration/Basic/main.js
@@ -36,6 +36,10 @@ import "@ui5/webcomponents-fiori/dist/UserMenu.js";
 import "@ui5/webcomponents-fiori/dist/UserMenuAccount.js";
 import "@ui5/webcomponents-fiori/dist/UserMenuItem.js";
 
+import "@ui5/webcomponents-fiori/dist/UserSettingsView.js";
+import "@ui5/webcomponents-fiori/dist/UserSettingsItem.js";
+import "@ui5/webcomponents-fiori/dist/UserSettingsDialog.js";
+
 import "@ui5/webcomponents-icons/dist/globe.js";
 import "@ui5/webcomponents-icons/dist/collaborate.js";
 import "@ui5/webcomponents-icons/dist/official-service.js";
@@ -58,6 +62,14 @@ import "@ui5/webcomponents-icons/dist/manager-insight.js";
 import "@ui5/webcomponents-icons/dist/action-settings.js"
 import "@ui5/webcomponents-icons/dist/sort.js"
 import "@ui5/webcomponents-icons/dist/expense-report.js";
+import "@ui5/webcomponents-icons/dist/action-settings.js";
+import "@ui5/webcomponents-icons/dist/user-settings.js";
+import "@ui5/webcomponents-icons/dist/person-placeholder.js";
+import "@ui5/webcomponents-icons/dist/palette.js";
+import "@ui5/webcomponents-icons/dist/iphone.js";
+import "@ui5/webcomponents-icons/dist/qr-code.js";
+import "@ui5/webcomponents-icons/dist/bell.js";
+import "@ui5/webcomponents-icons/dist/reset.js";
 
 const shellbar = document.getElementById("shellbar");
 
@@ -213,6 +225,7 @@ userMenu.addEventListener("item-click", function (event) {
 
 	switch (item) {
 		case "setting":
+			settingsDialog.open = true;
 			console.log("Open Setting Dialog");
 			break;
 		case "privacy-policy":
@@ -257,6 +270,88 @@ userMenu.addEventListener("sign-out-click", function (event) {
 });
 /* End User Menu */
 
+/* User Settings Dialog */
+const settingsDialog = document.getElementById("settings");
+const settingsDialogItems = [...document.getElementsByTagName("ui5-user-settings-item")];
+const mobileSecondPage = document.getElementById("mobile-second-page");
+const mobile1Button = document.getElementById("mobile1-button");
+const mobile2Button = document.getElementById("mobile2-button");
+const resetAllButton = document.getElementById("reset-all-button");
+const additionalDialog = document.getElementById("additionalDialog");
+const additionalDialogClosers = [...additionalDialog.querySelectorAll(".dialogCloser")];
+const resetAll = document.getElementById("resetAll");
+const resetPersonalization = document.getElementById("resetPersonalization");
+const toast = [...document.getElementsByTagName("ui5-toast")][0];
+const toastReset =  document.getElementById("toastReset");
+const toastResetAll =  document.getElementById("toastResetAll");
+const themeSave =document.getElementById("themeSave");
+
+mobile1Button.addEventListener("click", function () {
+	mobileSecondPage.selected = true;
+	mobileSecondPage.text = "iOS";
+});
+
+mobile2Button.addEventListener("click", function () {
+	mobileSecondPage.selected = true;
+	mobileSecondPage.text = "Android";
+});
+
+resetAllButton.addEventListener("click", function () {
+	additionalDialog.open = true;
+});
+
+additionalDialogClosers.forEach(btn => {
+	btn.addEventListener("click", () => {
+		additionalDialog.open = false;
+	});
+})
+
+themeSave.addEventListener("click", function () {
+	toast.open = true;
+});
+
+resetPersonalization.addEventListener("click", function () {
+	toastReset.open = true;
+});
+
+resetAll.addEventListener("click", function () {
+	toastResetAll.open = true;
+});
+
+settingsDialog.addEventListener("selection-change", function (event) {
+	console.log(`Selection change: ${event.detail.item.text}`, event.detail);
+	if(event.detail.item.text ==="Language & Region"){
+		event.detail.item.loading=true;
+		event.detail.item.loadingReason="Language & Region loading data...";
+		setTimeout(function(){
+			event.detail.item.loading=false;
+		}, 3000);
+	}
+});
+
+settingsDialogItems.forEach((settingsDialogItem) => {
+	settingsDialogItem.addEventListener("selection-change", function (event) {
+		console.log(`Selection change: ${event.detail.view.text}`, event.detail);
+	});
+});
+
+settingsDialog.addEventListener("open", function (event) {
+	console.log("Settings dialog opened", event);
+});
+
+settingsDialog.addEventListener("before-close", function (event) {
+	console.log("Settings dialog before close", event.detail);
+	if (!confirm("Are you sure you want to close the dialog?")) {
+		event.preventDefault();
+	}
+});
+
+settingsDialog.addEventListener("close", function (event) {
+	console.log("Settings dialog closed", event);
+});
+
+/* End User Settings Dialog */
+
 /* Search */
 const scopeData = [
     { name: "Laptop", scope: "products" },
@@ -290,7 +385,7 @@ createScopeItems();
 
 searchScope.addEventListener("ui5-scope-change", (event) => {
     const scope = event.detail.scope.text === "All" ? "" : event.detail.scope.text.toLowerCase();
-    
+
     searchScope.items.forEach(item => {
         item.remove();
     });
