efc1ba32d72ca597984f0150dd311419ecd589f8
Teodor Taushanov <teodor.taushanov@sap.com>
2025-05-30 14:37:30 +0300

    chore: address code comments

--- packages/base/src/util/getParentElement.ts
+++ packages/base/src/util/getParentElement.ts
@@ -1,5 +1,5 @@
 const getParentElement = (element: Element) : Element | null => {
-	return element.parentElement ? element.parentNode as Element : (element.parentNode as ShadowRoot).host;
+	return element.parentElement ? element.parentElement : (element.parentNode as ShadowRoot).host;
 };
 
 export default getParentElement;
