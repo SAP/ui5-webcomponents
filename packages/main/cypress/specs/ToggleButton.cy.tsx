5c1084890a0a97552687996281cb6928f0b48c95
Georgi Damyanov <georgi.damyanov@sap.com>
2025-06-05 12:28:31 +0300

    refactor: convert spaces to tabs

--- packages/main/cypress/specs/ToggleButton.cy.tsx
+++ packages/main/cypress/specs/ToggleButton.cy.tsx
@@ -3,111 +3,111 @@ import ToggleButton from "../../src/ToggleButton.js";
 import { ModifierKey } from "../support/commands/common/types.js";
 
 function testClick(shouldPreventClick: boolean, pressedKey?: ModifierKey) {
-    cy.mount(<ToggleButton>Toggle Button</ToggleButton>);
+	cy.mount(<ToggleButton>Toggle Button</ToggleButton>);
 
-    cy.get("[ui5-toggle-button]").as("toggleButton");
+	cy.get("[ui5-toggle-button]").as("toggleButton");
 
-    cy.get("@toggleButton")
-        .then($button => {
-            if (shouldPreventClick) {
-                $button.get(0).addEventListener("click", (e) => e.preventDefault());
-            }
-            $button.get(0).addEventListener("click", cy.stub().as("clicked"));
-        });
+	cy.get("@toggleButton")
+		.then($button => {
+			if (shouldPreventClick) {
+				$button.get(0).addEventListener("click", (e) => e.preventDefault());
+			}
+			$button.get(0).addEventListener("click", cy.stub().as("clicked"));
+		});
 
-    cy.get("@toggleButton").ui5ToggleButtonRealClick(shouldPreventClick, pressedKey);
+	cy.get("@toggleButton").ui5ToggleButtonRealClick(shouldPreventClick, pressedKey);
 
-    cy.get("@clicked")
-        .should("have.been.called");
+	cy.get("@clicked")
+		.should("have.been.called");
 
-    if (pressedKey) {
-        cy.get("@clicked").should("be.calledWithMatch", Cypress.sinon.match.has(pressedKey, true));
-    }
+	if (pressedKey) {
+		cy.get("@clicked").should("be.calledWithMatch", Cypress.sinon.match.has(pressedKey, true));
+	}
 }
 
 describe("Toggle Button general interaction tests", () => {
-    it("should toggle button to pressed state", () => {
-        testClick(false);
-    });
+	it("should toggle button to pressed state", () => {
+		testClick(false);
+	});
 
-    it("should not toggle button to pressed state when click is prevented", () => {
-        testClick(true);
-    });
+	it("should not toggle button to pressed state when click is prevented", () => {
+		testClick(true);
+	});
 
-    it("should not fire click event on a disabled toggle button", () => {
-        cy.mount(<ToggleButton disabled>Disabled Toggle Button</ToggleButton>);
+	it("should not fire click event on a disabled toggle button", () => {
+		cy.mount(<ToggleButton disabled>Disabled Toggle Button</ToggleButton>);
 
-        cy.get("[ui5-toggle-button]").as("toggleButton");
+		cy.get("[ui5-toggle-button]").as("toggleButton");
 
-        cy.get("@toggleButton")
-            .then($button => {
-                $button.get(0).addEventListener("click", cy.stub().as("clicked"));
-            });
+		cy.get("@toggleButton")
+			.then($button => {
+				$button.get(0).addEventListener("click", cy.stub().as("clicked"));
+			});
 
-        cy.get("@toggleButton").realClick();
+		cy.get("@toggleButton").realClick();
 
-        cy.get("@clicked")
-            .should("have.not.been.called")
+		cy.get("@clicked")
+			.should("have.not.been.called")
 
-        cy.get("@toggleButton")
-            .should("not.have.attr", "pressed");
-    });
+		cy.get("@toggleButton")
+			.should("not.have.attr", "pressed");
+	});
 });
 
 describe("Toggle Button general key interaction tests", () => {
-    function testKeyActionOnFocusedButton(key: Key) {
-        cy.mount(<ToggleButton>Toggle Button</ToggleButton>);
+	function testKeyActionOnFocusedButton(key: Key) {
+		cy.mount(<ToggleButton>Toggle Button</ToggleButton>);
 
-        cy.get("[ui5-toggle-button]").as("toggleButton");
+		cy.get("[ui5-toggle-button]").as("toggleButton");
 
-        cy.get<ToggleButton>("@toggleButton")
-            .shadow()
-            .find("button")
-            .focus();
+		cy.get<ToggleButton>("@toggleButton")
+			.shadow()
+			.find("button")
+			.focus();
 
-        cy.realPress(key);
+		cy.realPress(key);
 
-        cy.get("@toggleButton")
-            .should("have.attr", "pressed");
-    }
+		cy.get("@toggleButton")
+			.should("have.attr", "pressed");
+	}
 
-    it("should toggle button to pressed state on Space press", () => {
-        testKeyActionOnFocusedButton("Space");
-    });
+	it("should toggle button to pressed state on Space press", () => {
+		testKeyActionOnFocusedButton("Space");
+	});
 
-    it("should toggle button to pressed state on Enter press", () => {
-        testKeyActionOnFocusedButton("Enter");
-    });
+	it("should toggle button to pressed state on Enter press", () => {
+		testKeyActionOnFocusedButton("Enter");
+	});
 
-    it("should toggle button to pressed state on click with ctrl key pressed", () => {
-        testClick(false, "ctrlKey");
-    });
+	it("should toggle button to pressed state on click with ctrl key pressed", () => {
+		testClick(false, "ctrlKey");
+	});
 
-    it("should not toggle button to pressed state on click with ctrl key pressed when click is prevented", () => {
-        testClick(true, "ctrlKey");
-    });
+	it("should not toggle button to pressed state on click with ctrl key pressed when click is prevented", () => {
+		testClick(true, "ctrlKey");
+	});
 
-    it("should toggle button to pressed state on click with alt key pressed", () => {
-        testClick(false, "altKey");
-    });
+	it("should toggle button to pressed state on click with alt key pressed", () => {
+		testClick(false, "altKey");
+	});
 
-    it("should not toggle button to pressed state on click with alt key pressed when click is prevented", () => {
-        testClick(true, "altKey");
-    });
+	it("should not toggle button to pressed state on click with alt key pressed when click is prevented", () => {
+		testClick(true, "altKey");
+	});
 
-    it("should toggle button to pressed state on click with shift key pressed", () => {
-        testClick(false, "shiftKey");
-    });
+	it("should toggle button to pressed state on click with shift key pressed", () => {
+		testClick(false, "shiftKey");
+	});
 
-    it("should not toggle button to pressed state on click with shift key pressed when click is prevented", () => {
-        testClick(true, "shiftKey");
-    });
+	it("should not toggle button to pressed state on click with shift key pressed when click is prevented", () => {
+		testClick(true, "shiftKey");
+	});
 
-    it("should toggle button to pressed state on click with meta key pressed", () => {
-        testClick(false, "metaKey");
-    });
+	it("should toggle button to pressed state on click with meta key pressed", () => {
+		testClick(false, "metaKey");
+	});
 
-    it("should not toggle button to pressed state on click with meta key pressed when click is prevented", () => {
-        testClick(true, "metaKey");
-    });
+	it("should not toggle button to pressed state on click with meta key pressed when click is prevented", () => {
+		testClick(true, "metaKey");
+	});
 });
--- packages/main/cypress/support/commands/ToggleButton.commands.ts
+++ packages/main/cypress/support/commands/ToggleButton.commands.ts
@@ -2,23 +2,23 @@ import ToggleButton from "../../../src/ToggleButton.js";
 import { ModifierKey } from "./common/types.js";
 
 Cypress.Commands.add("ui5ToggleButtonRealClick", { prevSubject: true }, (subject: JQuery<ToggleButton>, isClickPrevented: boolean, pressedKey?: ModifierKey) => {
-    cy.wrap(subject).as("toggleButton");
-    if (pressedKey) {
-        cy.get("@toggleButton").realClick({ [pressedKey]: true });
-    } else {
-        cy.get("@toggleButton").realClick();
-    }
+	cy.wrap(subject).as("toggleButton");
+	if (pressedKey) {
+		cy.get("@toggleButton").realClick({ [pressedKey]: true });
+	} else {
+		cy.get("@toggleButton").realClick();
+	}
 
-    const assertion = isClickPrevented ? "not.have.attr" : "have.attr";
+	const assertion = isClickPrevented ? "not.have.attr" : "have.attr";
 
-    cy.get("@toggleButton")
-        .should(assertion, "pressed");
+	cy.get("@toggleButton")
+		.should(assertion, "pressed");
 });
 
 declare global {
-    namespace Cypress {
-        interface Chainable {
-            ui5ToggleButtonRealClick(this: Chainable<JQuery<ToggleButton>>, isClickPrevented: boolean, pressedKey?: ModifierKey): Chainable<JQuery<ToggleButton>>;
-        }
-    }
+	namespace Cypress {
+		interface Chainable {
+			ui5ToggleButtonRealClick(this: Chainable<JQuery<ToggleButton>>, isClickPrevented: boolean, pressedKey?: ModifierKey): Chainable<JQuery<ToggleButton>>;
+		}
+	}
 }
