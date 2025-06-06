a85a94c066e7cb4ce757ecf43e0cb6198c7abb03
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-06-06 11:23:58 +0300

    chore: add toDate description

--- packages/main/src/DynamicDateRange.ts
+++ packages/main/src/DynamicDateRange.ts
@@ -291,6 +291,8 @@ class DynamicDateRange extends UI5Element {
 	/**
 	 * Converts a `value` into concrete `startDate` and `endDate` JavaScript `Date` objects.
 	 *
+	 * @public
+	 * @param {value} TBD
 	 * @returns An array of two `Date` objects representing the start and end dates.
 	 */
 	toDates(value: DynamicDateRangeValue): Date[] {
