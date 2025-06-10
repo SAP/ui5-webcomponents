a131020857e380194bb261b568aeab12c7ae2242
Duygu Ramadan <duygu.ramadan@sap.com>
2025-06-10 11:47:27 +0300

    chore(ui5-busy-indicator): migrate wdio tests to cypress

--- packages/main/cypress/specs/BusyIndicator.cy.tsx
+++ packages/main/cypress/specs/BusyIndicator.cy.tsx
@@ -1,129 +1,189 @@
-// import { assert } from "chai";
-
 import BusyIndicator from "../../src/BusyIndicator.js";
+import Button from "../../src/Button.js";
+import Dialog from "../../src/Dialog.js";
+import List from "../../src/List.js";
+import Tree from "../../src/Tree.js";
+import TreeItem from "../../src/TreeItem.js";
 
 describe("BusyIndicator general interaction", () => {
-	// before(async () => {
-	// 	await browser.url(`test/pages/BusyIndicator.html`);
-	// });
-
 	it("tests event propagation", () => {
-		// const dynamicItem = await browser.$(">>>#busy-tree [ui5-tree-item] ui5-icon.ui5-li-tree-toggle-icon");
-		// const input = await browser.$("#tree-input");
+		const onClickStub = cy.stub().as("myStub");;
+		cy.mount(
+			<BusyIndicator id="busy-tree" class="busyindicator8auto">
+				<Tree id="treeDynamic">
+					<TreeItem id="treeItem" text="Has preloaded children" onClick={onClickStub}>
+						<TreeItem text="Child 1"></TreeItem>
+						<TreeItem text="Child 2"></TreeItem>
+					</TreeItem>
+				</Tree>
+			</BusyIndicator>
+		);
 
-		// await dynamicItem.click();
-		// await dynamicItem.keys("Space");
+		cy.get("#treeItem")
+			.shadow()
+			.find("ui5-icon.ui5-li-tree-toggle-icon")
+			.realClick();
 
-		// assert.strictEqual(await input.getProperty("value"), "0", "itemClick is not thrown");
+		cy.realPress("Space");
+
+		cy.get("@myStub").should("not.have.been.called");
 	});
 
 	it("test activation", () => {
-		// const busyIndicator = await browser.$("#busy-container");
-		// const busyArea = await busyIndicator.shadow$(".ui5-busy-indicator-busy-area");
+		cy.mount(
+			<BusyIndicator id="busy-container" class="busyindicator4auto" size="M" text="Loading">
+				<List id="fetch-list" no-data-text="No Data" header-text="Available Items" class="busyindicator5auto"></List>
+			</BusyIndicator>
+		);
 
-		// assert.notOk(await busyArea.isExisting(), "busy area is not yet created");
+		cy.get("#busy-container")
+			.shadow()
+			.find(".ui5-busy-indicator-busy-area")
+			.should("not.exist");
 
-		// await busyIndicator.setAttribute("active", "");
+		cy.get("#busy-container")
+			.invoke("attr", "active", "");
 
-		// await busyArea.waitForExist({
-		// 	timeout: 3000,
-		// 	timeoutMsg: "Busy area must be created after 3000ms"
-		// });
+		cy.wait(3000);
 
-		// // reset
-		// await busyIndicator.removeAttribute("active");
-		// assert.notOk(await busyArea.isExisting(), "busy area is removed");
+		cy.get("#busy-container")
+			.shadow()
+			.find(".ui5-busy-indicator-busy-area")
+			.should("exist");
+
+		cy.get("#busy-container")
+			.invoke("removeAttr", "active");
+
+		cy.get("#busy-container")
+			.shadow()
+			.find(".ui5-busy-indicator-busy-area")
+			.should("not.exist");
 	});
 
 	it("tests focus handling", () => {
-		// const busyIndicator = await browser.$("#indicator1");
-		// await busyIndicator.click();
+		cy.mount(<BusyIndicator size="M" active id="indicator1"></BusyIndicator>);
 
-		// let innerFocusElement = await browser.custom$("activeElement", "#indicator1");
+		cy.get("#indicator1").realClick();
 
-		// innerFocusElement = await browser.$(innerFocusElement);
-
-		// assert.strictEqual(await innerFocusElement.getAttribute("class"), "ui5-busy-indicator-busy-area", "The correct inner element is focused");
+		cy.get("#indicator1")
+			.shadow()
+			.find(".ui5-busy-indicator-busy-area")
+			.should("exist");
 	});
 
 	it("tests internal focused element attributes", () => {
-		// const busyIndicator = await browser.$("#indicator1");
-		// await busyIndicator.click();
-		// const innerFocusElement = await busyIndicator.shadow$(".ui5-busy-indicator-busy-area");
+		cy.mount(<BusyIndicator size="M" active id="indicator1"></BusyIndicator>);
 
-		// assert.strictEqual(await innerFocusElement.getAttribute("role"), "progressbar", "Correct 'role' is set");
-		// assert.strictEqual(await innerFocusElement.getAttribute("tabindex"), "0", "Correct 'tabindex' is set");
-		// assert.strictEqual(await innerFocusElement.getAttribute("aria-valuemin"), "0", "Correct 'aria-valuemin' is set");
-		// assert.strictEqual(await innerFocusElement.getAttribute("aria-valuemax"), "100", "Correct 'aria-valuemax' is set");
-		// assert.strictEqual(await innerFocusElement.getAttribute("aria-valuetext"), "Busy", "Correct 'aria-valuetext' is set");
+		cy.get("#indicator1").realClick();
+
+		cy.get("#indicator1")
+			.shadow()
+			.find(".ui5-busy-indicator-busy-area")
+			.should("have.attr", "role", "progressbar")
+			.and("have.attr", "tabindex", "0")
+			.and("have.attr", "aria-valuemin", "0")
+			.and("have.attr", "aria-valuemax", "100")
+			.and("have.attr", "aria-valuetext", "Busy");
 	});
 
 	it("tests content is not reachable with keyboard when active in both directions", () => {
-		// const beforeBusyIndicator = await browser.$("#beforeIndicatorWithBtn");
-		// const busyIndicator = await browser.$("#indicatorWithBtn");
-		// const afterBusyIndicator = await browser.$("#afterIndicatorWithBtn");
+		cy.mount(
+			<div id="test">
+				<Button id="beforeIndicatorWithBtn">focus stop before</Button>
+				<BusyIndicator id="indicatorWithBtn" size="M" active>
+					<Button>Hello World</Button>
+				</BusyIndicator>
+				<Button id="afterIndicatorWithBtn" >focus stop after</Button>
+			</div>
+		);
 
-		// await beforeBusyIndicator.click();
-		// await browser.keys("Tab");
-		// let activeElement = await browser.getActiveElement();
-		// assert.strictEqual(await browser.$(activeElement).getAttribute("id"), await busyIndicator.getAttribute("id"), "Correct element is focused with TAB");
+		cy.get("#beforeIndicatorWithBtn").realClick();
+		cy.realPress("Tab");
 
-		// await browser.keys("Tab");
-		// activeElement = await browser.getActiveElement();
-		// assert.strictEqual(await browser.$(activeElement).getAttribute("id"), await afterBusyIndicator.getAttribute("id"), "Correct element is focused with TAB");
+		cy.get("#test")
+			.find("[active]")
+			.should("have.id", "indicatorWithBtn");
 
-		// await browser.keys(["Shift", "Tab"]);
-		// activeElement = await browser.getActiveElement();
-		// assert.strictEqual(await browser.$(activeElement).getAttribute("id"), await busyIndicator.getAttribute("id"), "Correct element is focused with SHIFT + TAB");
+		cy.realPress("Tab");
+		cy.get("#afterIndicatorWithBtn").should("have.focus");
 
-		// await browser.keys(["Shift", "Tab"]);
-		// activeElement = await browser.getActiveElement();
-		// assert.strictEqual(await browser.$(activeElement).getAttribute("id"), await beforeBusyIndicator.getAttribute("id"), "Correct element is focused with SHIFT + TAB");
+		cy.realPress(["Shift", "Tab"]);
+		cy.get("#test")
+			.find("[active]")
+			.should("have.id", "indicatorWithBtn");
+
+		cy.realPress(["Shift", "Tab"]);
+
+		cy.get("#beforeIndicatorWithBtn").should("have.focus");
 	});
 
 	it("test inactive indicator in dialog - correct element from default slot is focused", () => {
-		// await browser.$("#open-dialog-inactive-indicator").click();
+		cy.mount(
+			<Dialog id="dialog-inactive-indicator">
+				<div slot="header">Dialog with focus issue</div>
+				<div>
+					<div>Buttons are wrapped in busy indicator, which is inactive.</div>
+					<BusyIndicator>
+						<Button id="dialog-inactive-indicator-focused-button">button 1</Button>
+						<Button>button 2</Button>
+					</BusyIndicator>
+				</div>
+			</Dialog>
+		);
 
-		// const activeElement = await browser.getActiveElement();
-		// assert.strictEqual(
-		// 	await browser.$(activeElement).getAttribute("id"),
-		// 	await browser.$("#dialog-inactive-indicator-focused-button").getAttribute("id"),
-		// 	"Correct element from default slot is focused"
-		// );
+		cy.get("#dialog-inactive-indicator")
+			.invoke("attr", "open", true);
 
-		// await browser.keys("Escape");
+		cy.get("#dialog-inactive-indicator-focused-button")
+			.should("have.focus");
 	});
 
 	it("delayed indicator in dialog - shouldn't attempt to focus before the indicator is visible", () => {
-		// await browser.$("#open-dialog-delayed-indicator").click();
+		cy.mount(
+			<Dialog id="dialog-delayed-indicator">
+				<BusyIndicator id="dialog-delayed-indicator-indicator" delay={5000} >
+					<span>Will become busy after 5 seconds</span>
+				</BusyIndicator>
+				<Button id="dialog-delayed-indicator-focus-stop">focus stop</Button>
+			</Dialog>
+		);
 
-		// const activeElement = await browser.getActiveElement();
-		// assert.strictEqual(
-		// 	await browser.$(activeElement).getAttribute("id"),
-		// 	"dialog-delayed-indicator-focus-stop",
-		// 	"Correct element is focused"
-		// );
+		cy.get("#dialog-delayed-indicator-indicator")
+			.invoke("attr", "active", true);
 
-		// await browser.keys("Escape");
+		cy.get("#dialog-delayed-indicator")
+			.invoke("attr", "open", true);
+
+		cy.get("#dialog-delayed-indicator-focus-stop")
+			.should("have.focus");
 	});
 
-	it.only("Height of the root element depends on the height of the Busy Indicator - issue 6668", () => {
+	it("Height of the root element depends on the height of the Busy Indicator - issue 6668", () => {
 		cy.mount(
-			<BusyIndicator id="busy-indicator-height"  active style="height: 100%;">
-		  <div style="background-color: orange; width: 200px; height: 100%;">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-		  </div>
-		</BusyIndicator>
+			<div style="display: flex;">
+				<div>
+					<BusyIndicator active style="height: 100%;">
+						<div style="background-color: orange; width: 200px; height: 100%;">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+							quasi, minus ullam aut eaque dolorem eos rem itaque unde, veritatis
+							consequuntur numquam! Repellat sunt, dignissimos fugit voluptate
+							animi repudiandae placeat?
+						</div>
+					</BusyIndicator>
+				</div>
+				<div>
+					<BusyIndicator id="busy-indicator-height" active style="height: 100%;">
+						<div style="background-color: orange; width: 200px; height: 100%;">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+						</div>
+					</BusyIndicator>
+				</div>
+			</div>
 		);
 
 		cy.get("#busy-indicator-height")
 			.shadow()
 			.find(".ui5-busy-indicator-root")
-			.should("have.css", "height", 144);
-
-		// const busyIndicator = await browser.$("#busy-indicator-height");
-		// const height = parseInt((await busyIndicator.shadow$(".ui5-busy-indicator-root").getCSSProperty("height")).value);
-
-		// assert.equal(height, 144, "Height of the root element inherits the height of the Busy Indicator");
+			.should("have.css", "height", "144px");
 	});
 });
