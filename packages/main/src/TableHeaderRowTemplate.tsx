bee98bbb721eeb5961d2927ae805e68212ccadc3
Cahit Guerguec <cahit.guerguec@sap.com>
2025-06-11 14:21:43 +0200

    feat(ui5-table-selection-multi): headerSelector property added
    - provides an enumeration property to switch in the multi-select case
    between a select all and a clear all option
    (in future further variants might be added).
    
    Fixes: #5873

--- packages/main/src/TableHeaderRowTemplate.tsx
+++ packages/main/src/TableHeaderRowTemplate.tsx
@@ -1,24 +1,41 @@
 import CheckBox from "./CheckBox.js";
 import TableHeaderCell from "./TableHeaderCell.js";
+import Icon from "./Icon.js";
+import IconMode from "./types/IconMode.js";
+import ClearAll from "@ui5/webcomponents-icons/dist/clear-all.js";
+import IconDesign from "./types/IconDesign.js";
 import type TableHeaderRow from "./TableHeaderRow.js";
 
 export default function TableHeaderRowTemplate(this: TableHeaderRow) {
 	return (
 		<>
-			{ this._hasRowSelector &&
+			{ this._hasSelector &&
 				<TableHeaderCell id="selection-cell"
 					aria-selected={this._isSelected}
 					aria-label={this._i18nSelection}
 					data-ui5-table-cell-fixed
 					data-ui5-table-selection-component
 				>
-					{ this._isMultiSelect &&
-						<CheckBox id="selection-component"
-							tabindex={-1}
-							checked={this._isSelected}
-							onChange={this._onSelectionChange}
-							accessibleName={this._i18nRowSelector}
-						></CheckBox>
+					{ !this._isMultiSelect ?
+						<></>
+						:
+						this._shouldRenderClearAll ?
+							<Icon
+								name={ClearAll}
+								mode={IconMode.Decorative}
+								showTooltip={this._hasSelectedRows}
+								accessibleName={this._hasSelectedRows ? this._i18nDeselectAllRows : undefined}
+								design={this._hasSelectedRows ? IconDesign.Default : IconDesign.NonInteractive}
+								onClick={this._onSelectionChange}
+							></Icon>
+							:
+							<CheckBox id="selection-component"
+								tabindex={-1}
+								checked={this._isSelected}
+								onChange={this._onSelectionChange}
+								accessibleName={this._i18nRowSelector}
+								title={this._isSelected ? this._i18nDeselectAllRows : this._i18nSelectAllRows}
+							></CheckBox>
 					}
 				</TableHeaderCell>
 			}
