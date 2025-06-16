93a01ebf080084faa3eef902c423d89ecb90dd8e
yanaminkova <yana.minkova@sap.com>
2025-06-16 14:39:18 +0300

    feat(ui5-shellbar-branding): fix

--- packages/fiori/config/wdio.conf.cjs
+++ packages/fiori/config/wdio.conf.cjs
@@ -1,10 +1,3 @@
 const wdio = require("@ui5/webcomponents-tools/components-package/wdio.js");
-
-wdio.config.services = [
-  ['chromedriver', {
-    chromedriverCustomPath: "C:\\Users\\I519825\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
-  }],
-  'devtools'
-];
-
+wdio.config.services.push("devtools");
 module.exports = wdio;
