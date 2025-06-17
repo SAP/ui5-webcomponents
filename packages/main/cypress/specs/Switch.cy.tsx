d45b7ad8a7fbd71ce7e98de1bcaf569b68188d79
Georgi Damyanov <georgi.damyanov@sap.com>
2025-06-17 10:44:18 +0300

    test: extract texts in variables

--- packages/main/cypress/specs/Switch.cy.tsx
+++ packages/main/cypress/specs/Switch.cy.tsx
@@ -41,21 +41,24 @@ describe("General events interactions", () => {
 });
 
 describe("General accesibility attributes", () => {
+	const geoLocationDescr = "Geographical location";
+	const gpsLocationDescr = "Use GPS location";
+
 	it("Should set correct 'accessible-name' attribute on the root", () => {
-		cy.mount(<Switch accessible-name="Geographical location" text-on="Yes" text-off="No"></Switch>);
+		cy.mount(<Switch accessible-name={geoLocationDescr} text-on="Yes" text-off="No"></Switch>);
 
 		cy.get("[ui5-switch]").as("switch")
 			.ui5SwitchCheckAttributeInShadowDomRoot("role", "switch");
 
 		cy.get("@switch")
-			.ui5SwitchCheckAttributeInShadowDomRoot("aria-label", "Geographical location No");
+			.ui5SwitchCheckAttributeInShadowDomRoot("aria-label", `${geoLocationDescr} No`);
 
 	});
 
 	it("Should set correct 'accessible-name-ref' attribute on the root", () => {
 		cy.mount(
 			<>
-				<Label id="descriptionText">Use GPS location</Label>
+				<Label id="descriptionText">{gpsLocationDescr}</Label>
 				<Switch accessible-name-ref="descriptionText" text-on="Yes" text-off="No"></Switch>
 			</>
 		);
@@ -64,14 +67,14 @@ describe("General accesibility attributes", () => {
 			.ui5SwitchCheckAttributeInShadowDomRoot("role", "switch");
 
 		cy.get("@switch")
-			.ui5SwitchCheckAttributeInShadowDomRoot("aria-label", "Use GPS location No");
+			.ui5SwitchCheckAttributeInShadowDomRoot("aria-label", `${gpsLocationDescr} No`);
 	});
 
 	it("Should set correct correct tooltip on the root", () => {
-		cy.mount(<Switch tooltip="Use GPS location" text-on="Yes" text-off="No"></Switch>);
+		cy.mount(<Switch tooltip={gpsLocationDescr} text-on="Yes" text-off="No"></Switch>);
 
 		cy.get("[ui5-switch]")
-			.ui5SwitchCheckAttributeInShadowDomRoot("title", "Use GPS location");
+			.ui5SwitchCheckAttributeInShadowDomRoot("title", gpsLocationDescr);
 	});
 
 	it("Should set correct attribute 'aria-label' when 'text-on' and 'text-off' attributes aren't set", () => {
