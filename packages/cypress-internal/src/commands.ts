8976a8ac79ece6e0d9fa712b20f6a0065eeaf9b9
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-07-02 13:27:14 +0300

    chore: test fix for tests

--- packages/cypress-internal/src/commands.ts
+++ packages/cypress-internal/src/commands.ts
@@ -3,6 +3,8 @@ import "cypress-real-events";
 import '@cypress/code-coverage/support';
 import "./acc_report/support.js";
 import "./helpers.js"
+// @ts-ignore
+import { renderFinished } from "@ui5/webcomponents-base/dist/Render.js";
 
 const realEventCmdCallback = (originalFn: any, element: any, ...args: any) => {
 	cy.get(element)
@@ -29,6 +31,56 @@ const commands = [
 
 commands.forEach(cmd => {
 	Cypress.Commands.overwrite(cmd as any, realEventCmdCallback)
+});
+
+
+Cypress.Commands.overwrite(
+	'focus',
+	(originalFn, subject, ...args) => {
+		// You can log for debugging
+		Cypress.log({
+			// @ts-ignore
+			name: `focus`,
+			// @ts-ignore
+			message: `${subject?.selector}`
+		});
+
+		// Check if the element is a custom element or has a shadowRoot
+		if ((subject as any)[0]?.shadowRoot || (subject as any)[0]?.tagName?.includes('-')) {
+			// Focus via native DOM API, might be more reliable for custom elements
+			(subject as any)[0]?.focus?.();
+
+			cy.wrap(subject, { log: false }).should("be.focused")
+
+			return cy.wrap(subject, { log: false }).should("be.focused");
+		}
+
+		// Otherwise, fallback to default Cypress behavior
+		return originalFn(subject, ...args);
+	}
+);
+
+Cypress.on("command:start:async", (e) => {
+	if (["realClick",
+		"realHover",
+		"realTouch",
+		"realSwipe",
+		"realMouseDown",
+		"realMouseUp",
+		"realMouseMove",
+		"get"
+		// @ts-ignore
+	].includes(e.attributes.name)) {
+		return new Cypress.Promise<boolean>(resolve => {
+			renderFinished().then(() => {
+				resolve(false);
+			});
+		});
+	}
+
+	return new Cypress.Promise<boolean>(resolve => {
+		resolve();
+	});
 });
 
 declare global {
