0c1aec2a0f60fa42800e7c8a750e60a4ec70a825
Vladislav Tasev <vladislav.tasev@sap.com>
2025-07-04 11:06:56 +0300

    chore: prevent patchPatcher errors for old OpenUI5 versions

--- packages/base/src/features/patchPatcher.ts
+++ packages/base/src/features/patchPatcher.ts
@@ -7,6 +7,10 @@ type OpenUI5Patcher = {
 
 const patchPatcher = (Patcher: OpenUI5Patcher) => {
 	const origOpenEnd = Patcher.prototype.openEnd;
+	if (!origOpenEnd) {
+		return; // To prevent errors for very old versions of OpenUI5
+	}
+
 	Patcher.prototype.openEnd = function openEnd() {
 		if (this._mAttributes.popover) {
 			delete this._mAttributes.popover; // The "popover" attribute will be managed externally, don't let Patcher remove it
