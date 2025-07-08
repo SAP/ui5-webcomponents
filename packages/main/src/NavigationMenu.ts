eb90f1510b4b3cf0d7d0d49d5e5903ae99e4faa0
Lidiya Georgieva <lidiya.georgieva@sap.com>
2025-07-08 11:44:03 +0300

    chore: edit comment

--- packages/main/src/NavigationMenu.ts
+++ packages/main/src/NavigationMenu.ts
@@ -72,7 +72,7 @@ class NavigationMenu extends Menu {
 			// respect mouseover only on desktop
 			opener = e.target as OpenerStandardListItem;
 
-			// If the opener is a <ui5-icon> inside a StandardListItem, we need to get the parent StandardListItem
+			// If the opener is a <ui5-icon> inside a Navigation item, we need to get the parent Navigation Item
 			if (opener.tagName === "UI5-ICON" && opener.parentElement) {
 				opener = opener.parentElement;
 			}
