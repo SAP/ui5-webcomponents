d218aab7da48d393cb1d000c36282be2a045eb58
Nikola Anachkov <nikola.anachkov@sap.com>
2025-07-21 12:57:49 +0300

    refactor: address comments

--- packages/fiori/cypress/specs/MediaGallery.cy.tsx
+++ packages/fiori/cypress/specs/MediaGallery.cy.tsx
@@ -2,6 +2,221 @@ import MediaGallery from "../../src/MediaGallery.js";
 import MediaGalleryItem from "../../src/MediaGalleryItem.js";
 import type UI5Element from "@ui5/webcomponents-base";
 
+describe("MediaGallery general interaction", () => {
+	beforeEach(() => {
+		cy.mount(
+			<MediaGallery>
+				<MediaGalleryItem>
+					<img src="./img/HT-1000.jpg" />
+					<img src="./img/HT-1000.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+				<MediaGalleryItem selected>
+					<img src="./img/HT-1010.jpg" />
+					<img src="./img/HT-1010.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+				<MediaGalleryItem>
+					<img src="./img/HT-1022.jpg" />
+					<img src="./img/HT-1022.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+			</MediaGallery>
+		);
+
+	});
+
+	it("fires selection-change on thumbnail click", () => {
+		cy.get("[ui5-media-gallery]").then(($gallery) => {
+			const gallery = $gallery[0];
+			gallery.addEventListener("ui5-selection-change", cy.stub().as("selectionChanged"));
+		});
+
+		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(2).click();
+		
+		cy.get("@selectionChanged")
+			.should("have.been.calledOnce")
+			.and("be.calledWithMatch", {
+				type: "ui5-selection-change"
+			});
+
+		cy.get("@selectionChanged").then((stub: any) => {
+			const event = stub.getCall(0).args[0];
+			const selectedItem = event.detail.item;
+			
+			cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(2).then(($item) => {
+				expect(selectedItem).to.equal($item[0]);
+			});
+		});
+	});
+
+	it("changes selection-flag on selection-change", () => {
+		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(1).as("oldSelectedItem");
+		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(2).as("newSelectedItem");
+
+		cy.get("@oldSelectedItem").should("have.prop", "selected", true);
+		cy.get("@newSelectedItem").should("have.prop", "selected", false);
+
+		cy.get("@newSelectedItem").click();
+
+		cy.get("@newSelectedItem").should("have.prop", "selected", true);
+		cy.get("@oldSelectedItem").should("have.prop", "selected", false);
+	});
+
+	it("does not fire selection-change on click on already selected item", () => {
+		cy.get("[ui5-media-gallery]").then(($gallery) => {
+			const gallery = $gallery[0];
+			gallery.addEventListener("ui5-selection-change", cy.stub().as("selectionChanged"));
+		});
+
+		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(1).as("selectedItem");
+		cy.get("@selectedItem").should("have.prop", "selected", true);
+		cy.get("@selectedItem").click();
+		
+		cy.get("@selectionChanged").should("not.have.been.called");
+	});
+
+	it("fires overflow-click on overflow button click", () => {
+		cy.get("[ui5-media-gallery]").then($gallery => {
+			const gallery = $gallery[0];
+			
+			gallery.addEventListener("ui5-overflow-click", cy.stub().as("overflowClicked"));
+
+			for (let i = 0; i < 10; i++) {
+				const item = document.createElement("ui5-media-gallery-item");
+				item.innerHTML = `
+					<img src="./img/HT-1000.jpg" />
+					<img src="./img/HT-1000.jpg" slot="thumbnail" />
+				`;
+				gallery.appendChild(item);
+			}
+		});
+
+		cy.get("[ui5-media-gallery]")
+			.shadow()
+			.find(".ui5-media-gallery-overflow ui5-button")
+			.should("exist")
+			.should("be.visible")
+			.click();
+
+		cy.get("@overflowClicked")
+			.should("have.been.calledOnce")
+			.and("be.calledWithMatch", {
+				type: "ui5-overflow-click"
+			});
+	});
+
+	it("fires display-area-click", () => {
+		cy.mount(
+			<MediaGallery interactiveDisplayArea>
+				<MediaGalleryItem>
+					<img src="./img/HT-1000.jpg" />
+					<img src="./img/HT-1000.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+				<MediaGalleryItem selected>
+					<img src="./img/HT-1010.jpg" />
+					<img src="./img/HT-1010.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+				<MediaGalleryItem>
+					<img src="./img/HT-1022.jpg" />
+					<img src="./img/HT-1022.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+			</MediaGallery>
+		);
+
+		cy.get("[ui5-media-gallery]").then(($gallery) => {
+			const gallery = $gallery[0];
+			gallery.addEventListener("ui5-display-area-click", cy.stub().as("displayAreaClicked"));
+		});
+
+		cy.get("[ui5-media-gallery]")
+			.shadow()
+			.find(".ui5-media-gallery-display")
+			.click();
+
+		cy.get("@displayAreaClicked")
+			.should("have.been.calledOnce")
+			.and("be.calledWithMatch", {
+				type: "ui5-display-area-click"
+			});
+	});
+
+	it("does not fire display-area-click when interactiveDisplayArea==false", () => {
+		cy.mount(
+			<MediaGallery>
+				<MediaGalleryItem>
+					<img src="./img/HT-1000.jpg" />
+					<img src="./img/HT-1000.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+				<MediaGalleryItem selected>
+					<img src="./img/HT-1010.jpg" />
+					<img src="./img/HT-1010.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+				<MediaGalleryItem>
+					<img src="./img/HT-1022.jpg" />
+					<img src="./img/HT-1022.jpg" slot="thumbnail" />
+				</MediaGalleryItem>
+			</MediaGallery>
+		);
+
+		cy.get("[ui5-media-gallery]").then(($gallery) => {
+			const gallery = $gallery[0];
+			gallery.addEventListener("ui5-display-area-click", cy.stub().as("displayAreaClicked"));
+		});
+
+		cy.get("[ui5-media-gallery]")
+			.shadow()
+			.find(".ui5-media-gallery-display")
+			.click();
+
+		cy.get("@displayAreaClicked").should("not.have.been.called");
+	});
+
+	it("fires selection-change after keyboard handling", () => {
+		cy.get("[ui5-media-gallery]").then(($gallery) => {
+			const gallery = $gallery[0];
+			gallery.addEventListener("ui5-selection-change", cy.stub().as("selectionChanged"));
+		});
+
+		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(0).click();
+		cy.get("@selectionChanged").should("have.been.called");
+
+		cy.realPress("ArrowDown");
+		cy.realPress("Enter");
+
+		cy.get("@selectionChanged").should("have.been.calledTwice");
+		cy.get("@selectionChanged").then((stub: any) => {
+			const secondEvent = stub.getCall(1).args[0];
+			const selectedItem = secondEvent.detail.item;
+			
+			cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(1).then(($item) => {
+				expect(selectedItem).to.equal($item[0]);
+			});
+		});
+
+		cy.realPress("ArrowUp");
+		cy.realPress("Space");
+
+		cy.get("@selectionChanged").should("have.been.calledThrice");
+		cy.get("@selectionChanged").then((stub: any) => {
+			const thirdEvent = stub.getCall(2).args[0];
+			const selectedItem = thirdEvent.detail.item;
+			
+			cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(0).then(($item) => {
+				expect(selectedItem).to.equal($item[0]);
+			});
+		});
+	});
+
+	it("changes selection when selected item is disabled", () => {
+		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(0).as("firstItem");
+		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(1).as("secondItem");
+
+		cy.get("@firstItem").click();
+		cy.get("@firstItem").invoke("prop", "disabled", true);
+
+		cy.get("@firstItem").should("have.prop", "selected", false);
+		cy.get("@secondItem").should("have.prop", "selected", true);
+	});
+});
+
 describe("MediaGallery - getFocusDomRef Method", () => {
 	it("should return undefined when the MediaGallery is empty", () => {
 		cy.mount(<MediaGallery></MediaGallery>);
@@ -58,222 +273,6 @@ describe("MediaGallery - getFocusDomRef Method", () => {
 
 				expect(mg.getFocusDomRef()).to.equal(item.getFocusDomRef());
 			});
-	});
-});
-
-describe("MediaGallery general interaction", () => {
-	beforeEach(() => {
-		cy.mount(
-			<div>
-				<span id="selectedIndexLabel"></span>
-				<span id="overflowClickCallCountLabel"></span>
-				<span id="displayAreaClickCallCountLabel"></span>
-				<MediaGallery>
-					<MediaGalleryItem>
-						<img src="./img/HT-1000.jpg" />
-						<img src="./img/HT-1000.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-					<MediaGalleryItem selected>
-						<img src="./img/HT-1010.jpg" />
-						<img src="./img/HT-1010.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-					<MediaGalleryItem>
-						<img src="./img/HT-1022.jpg" />
-						<img src="./img/HT-1022.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-				</MediaGallery>
-			</div>
-		);
-
-		cy.get("[ui5-media-gallery]").then(($gallery) => {
-			const gallery = $gallery[0];
-			let overflowClickCallCount = 0;
-			let displayAreaClickCallCount = 0;
-
-			gallery.addEventListener("ui5-selection-change", (event: CustomEvent) => {
-				const item = event.detail.item;
-				const index = Array.from(gallery.children).indexOf(item);
-				document.getElementById("selectedIndexLabel")!.innerText = index.toString();
-			});
-
-			gallery.addEventListener("ui5-overflow-click", (event: CustomEvent) => {
-				document.getElementById("overflowClickCallCountLabel")!.innerText = (++overflowClickCallCount).toString();
-			});
-
-			gallery.addEventListener("ui5-display-area-click", (event: CustomEvent) => {
-				document.getElementById("displayAreaClickCallCountLabel")!.innerText = (++displayAreaClickCallCount).toString();
-			});
-		});
-	});
-
-	it("fires selection-change on thumbnail click", () => {
-		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(2).click();
-		cy.get("#selectedIndexLabel").should("contain.text", "2");
-	});
-
-	it("changes selection-flag on selection-change", () => {
-		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(1).as("oldSelectedItem");
-		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(2).as("newSelectedItem");
-
-		cy.get("@oldSelectedItem").should("have.prop", "selected", true);
-		cy.get("@newSelectedItem").should("have.prop", "selected", false);
-
-		cy.get("@newSelectedItem").click();
-
-		cy.get("@newSelectedItem").should("have.prop", "selected", true);
-		cy.get("@oldSelectedItem").should("have.prop", "selected", false);
-	});
-
-	it("does not fire selection-change on click on already selected item", () => {
-		cy.get("#selectedIndexLabel").then($label => {
-			$label[0].innerText = "";
-		});
-
-		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(1).as("selectedItem");
-		cy.get("@selectedItem").click();
-		cy.get("#selectedIndexLabel").should("have.text", "");
-	});
-
-	it("fires overflow-click on overflow button click", () => {
-		cy.get("#overflowClickCallCountLabel").then($label => {
-			$label[0].innerText = "";
-		});
-
-		cy.get("[ui5-media-gallery]").then($gallery => {
-			const gallery = $gallery[0];
-			
-			for (let i = 0; i < 10; i++) {
-				const item = document.createElement("ui5-media-gallery-item");
-				item.innerHTML = `
-					<img src="./img/HT-1000.jpg" />
-					<img src="./img/HT-1000.jpg" slot="thumbnail" />
-				`;
-				gallery.appendChild(item);
-			}
-		});
-
-		cy.get("[ui5-media-gallery]")
-			.shadow()
-			.find(".ui5-media-gallery-overflow ui5-button")
-			.should("exist")
-			.should("be.visible")
-			.click();
-
-		cy.get("#overflowClickCallCountLabel").should("contain.text", "1");
-	});
-
-	it("fires display-area-click", () => {
-		cy.mount(
-			<div>
-				<span id="displayAreaClickCallCountLabel"></span>
-				<MediaGallery interactiveDisplayArea>
-					<MediaGalleryItem>
-						<img src="./img/HT-1000.jpg" />
-						<img src="./img/HT-1000.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-					<MediaGalleryItem selected>
-						<img src="./img/HT-1010.jpg" />
-						<img src="./img/HT-1010.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-					<MediaGalleryItem>
-						<img src="./img/HT-1022.jpg" />
-						<img src="./img/HT-1022.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-				</MediaGallery>
-			</div>
-		);
-
-		cy.get("[ui5-media-gallery]").then(($gallery) => {
-			const gallery = $gallery[0];
-			let displayAreaClickCallCount = 0;
-
-			gallery.addEventListener("ui5-display-area-click", (event: CustomEvent) => {
-				document.getElementById("displayAreaClickCallCountLabel")!.innerText = (++displayAreaClickCallCount).toString();
-			});
-		});
-
-		cy.get("#displayAreaClickCallCountLabel").then($label => {
-			$label[0].innerText = "";
-		});
-
-		cy.get("[ui5-media-gallery]")
-			.shadow()
-			.find(".ui5-media-gallery-display")
-			.click();
-
-		cy.get("#displayAreaClickCallCountLabel").should("contain.text", "1");
-	});
-
-	it("does not fire display-area-click when interactiveDisplayArea==false", () => {
-		cy.mount(
-			<div>
-				<span id="displayAreaClickCallCountLabel"></span>
-				<MediaGallery>
-					<MediaGalleryItem>
-						<img src="./img/HT-1000.jpg" />
-						<img src="./img/HT-1000.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-					<MediaGalleryItem selected>
-						<img src="./img/HT-1010.jpg" />
-						<img src="./img/HT-1010.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-					<MediaGalleryItem>
-						<img src="./img/HT-1022.jpg" />
-						<img src="./img/HT-1022.jpg" slot="thumbnail" />
-					</MediaGalleryItem>
-				</MediaGallery>
-			</div>
-		);
-
-		cy.get("[ui5-media-gallery]").then(($gallery) => {
-			const gallery = $gallery[0];
-			let displayAreaClickCallCount = 0;
-
-			gallery.addEventListener("ui5-display-area-click", (event: CustomEvent) => {
-				document.getElementById("displayAreaClickCallCountLabel")!.innerText = (++displayAreaClickCallCount).toString();
-			});
-		});
-
-		cy.get("#displayAreaClickCallCountLabel").then($label => {
-			$label[0].innerText = "";
-		});
-
-		cy.get("[ui5-media-gallery]")
-			.shadow()
-			.find(".ui5-media-gallery-display")
-			.click();
-
-		cy.get("#displayAreaClickCallCountLabel").should("have.text", "");
-	});
-
-	it("fires selection-change after keyboard handling", () => {
-		// Clear label
-		cy.get("#selectedIndexLabel").then($label => {
-			$label[0].innerText = "";
-		});
-
-		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(0).click();
-
-		cy.realPress("ArrowDown");
-		cy.realPress("Enter");
-
-		cy.get("#selectedIndexLabel").should("contain.text", "1");
-
-		cy.realPress("ArrowUp");
-		cy.realPress("Space");
-
-		cy.get("#selectedIndexLabel").should("contain.text", "0");
-	});
-
-	it("changes selection when selected item is disabled", () => {
-		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(0).as("firstItem");
-		cy.get("[ui5-media-gallery] [ui5-media-gallery-item]").eq(1).as("secondItem");
-
-		cy.get("@firstItem").click();
-		cy.get("@firstItem").invoke("prop", "disabled", true);
-
-		cy.get("@firstItem").should("have.prop", "selected", false);
-		cy.get("@secondItem").should("have.prop", "selected", true);
 	});
 });
 
