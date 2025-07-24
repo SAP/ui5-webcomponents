ccc4eba3b1d4262b98840963abb01296a81835b1
Georgi Damyanov <georgi.damyanov@sap.com>
2025-07-25 01:42:54 +0300

    feat: add getFocusDomRef method and tests for it

--- packages/main/src/MenuItem.ts
+++ packages/main/src/MenuItem.ts
@@ -424,6 +424,10 @@ class MenuItem extends ListItem implements IMenuItem {
 		return super.focus(focusOptions);
 	}
 
+	getFocusDomRef() {
+		return this._itemNavigation._getCurrentItem();
+	}
+
 	get _focusable() {
 		return true;
 	}
