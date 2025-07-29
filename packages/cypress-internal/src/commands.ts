d6b0fa29bd6e7679fc29d4ef471cad616ca0f861
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-07-29 10:08:17 +0300

    chore: unskip shellbar test and revert commands

--- packages/cypress-internal/src/commands.ts
+++ packages/cypress-internal/src/commands.ts
@@ -1,26 +1,14 @@
-import { mount } from '@ui5/cypress-ct-ui5-webc';
-import { renderFinished } from '@ui5/webcomponents-base/dist/Render.js';
+import { mount } from '@ui5/cypress-ct-ui5-webc'
 import "cypress-real-events";
 import '@cypress/code-coverage/support';
 import "./acc_report/support.js";
-import "./helpers.js";
-
-Cypress.Commands.add('waitRenderFinished', () => {
-	return cy.wrap(renderFinished(), { log: false });
-});
-
-Cypress.Commands.add('mount', args => {
-	cy.then(() => mount(args)).then(() => {
-		cy.waitRenderFinished();
-	});
-});
+import "./helpers.js"
 
 const realEventCmdCallback = (originalFn: any, element: any, ...args: any) => {
-	cy.waitRenderFinished();
-
 	cy.get(element)
 		.should($el => {
 			const el = $el[0];
+
 			const isCustom = el.tagName.includes("-");
 			const isUI5Element = el.isUI5Element;
 			const isVisible = !Cypress.dom.isHidden(el);
@@ -31,10 +19,12 @@ const realEventCmdCallback = (originalFn: any, element: any, ...args: any) => {
 				expect(isVisible, "Be visible").to.be.true;
 			}
 		})
-		.then(() => originalFn(element, ...args));
+		.then(() => {
+			return originalFn(element, ...args)
+		});
 };
 
-const realEventCommands = [
+const commands = [
 	"realClick",
 	"realHover",
 	"realTouch",
@@ -44,28 +34,16 @@ const realEventCommands = [
 	"realMouseMove"
 ];
 
-const realEventCommandsRest = [
-	"realPress",
-	"realType",
-];
-
-realEventCommands.forEach(cmd => {
-	Cypress.Commands.overwrite(cmd as any, realEventCmdCallback);
-});
-
-realEventCommandsRest.forEach(cmd => {
-	Cypress.Commands.overwrite(cmd as any, (originalFn, element, ...args) => {
-		return cy.waitRenderFinished().then(() => {
-			return originalFn(element, ...args);
-		});
-	});
+commands.forEach(cmd => {
+	Cypress.Commands.overwrite(cmd as any, realEventCmdCallback)
 });
 
 declare global {
 	namespace Cypress {
 		interface Chainable {
 			mount: typeof mount;
-			waitRenderFinished: () => Chainable<void>;
 		}
 	}
 }
+
+Cypress.Commands.add('mount', mount)
