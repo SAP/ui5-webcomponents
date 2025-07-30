c6c0ba9fcdfbea0f364daf0db25208f7766c6c0c
Stefan Dimitrov <stefan.dimitrov02@sap.com>
2025-07-30 14:03:21 +0300

    chore(ui5-tokenizer): migrate wdio tests to cypress

--- packages/main/cypress/specs/Tokenizer.cy.tsx
+++ packages/main/cypress/specs/Tokenizer.cy.tsx
@@ -1,12 +1,226 @@
 import Tokenizer from "../../src/Tokenizer.js";
 import Token from "../../src/Token.js";
 import type { UI5CustomEvent } from "@ui5/webcomponents-base/dist/index.js";
+import Button from "../../src/Button.js";
+import { TOKENIZER_ARIA_LABEL, MULTIINPUT_SHOW_MORE_TOKENS } from "../../src/generated/i18n/i18n-defaults.js";
 
 const onTokenDelete = (event: UI5CustomEvent<Tokenizer, "token-delete">) => {
 	event.detail.tokens.forEach(token => {
 		(event.target as Tokenizer).removeChild(token);
 	});
 };
+
+describe("General Interaction", () => {
+	it("tests focus handling when clicking on the tokenizer", () => {
+		cy.mount(
+			<>
+				<div style={{ display: "flex", flexDirection: "column", width: "240px" }}>
+					<Tokenizer>
+						<Token text="Andora"></Token>
+						<Token text="Bulgaria"></Token>
+						<Token text="So close no matter how far"></Token>
+						<Token text="Lose yourself in the music bum bam tupturup"></Token>
+					</Tokenizer>
+				</div>
+				<Button> Dummy button </Button>
+			</>
+		);
+
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.get("[ui5-tokenizer]")
+			.should("have.attr", "expanded");
+
+		cy.get("@firstToken")
+			.should("have.attr", "focused");
+
+		cy.get("@firstToken")
+			.should("have.attr", "selected");
+
+		cy.realPress("Tab");
+
+		cy.get("[ui5-tokenizer]")
+			.should("not.have.attr", "expanded");
+
+		cy.get("@firstToken")
+			.should("have.attr", "selected");
+
+		cy.realPress(["Shift", "Tab"]);
+
+		cy.get("[ui5-tokenizer]")
+			.should("have.attr", "expanded");
+
+		cy.get("@firstToken")
+			.should("have.attr", "focused");
+	});
+});
+
+describe("Tokenizer nMore Popover", () => {
+	it("tests opening of nMore Popover", () => {
+		cy.mount(
+			<div style={{display: "flex", flexDirection: "column", width: "240px"}}>
+				<Tokenizer id="nmore-tokenizer">
+					<Token text="Andora"></Token>
+					<Token text="Bulgaria"></Token>
+					<Token text="Canada"></Token>
+					<Token text="Denmark"></Token>
+					<Token text="Estonia"></Token>
+				</Tokenizer>
+			</div>
+		);
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find(".ui5-tokenizer-more-text")
+			.as("nMoreLabel");
+
+		cy.get("@nMoreLabel")
+			.realClick();
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find("[ui5-responsive-popover]")
+			.should("have.attr", "open");
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find("ui5-li")
+			.eq(0)
+			.should("be.focused");
+
+		cy.realPress("Escape");
+
+		cy.get("[ui5-token]")
+			.eq(4)
+			.should("have.attr", "focused");
+	});
+
+	it("tests F7 list item navigation", () => {
+		cy.mount(
+			<div style={{display: "flex", flexDirection: "column", width: "240px"}}>
+				<Tokenizer id="nmore-tokenizer">
+					<Token text="Andora"></Token>
+					<Token text="Bulgaria"></Token>
+					<Token text="Canada"></Token>
+					<Token text="Denmark"></Token>
+					<Token text="Estonia"></Token>
+				</Tokenizer>
+			</div>
+		);
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find(".ui5-tokenizer-more-text")
+			.as("nMoreLabel");
+
+		cy.get("@nMoreLabel")
+			.realClick();
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find("ui5-li")
+			.eq(0)
+			.as("firstLI");
+
+		cy.get("@firstLI")
+			.should("be.focused");
+
+		cy.get("@firstLI")
+			.shadow()
+			.find("[ui5-button]")
+			.as("deleteButton");
+
+		cy.realPress("F7");
+
+		cy.get("@deleteButton")
+			.should("be.focused");
+
+		cy.realPress("F7");
+
+		cy.get("@firstLI")
+			.should("be.focused");
+
+		cy.get("@deleteButton")
+			.should("not.be.focused");
+	});
+});
+
+describe("Readonly", () => {
+	it("should not fire token-delete when Tokenizer is readonly", () => {
+		cy.mount(
+			<Tokenizer readonly onTokenDelete={onTokenDelete}>
+				<Token text="Andora" readonly></Token>
+				<Token text="Bulgaria" readonly></Token>
+				<Token text="Canada" readonly></Token>
+				<Token text="Denmark" readonly></Token>
+				<Token text="Estonia" readonly></Token>
+			</Tokenizer>
+		);
+
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("Backspace");
+		cy.realPress("Delete");
+
+		cy.get("[ui5-token]")
+			.should("have.length", 5);
+	});
+
+	it("tests expanding of tokenizer + focus handling in readonly mode.", () => {
+		cy.mount(
+			<div style={{display: "flex", flexDirection: "column", width: "240px"}}>
+				<Tokenizer readonly onTokenDelete={onTokenDelete}>
+					<Token text="Andora" readonly></Token>
+					<Token text="Bulgaria" readonly></Token>
+					<Token text="Canada" readonly></Token>
+					<Token text="Denmark" readonly></Token>
+					<Token text="Estonia" readonly></Token>
+				</Tokenizer>
+			</div>
+		);
+
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("@secondToken")
+			.realClick();
+
+		cy.get("[ui5-tokenizer]")
+			.should("have.attr", "expanded");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected")
+
+		cy.get("@secondToken")
+			.should("be.focused");
+
+		cy.realPress("Tab");
+
+		cy.get("[ui5-tokenizer]")
+			.should("not.have.attr", "expanded");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected");
+
+		cy.get("@secondToken")
+			.should("not.be.focused");
+	});
+});
 
 describe("Tokenizer - multi-line and Clear All", () => {
 	it("'Clear All' link is rendered for multi-line tokenizer and show-clear-all set to true", () => {
@@ -302,5 +516,625 @@ describe("Tokenizer - Popover List Item Text Updates", () => {
 			.find("[ui5-responsive-popover] [ui5-list] [ui5-li]")
 			.eq(2)
 			.should("have.attr", "text", "Token 3");
+	});
+});
+
+describe("Accessibility", () => {
+	it("should test aria-readonly attribute", () => {
+		cy.mount(
+			<Tokenizer readonly>
+				<Token text="Andora" readonly></Token>
+			</Tokenizer>
+		);
+
+		cy.get("[ui5-tokenizer]")
+			.should("have.attr", "readonly");
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find(".ui5-tokenizer--list")
+			.should("have.attr", "aria-readonly");
+	});
+
+	it("should test aria-disabled attribute", () => {
+		cy.mount(
+			<Tokenizer disabled>
+				<Token>Andorra</Token>
+			</Tokenizer>
+		);
+
+		cy.get("[ui5-tokenizer]")
+			.should("have.attr", "disabled");
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find(".ui5-tokenizer--list")
+			.should("have.attr", "aria-disabled");
+	});
+
+	it("should test tokenizer content aria attributes",  () => {
+		cy.mount(
+			<Tokenizer>
+				<Token text="Andora"></Token>
+				<Token text="Bulgaria"></Token>
+			</Tokenizer>
+		);
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find(".ui5-tokenizer--list")
+			.as("list");
+
+		cy.get("@list")
+			.should("have.attr", "role", "listbox");
+
+		cy.get("@list")
+			.should("have.attr", "aria-label", TOKENIZER_ARIA_LABEL.defaultText);
+
+		cy.get("[ui5-tokenizer]")
+			.invoke("attr", "accessible-name", "Test label");
+
+		cy.get("@list")
+			.should("have.attr", "aria-label", "Test label");
+
+		cy.get("@list")
+			.should("have.attr", "aria-description", TOKENIZER_ARIA_LABEL.defaultText);
+	});
+
+	it("should test nMore aria attributes", () => {
+		cy.mount(
+			<div style={{ display: "flex", flexDirection: "column", width: "240px" }}>
+				<Tokenizer>
+					<Token text="Andora"></Token>
+					<Token text="Bulgaria"></Token>
+					<Token text="So close no matter how far"></Token>
+					<Token text="Lose yourself in the music bum bam tupturup"></Token>
+				</Tokenizer>
+			</div>
+		);
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find(".ui5-tokenizer-more-text")
+			.as("nMoreLabel");
+
+		cy.get("@nMoreLabel")
+			.should("have.attr", "role", "button");
+
+		cy.get("@nMoreLabel")
+			.should("have.attr", "aria-haspopup", "dialog");
+	});
+	it("nMore link should be translated", () => {
+		cy.mount(
+			<div style={{ display: "flex", flexDirection: "column", width: "240px" }}>
+				<Tokenizer>
+					<Token text="Andora"></Token>
+					<Token text="Bulgaria"></Token>
+					<Token text="So close no matter how far"></Token>
+					<Token text="Lose yourself in the music bum bam tupturup"></Token>
+				</Tokenizer>
+			</div>
+		);
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find(".ui5-tokenizer-more-text")
+			.as("nMoreLabel");
+
+		cy.get("[ui5-tokenizer]")
+			.then($tokenizer => {
+				const tokenizer = $tokenizer[0];
+				const resourceBundle = (tokenizer.constructor as any).i18nBundle;
+
+				cy.get("@nMoreLabel")
+					.should("have.text", resourceBundle.getText(MULTIINPUT_SHOW_MORE_TOKENS.defaultText, 2));
+			});
+	});
+});
+
+describe("Keyboard Handling", () => {
+	beforeEach(() => {
+		cy.mount(
+			<Tokenizer onTokenDelete={onTokenDelete}>
+				<Token text="Andora"></Token>
+				<Token text="Bulgaria"></Token>
+				<Token text="Canada"></Token>
+			</Tokenizer>
+		);
+	});
+	it("token selection", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("ArrowRight");
+
+		cy.get("@secondToken")
+			.should("be.focused");
+
+		cy.realPress("Space");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected");
+
+		cy.realPress("Space");
+
+		cy.get("@secondToken")
+			.should("not.have.attr", "selected");
+	});
+
+	it("LeftArrow/RightArrow navigation", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("ArrowRight");
+
+		cy.get("@secondToken")
+			.should("be.focused");
+
+		cy.realPress("ArrowLeft");
+
+		cy.get("@firstToken")
+			.should("be.focused");
+	});
+
+	it("Home/End navigation", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("lastToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("End");
+
+		cy.get("@lastToken")
+			.should("be.focused");
+
+		cy.realPress("Home");
+
+		cy.get("@firstToken")
+			.should("be.focused");
+	});
+
+	it("should select tokens with [Shift] key modifier", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress(["Shift", "ArrowRight"]);
+
+		cy.get("@firstToken")
+			.should("have.attr", "selected");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected");
+
+		cy.get("@secondToken")
+			.should("be.focused");
+
+		cy.realPress(["Shift", "ArrowLeft"]);
+
+		cy.get("@firstToken")
+			.should("have.attr", "selected");
+
+		cy.get("@firstToken")
+			.should("be.focused");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected");
+
+		cy.get("@secondToken")
+			.realClick();
+
+		cy.get("@firstToken")
+			.should("not.have.attr", "selected");
+
+		cy.get("@secondToken")
+			.should("not.have.attr", "selected");
+
+		cy.get("@secondToken")
+			.should("be.focused");
+	});
+
+	it("should select tokens with [Shift] + [End] key modifier", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("lastToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress(["Shift", "End"]);
+
+		cy.get("@firstToken")
+			.should("have.attr", "selected");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected");
+
+		cy.get("@lastToken")
+			.should("have.attr", "selected");
+
+		cy.get("@lastToken")
+			.should("be.focused");
+	});
+
+	it("should select tokens with [Shift] + [Home] key modifier", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("lastToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("End");
+
+		cy.get("@lastToken")
+			.should("be.focused");
+
+		cy.realPress(["Shift", "Home"]);
+
+		cy.get("@firstToken")
+			.should("have.attr", "selected");
+
+		cy.get("@firstToken")
+			.should("be.focused");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected");
+
+		cy.get("@lastToken")
+			.should("have.attr", "selected");
+	});
+
+	it("should select tokens with [Shift] + [PageDown] key modifier", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("lastToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress(["Shift", "PageDown"]);
+
+		cy.get("@firstToken")
+			.should("have.attr", "selected");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected");
+
+		cy.get("@lastToken")
+			.should("have.attr", "selected");
+
+		cy.get("@lastToken")
+			.should("be.focused");
+	});
+
+	it("should select tokens with [Shift] + [PageUp] key modifier", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("lastToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("End");
+
+		cy.get("@lastToken")
+			.should("be.focused");
+
+		cy.realPress(["Shift", "PageUp"]);
+
+		cy.get("@firstToken")
+			.should("have.attr", "selected");
+
+		cy.get("@firstToken")
+			.should("be.focused");
+
+		cy.get("@secondToken")
+			.should("have.attr", "selected");
+
+		cy.get("@lastToken")
+			.should("have.attr", "selected");
+	});
+
+	it("should delete first token on [Backspace]", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("Backspace");
+
+		cy.get("[ui5-tokenizer]")
+			.find("[ui5-token]")
+			.should("have.length", 2);
+	});
+
+	it("should delete all selected tokens on [Backspace]", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress(["Shift", "ArrowRight"]);
+
+		cy.realPress("Backspace");
+
+		cy.get("[ui5-tokenizer]")
+			.find("[ui5-token]")
+			.should("have.length", 1);
+
+		//TODO: check remaining token/s
+	});
+
+	it("should delete first and second token with [Backspace] when focus is on second", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("thirdToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("ArrowRight");
+		cy.realPress("Space");
+		cy.realPress("Backspace");
+
+		cy.get("[ui5-tokenizer]")
+			.find("[ui5-token]")
+			.should("have.length", 1);
+
+		//TODO: check remaining token/s
+	});
+
+	it("should delete first and third token with [Backspace] when focus is on second", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("thirdToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("ArrowRight");
+		cy.realPress("ArrowRight");
+		cy.realPress("Space");
+		cy.realPress("ArrowLeft");
+		cy.realPress("Backspace");
+
+		cy.get("[ui5-tokenizer]")
+			.find("[ui5-token]")
+			.should("have.length", 1);
+
+		//TODO: check remaining tokens
+		// cy.get("@secondToken")
+			// .should("have.text", "Bulgaria")
+	});
+
+	it("should delete first and third token with [Backspace] when focus is on fourth", () => {
+		cy.mount(
+			<Tokenizer onTokenDelete={onTokenDelete}>
+				<Token text="Andora"></Token>
+				<Token text="Bulgaria"></Token>
+				<Token text="Canada"></Token>
+				<Token text="Denmark"></Token>
+			</Tokenizer>
+		);
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("thirdToken");
+
+		cy.get("[ui5-token]")
+			.eq(3)
+			.as("fourthToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("ArrowRight");
+		cy.realPress("ArrowRight");
+		cy.realPress("Space");
+		cy.realPress("ArrowRight");
+		cy.realPress("Backspace");
+
+		cy.get("[ui5-tokenizer]")
+			.find("[ui5-token]")
+			.should("have.length", 2);
+
+		//TODO: check remaining tokens
+	});
+
+	it("should delete second token on [Delete]", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("thirdToken");
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("ArrowRight");
+		cy.realPress("Delete");
+
+		cy.get("[ui5-tokenizer]")
+			.find("[ui5-token]")
+			.should("have.length", 2);
+
+		//TODO: check remaining token/s
+	});
+
+	it("should delete first and second token with [Delete] when focus is on third", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.as("firstToken");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.as("secondToken");
+
+		cy.get("[ui5-token]")
+			.eq(2)
+			.as("thirdToken");
+
+
+		cy.get("@firstToken")
+			.realClick();
+
+		cy.realPress("ArrowRight");
+		cy.realPress("Space");
+		cy.realPress("ArrowRight");
+		cy.realPress("Delete");
+
+		cy.get("[ui5-tokenizer]")
+			.find("[ui5-token]")
+			.should("have.length", 1);
+
+		//TODO: check remaining token/s
+	});
+
+	it("should open popover on keyboard combination [Ctrl] + [I]", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.realClick();
+
+		cy.realPress(["Control", "i"]);
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find("[ui5-responsive-popover]")
+			.should("be.visible");
+
+		cy.realPress(["Control", "i"]);
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find("[ui5-responsive-popover]")
+			.should("not.be.visible");
+	});
+
+	it("should close popover on token selection via mouse", () => {
+		cy.get("[ui5-token]")
+			.eq(0)
+			.realClick();
+
+		cy.realPress(["Control", "i"]);
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find("[ui5-responsive-popover]")
+			.should("be.visible");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.realClick();
+
+		cy.get("[ui5-tokenizer]")
+			.shadow()
+			.find("[ui5-responsive-popover]")
+			.should("not.be.visible");
+
+		cy.get("[ui5-token]")
+			.eq(1)
+			.should("have.attr", "selected");
 	});
 });
