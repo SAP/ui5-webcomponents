828b8a12a616e4b22d09bf8934f9c4f0208f7a9d
Georgi Damyanov <georgi.damyanov@sap.com>
2025-08-08 02:22:02 +0300

    refactor: refactor sample

--- packages/website/docs/_samples/patterns/AIAcknowledgement/Scroll/main.js
+++ packages/website/docs/_samples/patterns/AIAcknowledgement/Scroll/main.js
@@ -5,24 +5,18 @@ import "@ui5/webcomponents/dist/CheckBox.js";
 import "@ui5/webcomponents/dist/Link.js";
 import "@ui5/webcomponents/dist/Panel.js";
 
-var dialog = document.getElementById("dialog");
-
+const dialog = document.getElementById("dialog");
 dialog.open = true;
 
-
 const okButton = document.getElementById("okButton");
+const termsPanel = document.getElementById("termsPanel");
 
 okButton.addEventListener("click", function (event) {
-    debugger;
-    console.log("ds");
-    const panel = document.getElementById("termsPanel");
-    const scrollContainer = panel.shadowRoot.querySelector('[part="content"]');
-    const isScrolledToBottom =
-      scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight - 1;
+	const isScrolledToBottom =
+		termsPanel.scrollTop + termsPanel.clientHeight >= termsPanel.scrollHeight - 1;
 
-    if (!isScrolledToBottom) {
-      event.preventDefault();
-      alert("Please scroll to the bottom of the terms to proceed.");
-      return;
-    }
+	if (!isScrolledToBottom) {
+		event.preventDefault();
+		alert("Please scroll to the end of the terms before continuing.");
+	}
 });
