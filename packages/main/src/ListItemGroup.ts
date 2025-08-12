e27162756715ae9b907edadb9cae00c7cab6e73a
Dobrin Dimchev <dobrin.dimchev@sap.com>
2025-08-12 13:35:01 +0300

    chore: remove unnecessarily getter

--- packages/main/src/ListItemGroup.ts
+++ packages/main/src/ListItemGroup.ts
@@ -232,10 +232,6 @@ class ListItemGroup extends UI5Element {
 
 		this.dropIndicatorDOM!.targetReference = null;
 	}
-
-	getFocusDomRef() {
-		return this.groupHeaderItem || this.items.at(0);
-	}
 }
 
 ListItemGroup.define();
