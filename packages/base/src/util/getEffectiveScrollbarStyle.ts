bbd42dd792a1c5e1300026062e63af23954f5f10
Teodor Taushanov <teodor.taushanov@sap.com>
2025-08-28 10:28:35 +0300

    chore(getEffectiveScrollbarStyle): align with design spec

--- packages/base/src/util/getEffectiveScrollbarStyle.ts
+++ packages/base/src/util/getEffectiveScrollbarStyle.ts
@@ -7,31 +7,8 @@ const getEffectiveScrollbarStyle = () => {
 	}
 
 	return `
-::-webkit-scrollbar:horizontal {
-	height: var(--sapScrollBar_Dimension);
-}
-
-::-webkit-scrollbar:vertical {
-	width: var(--sapScrollBar_Dimension);
-}
-
-::-webkit-scrollbar {
-	background-color: var(--sapScrollBar_TrackColor);
-	border-left: none;
-}
-
-::-webkit-scrollbar-thumb {
-	border-radius: var(--sapElement_BorderCornerRadius);
-	background-color: var(--sapScrollBar_FaceColor);
-	border: 0.125rem solid var(--sapScrollBar_TrackColor);
-}
-
-::-webkit-scrollbar-thumb:hover {
-	background-color: var(--sapScrollBar_Hover_FaceColor);
-}
-
-::-webkit-scrollbar-corner {
-	background-color: var(--sapScrollBar_TrackColor);
+:host {
+	scrollbar-color: var(--sapScrollBar_FaceColor) var(--sapScrollBar_TrackColor);
 }`;
 };
 
