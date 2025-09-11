84b0dfb4bdfd59bf653226fc5c9684c6f2698ea1
Nikola Anachkov <nikola.anachkov@sap.com>
2025-09-11 12:44:17 +0300

    fix(ui5-dynamic-page): remove role region from header root

--- packages/fiori/src/DynamicPageHeaderTemplate.tsx
+++ packages/fiori/src/DynamicPageHeaderTemplate.tsx
@@ -2,7 +2,7 @@ import type DynamicPageHeader from "./DynamicPageHeader.js";
 
 export default function DynamicPageHeaderTemplate(this: DynamicPageHeader) {
 	return (
-		<div class="ui5-dynamic-page-header-root" role="region">
+		<div class="ui5-dynamic-page-header-root">
 			<slot></slot>
 		</div>
 	);
